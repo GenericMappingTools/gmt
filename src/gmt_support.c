a1c563141aa435a919621f50e0e0314dcae9ee30
Paul Wessel <pwessel@hawaii.edu>
2001-03-20 22:43:55 +0000

    *** empty log message ***

--- src/gmt_support.c
+++ src/gmt_support.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_support.c,v 1.5 2001-03-19 18:15:08 pwessel Exp $
+ *	$Id: gmt_support.c,v 1.6 2001-03-20 22:43:55 pwessel Exp $
  *
  *	Copyright (c) 1991-2001 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -228,7 +228,7 @@ int GMT_getpen (char *line, struct GMT_PEN *pen)
 
 	/* Check for p which means pen thickness is given in points */
 	
-	points = (BOOLEAN) strchr (line, 'p');
+	points = (strchr (line, 'p')) ? TRUE : FALSE;
 	
 	/* First check if a pen thickness has been entered */
 	
