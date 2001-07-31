0546c534106dad1d68fed4ebbcdde46eb06588ca
Paul Wessel <pwessel@hawaii.edu>
2001-07-31 11:00:03 +0000

    Automatic update

--- registration/GMT_old_unique_sites.d
+++ registration/GMT_old_unique_sites.d
@@ -1,4 +1,4 @@
-#	$Id: GMT_old_unique_sites.d,v 1.47 2001-07-26 11:00:02 pwessel Exp $
+#	$Id: GMT_old_unique_sites.d,v 1.48 2001-07-31 11:00:03 pwessel Exp $
 #
 #	This is a list of unique, verified block-averaged locations
 #	of GMT registration sites.  It is obtained
@@ -346,6 +346,7 @@
 136.517	34.7333	1
 136.7	36.6667	1
 136.983	35.1667	2
+138	35	1
 138	36	2
 138.48	34.9834	3
 138.5	35.5	1
