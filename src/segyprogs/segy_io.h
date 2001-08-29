419085fbcd62123b11be8337cc3a94243b35db5b
Paul Wessel <pwessel@hawaii.edu>
2001-08-29 22:14:37 +0000

    *** empty log message ***

--- src/segyprogs/segy_io.h
+++ src/segyprogs/segy_io.h
@@ -1,5 +1,5 @@
 /************************ segy_io.h *******************************/
-/*  $Id: segy_io.h,v 1.3 2001-04-13 23:29:01 pwessel Exp $  */
+/*  $Id: segy_io.h,v 1.4 2001-08-29 22:14:37 pwessel Exp $  */
 /* segy_io.h:	Include file for segy_io.c, a suite of functions to */
 /* help reading and writing those annoying, redundant SEGY header */
 /* variables and for reading/writing to/from SEGY files.          */
@@ -16,7 +16,7 @@
 #define FALSE 0
 #endif
 
-#ifdef WIN32
+#ifdef _WIN32
 typedef short int16_t;
 typedef int int32_t;
 typedef unsigned short uint16_t;
