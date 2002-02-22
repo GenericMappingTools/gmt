4ee4bd7d317ff9b9eaece2a722e927266ecde2a1
Paul Wessel <pwessel@hawaii.edu>
2002-02-22 17:26:40 +0000

    *** empty log message ***

---
+++ trunk/share/time/tr.d
@@ -0,0 +1,28 @@
+# GMT Time language file for TR (turkish) mode [TR]
+#
+#	$Id: tr.d,v 1.1 2002-02-22 17:26:40 pwessel Exp $
+#-------------------------------------------------
+# Monthnames, with 3-char and 1-char abbreviations
+M	1	Ocak		Oca	O
+M	2	ûubat		Sub	S
+M	3	Mart		Mar	M
+M	4	Nisan		Nis	N
+M	5	Mayçs		May	M
+M	6	Haziran		Haz	H
+M	7	Temmuz		Tem	T
+M	8	Aßustos		Agu	A
+M	9	EylÅl		Eyl	E
+M	10	Ekim		Eki	E
+M	11	Kasçm		Kas	K
+M	12	Aralçk		Arl	A
+# Name and abbreviations for the word Week in this language
+U	1	Hafta		Hf	H
+# Weeknames, with 3-char and 1-char abbreviations
+# (List starts on Sunday regardless of TIME_WEEK_START)
+W	1	Pazar		Paz	P
+W	2	Pazartesi	Pts	P
+W	3	Salç		Sal	S
+W	4	Äarüamba	Car	C
+W	5	Perüembe	Per	P
+W	6	Cuma		Cum	C
+W	7	Cumartesi	Cts	C
