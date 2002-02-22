8f7ddffa48bef8c62f6aef9d2778d70b9c8e2b0a
Paul Wessel <pwessel@hawaii.edu>
2002-02-22 16:09:58 +0000

    *** empty log message ***

---
+++ share/time/de.d
@@ -0,0 +1,28 @@
+# GMT Time language file for german mode [DE]
+#
+#	$Id: de.d,v 1.1 2002-02-22 16:09:58 pwessel Exp $
+#-------------------------------------------------
+# Monthnames, with 3-char and 1-char abbreviations
+M	1	Januar		Jan	J
+M	2	Februar		Feb	F
+M	3	März		Mär	M
+M	4	April		Apr	A
+M	5	Mai		Mai	M
+M	6	Juni		Jun	J
+M	7	Juli		Jul	J
+M	8	August		Aug	A
+M	9	September	Sep	S
+M	10	Oktober		Okt	O
+M	11	November	Nov	N
+M	12	Dezember	Dez	D
+# Name and abbreviations for the word Week in this language
+U	1	Woche		Wo	W
+# Weeknames, with 3-char and 1-char abbreviations
+# (List starts on Sunday regardless of TIME_WEEK_START)
+W	1	Sonntag		Son	S
+W	2	Montag		Mon	M
+W	3	Dienstag	Die	D
+W	4	Mittwoch	Mit	M
+W	5	Donnerstag	Don	D
+W	6	Freitag		Fre	F
+W	7	Samstag		Sam	S
