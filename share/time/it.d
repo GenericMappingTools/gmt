211a5ed6082c578a090a784c2e1cb283cc4b5e93
Paul Wessel <pwessel@hawaii.edu>
2002-02-23 00:33:52 +0000

    *** empty log message ***

---
+++ share/time/it.d
@@ -0,0 +1,28 @@
+# GMT Time language file for IT (italian) mode [IT]
+#
+#	$Id: it.d,v 1.1 2002-02-23 00:33:52 pwessel Exp $
+#-------------------------------------------------
+# Monthnames, with 3-char and 1-char abbreviations
+M	1	Gennaio		Gen	G
+M	2	Febraio		Feb	F
+M	3	Marzo		Mar	M
+M	4	Aprile		Apr	A
+M	5	Maggio		Mag	M
+M	6	Giugno		Giu	G
+M	7	Luglio		Lug	L
+M	8	Agosto		Ago	A
+M	9	Settembre	Set	S
+M	10	Ottobre		Ott	O
+M	11	Novembre	Nov	N
+M	12	Dicembre	Dic	D
+# Name and abbreviations for the word Week in this language
+U	1	Settimana	Sett	S
+# Weeknames, with 3-char and 1-char abbreviations
+# (List starts on Sunday regardless of TIME_WEEK_START)
+W	1	Domenica	Dom	D
+W	2	Lunedì		Lun	L
+W	3	Martedì		Mar	M
+W	4	Mrcoledì	Mer	M
+W	5	Giovedì		Gio	G
+W	6	Venerdì		Ven	V
+W	7	Sabato		Sab	S
