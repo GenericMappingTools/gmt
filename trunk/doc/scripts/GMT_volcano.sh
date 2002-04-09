998703de0cd9ad40f580a4587171950f457d9059
Paul Wessel <pwessel@hawaii.edu>
2002-04-09 18:18:41 +0000

    *** empty log message ***

--- trunk/doc/scripts/GMT_volcano.sh
+++ trunk/doc/scripts/GMT_volcano.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#	$Id: GMT_volcano.sh,v 1.2 2001-09-23 23:41:15 pwessel Exp $
+#	$Id: GMT_volcano.sh,v 1.3 2002-04-09 18:18:41 pwessel Exp $
 
-echo "0 0" | psxy -R-0.5/0.5/-0.5/0.5 -JX2i -P -Ba0.25g0.05WSne -W1p -Skvolcano/1i > GMT_volcano.ps
+echo "0 0" | psxy -R-0.5/0.5/-0.5/0.5 -JX2i -P -Ba0.25g0.05WSne -W1p -Skvolcano/2i > GMT_volcano.ps
