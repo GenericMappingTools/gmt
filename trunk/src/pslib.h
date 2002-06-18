3fc6a2e71eec28d421e898a1278dc7727f158df0
Paul Wessel <pwessel@hawaii.edu>
2002-06-18 17:31:16 +0000

    *** empty log message ***

--- trunk/src/pslib.h
+++ trunk/src/pslib.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: pslib.h,v 1.15 2002-02-23 03:39:58 pwessel Exp $
+ *	$Id: pslib.h,v 1.16 2002-06-18 17:31:16 pwessel Exp $
  *
  *	Copyright (c) 1991-2002 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -77,7 +77,7 @@ struct rasterfile {
 #define RMT_EQUAL_RGB	1       /* red[ras_maplength/3], green[], blue[] follow */
 
 EXTERN_MSC int ps_line(double *x, double *y, int n, int type, int close, int split);
-EXTERN_MSC int ps_plotinit(char *plotfile, int overlay, int mode, double xoff, double yoff, double xscl, double yscl, int ncopies, int dpi, int unit, int *page_width, int *rgb, const char *, struct EPS *eps);
+EXTERN_MSC int ps_plotinit(char *plotfile, int overlay, int mode, double xoff, double yoff, double xscl, double yscl, int ncopies, int dpi, int unit, int *page_width, int *rgb, const char *encoding, struct EPS *eps);
 EXTERN_MSC void ps_arc(double x, double y, double radius, double az1, double az2, int status);
 EXTERN_MSC void ps_axis(double x, double y, double length, double val0, double val1, double annotation_int, char *label, double annotpointsize, int side);
 EXTERN_MSC void ps_circle(double x, double y, double size, int rgb[], int outline);
