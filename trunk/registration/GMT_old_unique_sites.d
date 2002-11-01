92342f929c250c5efda231de0beca33b17acb12e
Paul Wessel <pwessel@hawaii.edu>
2002-11-01 10:01:01 +0000

    Automatic update

--- trunk/registration/GMT_old_unique_sites.d
+++ trunk/registration/GMT_old_unique_sites.d
@@ -1,4 +1,4 @@
-#	$Id: GMT_old_unique_sites.d,v 1.214 2002-10-31 10:01:01 pwessel Exp $
+#	$Id: GMT_old_unique_sites.d,v 1.215 2002-11-01 10:01:01 pwessel Exp $
 #
 #	This is a list of unique, verified block-averaged locations
 #	of GMT registration sites.  It is obtained
@@ -10,11 +10,11 @@
 #	total number of registrations for each 15' bin.
 #
 # longitude(0-360)	latitude(-90/+90)	#of_sites
-10.5	60	7
-12.9772	52.4119	2
-16.6083	47.2611	13
-30.4967	50.3642	7
-73.9142	19.13	7
-85.55	40.5	14
-116	40	17
-121	31	17
+10.5	60	8
+12.9772	52.4119	3
+16.6083	47.2611	14
+30.4967	50.3642	8
+73.9142	19.13	8
+85.55	40.5	15
+116	40	18
+121	31	18
