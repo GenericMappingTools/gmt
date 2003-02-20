a2fd50674df961d8ecdeb6662f4336edcaa4bb7f
Paul Wessel <pwessel@hawaii.edu>
2003-02-20 19:01:21 +0000

    *** empty log message ***

--- trunk/src/gmt_support.c
+++ trunk/src/gmt_support.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_support.c,v 1.38 2003-02-20 19:00:52 pwessel Exp $
+ *	$Id: gmt_support.c,v 1.39 2003-02-20 19:01:21 pwessel Exp $
  *
  *	Copyright (c) 1991-2002 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -108,6 +108,7 @@ int GMT_check_hsv (double h, double s, double v)
 
 int GMT_check_cmyk (double cmyk[])
 {
+	int i;
 	for (i = 0; i < 4; i++) if (cmyk[i] < 0.0 || cmyk[i] > 100.0) return (TRUE);
 	return (FALSE);
 }
