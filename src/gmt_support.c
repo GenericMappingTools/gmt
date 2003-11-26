1f8cbd4540b9ab6ae608b891b209132c6b427960
Paul Wessel <pwessel@hawaii.edu>
2003-11-26 16:20:24 +0000

    *** empty log message ***

--- src/gmt_support.c
+++ src/gmt_support.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_support.c,v 1.52 2003-09-26 17:42:58 pwessel Exp $
+ *	$Id: gmt_support.c,v 1.53 2003-11-26 16:20:24 pwessel Exp $
  *
  *	Copyright (c) 1991-2002 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -2096,6 +2096,8 @@ int	GMT_non_zero_winding(double xp, double yp, double *x, double *y, int n_path)
 	int	i, j, k, jend, crossing_count, above;
 	double	y_sect;
 
+	if (n_path < 2) return 0;	/* Cannot be inside a null set or a point so default to outside */
+	
 	above = FALSE;
 	crossing_count = 0;
 
