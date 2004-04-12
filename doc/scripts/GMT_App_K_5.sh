c7214c55911db73dc126eea5a9d9615d9c3439ee
Paul Wessel <pwessel@hawaii.edu>
2004-04-12 04:01:58 +0000

    *** empty log message ***

--- doc/scripts/GMT_App_K_5.sh
+++ doc/scripts/GMT_App_K_5.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: GMT_App_K_5.sh,v 1.2 2004-04-10 17:19:14 pwessel Exp $
+#	$Id: GMT_App_K_5.sh,v 1.3 2004-04-12 04:01:58 pwessel Exp $
 #
-pscoast `./getbox -JE130.35/-0.2/1i -20 20 -20 20` -JE130.35/-0.2/3.5i -P -Df -Glilghtgray -W0.25p \
+pscoast `./getbox -JE130.35/-0.2/1i -20 20 -20 20` -JE130.35/-0.2/3.5i -P -Df -Glightgray -W0.25p \
    -N1/0.25tap -B10mg2mWSne > GMT_App_K_5.ps
