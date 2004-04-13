0614afc32a7b7eeaaf1ad73e23d38fedb9f2e42d
Paul Wessel <pwessel@hawaii.edu>
2004-04-13 17:40:02 +0000

    *** empty log message ***

--- trunk/doc/scripts/GMT_-B_time1.sh
+++ trunk/doc/scripts/GMT_-B_time1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: GMT_-B_time1.sh,v 1.6 2004-04-12 21:41:36 pwessel Exp $
+#	$Id: GMT_-B_time1.sh,v 1.7 2004-04-13 17:37:39 pwessel Exp $
 #
 gmtset PLOT_DATE_FORMAT -o ANNOT_FONT_SIZE_PRIMARY +9p
-psbasemap -R2000-4-1T/2000-5-25T/0/1 -JX6T/0.2 -Bpa1OS -Bsa7Rf1d -P > GMT_-B_time1.ps
+psbasemap -R2000-4-1T/2000-5-25T/0/1 -JX6T/0.2 -Bpa7Rf1d -Bsa1OS -P > GMT_-B_time1.ps
--- trunk/doc/scripts/GMT_linear_cal.sh
+++ trunk/doc/scripts/GMT_linear_cal.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: GMT_linear_cal.sh,v 1.1 2001-09-25 01:38:42 pwessel Exp $
+#	$Id: GMT_linear_cal.sh,v 1.2 2004-04-13 17:40:02 pwessel Exp $
 #
-gmtset PLOT_DATE_FORMAT o TIME_WEEK_START Sunday PLOT_CLOCK_FORMAT -hham
-psbasemap -R2001-9-24T/2001-9-29T/T07:0/T15:0 -JX4T/-2T -Bf1kif1Kg1d/a1Hg1hWsNe -P > GMT_linear_cal.ps
+gmtset PLOT_DATE_FORMAT o TIME_WEEK_START Sunday PLOT_CLOCK_FORMAT -hham TIME_FORMAT_PRIMARY full
+psbasemap -R2001-9-24T/2001-9-29T/T07:0/T15:0 -JX4T/-2T -Ba1Kf1kg1d/a1Hg1hWsNe -P > GMT_linear_cal.ps
