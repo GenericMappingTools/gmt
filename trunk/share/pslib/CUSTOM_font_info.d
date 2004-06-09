be4c1124abfafe481e116a95dbd9348baf590390
Paul Wessel <pwessel@hawaii.edu>
2004-06-09 21:54:11 +0000

    *** empty log message ***

--- trunk/share/pslib/CUSTOM_font_info.d
+++ trunk/share/pslib/CUSTOM_font_info.d
@@ -1,13 +1,12 @@
-#	$Id: CUSTOM_font_info.d,v 1.2 2004-05-27 04:05:50 pwessel Exp $
+#	$Id: CUSTOM_font_info.d,v 1.3 2004-06-09 21:54:10 pwessel Exp $
 #
 #	Listing of additional PostScript fonts than those found on most PS printers.
 #	Add your extra font information there.
 #	The fontheight below is the height of A for unit fontsize.
 #	Encoded = 0 if we may reencode this font as needed.
-#	Charwidth is average character width normalized by font size
 #
-#Fontname			Fontheight	Encoded	Charwidth
-Ryumin-Light-EUC-H		0.700		1	0.7
-Ryumin-Light-EUC-V		0.700		1	0.7
-GothicBBB-Medium-EUC-H		0.700		1	0.7
-GothicBBB-Medium-EUC-V		0.700		1	0.7
+#Fontname			Fontheight	Encoded
+Ryumin-Light-EUC-H		0.700		1
+Ryumin-Light-EUC-V		0.700		1
+GothicBBB-Medium-EUC-H		0.700		1
+GothicBBB-Medium-EUC-V		0.700		1
