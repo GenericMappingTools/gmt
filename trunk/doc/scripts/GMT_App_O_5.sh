98d46ac4d2fe4a702a12bf031c8845390a25e9eb
Paul Wessel <pwessel@hawaii.edu>
2004-06-24 05:02:06 +0000

    *** empty log message ***

--- trunk/doc/scripts/GMT_App_O_5.sh
+++ trunk/doc/scripts/GMT_App_O_5.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-#	$Id: GMT_App_O_5.sh,v 1.2 2004-06-13 22:30:18 pwessel Exp $
+#	$Id: GMT_App_O_5.sh,v 1.3 2004-06-24 05:02:06 pwessel Exp $
 #
 #	Makes Fig 5 for Appendix O (labeled lines)
 #
 
-PS=`basename $0 .sh`
 pscoast -R50/160/-15/15 -JM5.5i -Glightgray -A500 -K -P > GMT_App_O_5.ps
 grdcontour geoid.grd -JM -O -B20f10WSne -C10 -A20+d+s8 -GXcross.d -S10 -T:LH >> GMT_App_O_5.ps
