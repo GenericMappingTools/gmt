76b5c71b428c8e80dc3422ca2c5a7e961eff68c5
Paul Wessel <pwessel@hawaii.edu>
2004-08-17 01:48:06 +0000

    *** empty log message ***

--- doc/scripts/GMT_polar.sh
+++ doc/scripts/GMT_polar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#	$Id: GMT_polar.sh,v 1.2 2002-02-23 00:53:15 pwessel Exp $
+#	$Id: GMT_polar.sh,v 1.3 2004-08-17 01:48:06 pwessel Exp $
 #
 
 grdmath -R0/360/2/4 -I6/0.1 X 4 MUL PI MUL 180 DIV COS Y 2 POW MUL = test.grd
-grdcontour test.grd -JP3i -B30Ns -P -C2 -S4 > GMT_polar.ps
+grdcontour test.grd -JP3i -B30Ns -P -C2 -S4 --PLOT_DEGREE_FORMAT=+ddd > GMT_polar.ps
 rm -f test.grd
