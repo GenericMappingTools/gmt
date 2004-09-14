ec92e9c0b9fe150c1d784d5db3a34ce2e260b442
Paul Wessel <pwessel@hawaii.edu>
2004-09-14 21:59:35 +0000

    *** empty log message ***

--- trunk/doc/scripts/GMT_Defaults_1c.sh
+++ trunk/doc/scripts/GMT_Defaults_1c.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: GMT_Defaults_1c.sh,v 1.7 2004-07-14 00:46:17 pwessel Exp $
+#	$Id: GMT_Defaults_1c.sh,v 1.8 2004-09-14 21:59:35 pwessel Exp $
 #
 gmtset BASEMAP_TYPE plain PLOT_DATE_FORMAT "o dd" PLOT_CLOCK_FORMAT hh ANNOT_FONT_SIZE_PRIMARY +8p
 psbasemap -R2001-9-11T/2001-9-13T/0.01/100 -JX2.25T/2.25l -Bpa6Hf1hg6h:"x-axis label":/a1g3p:"y-axis label":WSne \
@@ -16,8 +16,8 @@ pstext -R0/2.25/0/2.25 -Jx1i -O -K -N << EOF >> GMT_Defaults_1c.ps
 2.35 0.9 7 0 1 LB PLOT_DATE_FORMAT
 2.35 0.6 7 0 1 LB PLOT_CLOCK_FORMAT
 2.35 0.3 7 0 1 LB TIME_LANGUAGE
-2.3 -0.25 7 0 1 LM ANNOT_FONT_SECONDARY
-2.3 -0.35 7 0 1 LM ANNOT_FONT_SIZE_SECONDARY
+2.4 -0.25 7 0 1 LM ANNOT_FONT_SECONDARY
+2.4 -0.35 7 0 1 LM ANNOT_FONT_SIZE_SECONDARY
 EOF
 psxy -R -Jx -O -Svs0.005i/0.04i/0.03i -N -Gblack << EOF >> GMT_Defaults_1c.ps
 0.65 2.4 0.9 2.3
@@ -25,8 +25,8 @@ psxy -R -Jx -O -Svs0.005i/0.04i/0.03i -N -Gblack << EOF >> GMT_Defaults_1c.ps
 -0.35 1.9 -0.05 1.9
 -0.35 0.25 -0.05 0.25
 -0.9 -0.3 -0.75 -0.85
-2.25 -0.25 1.95 -0.26
-2.25 -0.35 1.95 -0.30
+2.35 -0.25 2.1 -0.26
+2.35 -0.35 2.1 -0.30
 1.95 -0.55 1.7 -0.44
 1.95 -0.65 1.7 -0.5
 2.3 0.9 0.66 -0.2
