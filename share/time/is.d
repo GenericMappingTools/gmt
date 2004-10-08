85ee847c8fb38c0aa14cafc52477a1da45eccff7
Paul Wessel <pwessel@hawaii.edu>
2004-10-08 18:05:13 +0000

    *** empty log message ***

--- share/time/is.d
+++ share/time/is.d
@@ -1,6 +1,6 @@
-# GMT Time language file for ICELAND (icelandic) mode [IS]
+# GMT Time language file for IS (icelandic) mode [IS]
 #
-#                $Id: is.d,v 1.1 2004-10-07 17:47:25 pwessel Exp $
+#	$Id: is.d,v 1.2 2004-10-08 18:05:13 pwessel Exp $
 #-------------------------------------------------
 # Monthnames, with 3-char and 1-char abbreviations
 M	1	Janúar		Jan	J
@@ -8,21 +8,21 @@ M	2	Febrúar		Feb	F
 M	3	Mars		Mar	M
 M	4	Apríl		Apr	A
 M	5	Maí		Maí	M
-M	6	Juní		Jun	J
-M	7	Júlí		Jul	J
-M	8	Ágúst		Agu	A
+M	6	Júní		Jún	J
+M	7	Júlí		Júl	J
+M	8	Ágúst		Ágú	A
 M	9	September	Sep	S
 M	10	Október		Okt	O
-M	11	Nóvember	Nov	N
+M	11	Nóvember	Nóv	N
 M	12	Desember	Des	D
 # Name and abbreviations for the word Week in this language
-U	1	Vika		Vk	W
+U	1	Vika		Vik	V
 # Weeknames, with 3-char and 1-char abbreviations
 # (List starts on Sunday regardless of TIME_WEEK_START)
 W	1	Sunnudagur	Sun	S
 W	2	Mánudagur	Man	M
 W	3	Þriðjudagur	Þri	Þ
-W	4	Miðvikudagur	Mid	M
+W	4	Miðvikudagur	Mið	M
 W	5	Fimmtudagur	Fim	F
 W	6	Föstudagur	Fös	F
 W	7	Laugardagur	Lau	L
