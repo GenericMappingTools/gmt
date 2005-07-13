a5f0c6d81f70c3fd8709c108665363b8bea4a8b2
Paul Wessel <pwessel@hawaii.edu>
2005-07-13 23:27:32 +0000

    *** empty log message ***

--- src/pslib.h
+++ src/pslib.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: pslib.h,v 1.31 2005-07-07 09:17:49 pwessel Exp $
+ *	$Id: pslib.h,v 1.32 2005-07-13 23:27:32 pwessel Exp $
  *
  *	Copyright (c) 1991-2005 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -141,6 +141,8 @@ EXTERN_MSC void ps_set_height (char *param, double fontsize);
 EXTERN_MSC void ps_define_rgb (char *param, int rgb[]);
 EXTERN_MSC void ps_define_pen (char *param, int width, char *texture, int offset, int rgb[]);
 EXTERN_MSC void ps_rgb_to_mono (unsigned char *buffer, struct imageinfo *h);
+EXTERN_MSC int ps_read_rasheader  (FILE *fp, struct imageinfo *h, int i0, int i1);
+EXTERN_MSC int ps_write_rasheader (FILE *fp, struct imageinfo *h, int i0, int i1);
 
 /* For Encapsulated PostScript Headers:
 
