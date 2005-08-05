1fc5c6e35c57bb1324401fe70535055b2264feba
Remko Scharroo <remko.scharroo@eumetsat.int>
2005-08-05 19:29:59 +0000

    - Changes printf format %lf to %f

--- src/gmt_stat.c
+++ src/gmt_stat.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_stat.c,v 1.28 2005-07-27 00:41:59 pwessel Exp $
+ *	$Id: gmt_stat.c,v 1.29 2005-08-05 19:29:59 remko Exp $
  *
  *	Copyright (c) 1991-2005 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -2635,7 +2635,7 @@ double GMT_chebyshev (double x, int n)
 		exit (EXIT_FAILURE);
 	}
 	if (fabs (x) > 1.0) {
-		fprintf (stderr, "GMT: ERROR.  GMT_chebyshev given |x| > 1 (%lf)\n", x);
+		fprintf (stderr, "GMT: ERROR.  GMT_chebyshev given |x| > 1 (%f)\n", x);
 		exit (EXIT_FAILURE);
 	}
 	
