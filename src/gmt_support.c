1d95450d18e4bbb15231915c9a634525e4573542
Paul Wessel <pwessel@hawaii.edu>
2005-09-14 06:31:34 +0000

    *** empty log message ***

--- src/gmt_support.c
+++ src/gmt_support.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_support.c,v 1.177 2005-09-14 05:56:51 pwessel Exp $
+ *	$Id: gmt_support.c,v 1.178 2005-09-14 06:31:34 pwessel Exp $
  *
  *	Copyright (c) 1991-2005 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -6000,7 +6000,10 @@ double GMT_cartesian_dist (double x0, double y0, double x1, double y1)
 
 double GMT_flatearth_dist_km (double x0, double y0, double x1, double y1)
 {
-	/* Calculates the approximate flat earth distance in km */
+	/* Calculates the approximate flat earth distance in km.
+	   If difference in longitudes exceeds 180 we pick the other
+	   offset (360 - offset)
+	 */
 	double dlon;
 
 	dlon = x1 - x0;
