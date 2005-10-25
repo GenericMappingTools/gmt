6779e56ada5af3d04e6da4f5587c74fc96006a8b
Remko Scharroo <remko.scharroo@eumetsat.int>
2005-10-25 20:33:03 +0000

    - Small changes

--- trunk/doc/scripts/GMT_-OK.sh
+++ trunk/doc/scripts/GMT_-OK.sh
@@ -1,40 +1,109 @@
 #!/bin/sh
-#	$Id: GMT_-OK.sh,v 1.4 2004-04-10 17:19:14 pwessel Exp $
+#	$Id: GMT_-OK.sh,v 1.5 2005-10-25 20:33:03 remko Exp $
 #
 
-pstext -R0/2.7/0/2 -Jx1 -P -K -N << EOF > GMT_-OK.ps
-0.5	1.8	10	0	1	CM	HEADER
-0.5	1.35	10	0	1	CM	BODY@-1@-
-0.5	0.55	10	0	1	CM	BODY@-n@-
-0.5	0.1	10	0	1	CM	TRAILER
-1.15	1.8	9	0	0	LM	@%1%\035O@%% ommits the header.
-1.15	1.025	9	0	0	LM	2nd through n-1'th overlays
-1.15	0.875	9	0	0	LM	require both @%1%\035O@%% and @%1%\035K@%%.
-1.15	0.1	9	0	0	LM	@%1%\035K@%% ommits the trailer.
-EOF
-psxy -R -Jx -O -K -W0.25p,- << EOF >> GMT_-OK.ps
-0.5	0.8
-0.5	1.1
-EOF
-psxy -R -Jx -O -M -L -N -W1p << EOF >> GMT_-OK.ps
->
-0	0
-1	0
-1	0.2
-0	0.2
->
-0	0.3
-1	0.3
-1	0.8
-0	0.8
->
-0	1.1
-1	1.1
-1	1.6
-0	1.6
->
-0	1.7
-1	1.7
-1	1.9
+psxy -R0/2.7/0/2.5 -Jx1i -K -L -M -N -P << EOF > GMT_-OK.ps
+> -G200
+0	1.4
+1	1.4
+1	1.2
+0	1.2
+> -G200
 0	1.9
+1	1.9
+1	2.1
+0	2.1
+> -W1p
+0	1.2
+1	1.2
+1	2.1
+0	2.1
+EOF
+pstext -R -Jx -O -K -N << EOF >> GMT_-OK.ps
+0.5	2.3	9	0	0	CM	1-part PostScript file
+0.5	2.0	10	0	1	CM	HEADER
+0.5	1.65	10	0	1	CM	BODY@-1@-
+0.5	1.3	10	0	1	CM	TRAILER
+EOF
+
+psxy -R -Jx -X1.3i -O -K -L -M -N << EOF >> GMT_-OK.ps
+> -G200
+0	0.8
+1	0.8
+1	0.6
+0	0.6
+> -G200
+0	1.9
+1	1.9
+1	2.1
+0	2.1
+> -W1p
+0	0.6
+1	0.6
+1	1.3
+0	1.3
+> -W1p
+0	1.4
+1	1.4
+1	2.1
+0	2.1
+> -W1p
+0.5	1.3
+0.5	1.4
+EOF
+pstext -R -Jx -O -K -N << EOF >> GMT_-OK.ps
+0.5	2.3	9	0	0	CM	2-part PostScript file
+0.5	2.0	10	0	1	CM	HEADER
+0.5	1.65	10	0	1	CM	BODY@-1@-
+1.1	1.5	9	0	0	LM	@%1%\035K@%% ommits trailer
+1.1	1.2	9	0	0	LM	@%1%\035O@%% ommits header
+0.5	1.05	10	0	1	CM	BODY@-2@-
+0.5	0.7	10	0	1	CM	TRAILER
+EOF
+
+psxy -R -Jx -X2.35i -O -K -M -L -N << EOF >> GMT_-OK.ps
+> -G200
+0	0.2
+1	0.2
+1	0.0
+0	0.0
+> -G200
+0	1.9
+1	1.9
+1	2.1
+0	2.1
+> -W1p
+0	0.0
+1	0.0
+1	0.7
+0	0.7
+> -W1p
+0	0.8
+1	0.8
+1	1.3
+0	1.3
+> -W1p
+0	1.4
+1	1.4
+1	2.1
+0	2.1
+> -W1p
+0.5	1.3
+0.5	1.4
+> -W1p
+0.5	0.7
+0.5	0.8
+EOF
+pstext -R -Jx -O -N << EOF >> GMT_-OK.ps
+0.5	2.3	9	0	0	CM	n-part PostScript file
+0.5	2.0	10	0	1	CM	HEADER
+0.5	1.65	10	0	1	CM	BODY@-1@-
+1.1	1.5	9	0	0	LM	@%1%\035K@%% ommits trailer
+0.5	1.05	10	0	1	CM	BODY@-i@-
+1.1	1.25	9	0	0	LM	2nd through n-1'th
+1.1	1.1	9	0	0	LM	overlays require
+1.1	0.95	9	0	0	LM	both @%1%\035O@%% and @%1%\035K@%%
+1.1	0.6	9	0	0	LM	@%1%\035O@%% ommits header
+0.5	0.45	10	0	1	CM	BODY@-n@-
+0.5	0.1	10	0	1	CM	TRAILER
 EOF
