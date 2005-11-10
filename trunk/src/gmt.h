5419afa9596d2a50be04fd71404ba50a20dc09cb
Paul Wessel <pwessel@hawaii.edu>
2005-11-10 21:49:28 +0000

    *** empty log message ***

--- trunk/src/gmt.h
+++ trunk/src/gmt.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt.h,v 1.109 2005-10-28 17:23:04 remko Exp $
+ *	$Id: gmt.h,v 1.110 2005-11-10 21:49:28 pwessel Exp $
  *
  *	Copyright (c) 1991-2005 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -564,8 +564,6 @@ EXTERN_MSC PFI GMT_x_forward, GMT_x_inverse;	/*	Pointers to the selected linear 
 EXTERN_MSC PFI GMT_y_forward, GMT_y_inverse;	/*	Pointers to the selected linear functions */
 EXTERN_MSC PFI GMT_z_forward, GMT_z_inverse;	/*	Pointers to the selected linear functions */
 EXTERN_MSC PFD GMT_scan_time_string;		/*	pointer to functions that converts timestring to secs */
-EXTERN_MSC double DEG2M;			/*	Scale converting degrees to meters on spheroid	*/
-EXTERN_MSC double DEG2KM;			/*	Scale converting degrees to km on spheroid	*/
 
 #include "gmt_math.h"		/* Machine-dependent macros for non-POSIX math functions */
 #ifdef WIN32
