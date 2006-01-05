ce3f1fa9b7229a23b0bda7943f58c392914380b1
Paul Wessel <pwessel@hawaii.edu>
2006-01-05 05:33:14 +0000

    *** empty log message ***

--- trunk/doc/examples/ex23/job23.bat
+++ trunk/doc/examples/ex23/job23.bat
@@ -1,7 +1,7 @@
 REM
 REM             GMT EXAMPLE 23
 REM
-REM             $Id: job23.bat,v 1.8 2004-09-29 05:22:20 pwessel Exp $
+REM             $Id: job23.bat,v 1.9 2006-01-05 05:33:14 pwessel Exp $
 REM
 REM Purpose:    Plot distances from Rome and draw shortest paths
 REM
@@ -64,7 +64,7 @@ REM Sample the distance grid at the cities and use the distance in km for labels
 
 echo {printf "%%s %%s 12 0 1 CB %%d\n", $1, $2, int($NF+0.5)} > awk.2
 grdtrack -Gdist.grd cities.d | gawk -f awk.2 > pts.d
-pstext -R -J -O -D0/0.2i -N -Wwhiteo -C0.02i/0.02i pts.d >> example_23.ps
+pstext -R -J -O -D0/-0.2i -N -Wwhiteo -C0.02i/0.02i pts.d >> example_23.ps
 
 REM Clean up after ourselves:
 
