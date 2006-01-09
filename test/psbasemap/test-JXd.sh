8c0e803a1ecd2c24c428fc521719da4faafffd42
Remko Scharroo <remko.scharroo@eumetsat.int>
2006-01-09 20:27:17 +0000

    - Small changes

--- test/psbasemap/test-JXd.sh
+++ test/psbasemap/test-JXd.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-ps=test-JXd-1.ps
+psbasemap=psbasemap
+psxy=psxy
 
 plot1 () {
-   psbasemap -R -JX$1 -B20f10g20:Longitude:/20f10g20:Latitude: --PLOT_DEGREE_FORMAT=dddF -O -K $2
+   $psbasemap -R -JX$1 -B20f10g20:Longitude:/20f10g20:Latitude: --PLOT_DEGREE_FORMAT=dddF -O -K $2
    annot $1
 }
 
 annot () {
-   psxy -R -J -O -K -W2p/red -Gyellow <<%
+   $psxy -R -J -O -K -W2p/red -Gyellow <<%
 -40 0
 40 0
 40 -40
@@ -19,6 +20,7 @@ annot () {
 %
 }
 
+ps=test-JXd.ps
 psxy /dev/null -R-60/60/-60/60 -JX8c/8c -K -X4c -Y13c > $ps
 plot1 8c/8c >> $ps
 plot1 8cd/8c -X12c >> $ps
@@ -26,14 +28,7 @@ plot1 8c/8cd -Y-11c >> $ps
 plot1 8cd/8cd -X-12c >> $ps
 psxy /dev/null -R -J -O >> $ps
 
-ps=test-JXd-2.ps
-psxy /dev/null -R-60/60/-60/60 -JX8c/8c -K -X4c -Y13c > $ps
-plot1 8c/8c >> $ps
-psbasemap -R -JX8cd/8cd -B20f10g20/Sn --PLOT_DEGREE_FORMAT=dddF --BASEMAP_TYPE=plain -X12c -O -K >> $ps
-psbasemap -R -JX8c/8c -B:Longitude:/20f10g20:Latitude: --PLOT_DEGREE_FORMAT=dddF -O -K >> $ps
-annot 8cd/8c >> $ps
-psbasemap -R -JX8cd/8cd -B/20f10g20We --PLOT_DEGREE_FORMAT=dddF -Y-11c -O -K >> $ps
-psbasemap -R -JX8c/8c -B20f10g20:Longitude:/:Latitude: --PLOT_DEGREE_FORMAT=dddF -O -K >> $ps
-annot 8c/8cd >> $ps
-plot1 8cd/8cd -X-12c >> $ps
-psxy /dev/null -R -J -O >> $ps
+rm -f .gmtcommands4
+
+echo -n "Comparing test-JXd_orig.ps and test-JXd.ps: "
+compare -density 100 -metric PSNR test-JXd_orig.ps test-JXd.ps test-JXd_diff.png
