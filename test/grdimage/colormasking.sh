1df82b9a09d661dd5e22d833ba44c8080a09ac7d
Remko Scharroo <remko.scharroo@eumetsat.int>
2006-01-17 13:55:24 +0000

    - Removed local directories

--- test/grdimage/colormasking.sh
+++ test/grdimage/colormasking.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ps=colormasking.ps
 #grdmath -R0/3/0/3 -I1 X Y DIV = t.grd
-~altim/gmt/src/xyz2grd -R-0.5/2.5/-0.5/2.5 -I1 -F -Gt.grd <<%
+xyz2grd -R-0.5/2.5/-0.5/2.5 -I1 -F -Gt.grd <<%
 0 0 0.0
 0 1 0.2
 0 2 0.4
@@ -13,9 +13,9 @@ ps=colormasking.ps
 2 2 2.4
 %
 makecpt -Crainbow -T-0.1/2.5/0.2 > t.cpt
-~altim/gmt/src/grdimage t.grd -JX1c -Ct.cpt -K -P -X8c -Y8c > $ps
-~altim/gmt/src/grdimage t.grd -JX3c -Ct.cpt -K -O -X-1c -Y-1c -Q >> $ps
-~altim/gmt/src/grdimage t.grd -JX9c -Ct.cpt -K -O -X-3c -Y-3c -Q >> $ps
+grdimage t.grd -JX1c -Ct.cpt -K -P -X8c -Y8c > $ps
+grdimage t.grd -JX3c -Ct.cpt -K -O -X-1c -Y-1c -Q >> $ps
+grdimage t.grd -JX9c -Ct.cpt -K -O -X-3c -Y-3c -Q >> $ps
 psxy -R -J -Gp50/10:FwhiteB- -O >> $ps <<%
 1 0
 2 0
