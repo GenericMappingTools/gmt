741f425d41551a206c0ea8b745e5f1e98197d3ae
Remko Scharroo <remko.scharroo@eumetsat.int>
2006-02-20 02:41:13 +0000

    - Added grdcontour to the test

--- trunk/test/grdimage/grdimage.sh
+++ trunk/test/grdimage/grdimage.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ps=grdimage.ps
-grdimage=~altim/gmt/src/grdimage" t.grd -Ct.cpt -JX1i -B1/1"
+grdimage=grdimage" t.grd -Ct.cpt -JX1i -B1/1"
+grdcontour=grdcontour" t.grd -Ct.cpt -J -R -O"
 
 makegrd () {
 xyz2grd -I1 -Gt.grd $* <<%
@@ -23,42 +24,36 @@ pstext -R -J -N -O -K <<%
 %
 }
 
+plots () {
+$grdimage -K -R-0.5/2.5/-0.5/2.5 $1
+label $3 ""
+$grdimage -O -K -R0/2/0/2 -X4c
+$grdimage -O -K -R-0.5/1.5/0/2 -X4c
+$grdimage -O -K -R-1/3/-1/3 -X4c
+$grdimage -O -K -Towhite -R-0.5/2.5/-0.5/2.5 -X-12c -Y-4c
+label $3 -T
+$grdimage -O -K -Towhite -R0/2/0/2 -X4c
+$grdimage -O -K -Towhite -R-0.5/1.5/0/2 -X4c
+$grdimage -O -K -Towhite -R-1/3/-1/3 -X4c
+$grdimage -E50 -O -K -R-0.5/2.5/-0.5/2.5 -X-12c -Y-4c
+$grdcontour -K
+label $3 -E50
+$grdimage -E50 -O -K -R0/2/0/2 -X4c
+$grdcontour -K
+$grdimage -E50 -O -K -R-0.5/1.5/0/2 -X4c
+$grdcontour -K
+$grdimage -E50 -O -K -R-1/3/-1/3 -X4c
+$grdcontour $2
+}
+
 makecpt -Crainbow -T-0.1/2.5/0.2 > t.cpt
 makegrd -R0/2/0/2
 
-$grdimage -P -K -R-0.5/2.5/-0.5/2.5 -X4c -Y24c > $ps
-label "" "" >> $ps
-$grdimage -O -K -R0/2/0/2 -X4c >> $ps
-$grdimage -O -K -R-0.5/1.5/0/2 -X4c >> $ps
-$grdimage -O -K -R-1/3/-1/3 -X4c >> $ps
-$grdimage -O -K -Towhite -R-0.5/2.5/-0.5/2.5 -X-12c -Y-4c >> $ps
-label "" -T >> $ps
-$grdimage -O -K -Towhite -R0/2/0/2 -X4c >> $ps
-$grdimage -O -K -Towhite -R-0.5/1.5/0/2 -X4c >> $ps
-$grdimage -O -K -Towhite -R-1/3/-1/3 -X4c >> $ps
-$grdimage -E50 -O -K -R-0.5/2.5/-0.5/2.5 -X-12c -Y-4c >> $ps
-label "" -E50 >> $ps
-$grdimage -E50 -O -K -R0/2/0/2 -X4c >> $ps
-$grdimage -E50 -O -K -R-0.5/1.5/0/2 -X4c >> $ps
-$grdimage -E50 -O -K -R-1/3/-1/3 -X4c >> $ps
+plots "-P -X4c -Y24c" -K " " > $ps
 
 makegrd -R-0.5/2.5/-0.5/2.5 -F
 
-$grdimage -O -K -R-0.5/2.5/-0.5/2.5 -X-12c -Y-4c >> $ps
-label -F "" >> $ps
-$grdimage -O -K -R0/2/0/2 -X4c >> $ps
-$grdimage -O -K -R-0.5/1.5/0/2 -X4c >> $ps
-$grdimage -O -K -R-1/3/-1/3 -X4c >> $ps
-$grdimage -O -K -Towhite -R-0.5/2.5/-0.5/2.5 -X-12c -Y-4c >> $ps
-label -F -T >> $ps
-$grdimage -O -K -Towhite -R0/2/0/2 -X4c >> $ps
-$grdimage -O -K -Towhite -R-0.5/1.5/0/2 -X4c >> $ps
-$grdimage -O -K -Towhite -R-1/3/-1/3 -X4c >> $ps
-$grdimage -E50 -O -K -R-0.5/2.5/-0.5/2.5 -X-12c -Y-4c >> $ps
-label -F -E50 >> $ps
-$grdimage -E50 -O -K -R0/2/0/2 -X4c >> $ps
-$grdimage -E50 -O -K -R-0.5/1.5/0/2 -X4c >> $ps
-$grdimage -E50 -O    -R-1/3/-1/3 -X4c >> $ps
+plots "-O -X-12c -Y-4c" " " -F >> $ps
 
 rm -f t.grd t.cpt .gmtcommands4
 
