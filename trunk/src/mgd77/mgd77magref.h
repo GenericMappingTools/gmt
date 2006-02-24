0fc02fd6a10a1293f3c547f21f3f39b33f73ae62
Paul Wessel <pwessel@hawaii.edu>
2006-02-24 00:02:54 +0000

    *** empty log message ***

--- trunk/src/mgd77/mgd77magref.h
+++ trunk/src/mgd77/mgd77magref.h
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------
- *	$Id: mgd77magref.h,v 1.1 2006-02-16 11:53:24 pwessel Exp $	
+ *	$Id: mgd77magref.h,v 1.2 2006-02-24 00:02:54 pwessel Exp $	
  *
  *	File:	mgd77magref.h
  *
@@ -13,21 +13,21 @@
  *	Date:	1-Aug-2005
  * 
  *  References:
- *  Langel, R. A., 1987. The Main Field. In Geomagnetism, Volume I, (Ed. J. A. Jacobs). London: Academic Press. 
- *		pp249Ð512.
-
+ *    Langel, R. A., 1987. The Main Field. In Geomagnetism, Volume I,
+ *      (Ed. J. A. Jacobs). London: Academic Press, pp249-512.
+ *
  * ------------------------------------------------------------------*/
 /*		  Field,		Code,	Start,		End */
 		{"AWC 70",		1,		1965,		1970}, /* find interval */
 		{"AWC 75",		2,		1967,		1974},
 		{"IGRF-65",		3,		1960,		1965},
 		{"IGRF-75",		4,		1970,		1975},
-		{"GSFC-1266",	5,		1900,		1966},
-		{"GSFC-0674",	6,		9999,		9999}, /* find interval */
+		{"GSFC-1266",		5,		1900,		1966},
+		{"GSFC-0674",		6,		9999,		9999}, /* find interval */
 		{"UK 75",		7,		1970,		1975},
-		{"POGO 0368",	8,		9999,		9999}, /* find interval */
-		{"POGO 1068",	9,		1965,		1968},
-		{"POGO 0869",	10,		1965,		1968},
+		{"POGO 0368",		8,		9999,		9999}, /* find interval */
+		{"POGO 1068",		9,		1965,		1968},
+		{"POGO 0869",		10,		1965,		1968},
 		{"IGRF-80",		11,		1975,		1980},
 		{"IGRF-85",		12,		1980,		1985},
 		{"IGRF-90",		13,		1985,		1990}
