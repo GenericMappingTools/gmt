cd80bbc22fcb5f22dbb3512f3ff8ccc060c94721
Paul Wessel <pwessel@hawaii.edu>
2006-03-22 07:08:53 +0000

    *** empty log message ***

--- trunk/src/gmtsuppl.bat
+++ trunk/src/gmtsuppl.bat
@@ -1,7 +1,7 @@
 ECHO OFF
 REM ----------------------------------------------------
 REM
-REM	$Id: gmtsuppl.bat,v 1.27 2006-02-28 23:56:18 pwessel Exp $
+REM	$Id: gmtsuppl.bat,v 1.28 2006-03-22 07:08:53 pwessel Exp $
 REM
 REM
 REM	Copyright (c) 1991-2006 by P. Wessel and W. H. F. Smith
@@ -149,6 +149,7 @@ ECHO STEP 8: Make misc
 REM ----------------------------------------------------
 cd misc
 SET DIG="\"COM0:\""
+CL %COPT% gmtstitch.c %GMTLIB%
 CL %COPT% psmegaplot.c %GMTLIB%
 CL %COPT% ps2raster.c %GMTLIB%
 CL %COPT% makepattern.c %GMTLIB%
