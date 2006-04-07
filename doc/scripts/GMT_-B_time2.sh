c570f2f345b52a4c8d9301b4448f3dd198281c91
Paul Wessel <pwessel@hawaii.edu>
2006-04-07 22:07:46 +0000

    *** empty log message ***

--- doc/scripts/GMT_-B_time2.sh
+++ doc/scripts/GMT_-B_time2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#	$Id: GMT_-B_time2.sh,v 1.3 2004-04-13 17:37:39 pwessel Exp $
+#	$Id: GMT_-B_time2.sh,v 1.4 2006-04-07 22:07:46 pwessel Exp $
 #
 gmtset PLOT_CLOCK_FORMAT hh:mm
-psbasemap -R2001-9-11T/2001-9-13T/0/1 -JX6t/0.2 -Bpa6Hf1h -Bsa1KS -P -K > GMT_-B_time2.ps
+psbasemap -R1969-7-21T/1969-7-23T/0/1 -JX6t/0.2 -Bpa6Hf1h -Bsa1KS -P -K > GMT_-B_time2.ps
 gmtset PLOT_DATE_FORMAT "o dd"
 psbasemap -R -JX -Bpa6Hf1h -Bsa1DS -O -Y0.65i >> GMT_-B_time2.ps
