8009626110d2ae0952068f8c87753c231daf26bd
Remko Scharroo <remko.scharroo@eumetsat.int>
2006-10-30 02:59:36 +0000

    - All sorts of improvements

--- doc/scripts/GMT_-B_time1.sh
+++ doc/scripts/GMT_-B_time1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: GMT_-B_time1.sh,v 1.7 2004-04-13 17:37:39 pwessel Exp $
+#	$Id: GMT_-B_time1.sh,v 1.8 2006-10-30 02:59:36 remko Exp $
 #
 gmtset PLOT_DATE_FORMAT -o ANNOT_FONT_SIZE_PRIMARY +9p
-psbasemap -R2000-4-1T/2000-5-25T/0/1 -JX6T/0.2 -Bpa7Rf1d -Bsa1OS -P > GMT_-B_time1.ps
+psbasemap -R2000-4-1T/2000-5-25T/0/1 -JX6/0.2 -Bpa7Rf1d -Bsa1OS -P > GMT_-B_time1.ps
--- doc/scripts/GMT_linear_cal.sh
+++ doc/scripts/GMT_linear_cal.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: GMT_linear_cal.sh,v 1.2 2004-04-13 17:40:02 pwessel Exp $
+#	$Id: GMT_linear_cal.sh,v 1.3 2006-10-30 02:59:36 remko Exp $
 #
 gmtset PLOT_DATE_FORMAT o TIME_WEEK_START Sunday PLOT_CLOCK_FORMAT -hham TIME_FORMAT_PRIMARY full
-psbasemap -R2001-9-24T/2001-9-29T/T07:0/T15:0 -JX4T/-2T -Ba1Kf1kg1d/a1Hg1hWsNe -P > GMT_linear_cal.ps
+psbasemap -R2001-9-24T/2001-9-29T/T07:0/T15:0 -JX4i/-2i -Ba1Kf1kg1d/a1Hg1hWsNe -P > GMT_linear_cal.ps
