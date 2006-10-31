1ea53ed05fc7bb6a1afc64209f6bf8cb04206dcb
Remko Scharroo <remko.scharroo@eumetsat.int>
2006-10-31 18:01:10 +0000

    - share/coast directory shouldn't be wiped away

--- trunk/guru/cvsclean.sh
+++ trunk/guru/cvsclean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: cvsclean.sh,v 1.3 2004-08-18 23:19:44 pwessel Exp $
+#	$Id: cvsclean.sh,v 1.4 2006-10-31 18:01:10 remko Exp $
 
 trap 'rm -f $$.*; exit 1' 1 2 3 15
 
@@ -25,21 +25,7 @@ find . -name ".#*" >>$$.lis
 # the result of grep is redirected to rm command
 cat $$.lis | awk '{printf "ls -d -1 %s 2>/dev/null\n",$0}' |sh | grep -v \
 -e "guru/gmtguru.macros$" \
--e "share/binned_border_c.cdf$" \
--e "share/binned_border_f.cdf$" \
--e "share/binned_border_h.cdf$" \
--e "share/binned_border_i.cdf$" \
--e "share/binned_border_l.cdf$" \
--e "share/binned_GSHHS_c.cdf$" \
--e "share/binned_GSHHS_f.cdf$" \
--e "share/binned_GSHHS_h.cdf$" \
--e "share/binned_GSHHS_i.cdf$" \
--e "share/binned_GSHHS_l.cdf$" \
--e "share/binned_river_c.cdf$" \
--e "share/binned_river_f.cdf$" \
--e "share/binned_river_h.cdf$" \
--e "share/binned_river_i.cdf$" \
--e "share/binned_river_l.cdf$" \
+-e "share/coast$" \
 | $AWK '{printf "rm -rvf \"%s\"\n",$0}' |sh
 
 # delete the killfile
