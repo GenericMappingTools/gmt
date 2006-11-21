947f1db08c30ba78176d9bd3ca7c5368c50c931c
Remko Scharroo <remko.scharroo@eumetsat.int>
2006-11-21 22:08:25 +0000

    - Changed all abbreviation to lower case (can always be made uppercase)

--- trunk/share/time/nl.d
+++ trunk/share/time/nl.d
@@ -1,28 +1,28 @@
 # GMT Time language file for NL (Dutch/Netherlands) mode [NL]
 #
-#       $Id: nl.d,v 1.2 2006-11-17 01:42:00 remko Exp $
+#       $Id: nl.d,v 1.3 2006-11-21 22:08:25 remko Exp $
 #-------------------------------------------------
 # Monthnames, with 3-char and 1-char abbreviations
-M	1	januari		jan	J
-M	2	februari	feb	F
-M	3	maart		mar	M
-M	4	april		apr	A
-M	5	mei		mei	M
-M	6	juni		jun	J
-M	7	juli		jul	J
-M	8	augustus	aug	A
-M	9	september	sep	S
-M	10	oktober		okt	O
-M	11	november	nov	N
-M	12	december	dec	D
+M	1	januari		jan	j
+M	2	februari	feb	f
+M	3	maart		mar	m
+M	4	april		apr	a
+M	5	mei		mei	m
+M	6	juni		jun	j
+M	7	juli		jul	j
+M	8	augustus	aug	a
+M	9	september	sep	s
+M	10	oktober		okt	o
+M	11	november	nov	n
+M	12	december	dec	d
 # Name and abbreviations for the word Week in this language
-U	1	week		wk	W
+U	1	week		wk	w
 # Weeknames, with 3-char and 1-char abbreviations
 # (List starts on Sunday regardless of TIME_WEEK_START)
-W	1	zondag		zo	Z
-W	2	maandag		ma	M
-W	3	dinsdag		di	D
-W	4	woensdag	wo	W
-W	5	donderdag	do	D
-W	6	vrijdag		vr	V
-W	7	zaterdag	za	Z
+W	1	zondag		zo	z
+W	2	maandag		ma	m
+W	3	dinsdag		di	d
+W	4	woensdag	wo	w
+W	5	donderdag	do	d
+W	6	vrijdag		vr	v
+W	7	zaterdag	za	z
