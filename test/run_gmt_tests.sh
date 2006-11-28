71ee14ab5f43d9fe0a024d44517c85db619cfef8
Paul Wessel <pwessel@hawaii.edu>
2006-11-28 16:07:58 +0000

    Also append -Z<val> in multisegment headers made by grdcontour and pscontour

--- test/run_gmt_tests.sh
+++ test/run_gmt_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: run_gmt_tests.sh,v 1.1 2006-03-06 09:43:48 pwessel Exp $
+#	$Id: run_gmt_tests.sh,v 1.2 2006-11-28 16:07:58 pwessel Exp $
 #
 #	test script for GMT/test directory
 #
@@ -7,17 +7,29 @@
 # Bourne or csh scripts found in each directory
 
 # Get all directories below GMT/test and filter out the CVS directories
-find . -type d -print -mindepth 1 | grep -v CVS > $$.dirs
+find . -type d -print -mindepth 1 | grep -v CVS > /tmp/$$.dirs
 
 # Set here and use it to reset the dir in case a script fails to finish properly
 
 here=`pwd`
 while read dir; do
 	cd $here/$dir
-	for script in *.*sh; do	# Looks for both *.sh and *.csh
-		cd $here/$dir
-		sh $script
-	done
-done < $$.dirs
+	# Look for Bourne shell scripts
+	ls *.sh > /tmp/$$.lis
+	if [ -s /tmp/$$.lis ]; then	# Found Bourne shell scripts
+		for script in *.sh; do
+			cd $here/$dir
+			sh $script
+		done
+	fi
+	# Look for C shell scripts
+	ls *.csh > /tmp/$$.lis
+	if [ -s /tmp/$$.lis ]; then	# Found C shell scripts
+		for script in *.csh; do
+			cd $here/$dir
+			csh $script
+		done
+	fi
+done < /tmp/$$.dirs
 cd $here
-rm -f $$.dirs
+rm -f /tmp/$$.*
