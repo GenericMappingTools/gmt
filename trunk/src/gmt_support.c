a938fbc64c877d1c3d2ba1f923bcd7abd008c14a
Paul Wessel <pwessel@hawaii.edu>
2006-11-28 16:07:58 +0000

    Also append -Z<val> in multisegment headers made by grdcontour and pscontour

--- trunk/src/gmt_support.c
+++ trunk/src/gmt_support.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_support.c,v 1.273 2006-11-20 16:20:07 remko Exp $
+ *	$Id: gmt_support.c,v 1.274 2006-11-28 16:07:58 pwessel Exp $
  *
  *	Copyright (c) 1991-2006 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -3466,7 +3466,7 @@ void GMT_dump_contour (double *xx, double *yy, int nn, double cval, int id, BOOL
 		}
 		else	/* Later we append to it */
 			fp = fopen (file, "a+");
-		sprintf (GMT_io.segment_header, "%c %g contour\n", GMT_io.EOF_flag[GMT_OUT], cval);
+		sprintf (GMT_io.segment_header, "%c %g contour -Z%g\n", GMT_io.EOF_flag[GMT_OUT], cval, cval);
 		GMT_write_segmentheader (fp, 3);
 	}
 	else {
