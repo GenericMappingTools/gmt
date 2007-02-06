13384c646dd3fba918c5e0a6e607d35a525aafb7
Paul Wessel <pwessel@hawaii.edu>
2007-02-06 17:56:25 +0000

    psxy -Sq has no symbol size but the GMT_parse_symbol tried anyway and gave a warning.

--- src/gmt_init.c
+++ src/gmt_init.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_init.c,v 1.274 2007-01-31 21:51:03 remko Exp $
+ *	$Id: gmt_init.c,v 1.275 2007-02-06 17:56:25 pwessel Exp $
  *
  *	Copyright (c) 1991-2007 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -5026,8 +5026,10 @@ int GMT_parse_symbol_option (char *text, struct GMT_SYMBOL *p, int mode, BOOLEAN
 		}
 	}
 	else {
+		char s_upper;
 		n = sscanf (text, "%c%[^/]/%s", &symbol_type, txt_a, txt_b);
-		if (toupper ((int)symbol_type) != 'V')
+		s_upper = (char)toupper ((int)symbol_type);
+		if (!(s_upper == 'V' || s_upper == 'Q'))
 			p->size_x = p->given_size_x = GMT_convert_units (txt_a, GMT_INCH);
 		if (n == 3)
 			p->size_y = p->given_size_y = GMT_convert_units (txt_b, GMT_INCH);
