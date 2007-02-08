05908da289d85f928caa337918fea81c469bf32e
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-02-08 21:43:12 +0000

    - Now also includes the drawing of the bullseye

--- trunk/doc/scripts/GMT_volcano.sh
+++ trunk/doc/scripts/GMT_volcano.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-#	$Id: GMT_volcano.sh,v 1.4 2006-10-24 01:53:19 remko Exp $
+#	$Id: GMT_volcano.sh,v 1.5 2007-02-08 21:43:12 remko Exp $
 
-echo "0 0" | psxy -R-0.5/0.5/-0.5/0.5 -JX2i -P -Ba0.25g0.05WSne -Wthick -Skvolcano/2i > GMT_volcano.ps
+ln -s ../../examples/ex20/bullseye.def .
+echo "0 0" | psxy -R-0.5/0.5/-0.5/0.5 -JX2i -P -Ba0.25g0.05WSne -Wthick -Skvolcano/2i -K > GMT_volcano.ps
+echo "0 0" | psxy -R -J -N -Ba0.25g0.05wSnE -Wthick -Skbullseye/2i -O -X2.5i >> GMT_volcano.ps
+rm -f bullseye.def
