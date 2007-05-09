27401c9bfee9af2ce9fb0e914a6cf36a2c78e66f
Paul Wessel <pwessel@hawaii.edu>
2007-05-09 00:30:57 +0000

    *** empty log message ***

---
+++ trunk/src/coast/polygon_findarea.c
@@ -0,0 +1,60 @@
+/*
+ *	$Id: polygon_findarea.c,v 1.1 2007-05-09 00:30:57 pwessel Exp $
+ * Based on polygon_findlevel but limited to just compute polygon areas.
+ */
+#include "wvs.h"
+
+int main (int argc, char **argv) {
+	int k, ID, sign, n_alloc = 0;
+	double size;
+	double *flon = NULL, *flat = NULL;
+	FILE *fp;
+	struct LONGPAIR p;
+	struct GMT3_POLY h;
+	
+	if (argc == 1) {
+		fprintf (stderr, "usage: polygon_findarea final_dbase.b [ID]\n");
+		fprintf (stderr, "Computes all areas unless a specific ID is given\n");
+		fprintf (stderr, "Will write areas to stdout.\n");
+		exit (-1);
+	}
+
+	ID = (argc == 3) ? atoi (argv[2]) : -1;
+	
+	fprintf (stderr, "Read headers\n");
+	fp = fopen (argv[1], "r");
+	
+	/* Reset ids as we go along */
+	
+	area_init ();
+
+	while (pol_readheader (&h, fp) == 1) {
+		if (ID >= 0 && h.id != ID) {	/* Skip this polygon */
+			fseek (fp, h.n * sizeof(struct LONGPAIR), 1);
+			continue;
+		}
+		if (h.n > n_alloc) {
+			n_alloc = h.n;
+			flon = (double *) GMT_memory ((void *)flon, n_alloc, sizeof(double), "polygon_findarea");
+			flat = (double *) GMT_memory ((void *)flat, n_alloc, sizeof(double), "polygon_findarea");
+		}
+		for (k = 0; k < h.n; k++) {
+			if (pol_fread (&p, 1, fp) != 1) {
+				fprintf(stderr,"polygon_findarea:  ERROR  reading file.\n");
+				exit(-1);
+			}
+			if (h.greenwich && p.x > h.datelon) p.x -= M360;
+			flon[k] = p.x * 1.0e-6;
+			flat[k] = p.y * 1.0e-6;
+		}
+		size = 1.0e-6 * area_size (flon, flat, h.n, &sign); /* in km^2 */
+		printf ("%d\t%.12g\n", ID, size * sign);
+	}
+	
+	fclose (fp);
+	
+	free ((void *)flon);
+	free ((void *)flat);
+	
+	exit (0);
+}	
--- trunk/src/coast/polygon_findlevel.c
+++ trunk/src/coast/polygon_findlevel.c
@@ -1,5 +1,5 @@
 /*
- *	$Id: polygon_findlevel.c,v 1.9 2007-03-24 01:42:07 pwessel Exp $
+ *	$Id: polygon_findlevel.c,v 1.10 2007-05-09 00:30:57 pwessel Exp $
  */
 #include "wvs.h"
 
@@ -18,12 +18,10 @@ int *lon, *lat;
 double *flon, *flat;
 struct LONGPAIR *pp;
 
-int non_zero_winding2 (int xp, int yp, int *x, int *y, int n_path);
-
 int main (int argc, char **argv) {
 	int i, j, k, n_id, pos, id, id1, id2, idmax, intest, sign, max_level, n, n_of_this[6];
 	int n_reset = 0, old, bad = 0, ix0, off, set, fast = 0;
-	double x0, west1, west2, east1, east2, size, val, area_size();
+	double x0, west1, west2, east1, east2, size, val;
 	FILE *fp, *fp2, *fpx, *fps;
 	struct LONGPAIR p;
 	char line[80];
@@ -83,13 +81,7 @@ int main (int argc, char **argv) {
 
 		fprintf (stderr, "\n\nFind area and direction of polygons\n");
 
-		gmtdefs.ellipsoid = GMT_N_ELLIPSOIDS-1;
-		project_info.projection = GMT_LAMB_AZ_EQ;
-		project_info.unit = GMT_M;
-		project_info.pars[2] = 39.3700787401574814;
-		project_info.region = 1;
-		gmtdefs.line_step = 1.0e7;	/* To avoid nlon/nlat being huge */
-		project_info.degree[0] = project_info.degree[1] = TRUE;
+		area_init ();
 
 		flon = (double *) GMT_memory (CNULL, blob[0].h.n, sizeof(double), "polygon_findlevel");
 		flat = (double *) GMT_memory (CNULL, blob[0].h.n, sizeof(double), "polygon_findlevel");
@@ -436,248 +428,3 @@ int main (int argc, char **argv) {
 	
 	exit (0);
 }	
-
-double area_size (double x[], double y[], int n, int *sign)
-{
-	int i;
-	double west, east, south, north, lon, lat, xx, yy, size, ix, iy, area();
-	
-	west = south = 1.0e100;
-	east = north = -1.0e100;
-	lon = lat = 0.0;
-	
-	/* Estimate 'average' position */
-	
-	for (i = 0; i < n; i++) {
-		lon += x[i];
-		lat += y[i];
-		west = MIN (west, x[i]);
-		east = MAX (east, x[i]);
-		south = MIN (south, y[i]);
-		north = MAX (north, y[i]);
-	}
-	lon /= n;
-	lat /= n;
-		
-	project_info.pars[0] = lon;
-	project_info.pars[1] = lat;
-	GMT_err_fail (GMT_map_setup (west, east, south, north), "");
-	
-	ix = 1.0 / project_info.x_scale;
-	iy = 1.0 / project_info.y_scale;
-	
-	for (i = 0; i < n; i++) {
-		GMT_geo_to_xy (x[i], y[i], &xx, &yy);
-		x[i] = (xx - project_info.x0) * ix;
-		y[i] = (yy - project_info.y0) * iy;
-	}
-	
-	size = area (x, y, n);
-	*sign = (size < 0.0) ? -1 : 1;
-	return (fabs (size));
-}
-
-double area (double x[], double y[], int n) {
-	int i;
-	double area, xold, yold;
-	
-	/* Sign will be +ve if polygon is CW, negative if CCW */
-	
-	area = yold = 0.0;
-	xold = x[n-1];
-	yold = y[n-1];
-	
-	for (i = 0; i < n; i++) {
-		area += (xold - x[i]) * (yold + y[i]);
-		xold = x[i];
-		yold = y[i];
-	}
-	return (0.5 * area);
-}
-
-int non_zero_winding2 (int xp, int yp, int *x, int *y, int n_path)
-{
-	/* Routine returns (2) if (xp,yp) is inside the
-	   polygon x[n_path], y[n_path], (0) if outside,
-	   and (1) if exactly on the path edge.
-	   Uses non-zero winding rule in Adobe PostScript
-	   Language reference manual, section 4.6 on Painting.
-	   Path should have been closed first, so that
-	   x[n_path-1] = x[0], and y[n_path-1] = y[0].
-
-	   This is version 2, trying to kill a bug
-	   in above routine:  If point is on X edge,
-	   fails to discover that it is on edge.
-
-	   We are imagining a ray extending "up" from the
-	   point (xp,yp); the ray has equation x = xp, y >= yp.
-	   Starting with crossing_count = 0, we add 1 each time
-	   the path crosses the ray in the +x direction, and
-	   subtract 1 each time the ray crosses in the -x direction.
-	   After traversing the entire polygon, if (crossing_count)
-	   then point is inside.  We also watch for edge conditions.
-
-	   If two or more points on the path have x[i] == xp, then
-	   we have an ambiguous case, and we have to find the points
-	   in the path before and after this section, and check them.
-	   */
-	
-	int	i, j, k, jend, crossing_count, above;
-	double	y_sect;
-
-	above = FALSE;
-	crossing_count = 0;
-
-	/* First make sure first point in path is not a special case:  */
-	j = jend = n_path - 1;
-	if (x[j] == xp) {
-		/* Trouble already.  We might get lucky:  */
-		if (y[j] == yp) return(1);
-
-		/* Go backward down the polygon until x[i] != xp:  */
-		if (y[j] > yp) above = TRUE;
-		i = j - 1;
-		while (x[i] == xp && i > 0) {
-			if (y[i] == yp) return (1);
-			if (!(above) && y[i] > yp) above = TRUE;
-			i--;
-		}
-
-		/* Now if i == 0 polygon is degenerate line x=xp;
-		   since we know xp,yp is inside bounding box,
-		   it must be on edge:  */
-		if (i == 0) return(1);
-
-		/* Now we want to mark this as the end, for later:  */
-		jend = i;
-
-		/* Now if (j-i)>1 there are some segments the point could be exactly on:  */
-		for (k = i+1; k < j; k++) {
-			if ( (y[k] <= yp && y[k+1] >= yp) || (y[k] >= yp && y[k+1] <= yp) ) return (1);
-		}
-
-
-		/* Now we have arrived where i is > 0 and < n_path-1, and x[i] != xp.
-			We have been using j = n_path-1.  Now we need to move j forward 
-			from the origin:  */
-		j = 1;
-		while (x[j] == xp) {
-			if (y[j] == yp) return (1);
-			if (!(above) && y[j] > yp) above = TRUE;
-			j++;
-		}
-
-		/* Now at the worst, j == jstop, and we have a polygon with only 1 vertex
-			not at x = xp.  But now it doesn't matter, that would end us at
-			the main while below.  Again, if j>=2 there are some segments to check:  */
-		for (k = 0; k < j-1; k++) {
-			if ( (y[k] <= yp && y[k+1] >= yp) || (y[k] >= yp && y[k+1] <= yp) ) return (1);
-		}
-
-
-		/* Finally, we have found an i and j with points != xp.  If (above) we may have crossed the ray:  */
-		if (above && x[i] < xp && x[j] > xp) 
-			crossing_count++;
-		else if (above && x[i] > xp && x[j] < xp) 
-			crossing_count--;
-
-		/* End nightmare scenario for x[0] == xp.  */
-	}
-
-	else {
-		/* Get here when x[0] != xp:  */
-		i = 0;
-		j = 1;
-		while (x[j] == xp && j < jend) {
-			if (y[j] == yp) return (1);
-			if (!(above) && y[j] > yp) above = TRUE;
-			j++;
-		}
-		/* Again, if j==jend, (i.e., 0) then we have a polygon with only 1 vertex
-			not on xp and we will branch out below.  */
-
-		/* if ((j-i)>2) the point could be on intermediate segments:  */
-		for (k = i+1; k < j-1; k++) {
-			if ( (y[k] <= yp && y[k+1] >= yp) || (y[k] >= yp && y[k+1] <= yp) ) return (1);
-		}
-
-		/* Now we have x[i] != xp and x[j] != xp.
-			If (above) and x[i] and x[j] on opposite sides, we are certain to have crossed the ray.
-			If not (above) and (j-i)>1, then we have not crossed it.
-			If not (above) and j-i == 1, then we have to check the intersection point.  */
-
-		if (x[i] < xp && x[j] > xp) {
-			if (above) 
-				crossing_count++;
-			else if ( (j-i) == 1) {
-				y_sect = y[i] + (y[j] - y[i]) * ( ((double)(xp - x[i])) / ((double)(x[j] - x[i])) );
-				if (rint (y_sect) == yp) return (1);
-				if (y_sect > yp) crossing_count++;
-			}
-		}
-		if (x[i] > xp && x[j] < xp) {
-			if (above) 
-				crossing_count--;
-			else if ( (j-i) == 1) {
-				y_sect = y[i] + (y[j] - y[i]) * ( ((double)(xp - x[i])) / ((double)(x[j] - x[i])) );
-				if (rint (y_sect) == yp) return (1);
-				if (y_sect > yp) crossing_count--;
-			}
-		}
-					
-		/* End easier case for x[0] != xp  */
-	}
-
-	/* Now MAIN WHILE LOOP begins:
-		Set i = j, and search for a new j, and do as before.  */
-
-	i = j;
-	while (i < jend) {
-		above = FALSE;
-		j = i+1;
-		while (x[j] == xp) {
-			if (y[j] == yp) return (1);
-			if (!(above) && y[j] > yp) above = TRUE;
-			j++;
-		}
-		/* if ((j-i)>2) the point could be on intermediate segments:  */
-		for (k = i+1; k < j-1; k++) {
-			if ( (y[k] <= yp && y[k+1] >= yp) || (y[k] >= yp && y[k+1] <= yp) ) return (1);
-		}
-
-		/* Now we have x[i] != xp and x[j] != xp.
-			If (above) and x[i] and x[j] on opposite sides, we are certain to have crossed the ray.
-			If not (above) and (j-i)>1, then we have not crossed it.
-			If not (above) and j-i == 1, then we have to check the intersection point.  */
-
-		if (x[i] < xp && x[j] > xp) {
-			if (above) 
-				crossing_count++;
-			else if ( (j-i) == 1) {
-				y_sect = y[i] + (y[j] - y[i]) * ( ((double)(xp - x[i])) / ((double)(x[j] - x[i])) );
-				if (rint (y_sect) == yp) return (1);
-				if (y_sect > yp) crossing_count++;
-			}
-		}
-		if (x[i] > xp && x[j] < xp) {
-			if (above) 
-				crossing_count--;
-			else if ( (j-i) == 1) {
-				y_sect = y[i] + (y[j] - y[i]) * ( ((double)(xp - x[i])) / ((double)(x[j] - x[i])) );
-				if (rint (y_sect) == yp) return (1);
-				if (y_sect > yp) crossing_count--;
-			}
-		}
-
-		/* That's it for this piece.  Advance i:  */
-
-		i = j;
-	}
-
-	/* End of MAIN WHILE.  Get here when we have gone all around without landing on edge.  */
-
-	if (crossing_count)
-		return(2);
-	else
-		return(0);
-}
