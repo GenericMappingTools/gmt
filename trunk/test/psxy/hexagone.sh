42de61aee5cc6660bdc02cb038420137c7d937ce
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-05-21 16:31:31 +0000

    - Added l'Hexagone problem

---
+++ trunk/test/psxy/hexagone.sh
@@ -0,0 +1,21 @@
+ps=hexagone.ps
+
+cat > hexagone.dat <<%
+-4.5 48.5
+-2 43.5
+3 42.5
+7.5 44
+8 49
+2.5 51
+%
+
+psxy hexagone.dat -R-5/9/42/52 -JM10c -P -L -Gpurple -K > $ps
+pscoast -R -J -Dl -Wthin -Ia/thin -N1/thick,red -B2 -O -K >> $ps
+
+psxy hexagone.dat -R1/10/47/52 -JM15c -Y12c -L -Gpurple -O -K >> $ps
+pscoast -R -J -Dl -Wthin -Ia/thin -N1/thick,red -B1 -O >> $ps
+
+rm -f hexagone.dat .gmtcommands4
+
+echo -n "Comparing hexagone_orig.ps and hexagone.ps: "
+compare -density 100 -metric PSNR hexagone_orig.ps hexagone.ps hexagone_diff.png
