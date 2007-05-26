f407e2d165429fd443d1745b7611b40c9a4c8faf
Paul Wessel <pwessel@hawaii.edu>
2007-05-26 02:56:52 +0000

    Added some more testing

--- test/blockmean/xy2ij.sh
+++ test/blockmean/xy2ij.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-#	$Id: xy2ij.sh,v 1.2 2006-02-16 05:59:28 pwessel Exp $
+#	$Id: xy2ij.sh,v 1.3 2007-05-26 02:56:52 pwessel Exp $
 #
 # Test to make sure the (x,y) <--> (i,j) macros work correctly
 # We use -R0/5/0/5 -I1 for pixel and gridline registrations
 # Points are prepared that will land in various tiles, some will
 # fall exactly on tile boundaries.  One point will fall outside -R
 # but should be considered for the gridline registrated case.
+echo -n "GMT: Test blockmean's (x,y) <--> (i,j) conversions:		"
 cat << EOF > pixel.d
 0.0	4.1	0
 0.65	0.7	1
@@ -30,7 +31,6 @@ psxy -R -J pixel.d -Sc0.125 -Gwhite -Wfaint -O -K -N >> xy2ij.ps
 awk '{print $1, $2, 8, 0, 0, "CM", $3}' pixel.d | pstext -R -J -O -K -N >> xy2ij.ps
 psxy -R -J pixel_ij.d -Sc0.15 -Gblack -O -K -N >> xy2ij.ps
 awk '{print $1, $2, 8, 0, 0, "CM", $3}' pixel_ij.d | pstext -R -J -O -K -Gwhite -N >> xy2ij.ps
-rm -f pixel*
 # Do gridline registration with a similar data set (mostly offset by 0.5)
 cat << EOF > grid.d
 -0.5	4.1	0
@@ -54,5 +54,18 @@ awk '{print $1, $2, 8, 0, 0, "CM", $3}' grid.d | pstext -R -J -O -K -N >> xy2ij.
 psxy -R -J grid_ij.d -Sc0.15 -Gblack -O -K -N >> xy2ij.ps
 awk '{print $1, $2, 8, 0, 0, "CM", $3}' grid_ij.d | pstext -R -J -O -K -Gwhite -N >> xy2ij.ps
 psxy -R -J -O /dev/null >> xy2ij.ps
-rm -f grid*
-gv xy2ij.ps &
+
+diff pixel_xy.d pixel_xy.d.orig  > log
+diff pixel_ij.d pixel_ij.d.orig >> log
+diff grid_xy.d  grid_xy.d.orig  >> log
+diff grid_ij.d  grid_ij.d.orig  >> log
+if [ -s fail ]; then
+	echo "[FAILED]"
+else
+	echo "[OK"]
+		rm -f log
+fi
+rm -f grid*.d pixel*.d
+
+# Examine plot for clues if things go wrong
+#gv xy2ij.ps &
