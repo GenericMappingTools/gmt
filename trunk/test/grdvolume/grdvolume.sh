6c76f0aab74df2c55f8eae9f52aba27abe5b12e8
Paul Wessel <pwessel@hawaii.edu>
2007-05-28 02:35:39 +0000

    *** empty log message ***

--- trunk/test/grdvolume/grdvolume.sh
+++ trunk/test/grdvolume/grdvolume.sh
@@ -14,11 +14,11 @@ xyz2grd -I1 -Gt.grd -Za $1 <<%
 %
 echo
 echo xyz2grd $1 \; grdvolume $2 -Sk:
-~/gmt/src/grdvolume t.grd $2 -Sk
+grdvolume t.grd $2 -Sk
 echo xyz2grd $1 \; grdvolume $2:
-~/gmt/src/grdvolume t.grd $2
+grdvolume t.grd $2
 echo xyz2grd $1 \; grdvolume $2 -L-1:
-~/gmt/src/grdvolume t.grd $2 -L-1
+grdvolume t.grd $2 -L-1
 }
 
 testcase () {
@@ -26,13 +26,20 @@ makegrd -R0/2/0/2
 makegrd "-R0/3/0/3 -F"
 makegrd "-R0/2/0/2 -N0"
 makegrd "-R0/3/0/3 -F -N0"
-makegrd -R0/2/0/2 "-C0/0.8/0.4 -Vl"
+makegrd -R0/2/0/2 "-C0/0.8/0.4"
 }
+
+echo -n "GMT: Test grdvolume output for various grid registrations:	"
 
 testcase > grdvolume.log
 
-diff grdvolume.{log,out}
+diff grdvolume.{log,out} > log
 
-[[ $? == 0 ]] && echo "Test passed"
+if [ $? == 0 ]; then
+	echo "[OK]"
+	rm -f log
+else
+	echo "[FAILED"]
+fi
 
 rm -f t.grd .gmtcommands4
