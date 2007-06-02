f55ee00e2ab82e15c091878702e8eedbadf5d1e7
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-06-02 13:11:28 +0000

    - Also allow running of single directory

--- trunk/test/run_gmt_tests.sh
+++ trunk/test/run_gmt_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: run_gmt_tests.sh,v 1.5 2007-05-31 02:52:05 pwessel Exp $
+#	$Id: run_gmt_tests.sh,v 1.6 2007-06-02 13:11:28 remko Exp $
 #
 #	test script for GMT/test directory
 #
@@ -7,7 +7,11 @@
 # Bourne scripts found in each directory
 
 # Get all directories below GMT/test and filter out the CVS directories
-find . -type d -print -mindepth 1 | grep -v CVS > /tmp/$$.dirs
+if [ $# -eq 0 ] ; then
+   dirs=`find . -mindepth 1 -type d -print | grep -v CVS`
+else
+   dirs=$*
+fi
 
 # Set here and use it to reset the dir in case a script fails to finish properly
 
@@ -16,17 +20,18 @@ find . -type d -print -mindepth 1 | grep -v CVS > /tmp/$$.dirs
 rm -f fail_count.d
 touch fail_count.d
 here=`pwd`
-while read dir; do
+for dir in $dirs; do
 	cd $here/$dir
 	# Look for Bourne shell scripts
 	ls *.sh > /tmp/$$.lis
 	if [ -s /tmp/$$.lis ]; then	# Found Bourne shell scripts
+		rm -f /tmp/$$.lis
 		for script in *.sh; do
 			cd $here/$dir
 			sh $script
 		done
 	fi
-done < /tmp/$$.dirs
+done
 cd $here
 wc -l fail_count.d | awk '{printf "GMT test script failures: %d\n", $1}'
-rm -f /tmp/$$.* fail_count.d
+rm -f fail_count.d
