e3e1e27b6f0eb5fb724a14c35a61a2a433eef551
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-06-05 15:44:51 +0000

    -  Made simpler. Fewer changes in -R and -J

--- doc/scripts/GMT_App_K_1.sh
+++ doc/scripts/GMT_App_K_1.sh
@@ -1,8 +1,7 @@
 #!/bin/sh 
-#	$Id: GMT_App_K_1.sh,v 1.10 2007-02-08 21:46:27 remko Exp $
+#	$Id: GMT_App_K_1.sh,v 1.11 2007-06-05 15:44:51 remko Exp $
 #
 gmtset GRID_CROSS_SIZE_PRIMARY 0 OBLIQUE_ANNOTATION 22 ANNOT_MIN_SPACING 0.3
-pscoast `./getbox.sh -JE130.35/-0.2/1i -9000 9000 -9000 9000` -JE130.35/-0.2/3.5i -P -Dc \
+pscoast `./getbox.sh -JE130.35/-0.2/3.5i 9000` -J -P -Dc \
 	-A500 -Glightgray -Wthinnest -N1/thinnest,- -B20g20WSne -K > GMT_App_K_1.ps
-./getrect.sh -JE130.35/-0.2/1i -2000 2000 -2000 2000 | psxy -R -JE130.35/-0.2/3.5i -O \
-	-Wthicker -L -A >> GMT_App_K_1.ps
+./getrect.sh 2000 | psxy -R -J -O -Wthicker -L -A >> GMT_App_K_1.ps
