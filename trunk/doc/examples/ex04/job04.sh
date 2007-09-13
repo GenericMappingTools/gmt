4393bc51a45b6f30acfcbc284e9f40d04f65481d
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-09-13 17:21:16 +0000

    Inconsistency with job04.csh in contouring. No longer call job4c.sh

--- trunk/doc/examples/ex04/job04.sh
+++ trunk/doc/examples/ex04/job04.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #		GMT EXAMPLE 04
 #
-#		$Id: job04.sh,v 1.9 2007-08-23 02:34:44 remko Exp $
+#		$Id: job04.sh,v 1.10 2007-09-13 17:21:16 remko Exp $
 #
 # Purpose:	3-D mesh plot of Hawaiian topography and geoid
 # GMT progs:	grdcontour, grdview, pscoast, pstext
@@ -9,9 +9,8 @@
 #
 echo '-10     255     0       255' > zero.cpt
 echo '0       100     10      100' >> zero.cpt
-grdcontour HI_geoid4.nc -Jm0.45i -E60/30 -R195/210/18/25 -C1 -A5 -Gd4i -K -P -X1.5i -Y1.5i -U/-1.25i/-1.25i/"Example 4 in Cookbook" > example_04.ps
+grdcontour HI_geoid4.nc -Jm0.45i -E60/30 -R195/210/18/25 -C1 -A5+o -Gd4i -K -P -X1.5i -Y1.5i -U/-1.25i/-1.25i/"Example 4 in Cookbook" > example_04.ps
 pscoast -J -E60/30 -R -B2/2NEsw -Gblack -O -K -T209/19.5/1i >> example_04.ps
 grdview HI_topo4.nc -J -Jz0.34i -Czero.cpt -E60/30 -R195/210/18/25/-6/4 -N-6/lightgray -Qsm -O -K -B2/2/2:"Topo (km)":neswZ -Y2.2i >> example_04.ps
 echo '3.25 5.75 60 0.0 33 BC H@#awaiian@# R@#idge' | pstext -R0/10/0/10 -Jx1i -O >> example_04.ps
 rm -f zero.cpt .gmt*
-sh job4c.sh
