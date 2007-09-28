7b26cbe39b4f012af76d300b8a0bcb1b7056da1a
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-09-28 17:37:06 +0000

    - Moved functions defined in gmt_stat.c from gmt_math.h to gmt_stat.h

--- trunk/src/gmt_stat.h
+++ trunk/src/gmt_stat.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_stat.h,v 1.6 2007-09-24 18:51:50 guru Exp $
+ *	$Id: gmt_stat.h,v 1.7 2007-09-28 17:37:06 remko Exp $
  *
  *	Copyright (c) 1991-2007 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -19,6 +19,31 @@
 #ifndef _GMT_STAT_H
 #define _GMT_STAT_H
 
+/* These functions used by grdmath and gmtmath are declared in gmt_stat.c.
+ * There are no ANSI-C equivalents so these prototypes are always set here */
+
+EXTERN_MSC double GMT_bei(double x);
+EXTERN_MSC double GMT_ber(double x);
+EXTERN_MSC double GMT_kei(double x);
+EXTERN_MSC double GMT_ker(double x);
+EXTERN_MSC double GMT_plm(int l, int m, double x);
+EXTERN_MSC double GMT_plm_bar(int l, int m, double x, BOOLEAN ortho);
+EXTERN_MSC double GMT_factorial(int n);
+EXTERN_MSC double GMT_i0(double x);
+EXTERN_MSC double GMT_i1(double x);
+EXTERN_MSC double GMT_in(int n, double x);
+EXTERN_MSC double GMT_k0(double x);
+EXTERN_MSC double GMT_k1(double x);
+EXTERN_MSC double GMT_kn(int n, double x);
+EXTERN_MSC double GMT_dilog(double x);
+EXTERN_MSC double GMT_sinc(double x);
+EXTERN_MSC double GMT_erfinv(double x);
+EXTERN_MSC double GMT_rand(void);
+EXTERN_MSC double GMT_nrand(void);
+EXTERN_MSC double GMT_lrand(void);
+EXTERN_MSC int GMT_chebyshev(double x, int n, double *t);
+EXTERN_MSC double GMT_corrcoeff (double *x, double *y, size_t n, int mode);
+EXTERN_MSC double GMT_corrcoeff_f (float *x, float *y, size_t n, int mode);
 EXTERN_MSC double GMT_Fcrit (double alpha, double nu1, double nu2);
 EXTERN_MSC double GMT_chi2crit (double alpha, double nu);
 EXTERN_MSC double GMT_extreme (double x[], size_t n, double x_default, int kind, int way);
