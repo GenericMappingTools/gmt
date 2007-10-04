8a522394967f0f48e2f4f17d25b07f63dcf45303
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-10-04 15:06:59 +0000

    - Added perspective view example to Cookbook

---
+++ trunk/doc/scripts/GMT_perspective.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#	$Id: GMT_perspective.sh,v 1.1 2007-10-04 15:06:59 remko Exp $
+#
+
+pscoast -Rg -JG4/52/230/90/60/180/60/60/5i -B2g2/1g1 -Ia -Di -Glightgray -Wthinnest -P > GMT_perspective.ps
