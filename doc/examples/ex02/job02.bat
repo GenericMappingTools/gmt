64f349aefbfd5b134ce372c4bcdd4032ad80a675
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-10-05 02:46:37 +0000

    - psscale for geoid plot should not have had -L next to -B

--- doc/examples/ex02/job02.bat
+++ doc/examples/ex02/job02.bat
@@ -1,6 +1,6 @@
 REM		GMT EXAMPLE 02
 REM
-REM		$Id: job02.bat,v 1.8 2006-10-23 18:14:11 remko Exp $
+REM		$Id: job02.bat,v 1.9 2007-10-05 02:46:37 remko Exp $
 REM
 REM Purpose:	Make two color images based gridded data
 REM GMT progs:	gmtset, grd2cpt, grdgradient, grdimage, makecpt, psscale, pstext
@@ -13,7 +13,7 @@ if %master%==y cd ex02
 gmtset HEADER_FONT_SIZE 30 OBLIQUE_ANNOTATION 0
 makecpt -Crainbow -T-2/14/2 > g.cpt
 grdimage HI_geoid2.nc -R160/20/220/30r -JOc190/25.5/292/69/4.5i -E50 -K -P -U/-1.25i/-1i/"Example 2 in Cookbook" -B10 -Cg.cpt -X1.5i -Y1.25i > example_02.ps
-psscale -Cg.cpt -D5.1i/1.35i/2.88i/0.4i -O -K -L -B2:GEOID:/:m: -E >> example_02.ps
+psscale -Cg.cpt -D5.1i/1.35i/2.88i/0.4i -O -K -B2:GEOID:/:m: -E >> example_02.ps
 grd2cpt HI_topo2.nc -Crelief -Z > t.cpt
 grdgradient HI_topo2.nc -A0 -Nt -GHI_topo2_int.grd
 grdimage HI_topo2.nc -IHI_topo2_int.grd -R -J -E50 -B10:."H@#awaiian@# T@#opo and @#G@#eoid:" -O -K -Ct.cpt -Y4.5i >> example_02.ps
--- doc/examples/ex02/job02.sh
+++ doc/examples/ex02/job02.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #		GMT EXAMPLE 02
 #
-#		$Id: job02.sh,v 1.10 2007-09-13 17:23:59 remko Exp $
+#		$Id: job02.sh,v 1.11 2007-10-05 02:46:37 remko Exp $
 #
 # Purpose:	Make two color images based gridded data
 # GMT progs:	gmtset, grd2cpt, grdgradient, grdimage, makecpt, psscale, pstext
@@ -10,7 +10,7 @@
 gmtset HEADER_FONT_SIZE 30 OBLIQUE_ANNOTATION 0
 makecpt -Crainbow -T-2/14/2 > g.cpt
 grdimage HI_geoid2.nc -R160/20/220/30r -JOc190/25.5/292/69/4.5i -E50 -K -P -U/-1.25i/-1i/"Example 2 in Cookbook" -B10 -Cg.cpt -X1.5i -Y1.25i > example_02.ps
-psscale -Cg.cpt -D5.1i/1.35i/2.88i/0.4i -O -K -L -B2:GEOID:/:m: -E >> example_02.ps
+psscale -Cg.cpt -D5.1i/1.35i/2.88i/0.4i -O -K -B2:GEOID:/:m: -E >> example_02.ps
 grd2cpt HI_topo2.nc -Crelief -Z > t.cpt
 grdgradient HI_topo2.nc -A0 -Nt -GHI_topo2_int.grd
 grdimage HI_topo2.nc -IHI_topo2_int.grd -R -J -E50 -B10:."H@#awaiian@# T@#opo and @#G@#eoid:" -O -K -Ct.cpt -Y4.5i >> example_02.ps
