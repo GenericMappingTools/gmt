72e9b7f03e94b5aa31d7ef7fbfa240ded7c19ae6
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-10-22 18:27:26 +0000

    - Avoid overlap between bottom and top plot

--- trunk/test/grdimage/grdwrap.sh
+++ trunk/test/grdimage/grdwrap.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-#	$Id: grdwrap.sh,v 1.1 2007-10-22 16:13:57 guru Exp $
+#	$Id: grdwrap.sh,v 1.2 2007-10-22 18:27:26 remko Exp $
 
 ps=grdwrap.ps
 grdmath -R0/360/-90/90 -I1 X Y MUL = t.grd
 makecpt -Crainbow -T-33000/33000/1000 > t.cpt
 
 grdimage t.grd -Ct.cpt -JQ-147/6i -Ct.cpt -B30 -K > $ps
-grdimage t.grd -Ct.cpt -JQ-147.533/6i -Ct.cpt -B30 -O -K -Y3.5i >> $ps
+grdimage t.grd -Ct.cpt -JQ-147.533/6i -Ct.cpt -B30 -O -K -Y3.75i >> $ps
 echo -n "$0: Test grdimage for wrapping of global:			"
 
 rm -f t.grd t.cpt .gmtcommands4
