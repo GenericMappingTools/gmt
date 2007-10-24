c4f4628ec02a11733cfef9662eeec64d305cf01d
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-10-24 01:11:41 +0000

    - New grdwrap script

--- trunk/test/grdimage/grdwrap.sh
+++ trunk/test/grdimage/grdwrap.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-#	$Id: grdwrap.sh,v 1.2 2007-10-22 18:27:26 remko Exp $
+#	$Id: grdwrap.sh,v 1.3 2007-10-24 01:11:41 remko Exp $
 
 ps=grdwrap.ps
-grdmath -R0/360/-90/90 -I1 X Y MUL = t.grd
-makecpt -Crainbow -T-33000/33000/1000 > t.cpt
+grdmath -Rg -I1 X SIND Y COSD MUL = t.grd=ns/0.0001
+makecpt -Cpolar -T-1/1/0.01 > t.cpt
 
 grdimage t.grd -Ct.cpt -JQ-147/6i -Ct.cpt -B30 -K > $ps
-grdimage t.grd -Ct.cpt -JQ-147.533/6i -Ct.cpt -B30 -O -K -Y3.75i >> $ps
+grdimage t.grd -Ct.cpt -JQ-147.533/6i -Ct.cpt -B30 -O -Y3.75i >> $ps
 echo -n "$0: Test grdimage for wrapping of global:			"
 
 rm -f t.grd t.cpt .gmtcommands4
