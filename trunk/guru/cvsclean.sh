3614e986e97e79a48e88a7e3b1dedaf19efcc2f4
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-11-29 14:35:41 +0000

    guru/gmtguru.macros got accidently removed

--- trunk/guru/cvsclean.sh
+++ trunk/guru/cvsclean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: cvsclean.sh,v 1.6 2007-11-29 14:13:04 remko Exp $
+#	$Id: cvsclean.sh,v 1.7 2007-11-29 14:35:41 remko Exp $
 
 # List all backup files
 find . -name "*~" -o -name ".*~" -o -name "*.bak" -o -name ".*.bak" -o -name ".#*" -o -name ".*.swp" > $$.lis 
@@ -11,7 +11,7 @@ done
 # Remove the exceptions from the list
 # These are non-cvs files, we nevertheless want to keep
 # The result of grep is redirected to rm command
-xargs echo "ls -d -1 2>/dev/null" < $$.lis | sh | grep -v -e "share/coast$" | xargs rm -rvf
+xargs echo "ls -d -1 2>/dev/null" < $$.lis | sh | grep -v -e "guru/gmtguru.macros$" -e "share/coast$" | xargs rm -rvf
 
 # Delete the killfile
 rm -f $$.lis
