5080102313c3bc41947f9df1c321ed133d53a8ef
Remko Scharroo <remko.scharroo@eumetsat.int>
2007-12-07 21:39:13 +0000

    - Made some minor changes

--- src/shelltest.sh
+++ src/shelltest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: shelltest.sh,v 1.2 2007-12-07 19:23:19 guru Exp $
+#	$Id: shelltest.sh,v 1.3 2007-12-07 21:39:13 remko Exp $
 #
 # Testing gmt_shell_functions
 
@@ -7,16 +7,19 @@
 
 gmt_message "Testing the GMT shell functions"
 
+gmt_init_tmpdir
+gmt_message "Created temporary directory $GMT_TMPDIR"
+
 # Test record counter
 
-cat << EOF > crap.txt
+cat << EOF > $GMT_TMPDIR/crap.txt
 1 2
 3 4
 5 6
 7 8
 EOF
 
-n=`gmt_nrecords crap.txt`
+n=`gmt_nrecords $GMT_TMPDIR/crap.txt`
 gmt_message "We got $n records"
 
 rec=0
@@ -34,11 +37,14 @@ done < crap.txt
 R=`gmt_get_region crap.txt -I2`
 gmt_message "Found the table region to be $R"
 
-grdmath -R0/50/10/90 -I10 X Y MUL = crap.grd
+grdmath -R0/50/10/90 -I10 X Y MUL = $GMT_TMPDIR/crap.grd
 
-R=`gmt_get_gridregion crap.grd`
+R=`gmt_get_gridregion $GMT_TMPDIR/crap.grd`
 gmt_message "Found the grid region to be $R"
 
 PS=`gmt_set_psfile $0`
 
 gmt_message "PS name is $PS"
+
+gmt_message "Removing temporary directory $GMT_TMPDIR"
+gmt_remove_tmpdir
