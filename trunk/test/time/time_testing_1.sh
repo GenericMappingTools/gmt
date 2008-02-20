6f3448b4244745d4ecfe29d883934880c4f9de66
Remko Scharroo <remko.scharroo@eumetsat.int>
2008-02-20 16:02:49 +0000

    - Includes time system info internally since gmt_time_systems.h has become obsolete

--- trunk/test/time/time_testing_1.sh
+++ trunk/test/time/time_testing_1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$Id: time_testing_1.sh,v 1.6 2007-12-20 18:22:44 remko Exp $
+#	$Id: time_testing_1.sh,v 1.7 2008-02-20 16:02:49 remko Exp $
 #
 # This script runs some simple test to verify the that new time scheme
 # has been implemented successfully
@@ -14,7 +14,13 @@
 . ../functions.sh
 header "Test time conversions (rata die)"
 
-sed -e 's/"//g' ../../src/gmt_time_systems.h | awk -F, '{if (NR < 8) print $2, 1, $4, $5 }' | \
-	gmtconvert --TIME_SYSTEM=rata -fi0T -fo0t --D_FORMAT=%.12g | awk '{if ($1 != ($3+$4)) print $0}' > fail
+( gmtconvert --TIME_SYSTEM=rata -fi0T -fo0t --D_FORMAT=%.12g | awk '{if ($1 != $2) print $0}' > fail ) <<%
+ 2000-01-01T12:00:00    730120.5
+-4713-11-25T12:00:00  -1721423.5
+ 1858-11-17T00:00:00    678576.0
+ 1985-01-01T00:00:00    724642.0
+ 1970-01-01T00:00:00    719163.0
+ 0001-01-01T00:00:00         1.0
+%
 
 passfail time_testing_1
