87782520c4a14da529ed363cf0852f21a1432ab3
Remko Scharroo <remko.scharroo@eumetsat.int>
2008-02-21 22:39:54 +0000

    - GMT_App_M.sh renamed to GMT_App_M_1.sh - GMT_App_M_2.csh added

---
+++ trunk/doc/scripts/GMT_App_M_1.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#	$Id: GMT_App_M_1.sh,v 1.1 2008-02-21 22:39:54 remko Exp $
+#
+#	Makes the insert for Appendix M(cpt)
+#
+
+trap 'rm -f $$.*; exit 1' 1 2 3 15
+
+grep -v '#' ../../share/conf/gmt_cpt.conf | cut -d: -f1 | sort -r > $$.lis
+
+ps=GMT_App_M_1.ps
+n=`cat $$.lis | wc -l`
+
+# dy is line spacing and y0 is total box height
+
+gmtset FRAME_PEN thinner ANNOT_FONT_SIZE_PRIMARY 8 TICK_LENGTH 0.04i ANNOT_OFFSET_PRIMARY 0.04i
+psbasemap -R0/6.1/0/6.9 -Jx1i -P -K -B0 > $ps
+
+i=1
+y=0.475
+y2=0.35
+dy=0.625
+while [ $i -le 22 ]
+do
+	j=`expr $i + 1`
+	left=`sed -n ${j}p $$.lis`
+	right=`sed -n ${i}p $$.lis`
+	makecpt -C$left -Z > $$.left.cpt
+	makecpt -C$left -T-1/1/0.25 > $$.left2.cpt
+	makecpt -C$right -Z > $$.right.cpt
+	makecpt -C$right -T-1/1/0.25 > $$.right2.cpt
+	psscale -D1.55/$y/2.70/0.125h -C$$.left.cpt -B0 -O -K >> $ps
+	psscale -D4.50/$y/2.70/0.125h -C$$.right.cpt -B0 -O -K >> $ps
+	psscale -D1.55/$y2/2.70/0.125h -C$$.left2.cpt -B0.5 -O -K >> $ps
+	psscale -D4.50/$y2/2.70/0.125h -C$$.right2.cpt -B0.5 -O -K >> $ps
+	echo "1.5 $y 9 0 1 CB $left" | pstext -R -J -O -K -D0/0.05 >> $ps
+	echo "4.5 $y 9 0 1 CB $right" | pstext -R -J -O -K -D0/0.05 >> $ps
+	i=`expr $i + 2`
+	y=`gmtmath -Q $y $dy ADD =`
+	y2=`gmtmath -Q $y2 $dy ADD =`
+done
+
+psxy -R -J -O /dev/null >> $ps
+rm -f $$.*
