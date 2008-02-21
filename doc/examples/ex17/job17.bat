03c96ec961bba7f05367f07cdcb287be6320ed16
Remko Scharroo <remko.scharroo@eumetsat.int>
2008-02-21 22:36:40 +0000

    - Added color legend

--- doc/examples/ex17/job17.bat
+++ doc/examples/ex17/job17.bat
@@ -1,6 +1,6 @@
 REM		GMT EXAMPLE 17
 REM
-REM		$Id: job17.bat,v 1.7 2006-10-30 19:09:11 remko Exp $
+REM		$Id: job17.bat,v 1.8 2008-02-21 22:36:40 remko Exp $
 REM
 REM Purpose:	Illustrates clipping of images using coastlines
 REM GMT progs:	grd2cpt, grdgradient, grdimage, pscoast, pstext
@@ -30,6 +30,10 @@ grdimage india_topo.nc -Iindia_topo_i.grd -J -Cgray.cpt -O -K >> example_17.ps
 REM Finally undo clipping and overlay basemap
 
 pscoast -R -J -O -K -Q -B10f5:."Clipping of Images": >> example_17.ps
+
+REM Put a color legend on top of the land mask
+
+psscale -D4i/7.6i/4i/0.2ih -Cgeoid.cpt -B5f1/:m: -I -O -K >> example_17.ps
 
 REM Add a text paragraph (Note double %% to get a single % in DOS)
 echo # 90 -10 12 0 4 RB 12p 3i j > tmp
