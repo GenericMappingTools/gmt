cddc4de6d3c80f80d85b840d7cb6a90a38a9853f
Paul Wessel <pwessel@hawaii.edu>
2008-02-27 16:47:37 +0000

    Temp fix for ps_shorten_path

--- src/pslib.c
+++ src/pslib.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: pslib.c,v 1.164 2008-02-25 17:27:08 guru Exp $
+ *	$Id: pslib.c,v 1.165 2008-02-27 16:47:37 guru Exp $
  *
  *	Copyright (c) 1991-2008 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -1040,6 +1040,71 @@ void ps_line_ (double *x, double *y, int *n, int *type, int *close, int *split)
 }
 
 int ps_shorten_path (double *x, double *y, int n, int *ix, int *iy)
+{
+	double old_slope, new_slope, dx, dy, old_dir, new_dir;
+	int i, j, k, *xx, *yy, fixed;
+
+	if (n < 2) return (0);
+
+	xx = (int *) ps_memory (VNULL, (size_t)n, sizeof (int));
+	yy = (int *) ps_memory (VNULL, (size_t)n, sizeof (int));
+
+	xx[0] = irint (x[0] * PSL->internal.scale);
+	yy[0] = irint (y[0] * PSL->internal.scale);
+
+	for (i = j = 1; i < n; i++) {
+		xx[j] = irint (x[i] * PSL->internal.scale);
+		yy[j] = irint (y[i] * PSL->internal.scale);
+		if (xx[j] != xx[j-1] || yy[j] != yy[j-1]) j++;
+	}
+	n = j;
+
+	if (n < 2) {
+		ps_free ((void *)xx);
+		ps_free ((void *)yy);
+		return (0);
+	}
+
+	ix[0] = xx[0];	iy[0] = yy[0];	k = 1;
+
+	dx = xx[1] - xx[0];
+	dy = yy[1] - yy[0];
+	fixed = (dx == 0.0 && dy == 0.0);
+	old_slope = (fixed) ? 1.01e100 : ((dx == 0) ? copysign (1.0e100, dy) : dy / dx);
+	old_dir = (dx >= 0.0) ? 1 : -1;
+
+	for (i = 1; i < n-1; i++) {
+		dx = xx[i+1] - xx[i];
+		dy = yy[i+1] - yy[i];
+		fixed = (dx == 0.0 && dy == 0.0);
+		new_slope = (fixed) ? 1.01e100 : ((dx == 0) ? copysign (1.0e100, dy) : dy / dx);
+		if (fixed) continue;	/* Didnt move */
+
+		new_dir = (dx >= 0.0) ? 1 : -1;
+		if (new_slope != old_slope || new_dir != old_dir) {
+			ix[k] = xx[i];
+			iy[k] = yy[i];
+			k++;
+			old_slope = new_slope;
+			old_dir = new_dir;
+		}
+	}
+	dx = xx[n-1] - xx[n-2];
+	dy = yy[n-1] - yy[n-2];
+	fixed = (dx == 0.0 && dy == 0.0 && (k > 1 && ix[k-1] == xx[n-1] && iy[k-1] == yy[n-1]));	/* Didnt move */
+	if (!fixed) {
+		ix[k] = xx[n-1];
+		iy[k] = yy[n-1];
+		k++;
+	}
+
+	ps_free ((void *)xx);
+	ps_free ((void *)yy);
+
+	return (k);
+}
+
+int ps_shorten_path_new (double *x, double *y, int n, int *ix, int *iy)
 {
 	/* Simplifies the (x,y) array by converting it to pixel coordinates (ix,iy)
 	 * and eliminating repeating points and intermediate points along straight
