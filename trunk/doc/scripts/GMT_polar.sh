eefcca312bafd9296157e6277abbc9a7fa16530a
Remko Scharroo <remko.scharroo@eumetsat.int>
2008-02-29 03:23:52 +0000

    - Renamed temporary grid file

--- trunk/doc/scripts/GMT_polar.sh
+++ trunk/doc/scripts/GMT_polar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#	$Id: GMT_polar.sh,v 1.3 2004-08-17 01:48:06 pwessel Exp $
+#	$Id: GMT_polar.sh,v 1.4 2008-02-29 03:23:52 remko Exp $
 #
 
-grdmath -R0/360/2/4 -I6/0.1 X 4 MUL PI MUL 180 DIV COS Y 2 POW MUL = test.grd
-grdcontour test.grd -JP3i -B30Ns -P -C2 -S4 --PLOT_DEGREE_FORMAT=+ddd > GMT_polar.ps
-rm -f test.grd
+grdmath -R0/360/2/4 -I6/0.1 X 4 MUL PI MUL 180 DIV COS Y 2 POW MUL = $$.grd
+grdcontour $$.grd -JP3i -B30Ns -P -C2 -S4 --PLOT_DEGREE_FORMAT=+ddd > GMT_polar.ps
+rm -f $$.grd
