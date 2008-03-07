762ea23b7262a2a48ba9eb0e4700af8d7f14f1f7
Remko Scharroo <remko.scharroo@eumetsat.int>
2008-03-07 17:07:47 +0000

    - Added R, G, B, H, S, V arrows

--- doc/examples/ex11/job11.bat
+++ doc/examples/ex11/job11.bat
@@ -1,9 +1,10 @@
-REM		$Id: job11.bat,v 1.10 2007-05-14 21:35:33 remko Exp $
+REM		$Id: job11.bat,v 1.11 2008-03-07 17:07:46 remko Exp $
 REM		GMT EXAMPLE 11
 REM
 REM Purpose:	Create a 3-D RGB Cube
 REM GMT progs:	gmtset, grdimage, grdmath, pstext, psxy
 REM DOS calls:	echo, del
+set ps=example_11.ps
 
 REM Use psxy to plot "cut-along-the-dotted" lines.
 echo GMT EXAMPLE 11
@@ -13,7 +14,7 @@ if %master%==y cd ex11
 
 gmtset TICK_LENGTH 0 COLOR_MODEL rgb CHAR_ENCODING Standard+
 
-psxy cut-here.dat -Wthinnest,. -M -R-51/306/0/1071 -JX3.5i/10.5i -X2.5i -Y0.5i -P -U/-2.0i/-0.2i/"Example 11 in Cookbook" -K > example_11.ps
+psxy cut-here.dat -Wthinnest,. -M -R-51/306/0/1071 -JX3.5i/10.5i -X2.5i -Y0.5i -P -U/-2.0i/-0.2i/"Example 11 in Cookbook" -K > %ps%
 
 REM First, create grids of ascending X and Y and constant 0.
 REM These are to be used to represent R, G and B values of the darker 3 faces of the cube.
@@ -22,23 +23,32 @@ grdmath -I1 -R0/255/0/255 X = x.grd
 grdmath -I1 -R Y = y.grd
 grdmath -I1 -R 0 = c.grd
 
-grdimage x.grd y.grd c.grd -JX2.5i/-2.5i -R -K -O -X0.5i >> example_11.ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> example_11.ps
-echo 128 128 12 -45 1 MC 60\217 | pstext -Gwhite -J -R -K -O >> example_11.ps
-echo 102  26 12 -90 1 MC 0.4 | pstext -Gwhite -J -R -K -O >> example_11.ps
-echo 204  26 12 -90 1 MC 0.8 | pstext -Gwhite -J -R -K -O >> example_11.ps
+grdimage x.grd y.grd c.grd -JX2.5i/-2.5i -R -K -O -X0.5i >> %ps%
+psxy -M rays.dat -J -R -K -O -Bwesn >> %ps%
+echo 128 128 12 -45 1 MC 60\217 | pstext -Gwhite -J -R -K -O >> %ps%
+echo 102  26 12 -90 1 MC 0.4 | pstext -Gwhite -J -R -K -O >> %ps%
+echo 204  26 12 -90 1 MC 0.8 | pstext -Gwhite -J -R -K -O >> %ps%
+echo 10 140 16 180 1 MC G | pstext -Gwhite -J -R -K -O >> %ps%
+echo 0 0 0 128 | psxy -N -Svs -Gwhite -J -R -K -O >> %ps%
 
-grdimage x.grd c.grd y.grd -JX2.5i/2.5i -R -K -O -Y2.5i >> example_11.ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> example_11.ps
-echo 128 128 12 45 1 MC 300\217 | pstext -Gwhite -J -R -K -O >> example_11.ps
-echo 26 102 12 0 1 MC 0.4 | pstext -Gwhite -J -R -K -O >> example_11.ps
-echo 26 204 12 0 1 MC 0.8 | pstext -Gwhite -J -R -K -O >> example_11.ps
+grdimage x.grd c.grd y.grd -JX2.5i/2.5i -R -K -O -Y2.5i >> %ps%
+psxy -M rays.dat -J -R -K -O -Bwesn >> %ps%
+echo 128 128 12 45 1 MC 300\217 | pstext -Gwhite -J -R -K -O >> %ps%
+echo 26 102 12 0 1 MC 0.4 | pstext -Gwhite -J -R -K -O >> %ps%
+echo 26 204 12 0 1 MC 0.8 | pstext -Gwhite -J -R -K -O >> %ps%
+echo 140 10 16 -90 1 MC R | pstext -Gwhite -J -R -K -O >> %ps%
+echo 100 100 16 -45 1 MC V | pstext -Gwhite -J -R -K -O >> %ps%
+echo 0 0 128 0 | psxy -N -Svs -Gwhite -J -R -K -O >> %ps%
+echo 0 0 90 90 | psxy -N -Svs -Gwhite -J -R -K -O >> %ps%
 
-grdimage c.grd x.grd y.grd -JX-2.5i/2.5i -R -K -O -X-2.5i >> example_11.ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> example_11.ps
-echo 128 128 12 135 1 MC 180\217 | pstext -Gwhite -J -R -K -O >> example_11.ps
-echo 102 26 12 90 1 MC 0.4 | pstext -Gwhite -J -R -K -O >> example_11.ps
-echo 204 26 12 90 1 MC 0.8 | pstext -Gwhite -J -R -K -O >> example_11.ps
+grdimage c.grd x.grd y.grd -JX-2.5i/2.5i -R -K -O -X-2.5i >> %ps%
+psxy -M rays.dat -J -R -K -O -Bwesn >> %ps%
+echo 128 128 12 135 1 MC 180\217 | pstext -Gwhite -J -R -K -O >> %ps%
+echo 102 26 12 90 1 MC 0.4 | pstext -Gwhite -J -R -K -O >> %ps%
+echo 204 26 12 90 1 MC 0.8 | pstext -Gwhite -J -R -K -O >> %ps%
+echo 10 140 16 0 1 MC B | pstext -Gwhite -J -R -K -O >> %ps%
+echo 0 0 0 128 | psxy -N -Svs -Gwhite -J -R -K -O >> %ps%
+echo 0 0 128 0 | psxy -N -Svs -Gwhite -J -R -K -O >> %ps%
 
 REM Second, create grids of descending X and Y and constant 255.
 REM These are to be used to represent R, G and B values of the lighter 3 faces of the cube.
@@ -47,24 +57,28 @@ grdmath -I1 -R 255 X SUB = x.grd
 grdmath -I1 -R 255 Y SUB = y.grd
 grdmath -I1 -R 255       = c.grd
 
-grdimage x.grd y.grd c.grd -JX-2.5i/-2.5i -R -K -O -X2.5i -Y2.5i >> example_11.ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> example_11.ps
-echo 128 128 12 225 1 MC 240\217 | pstext -J -R -K -O >> example_11.ps
-echo 102 26 12 270 1 MC 0.4 | pstext -J -R -K -O >> example_11.ps
-echo 204 26 12 270 1 MC 0.8 | pstext -J -R -K -O >> example_11.ps
+grdimage x.grd y.grd c.grd -JX-2.5i/-2.5i -R -K -O -X2.5i -Y2.5i >> %ps%
+psxy -M rays.dat -J -R -K -O -Bwesn >> %ps%
+echo 128 128 12 225 1 MC 240\217 | pstext -J -R -K -O >> %ps%
+echo 102 26 12 270 1 MC 0.4 | pstext -J -R -K -O >> %ps%
+echo 204 26 12 270 1 MC 0.8 | pstext -J -R -K -O >> %ps%
 
-grdimage c.grd y.grd x.grd -JX2.5i/-2.5i -R -K -O -X2.5i >> example_11.ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> example_11.ps
-echo 128 128 12 -45 1 MC 0\217 | pstext -J -R -K -O >> example_11.ps
-echo 26 102 12 0 1 MC 0.4 | pstext -J -R -K -O >> example_11.ps
-echo 26 204 12 0 1 MC 0.8 | pstext -J -R -K -O >> example_11.ps
+grdimage c.grd y.grd x.grd -JX2.5i/-2.5i -R -K -O -X2.5i >> %ps%
+psxy -M rays.dat -J -R -K -O -Bwesn >> %ps%
+echo 128 128 12 -45 1 MC 0\217 | pstext -J -R -K -O >> %ps%
+echo 26 102 12 0 1 MC 0.4 | pstext -J -R -K -O >> %ps%
+echo 26 204 12 0 1 MC 0.8 | pstext -J -R -K -O >> %ps%
+echo 100 100 16 45 1 MC S | pstext -Gblack -J -R -K -O >> %ps%
+echo 204 66 16 90 1 MC H | pstext -Gblack -J -R -K -O >> %ps%
+echo 0 0 90 90 | psxy -N -Svs -Gblack -J -R -K -O >> %ps%
+echo 204 204 204 76 | psxy -N -Svs -Gblack -J -R -K -O >> %ps%
 
-grdimage x.grd c.grd y.grd -JX-2.5i/2.5i -R -K -O -X-2.5i -Y2.5i >> example_11.ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> example_11.ps
-echo 128 128 12 135 1 MC 120\217 | pstext -J -R -K -O >> example_11.ps
-echo 26 102 12 180 1 MC 0.4 | pstext -J -R -K -O >> example_11.ps
-echo 26 204 12 180 1 MC 0.8 | pstext -J -R -K -O >> example_11.ps
-echo 200 200 16 225 1 MC GMT 4 | pstext -J -R -O >> example_11.ps
+grdimage x.grd c.grd y.grd -JX-2.5i/2.5i -R -K -O -X-2.5i -Y2.5i >> %ps%
+psxy -M rays.dat -J -R -K -O -Bwesn >> %ps%
+echo 128 128 12 135 1 MC 120\217 | pstext -J -R -K -O >> %ps%
+echo 26 102 12 180 1 MC 0.4 | pstext -J -R -K -O >> %ps%
+echo 26 204 12 180 1 MC 0.8 | pstext -J -R -K -O >> %ps%
+echo 200 200 16 225 1 MC GMT 4 | pstext -J -R -O >> %ps%
 
 del *.grd
 del .gmt*
