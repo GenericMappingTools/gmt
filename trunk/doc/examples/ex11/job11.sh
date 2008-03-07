bdc82fdead81cb320f245860d6a70813a5fde677
Remko Scharroo <remko.scharroo@eumetsat.int>
2008-03-07 17:07:47 +0000

    - Added R, G, B, H, S, V arrows

--- trunk/doc/examples/ex11/job11.sh
+++ trunk/doc/examples/ex11/job11.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #		GMT EXAMPLE 11
-#		$Id: job11.sh,v 1.11 2008-02-22 21:10:42 remko Exp $
+#		$Id: job11.sh,v 1.12 2008-03-07 17:07:47 remko Exp $
 #
 # Purpose:	Create a 3-D RGB Cube
 # GMT progs:	gmtset, grdimage, grdmath, pstext, psxy
@@ -22,22 +22,31 @@ grdmath -I1 -R Y = y.grd
 grdmath -I1 -R 0 = c.grd
 
 grdimage x.grd y.grd c.grd -JX2.5i/-2.5i -R -K -O -X0.5i >> $ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> $ps
+psxy -M -Wthinner,white,- rays.dat -J -R -K -O -Bwesn >> $ps
 echo 128 128 12 -45 1 MC "60\217" | pstext -Gwhite -J -R -K -O >> $ps
-echo 102  26 12 -90 1 MC 0.4 | pstext -Gwhite -J -R -K -O >> $ps
-echo 204  26 12 -90 1 MC 0.8 | pstext -Gwhite -J -R -K -O >> $ps
+echo 102 26 12 -90 1 MC 0.4 | pstext -Gwhite -J -R -K -O >> $ps
+echo 204 26 12 -90 1 MC 0.8 | pstext -Gwhite -J -R -K -O >> $ps
+echo 10 140 16 180 1 MC G | pstext -Gwhite -J -R -K -O >> $ps
+echo 0 0 0 128 | psxy -N -Svs -Gwhite -J -R -K -O >> $ps
 
 grdimage x.grd c.grd y.grd -JX2.5i/2.5i -R -K -O -Y2.5i >> $ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> $ps
+psxy -M -Wthinner,white,- rays.dat -J -R -K -O -Bwesn >> $ps
 echo 128 128 12 45 1 MC "300\217" | pstext -Gwhite -J -R -K -O >> $ps
 echo 26 102 12 0 1 MC 0.4 | pstext -Gwhite -J -R -K -O >> $ps
 echo 26 204 12 0 1 MC 0.8 | pstext -Gwhite -J -R -K -O >> $ps
+echo 140 10 16 -90 1 MC R | pstext -Gwhite -J -R -K -O >> $ps
+echo 100 100 16 -45 1 MC V | pstext -Gwhite -J -R -K -O >> $ps
+echo 0 0 128 0 | psxy -N -Svs -Gwhite -J -R -K -O >> $ps
+echo 0 0 90 90 | psxy -N -Svs -Gwhite -J -R -K -O >> $ps
 
 grdimage c.grd x.grd y.grd -JX-2.5i/2.5i -R -K -O -X-2.5i >> $ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> $ps
+psxy -M -Wthinner,white,- rays.dat -J -R -K -O -Bwesn >> $ps
 echo 128 128 12 135 1 MC "180\217" | pstext -Gwhite -J -R -K -O >> $ps
 echo 102 26 12 90 1 MC 0.4 | pstext -Gwhite -J -R -K -O >> $ps
 echo 204 26 12 90 1 MC 0.8 | pstext -Gwhite -J -R -K -O >> $ps
+echo 10 140 16 0 1 MC B | pstext -Gwhite -J -R -K -O >> $ps
+echo 0 0 0 128 | psxy -N -Svs -Gwhite -J -R -K -O >> $ps
+echo 0 0 128 0 | psxy -N -Svs -Gwhite -J -R -K -O >> $ps
 
 # Second, create grids of descending X and Y and constant 255.
 # These are to be used to represent R, G and B values of the lighter 3 faces of the cube.
@@ -47,19 +56,23 @@ grdmath -I1 -R 255 Y SUB = y.grd
 grdmath -I1 -R 255       = c.grd
 
 grdimage x.grd y.grd c.grd -JX-2.5i/-2.5i -R -K -O -X2.5i -Y2.5i >> $ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> $ps
+psxy -M -Wthinner,black,- rays.dat -J -R -K -O -Bwesn >> $ps
 echo 128 128 12 225 1 MC "240\217" | pstext -J -R -K -O >> $ps
 echo 102 26 12 270 1 MC 0.4 | pstext -J -R -K -O >> $ps
 echo 204 26 12 270 1 MC 0.8 | pstext -J -R -K -O >> $ps
 
 grdimage c.grd y.grd x.grd -JX2.5i/-2.5i -R -K -O -X2.5i >> $ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> $ps
+psxy -M -Wthinner,black,- rays.dat -J -R -K -O -Bwesn >> $ps
 echo 128 128 12 -45 1 MC "0\217" | pstext -J -R -K -O >> $ps
 echo 26 102 12 0 1 MC 0.4 | pstext -J -R -K -O >> $ps
 echo 26 204 12 0 1 MC 0.8 | pstext -J -R -K -O >> $ps
+echo 100 100 16 45 1 MC S | pstext -Gblack -J -R -K -O >> $ps
+echo 204 66 16 90 1 MC H | pstext -Gblack -J -R -K -O >> $ps
+echo 0 0 90 90 | psxy -N -Svs -Gblack -J -R -K -O >> $ps
+echo 204 204 204 76 | psxy -N -Svs -Gblack -J -R -K -O >> $ps
 
 grdimage x.grd c.grd y.grd -JX-2.5i/2.5i -R -K -O -X-2.5i -Y2.5i >> $ps
-psxy -M rays.dat -J -R -K -O -Bwesn >> $ps
+psxy -M -Wthinner,black,- rays.dat -J -R -K -O -Bwesn >> $ps
 echo 128 128 12 135 1 MC "120\217" | pstext -J -R -K -O >> $ps
 echo 26 102 12 180 1 MC 0.4 | pstext -J -R -K -O >> $ps
 echo 26 204 12 180 1 MC 0.8 | pstext -J -R -K -O >> $ps
