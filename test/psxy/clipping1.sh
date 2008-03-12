74ebb8d43e49de5d0f10caddd9b92ed8b730dd37
Paul Wessel <pwessel@hawaii.edu>
2008-03-12 00:03:49 +0000

    Fixed clipping bug in -JG reported by Stafan Maus 2/21/08.  Added his examples as new psxy test scripts

---
+++ test/psxy/clipping1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#	$Id: clipping1.sh,v 1.1 2008-03-12 00:03:49 guru Exp $
+#
+# Check clipping of lines crossing over the horizon AND dateline (N pole)
+
+. ../functions.sh
+header "Test psxy -JG for clipping line crossing horizon"
+
+ps=clipping1.ps
+
+psxy clipline1.xy -P -K -JG0/90/4.5i -R-180/180/40/90 -B30g10 -Wthick,red -X1i -Y0.75i --BASEMAP_TYPE=plain > $ps 
+psxy clipline1.xy -O -JG0/90/4.5i -R-180/180/0/90 -B30g10 -Wthick,red -X2i -Y5i --BASEMAP_TYPE=plain >> $ps
+
+rm -f .gmtcommands4
+
+pscmp
