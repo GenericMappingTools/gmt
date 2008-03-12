fe979477a0678cbb4b75a2c244e5ede3c2fd4600
Paul Wessel <pwessel@hawaii.edu>
2008-03-12 02:27:09 +0000

    Fixed bugs in genper clip path and annotation justification

--- doc/scripts/GMT_perspective.sh
+++ doc/scripts/GMT_perspective.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#	$Id: GMT_perspective.sh,v 1.2 2007-10-04 15:49:37 remko Exp $
+#	$Id: GMT_perspective.sh,v 1.3 2008-03-12 02:27:08 guru Exp $
 #
 
 pscoast -Rg -JG4/52/230/90/60/180/60/60/5i -B2g2/1g1 -Ia -Di -Glightgray -Wthinnest -P \
-	> GMT_perspective.ps
+	--ANNOT_MIN_SPACING=0.25i > GMT_perspective.ps
