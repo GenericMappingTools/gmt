b57beab5c886994300f61a51c56b31c721703b02
Paul Wessel <pwessel@hawaii.edu>
2008-03-27 07:28:38 +0000

    Modified code to avoid getting "dereferencing type-punned pointer will break strict-aliasing rules" warnings

--- src/gmt_nan.h
+++ src/gmt_nan.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_nan.h,v 1.3 2008-01-23 03:22:48 guru Exp $
+ *	$Id: gmt_nan.h,v 1.4 2008-03-27 07:28:38 guru Exp $
  *
  *	Copyright (c) 1991-2008 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -42,11 +42,15 @@
 #define GMT_make_fnan(x) (x = FLT_MAX)
 #define GMT_make_dnan(x) (x = DBL_MAX)
 #elif defined(WIN32)
-#define GMT_make_fnan(x) (((unsigned int *) &x)[0] = 0x7fffffff)
-#define GMT_make_dnan(x) (((unsigned int *) &x)[0] = 0xffffffff, ((unsigned int *) &x)[1] = 0x7fffffff)
+#define GMT_make_fnan(x) { void *v; unsigned int *i; v = (void *)&x; i = (unsigned int *)v; *i = 0x7fffffff;}
+#define GMT_make_dnan(x) { void *v; unsigned int *i; v = (void *)&x; i = (unsigned int *)v; i[0] = 0xffffffff; i[1] = 0x7fffffff;}
+/* #define GMT_make_fnan(x) (((unsigned int *) &x)[0] = 0x7fffffff)
+#define GMT_make_dnan(x) (((unsigned int *) &x)[0] = 0xffffffff, ((unsigned int *) &x)[1] = 0x7fffffff) */
 #else
-#define GMT_make_fnan(x) (((unsigned int *) &x)[0] = 0xffffffff)
-#define GMT_make_dnan(x) (((unsigned int *) &x)[0] = 0xffffffff, ((unsigned int *) &x)[1] = 0xffffffff)
+#define GMT_make_fnan(x) { void *v; unsigned int *i; v = (void *)&x; i = (unsigned int *)v; *i = 0xffffffff;}
+#define GMT_make_dnan(x) { void *v; unsigned int *i; v = (void *)&x; i = (unsigned int *)v; i[0] = 0xffffffff; i[1] = 0xffffffff;}
+/* #define GMT_make_fnan(x) (((unsigned int *) &x)[0] = 0xffffffff)
+#define GMT_make_dnan(x) (((unsigned int *) &x)[0] = 0xffffffff, ((unsigned int *) &x)[1] = 0xffffffff) */
 #endif
 
 #if defined(NO_IEEE)
