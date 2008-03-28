efbcadd4dbffd39df9c5b42e478df1a53e2e1b80
Remko Scharroo <remko.scharroo@eumetsat.int>
2008-03-28 01:55:15 +0000

    - New comparison metric

--- test/functions.sh
+++ test/functions.sh
@@ -1,5 +1,5 @@
 #
-#	$Id: functions.sh,v 1.2 2007-11-15 04:20:41 remko Exp $
+#	$Id: functions.sh,v 1.3 2008-03-28 01:54:28 remko Exp $
 #
 # Functions to be used with test scripts
 
@@ -11,14 +11,20 @@ header () {
 # Compare the ps file with its original. Check $1.ps (if $1 given) or $ps
 pscmp () {
 	file=${1:-`basename $ps .ps`}
-	compare -density 100 -metric PSNR $file.ps orig/$file.ps $file.png 2>&1 | grep -v inf > fail
-	passfail $file
+	ae=`compare -density 100 -fuzz 1% -metric AE $file.ps orig/$file.ps $file.png 2>&1`
+	if [ "$ae" == "0" ]; then
+        	echo "[PASS]"
+        	rm -f $file.png
+	else
+        	echo "[FAIL]"
+		echo $file: $ae failed pixels >> ../fail_count.d
+	fi
 }
 
 passfail () {
 	if [ -s fail ]; then
         	echo "[FAIL]"
-		echo $1 >> ../fail_count.d
+		echo $1: `wc -l fail`ed lines >> ../fail_count.d
 		mv -f fail $1.log
 	else
         	echo "[PASS]"
