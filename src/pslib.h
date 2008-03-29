0490b3d54f4ec132b952185a0b0e75d93d4cc6b4
Paul Wessel <pwessel@hawaii.edu>
2008-03-29 20:49:25 +0000

    All exits are now PS_exit or GMT_exit macros

--- src/pslib.h
+++ src/pslib.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: pslib.h,v 1.45 2008-03-24 08:58:32 guru Exp $
+ *	$Id: pslib.h,v 1.46 2008-03-29 20:49:25 guru Exp $
  *
  *	Copyright (c) 1991-2008 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -49,6 +49,13 @@ typedef long PS_LONG;			/* A signed 4 (or 8-byte for 64-bit) integer */
 
 #ifndef EXTERN_MSC
 #define EXTERN_MSC extern MSC_EXTRA_PSL
+#endif
+
+/* Macro for exit since this should be returned when called from Matlab */
+#ifdef DO_NOT_EXIT
+#define PS_exit(code) return(code)
+#else
+#define PS_exit(code) exit(code)
 #endif
 
 /* So unless DLL_PSL is defined, EXTERN_MSC is simply extern */
