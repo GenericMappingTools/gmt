e417ddad95df9f7c6df7a98e7dd3b7b4f140f981
Remko Scharroo <remko.scharroo@eumetsat.int>
2008-04-04 18:22:28 +0000

    - Made -JE and -JA similar

--- test/pscoast/pscoast_JE.sh
+++ test/pscoast/pscoast_JE.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$Id: pscoast_JE.sh,v 1.1 2008-02-01 22:23:11 guru Exp $
+#	$Id: pscoast_JE.sh,v 1.2 2008-04-04 18:22:28 remko Exp $
 # Make sure when fixed it works for all resolutions -D?
 
 ps=pscoast_JE.ps
@@ -8,6 +8,6 @@ ps=pscoast_JE.ps
 . ../functions.sh
 header "Test pscoast for JE plot of Germany"
 
-pscoast -JE13:25/52:31/10/7i -Rg -Gred -Dl -P -B0 > $ps
+pscoast -JE13:25/52:31/10/7i -Rg -Gred -Sblue -Dl -P > $ps
 
 pscmp
