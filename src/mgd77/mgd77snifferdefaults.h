b2ab55f5e31f27a79c6d64d4d4ad10fc55b513e5
Michael Chandler <mtchandl@hawaii.edu>
2008-04-08 05:24:16 +0000

    Track outliers in decimate function, if more than 5% outliers use the maximum possible MGD77 values for the 2D bin table in the decimate function.

--- src/mgd77/mgd77snifferdefaults.h
+++ src/mgd77/mgd77snifferdefaults.h
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------
- *	$Id: mgd77snifferdefaults.h,v 1.11 2008-01-23 03:22:49 guru Exp $	
+ *	$Id: mgd77snifferdefaults.h,v 1.12 2008-04-08 05:24:16 mtchandl Exp $	
  *      See COPYING file for copying and redistribution conditions.
  *
  *    Copyright (c) 2004-2008 by P. Wessel and M. T. Chandler
@@ -15,31 +15,31 @@
  *	Date:	23-Feb-2004
  * 
  * ------------------------------------------------------------------
- * Abbrev,     min,     max,  binwidth,  maxdt,  maxds,  maxdz,  maxArea*/
-{   "drt",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{    "tz",     -13,      12,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{  "year",    1939,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{ "month",       1,      12,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{   "day",       1,      31,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{  "hour",       0,      24,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{   "min",       0,      60,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{   "lat",     -90,      90,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{   "lon",    -180,     180,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{   "ptc",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{   "twt",       0,      15,        .1,  .0013,      1,    .15,    FALSE},
-{ "depth",       0,   11000,        50,      1,   1000,    104,   100000},
-{   "bcc",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{   "btc",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{  "mtf1",   19000,   72000,       200,    .32,    200,     40,    FALSE},
-{  "mtf2",   19000,   72000,       200,    .32,    200,     40,    FALSE},
-{   "mag",   -1000,    1000,        10,    .29,    200,     38,    FALSE},
-{ "msens",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{  "diur",    -100,     100,         2,    .05,     20,      3,    FALSE},
-{   "msd",   -1000,    1100,        10,    .33,    100,     10,    FALSE},
-{  "gobs",  977600,  983800,         5,   .043,    100,    3.6,    FALSE},
-{   "eot",    -150,     150,         2,   .033,    100,    2.4,    FALSE},
-{   "faa",    -400,     550,         5,   .045,    100,    4.8,    50000},
-{   "nqc",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{    "id",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{   "sln",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE},
-{  "sspn",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,    FALSE}
+ * Abbrev,     min,     max,  binwidth,  maxdt,  maxds,  maxdz,  binmin, binmax, maxArea*/
+{   "drt",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{    "tz",     -13,      12,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{  "year",    1939,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{ "month",       1,      12,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{   "day",       1,      31,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{  "hour",       0,      24,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{   "min",       0,      60,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{   "lat",     -90,      90,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{   "lon",    -180,     180,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{   "ptc",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{   "twt",       0,      15,        .1,  .0013,      1,    .15,   FALSE,  FALSE,  FALSE},
+{ "depth",       0,   11000,        50,      1,   1000,    104, -100000, 100000, 100000},
+{   "bcc",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{   "btc",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{  "mtf1",   19000,   72000,       200,    .32,    200,     40,   FALSE,  FALSE,  FALSE},
+{  "mtf2",   19000,   72000,       200,    .32,    200,     40,   FALSE,  FALSE,  FALSE},
+{   "mag",   -1000,    1000,        10,    .29,    200,     38,  -10000,  10000,  FALSE},
+{ "msens",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{  "diur",    -100,     100,         2,    .05,     20,      3,   FALSE,  FALSE,  FALSE},
+{   "msd",   -1000,    1100,        10,    .33,    100,     10,   FALSE,  FALSE,  FALSE},
+{  "gobs",  977600,  983800,         5,   .043,    100,    3.6,   FALSE,  FALSE,  FALSE},
+{   "eot",    -150,     150,         2,   .033,    100,    2.4,   FALSE,  FALSE,  FALSE},
+{   "faa",    -400,     550,         5,   .045,    100,    4.8,   -1000,   1000,  50000},
+{   "nqc",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{    "id",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{   "sln",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE},
+{  "sspn",   FALSE,   FALSE,     FALSE,  FALSE,  FALSE,  FALSE,   FALSE,  FALSE,  FALSE}
