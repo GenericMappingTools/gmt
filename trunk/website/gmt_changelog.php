d9c02bf1689e00dc63e7bc1c99a7cd55099cb40b
Paul Wessel <pwessel@hawaii.edu>
2008-04-25 01:31:52 +0000

    *** empty log message ***

--- trunk/website/gmt_changelog.php
+++ trunk/website/gmt_changelog.php
@@ -1,6 +1,6 @@
-<!--    $Id: gmt_changelog.php,v 1.1 2008-04-25 01:30:46 guru Exp $      -->
-// This simple PHP script will display the latest ChangeLog written to the gmt web directory
-// from the cvs server, which happens once each night
+<!--    $Id: gmt_changelog.php,v 1.2 2008-04-25 01:31:52 guru Exp $      -->
+<!-- This simple PHP script will display the latest ChangeLog written to the gmt web directory      -->
+<!-- from the cvs server, which happens once each night      -->
 <HTML>
 <HEAD>
 <TITLE>GMT ChangeLog</TITLE>
