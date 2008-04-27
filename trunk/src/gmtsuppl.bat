90bc047e3093f1a476bcbe2afd8a9f8803472823
Paul Wessel <pwessel@hawaii.edu>
2008-04-27 00:14:19 +0000

    Updated SOEST logo, added other logos

--- trunk/src/gmtsuppl.bat
+++ trunk/src/gmtsuppl.bat
@@ -1,7 +1,7 @@
 ECHO OFF
 REM ----------------------------------------------------
 REM
-REM	$Id: gmtsuppl.bat,v 1.39 2008-04-26 22:36:16 guru Exp $
+REM	$Id: gmtsuppl.bat,v 1.40 2008-04-27 00:14:18 guru Exp $
 REM
 REM
 REM	Copyright (c) 1991-2008 by P. Wessel and W. H. F. Smith
@@ -23,7 +23,7 @@ REM the GMT 4 supplemental programs under WIN32 using
 REM Microsoft Visual C/C++ tools.  Not yet set up for mex.
 REM Note: Optimizing all at /O2 except meca which seems unstable
 REM
-REM Author: Paul Wessel, 25-APR-2008
+REM Author: Paul Wessel, 26-APR-2008
 REM ----------------------------------------------------
 REM
 REM How to build GMT under Windows:
@@ -46,6 +46,10 @@ REM	    change CHOICE to "static" here:
 REM
 SET CHOICE="dynamic"
 REM SET CHOICE="static"
+REM
+REM STEP e: Specify your compiler (currently set to MS CL)
+SET CC=CL
+REM SET CC=icl
 REM ----------------------------------------------------
 REM STOP HERE - THE REST IS AUTOMATIC
 REM ----------------------------------------------------
@@ -61,7 +65,7 @@ REM ----------------------------------------------------
 ECHO STEP 1: Make dbase
 REM ----------------------------------------------------
 cd dbase
-CL %COPT% grdraster.c %GMTLIB%
+%CC% %COPT% grdraster.c %GMTLIB%
 del *.obj
 move *.exe %BINDIR%
 cd ..
@@ -69,9 +73,9 @@ REM ----------------------------------------------------
 ECHO STEP 2: Make gshhs
 REM ----------------------------------------------------
 cd gshhs
-CL %COPT% gshhs.c %GMTLIB%
-CL %COPT% gshhs_dp.c %GMTLIB%
-CL %COPT% gshhstograss.c %GMTLIB%
+%CC% %COPT% gshhs.c %GMTLIB%
+%CC% %COPT% gshhs_dp.c %GMTLIB%
+%CC% %COPT% gshhstograss.c %GMTLIB%
 del *.obj
 move *.exe %BINDIR%
 cd ..
@@ -79,7 +83,7 @@ REM ----------------------------------------------------
 ECHO STEP 3: Make imgsrc
 REM ----------------------------------------------------
 cd imgsrc
-CL %COPT% img2mercgrd.c gmt_imgsubs.c %GMTLIB%
+%CC% %COPT% img2mercgrd.c gmt_imgsubs.c %GMTLIB%
 del *.obj
 move *.exe %BINDIR%
 cd ..
@@ -87,11 +91,11 @@ REM ----------------------------------------------------
 ECHO STEP 4: Make meca
 REM ----------------------------------------------------
 cd meca
-CL %COPT2% /c nrutil.c distaz.c utilmeca.c utilstrain.c submeca.c utilvelo.c
-CL %COPT2% pscoupe.c utilmeca.obj submeca.obj distaz.obj nrutil.obj %GMTLIB%
-CL %COPT2% psmeca.c utilmeca.obj nrutil.obj %GMTLIB%
-CL %COPT2% pspolar.c utilmeca.obj submeca.obj distaz.obj nrutil.obj %GMTLIB%
-CL %COPT2% psvelo.c utilvelo.obj utilstrain.obj %GMTLIB%
+%CC% %COPT2% /c nrutil.c distaz.c utilmeca.c utilstrain.c submeca.c utilvelo.c
+%CC% %COPT2% pscoupe.c utilmeca.obj submeca.obj distaz.obj nrutil.obj %GMTLIB%
+%CC% %COPT2% psmeca.c utilmeca.obj nrutil.obj %GMTLIB%
+%CC% %COPT2% pspolar.c utilmeca.obj submeca.obj distaz.obj nrutil.obj %GMTLIB%
+%CC% %COPT2% psvelo.c utilvelo.obj utilstrain.obj %GMTLIB%
 del *.obj
 move *.exe %BINDIR%
 cd ..
@@ -103,15 +107,15 @@ REM ----------------------------------------------------
 ECHO STEP 6: Make mgd77
 REM ----------------------------------------------------
 cd mgd77
-CL %COPT2% /c mgd77.c
+%CC% %COPT2% /c mgd77.c
 lib /out:mgd77.lib mgd77.obj
-CL %COPT% mgd77convert.c mgd77.lib %GMTLIB%
-CL %COPT% mgd77info.c	 mgd77.lib %GMTLIB%
-CL %COPT% mgd77list.c	 mgd77.lib %GMTLIB%
-CL %COPT% mgd77manage.c	 mgd77.lib %GMTLIB%
-CL %COPT% mgd77path.c	 mgd77.lib %GMTLIB%
-CL %COPT% mgd77sniffer.c mgd77.lib %GMTLIB%
-CL %COPT% mgd77track.c	 mgd77.lib %GMTLIB%
+%CC% %COPT% mgd77convert.c mgd77.lib %GMTLIB%
+%CC% %COPT% mgd77info.c	 mgd77.lib %GMTLIB%
+%CC% %COPT% mgd77list.c	 mgd77.lib %GMTLIB%
+%CC% %COPT% mgd77manage.c	 mgd77.lib %GMTLIB%
+%CC% %COPT% mgd77path.c	 mgd77.lib %GMTLIB%
+%CC% %COPT% mgd77sniffer.c mgd77.lib %GMTLIB%
+%CC% %COPT% mgd77track.c	 mgd77.lib %GMTLIB%
 del *.obj
 move mgd77.lib %LIBDIR%
 move *.exe %BINDIR%
@@ -120,20 +124,20 @@ REM ----------------------------------------------------
 ECHO STEP 7: Make mgg
 REM ----------------------------------------------------
 cd mgg
-IF %CHOICE%=="dynamic" CL %COPT% %DLL_NETCDF% /FD /ML /DDLL_EXPORT /c gmt_mgg.c
+IF %CHOICE%=="dynamic" %CC% %COPT% %DLL_NETCDF% /FD /ML /DDLL_EXPORT /c gmt_mgg.c
 IF %CHOICE%=="dynamic" LINK %LOPT% /out:gmt_mgg.dll /implib:gmt_mgg.lib gmt_mgg.obj %GMTLIB%
-IF %CHOICE%=="static"  CL %COPT% %DLL_NETCDF% /DDLL_EXPORT /c gmt_mgg.c
+IF %CHOICE%=="static"  %CC% %COPT% %DLL_NETCDF% /DDLL_EXPORT /c gmt_mgg.c
 IF %CHOICE%=="static"  lib /out:gmt_mgg.lib gmt_mgg.obj
-CL %COPT% binlegs.c    gmt_mgg.lib %GMTLIB%
-CL %COPT% gmt2bin.c    gmt_mgg.lib %GMTLIB%
-CL %COPT% gmt2dat.c    gmt_mgg.lib %GMTLIB%
-CL %COPT% dat2gmt.c    gmt_mgg.lib %GMTLIB%
-CL %COPT% gmtinfo.c    gmt_mgg.lib %GMTLIB%
-CL %COPT% gmtlegs.c    gmt_mgg.lib %GMTLIB%
-CL %COPT% /I..\x_system gmtlist.c    gmt_mgg.lib %GMTLIB%
-CL %COPT% gmtpath.c    gmt_mgg.lib %GMTLIB%
-CL %COPT% gmttrack.c   gmt_mgg.lib %GMTLIB%
-CL %COPT% mgd77togmt.c gmt_mgg.lib %GMTLIB%
+%CC% %COPT% binlegs.c    gmt_mgg.lib %GMTLIB%
+%CC% %COPT% gmt2bin.c    gmt_mgg.lib %GMTLIB%
+%CC% %COPT% gmt2dat.c    gmt_mgg.lib %GMTLIB%
+%CC% %COPT% dat2gmt.c    gmt_mgg.lib %GMTLIB%
+%CC% %COPT% gmtinfo.c    gmt_mgg.lib %GMTLIB%
+%CC% %COPT% gmtlegs.c    gmt_mgg.lib %GMTLIB%
+%CC% %COPT% /I..\x_system gmtlist.c    gmt_mgg.lib %GMTLIB%
+%CC% %COPT% gmtpath.c    gmt_mgg.lib %GMTLIB%
+%CC% %COPT% gmttrack.c   gmt_mgg.lib %GMTLIB%
+%CC% %COPT% mgd77togmt.c gmt_mgg.lib %GMTLIB%
 del *.obj
 IF %CHOICE%=="dynamic" move gmt_mgg.dll %BINDIR%
 IF %CHOICE%=="dynamic" move gmt_mgg.exp %LIBDIR%
@@ -144,11 +148,11 @@ REM ----------------------------------------------------
 ECHO STEP 8: Make misc
 REM ----------------------------------------------------
 cd misc
-REM CL %COPT% gmtdigitize.c %GMTLIB%
-CL %COPT% gmtstitch.c %GMTLIB%
-CL %COPT% psmegaplot.c %GMTLIB%
-CL %COPT% makepattern.c %GMTLIB%
-CL %COPT% nc2xy.c %GMTLIB%
+REM %CC% %COPT% gmtdigitize.c %GMTLIB%
+%CC% %COPT% gmtstitch.c %GMTLIB%
+%CC% %COPT% psmegaplot.c %GMTLIB%
+%CC% %COPT% makepattern.c %GMTLIB%
+%CC% %COPT% nc2xy.c %GMTLIB%
 del *.obj
 move *.exe %BINDIR%
 cd ..
@@ -156,11 +160,11 @@ REM ----------------------------------------------------
 ECHO STEP 9: Make segyprogs
 REM ----------------------------------------------------
 cd segyprogs
-CL %COPT% /c segy_io.c
+%CC% %COPT% /c segy_io.c
 LIB /OUT:segy_io.lib segy_io.obj
-CL %COPT% pssegy.c  segy_io.lib %GMTLIB%
-CL %COPT% pssegyz.c segy_io.lib %GMTLIB%
-CL %COPT% segy2grd.c segy_io.lib %GMTLIB%
+%CC% %COPT% pssegy.c  segy_io.lib %GMTLIB%
+%CC% %COPT% pssegyz.c segy_io.lib %GMTLIB%
+%CC% %COPT% segy2grd.c segy_io.lib %GMTLIB%
 del *.obj
 move segy_io.lib %LIBDIR%
 move *.exe %BINDIR%
@@ -169,16 +173,16 @@ REM ----------------------------------------------------
 ECHO STEP 10: Make spotter
 REM ----------------------------------------------------
 cd spotter
-IF %CHOICE%=="dynamic" CL %COPT% %DLL_NETCDF% /FD /ML /DDLL_EXPORT /c libspotter.c
+IF %CHOICE%=="dynamic" %CC% %COPT% %DLL_NETCDF% /FD /ML /DDLL_EXPORT /c libspotter.c
 IF %CHOICE%=="dynamic" LINK %LOPT% /out:spotter.dll /implib:spotter.lib libspotter.obj %GMTLIB%
-IF %CHOICE%=="static"  CL %COPT% %DLL_NETCDF% /DDLL_EXPORT /c libspotter.c
+IF %CHOICE%=="static"  %CC% %COPT% %DLL_NETCDF% /DDLL_EXPORT /c libspotter.c
 IF %CHOICE%=="static"  lib /out:spotter.lib libspotter.obj
-CL %COPT% backtracker.c   spotter.lib %GMTLIB%
-CL %COPT% grdrotater.c    spotter.lib %GMTLIB%
-CL %COPT% grdspotter.c    spotter.lib %GMTLIB%
-CL %COPT% hotspotter.c    spotter.lib %GMTLIB%
-CL %COPT% originator.c    spotter.lib %GMTLIB%
-CL %COPT% rotconverter.c  spotter.lib %GMTLIB%
+%CC% %COPT% backtracker.c   spotter.lib %GMTLIB%
+%CC% %COPT% grdrotater.c    spotter.lib %GMTLIB%
+%CC% %COPT% grdspotter.c    spotter.lib %GMTLIB%
+%CC% %COPT% hotspotter.c    spotter.lib %GMTLIB%
+%CC% %COPT% originator.c    spotter.lib %GMTLIB%
+%CC% %COPT% rotconverter.c  spotter.lib %GMTLIB%
 del *.obj
 IF %CHOICE%=="dynamic" move spotter.dll %BINDIR%
 IF %CHOICE%=="dynamic" move spotter.exp %LIBDIR%
@@ -189,14 +193,14 @@ REM ----------------------------------------------------
 ECHO STEP 11: Make x2sys
 REM ----------------------------------------------------
 cd x2sys
-CL %COPT2% /I..\mgd77 /I..\mgg /c x2sys.c
+%CC% %COPT2% /I..\mgd77 /I..\mgg /c x2sys.c
 lib /out:x2sys.lib x2sys.obj
-CL %COPT% /I..\mgd77 /I..\mgg x2sys_binlist.c  x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
-CL %COPT% /I..\mgd77 /I..\mgg x2sys_cross.c x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
-CL %COPT% /I..\mgd77 /I..\mgg x2sys_datalist.c  x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
-CL %COPT% /I..\mgd77 /I..\mgg x2sys_get.c  x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
-CL %COPT% /I..\mgd77 /I..\mgg x2sys_init.c  x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
-CL %COPT% /I..\mgd77 /I..\mgg x2sys_put.c  x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
+%CC% %COPT% /I..\mgd77 /I..\mgg x2sys_binlist.c  x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
+%CC% %COPT% /I..\mgd77 /I..\mgg x2sys_cross.c x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
+%CC% %COPT% /I..\mgd77 /I..\mgg x2sys_datalist.c  x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
+%CC% %COPT% /I..\mgd77 /I..\mgg x2sys_get.c  x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
+%CC% %COPT% /I..\mgd77 /I..\mgg x2sys_init.c  x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
+%CC% %COPT% /I..\mgd77 /I..\mgg x2sys_put.c  x2sys.lib %LIBDIR%\mgd77.lib %LIBDIR%\gmt_mgg.lib %GMTLIB%
 del *.obj
 move x2sys.lib %LIBDIR%
 move *.exe %BINDIR%
@@ -207,15 +211,15 @@ REM ----------------------------------------------------
 cd x_system
 SET COPT2=%COPT% /I..\mgg
 SET GMTLIB2=%LIBDIR%\gmt_mgg.lib %GMTLIB%
-CL %COPT2% x_edit.c %GMTLIB2%
-CL %COPT2% x_init.c %GMTLIB2%
-CL %COPT2% x_list.c %GMTLIB2%
-CL %COPT2% x_over.c %GMTLIB2%
-CL %COPT2% x_remove.c %GMTLIB2%
-CL %COPT2% x_report.c %GMTLIB2%
-CL %COPT2% x_setup.c %GMTLIB2%
-CL %COPT2% x_solve_dc_drift.c %GMTLIB2%
-CL %COPT2% x_update.c %GMTLIB2%
+%CC% %COPT2% x_edit.c %GMTLIB2%
+%CC% %COPT2% x_init.c %GMTLIB2%
+%CC% %COPT2% x_list.c %GMTLIB2%
+%CC% %COPT2% x_over.c %GMTLIB2%
+%CC% %COPT2% x_remove.c %GMTLIB2%
+%CC% %COPT2% x_report.c %GMTLIB2%
+%CC% %COPT2% x_setup.c %GMTLIB2%
+%CC% %COPT2% x_solve_dc_drift.c %GMTLIB2%
+%CC% %COPT2% x_update.c %GMTLIB2%
 del *.obj
 move *.exe %BINDIR%
 cd ..
