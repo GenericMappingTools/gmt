fc82e7ad198209b42183f5e34c2d191c6b5a6e14
Paul Wessel <pwessel@hawaii.edu>
2008-05-13 19:09:54 +0000

    *** empty log message ***

---
+++ doc/examples/ex10/agu2008.d
@@ -0,0 +1,8 @@
+#	$Id: agu2008.d,v 1.1 2008-05-13 19:09:54 guru Exp $
+#	AGU Membership as of Jan 2008. Pr Jon Sears (jsears@agu.org)
+-100    42      34241	NA	North America
+-60     -20     547	SA	South America
+20      10      139	AF	Africa
+90      30      5183	AS	Asia
+10      50      8371	EU	Europe
+140     -30     837	AU	Australia
