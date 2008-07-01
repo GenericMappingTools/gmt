2068257ef9b3e77a41b6d27d5615a5934761d670
Michael Chandler <mtchandl@hawaii.edu>
2008-07-01 00:13:55 +0000

    Adjusted IGRF start years to agree with http://www.ngdc.noaa.gov/IAGA/vmod/igrf.html.

--- src/mgd77/mgd77magref.h
+++ src/mgd77/mgd77magref.h
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------
- *	$Id: mgd77magref.h,v 1.10 2008-03-27 06:50:54 guru Exp $	
+ *	$Id: mgd77magref.h,v 1.11 2008-07-01 00:13:55 mtchandl Exp $	
  *      See COPYING file for copying and redistribution conditions.
  *
  *    Copyright (c) 2005-2008 by P. Wessel and M. T. Chandler
@@ -18,21 +18,26 @@
  *    Langel, R. A., 1987. The Main Field. In Geomagnetism, Volume I,
  *      (Ed. J. A. Jacobs). London: Academic Press, pp249-512.
  *
+ *    National Geophysical Data Center, 2005. IAGA V-MOD Geomagnetic
+ *    Field Modeling: International Geomagnetic Reference Field IGRF-10.
+ *    30 June 2008. <http://www.ngdc.noaa.gov/IAGA/vmod/igrf.html>.
  * ------------------------------------------------------------------*/
 /*		  Field,		Code,	Start,		End */
 		{"Unused",      0,      9999,       9999},
 		{"AWC 70",		1,		1965,		1970},
 		{"AWC 75",		2,		1967,		1974},
-		{"IGRF-65",		3,		1965,		1970},
-		{"IGRF-75",		4,		1975,		1980},
+		{"IGRF-65",		3,		1955,		1970},
+		{"IGRF-75",		4,		1955,		1980},
 		{"GSFC-1266",	5,		1900,		1966},
 		{"GSFC-0674",	6,		9999,		9999},
 		{"UK 75",		7,		1970,		1975},
 		{"POGO 0368",	8,		1965,		1968},
 		{"POGO 1068",	9,		1965,		1968},
 		{"POGO 0869",	10,		1965,		1968},
-		{"IGRF-80",		11,		1980,		1985},
-		{"IGRF-85",		12,		1985,		1990},
-		{"IGRF-90",		13,		1990,		1995},
-		{"IGRF-95",		14,		1995,		2000},
+		{"IGRF-80",		11,		1965,		1985},
+		{"IGRF-85",		12,		1945,		1990},
+		{"IGRF-90",		13,		1945,		1995},
+		{"IGRF-95",		14,		1900,		2000},
+		{"IGRF-00",     15,     1900,       2005},
+		{"IGRF-05",     16,     1900,       2010},
 		{"Other",       88,     9999,       9999}
