61ced3bc7dd9ebd774df4eceb23ab796feb5f717
Paul Wessel <pwessel@hawaii.edu>
2008-08-25 00:48:43 +0000

    *** empty log message ***

--- guru/nightly_gmt_cvsupdate.sh
+++ guru/nightly_gmt_cvsupdate.sh
@@ -1,8 +1,7 @@
 #/bin/sh
-# $Id: nightly_gmt_cvsupdate.sh,v 1.1 2008-08-25 00:47:58 guru Exp $
+# $Id: nightly_gmt_cvsupdate.sh,v 1.2 2008-08-25 00:48:43 guru Exp $
 # This script is used to get the latest GMT CVS changes and compile and install everything.
-# We do that by first getting the changes and then build executables and documentation.
-# PS: For some reason make site crashes in latex.  Until I can figure it out we just update executables
+# We do that by first getting the changes and then build and install executables.
 cd /Users/pwessel/UH/RESEARCH/PROJECTS/GMTdev/GMT
 make update
 make install
