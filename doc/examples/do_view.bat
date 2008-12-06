dfb7279e1a6d15e523105473ba6f02feb2db2724
Paul Wessel <pwessel@hawaii.edu>
2008-12-06 02:40:10 +0000

    Adding new example 29 for spherical surface gridding

--- doc/examples/do_view.bat
+++ doc/examples/do_view.bat
@@ -1,14 +1,14 @@
 echo OFF
 REM
-REM	$Id: do_view.bat,v 1.6 2008-06-12 22:06:19 guru Exp $
+REM	$Id: do_view.bat,v 1.7 2008-12-06 02:40:10 guru Exp $
 REM
 REM DOS batch script to view all GMT examples.
 REM Assumes gsview32 is in the path and that
 REM all examples have been run with do_examples
 
-echo Loop over all 28 examples and view each plot
+echo Loop over all 29 examples and view each plot
 
-for %%d in (01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28) do call view %%d
+for %%d in (01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29) do call view %%d
 
 echo "Completed viewing all examples"
 echo ON
--- website/webexamples.sh
+++ website/webexamples.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #-----------------------------------------------------------------------------
-#	 $Id: webexamples.sh,v 1.17 2008-06-12 22:06:19 guru Exp $
+#	 $Id: webexamples.sh,v 1.18 2008-12-06 02:40:10 guru Exp $
 #
 #	webexamples.sh - Automatic generation of the GMT examples pages
 #
@@ -20,7 +20,7 @@
 #	gmt/examples/example_??_100dpi.png (using ps2raster)
 #-----------------------------------------------------------------------------
 
-n_examples=28
+n_examples=29
 
 GMT050dpi="ps2raster -E50 -Tg -P"
 GMT100dpi="ps2raster -E100 -Tg -P"
@@ -142,6 +142,9 @@ surfaces.</A></LI>
 
 <LI>
 <A HREF="examples/ex28/gmt_example_28.html">Mixing UTM and geographic data.</A></LI>
+
+<LI>
+<A HREF="examples/ex29/gmt_example_29.html">Spherical surface gridding using Green's functions.</A></LI>
 </OL>
 </BODY>
 </HTML>
