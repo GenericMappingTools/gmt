76dfb900b4890e14c5e930f674de392947f19334
Paul Wessel <pwessel@hawaii.edu>
2008-12-06 00:59:35 +0000

    *** empty log message ***

---
+++ trunk/doc/examples/ex29/job29.bat
@@ -0,0 +1,46 @@
+REM             GMT EXAMPLE 29
+REM             $Id: job29.bat,v 1.1 2008-12-06 00:59:35 guru Exp $
+REM
+REM Purpose:	Illustrates spherical surface gridding with Green's function of splines
+REM GMT progs:	makecpt, grdcontour, grdgradient, grdimage, grdmath greenspline, psscale, pstext
+REM DOS calls:	del, echo
+REM
+
+echo GMT EXAMPLE 29
+set master=y
+if exist job29.bat set master=n
+if %master%==y cd ex29
+
+REM This example uses 370 radio occultation data for Mars to grid the topography.
+REM Data and information from Smith, D. E., and M. T. Zuber (1996), The shape of
+REM Mars and the topographic signature of the hemispheric dichotomy, Science, 271, 184–187.
+
+REM Make Mars ellipsoid given their three best-fitting axes:
+set a=3399.472
+set b=3394.329
+set c=3376.502
+grdmath -Rg -I2 X COSD %a% DIV DUP MUL X SIND %b% DIV DUP MUL ADD Y COSD DUP MUL MUL Y SIND %c% DIV DUP MUL ADD SQRT INV = ellipsoid.nc
+
+REM  Do both Parker and Wessel/Becker solutions (tension = 0.9975)
+greenspline -Rellipsoid.nc mars370.in -D4 -Sp -Gmars.nc
+greenspline -Rellipsoid.nc mars370.in -D4 -SQ0.9975/500001 -Gmars2.nc
+REM Scale to km and remove ellipsoid
+grdmath mars.nc 1000 DIV ellipsoid.nc SUB = mars.nc
+grdmath mars2.nc 1000 DIV ellipsoid.nc SUB = mars2.nc
+makecpt -Crainbow -T-7/15/1 -Z > mars.cpt
+grdgradient mars2.nc -M -Ne0.75 -A45 -Gmars2_i.nc
+grdimage mars2.nc -Imars2_i.nc -Cmars.cpt -B30g30Wsne -JH0/6i -P -K -U"Example 29 in Cookbook" --ANNOT_FONT_SIZE=12 > example_29.ps
+grdcontour mars2.nc -J -O -K -C1 -A5 -Glz+/z- >> example_29.ps
+psxy -Rg -J -O -K -Sc0.045i -Gblack mars370.in  >> example_29.ps
+echo 0 90 14 0 1 LB b) | pstext -R -J -O -K -N -D-3i/-0.2i >> example_29.ps
+grdgradient mars.nc -M -Ne0.75 -A45 -Gmars_i.nc
+grdimage mars.nc -Imars_i.nc -Cmars.cpt -B30g30Wsne -J -O -K -Y3.6i --ANNOT_FONT_SIZE=12 >> example_29.ps
+grdcontour mars.nc -J -O -K -C1 -A5 -Glz+/z- >> example_29.ps
+psxy -Rg -J -O -K -Sc0.045i -Gblack mars370.in  >> example_29.ps
+psscale -Cmars.cpt -O -K -D3i/-0.1i/5i/0.1ih -I --ANNOT_FONT_SIZE=12 -B2f1/:km: >> example_29.ps
+echo 0 90 14 0 1 LB a) | pstext -R -J -O -N -D-3i/-0.2i >> example_29.ps
+REM Clean up
+del *.nc
+del mars.cpt
+del .gmt*
+if %master%==y cd ..
---
+++ trunk/doc/examples/ex29/job29.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#		GMT EXAMPLE 29
+#		$Id: job29.sh,v 1.1 2008-12-06 00:59:35 guru Exp $
+#
+# Purpose:	Illustrates spherical surface gridding with Green's function of splines
+# GMT progs:	makecpt, grdcontour, grdgradient, grdimage, grdmath greenspline, psscale, pstext
+# Unix progs:	rm, echo
+#
+ps=example_29.ps
+
+# This example uses 370 radio occultation data for Mars to grid the topography.
+# Data and information from Smith, D. E., and M. T. Zuber (1996), The shape of
+# Mars and the topographic signature of the hemispheric dichotomy, Science, 271, 184–187.
+
+# Make Mars ellipsoid given their three best-fitting axes:
+a=3399.472
+b=3394.329
+c=3376.502
+grdmath -Rg -I2 -F X COSD $a DIV DUP MUL X SIND $b DIV DUP MUL ADD Y COSD DUP MUL MUL Y SIND $c DIV \
+	DUP MUL ADD SQRT INV = ellipsoid.nc
+
+#  Do both Parker and Wessel/Becker solutions (tension = 0.9975)
+greenspline -Rellipsoid.nc mars370.in -D4 -Sp -Gmars.nc
+greenspline -Rellipsoid.nc mars370.in -D4 -SQ0.9975/500001 -Gmars2.nc
+# Scale to km and remove ellipsoid
+grdmath mars.nc 1000 DIV ellipsoid.nc SUB = mars.nc
+grdmath mars2.nc 1000 DIV ellipsoid.nc SUB = mars2.nc
+makecpt -Crainbow -T-7/15/1 -Z > mars.cpt
+grdgradient mars2.nc -M -Ne0.75 -A45 -Gmars2_i.nc
+grdimage mars2.nc -Imars2_i.nc -Cmars.cpt -B30g30Wsne -JH0/6i -P -K \
+	-U"Example 29 in Cookbook" --ANNOT_FONT_SIZE=12 > $ps
+grdcontour mars2.nc -J -O -K -C1 -A5 -Glz+/z- >> $ps
+psxy -Rg -J -O -K -Sc0.045i -Gblack mars370.in  >> $ps
+echo "0 90 14 0 1 LB b)" | pstext -R -J -O -K -N -D-3i/-0.2i >> $ps
+grdgradient mars.nc -M -Ne0.75 -A45 -Gmars_i.nc
+grdimage mars.nc -Imars_i.nc -Cmars.cpt -B30g30Wsne -J -O -K -Y3.6i  --ANNOT_FONT_SIZE=12 >> $ps
+grdcontour mars.nc -J -O -K -C1 -A5 -Glz+/z- >> $ps
+psxy -Rg -J -O -K -Sc0.045i -Gblack mars370.in  >> $ps
+psscale -Cmars.cpt -O -K -D3i/-0.1i/5i/0.1ih -I --ANNOT_FONT_SIZE=12 -B2f1/:km: >> $ps
+echo "0 90 14 0 1 LB a)" | pstext -R -J -O -N -D-3i/-0.2i >> $ps
+# Clean up
+rm -f *.nc mars.cpt .gmt*
