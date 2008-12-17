71624940789a1be7d4560f7146ae0d84fbaee292
Paul Wessel <pwessel@hawaii.edu>
2008-12-17 18:42:55 +0000

    *** empty log message ***

---
+++ test/pscoast/pscoast_mess.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+#	$Id: pscoast_mess.sh,v 1.1 2008-12-17 18:42:55 guru Exp $
+
+ps=pscoast_mess.ps
+
+. ../functions.sh
+header "Test pscoast for correct polygon assembly"
+
+pscoast -JL153/-28/-26/-27/6i -Ba1/a1WSne -P -K -R151.5/-28.5/154.0/-27r -Dh -A20/1/1 -W0.5p -Glightgreen -Slightblue > $ps
+pscoast -J -Ba1/a1WSne -O -R -Di -A20/1/1 -W0.5p -Glightgreen -Slightblue -Y5i >> $ps
+
+pscmp
