a45de3f2a91cab6bdd9320623f433796d04a8951
Paul Wessel <pwessel@hawaii.edu>
2009-01-09 04:02:48 +0000

    The annual copyright year update

--- trunk/src/sph/sph.c
+++ trunk/src/sph/sph.c
@@ -1,7 +1,7 @@
 /*--------------------------------------------------------------------
- *	$Id: sph.c,v 1.4 2008-03-24 20:26:32 myself Exp $
+ *	$Id: sph.c,v 1.5 2009-01-09 04:02:48 myself Exp $
  *
- *	Copyright (c) 1991-2008 by P. Wessel and W. H. F. Smith
+ *	Copyright (c) 2008-2009 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
  *
  *	This program is free software; you can redistribute it and/or modify
