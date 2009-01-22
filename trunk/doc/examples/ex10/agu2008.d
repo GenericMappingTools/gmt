b90e61aec4723d49657ccdf4974b3a56d4475778
Paul Wessel <pwessel@hawaii.edu>
2009-01-22 23:55:39 +0000

    Updated to end-of-2008 numbers

--- trunk/doc/examples/ex10/agu2008.d
+++ trunk/doc/examples/ex10/agu2008.d
@@ -1,8 +1,8 @@
-#	$Id: agu2008.d,v 1.1 2008-05-13 19:09:54 guru Exp $
-#	AGU Membership as of Jan 2008. Pr Jon Sears (jsears@agu.org)
--100    42      34241	NA	North America
--60     -20     547	SA	South America
-20      10      139	AF	Africa
-90      30      5183	AS	Asia
-10      50      8371	EU	Europe
-140     -30     837	AU	Australia
+#	$Id: agu2008.d,v 1.2 2009-01-22 23:55:39 guru Exp $
+#	AGU Membership as of Dec 2008. Pr Jon Sears (jsears@agu.org)
+-100    42      38096	NA	North America
+-60     -20     647	SA	South America
+20      10      156	AF	Africa
+90      30      5742	AS	Asia
+10      50      9667	EU	Europe
+140     -30     1298	AU	Australia
