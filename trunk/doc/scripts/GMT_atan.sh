da3b378d0f443b2006b078f92db005007f15aa8e
Remko Scharroo <remko.scharroo@eumetsat.int>
2009-02-13 21:05:14 +0000

    - Relocation of examples and tutorial directory

--- trunk/doc/scripts/GMT_atan.sh
+++ trunk/doc/scripts/GMT_atan.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#	$Id: GMT_atan.sh,v 1.5 2006-10-25 03:01:36 pwessel Exp $
+#	$Id: GMT_atan.sh,v 1.6 2009-02-13 21:05:14 remko Exp $
 #
 
 trap 'rm -f $$.*; exit 1' 1 2 3 15
 
-grdgradient -A45 ../../tutorial/us.nc -N -M -G$$.t.grd
+grdgradient -A45 ../../share/doc/gmt/tutorial/us.nc -N -M -G$$.t.grd
 grd2xyz -Z $$.t.grd > $$.d
 pshistogram $$.d -R-0.75/0.75/0/20 -JX1.5/1 -B0.5/5f5WSne -W0.01 -P -K -Gblack -Z1 > GMT_atan.ps
 
@@ -26,7 +26,7 @@ psxy -R -J -O -K -M -Wthinnest << EOF >> GMT_atan.ps
 0	-1
 0	1
 EOF
-grdgradient -A45 ../../tutorial/us.nc -Nt -M -G$$.tt.grd
+grdgradient -A45 ../../share/doc/gmt/tutorial/us.nc -Nt -M -G$$.tt.grd
 grd2xyz -Z $$.tt.grd > $$.d
 pshistogram $$.d -R-0.75/0.75/0/5 -JX1.5/1 -B0.5/2f1WSne -W0.01 -O  -K -Gblack -X1.85 -Z1 >> GMT_atan.ps
 pstext -R -J -O << EOF >> GMT_atan.ps
