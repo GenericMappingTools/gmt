12d34b4110f1e85ca47c0e59c792a00ffe62234a
Remko Scharroo <remko.scharroo@eumetsat.int>
2009-02-15 20:22:05 +0000

    - PLOT_DEGREE_FORMAT ddd:mm:ss is now default. No need to set it.

--- doc/scripts/GMT_mercator.sh
+++ doc/scripts/GMT_mercator.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#	$Id: GMT_mercator.sh,v 1.4 2004-07-13 18:47:09 pwessel Exp $
+#	$Id: GMT_mercator.sh,v 1.5 2009-02-15 20:22:05 remko Exp $
 #
 
-gmtset PLOT_DEGREE_FORMAT ddd:mm:ss BASEMAP_TYPE fancy
+gmtset BASEMAP_TYPE fancy
 pscoast -R0/360/-70/70 -Jm1.2e-2i -Ba60f30/a30f15 -Dc -A5000 -Gblack -P > GMT_mercator.ps
--- doc/scripts/GMT_stereographic_rect.sh
+++ doc/scripts/GMT_stereographic_rect.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#	$Id: GMT_stereographic_rect.sh,v 1.6 2007-02-08 21:46:28 remko Exp $
+#	$Id: GMT_stereographic_rect.sh,v 1.7 2009-02-15 20:22:05 remko Exp $
 #
 
-gmtset PLOT_DEGREE_FORMAT ddd:mm:ss OBLIQUE_ANNOTATION 30
+gmtset OBLIQUE_ANNOTATION 30
 pscoast -R-25/59/70/72r -JS10/90/11c -B30g10/5g5 -Dl -A250 -Glightgray -Wthinnest -P \
 	> GMT_stereographic_rect.ps
