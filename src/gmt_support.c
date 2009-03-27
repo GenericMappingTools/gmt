53e298486a4c463fc13ac55cfff08e5fcd155c97
Paul Wessel <pwessel@hawaii.edu>
2009-03-27 00:44:14 +0000

    Fixed array out of bounds in read_cpt

--- src/gmt_support.c
+++ src/gmt_support.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_support.c,v 1.387 2009-03-26 21:04:30 guru Exp $
+ *	$Id: gmt_support.c,v 1.388 2009-03-27 00:44:14 guru Exp $
  *
  *	Copyright (c) 1991-2009 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -1678,8 +1678,8 @@ int GMT_read_cpt (char *cpt_file)
 			   take the shorter distance instead. This does not apply for HSV color tables, since there we assume
 			   that the H values are intentional and one might WANT to interpolate over more than 180 degrees. */
 			if (!(gmtdefs.color_model & GMT_READ_HSV)) {
-				if (GMT_lut[n].hsv_diff[i] < -180.0) GMT_lut[n].hsv_diff[i] += 360.0;
-				if (GMT_lut[n].hsv_diff[i] >  180.0) GMT_lut[n].hsv_diff[i] -= 360.0;
+				if (GMT_lut[n].hsv_diff[0] < -180.0) GMT_lut[n].hsv_diff[0] += 360.0;
+				if (GMT_lut[n].hsv_diff[0] >  180.0) GMT_lut[n].hsv_diff[0] -= 360.0;
 			}
 		}
 
