baed53121ac9c873d8a46096fbce4cd53271342c
Paul Wessel <pwessel@hawaii.edu>
2009-03-27 23:04:08 +0000

    Had ps2raster and open pdf which will fail off OS X.  Now just gv the PS.

--- src/sph/sph_ex_1.sh
+++ src/sph/sph_ex_1.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-#	$Id: sph_ex_1.sh,v 1.1 2009-03-16 18:47:31 myself Exp $
+#	$Id: sph_ex_1.sh,v 1.2 2009-03-27 23:04:08 guru Exp $
 # Example of gridding with sphinterpolate
 PS=`basename $0 '.sh'`.ps
-PDF=`basename $0 '.sh'`.pdf
 makecpt -Crainbow -T-7000/15000/1000 -Z > $$.cpt
 sphinterpolate mars370.txt -Rg -I1 -Q0 -G$$.nc -V
 grdimage $$.nc -JH0/4.5i -B30g30:."-Q0": -C$$.cpt -X0.8i -Y5.5i -K --HEADER_OFFSET=0i --HEADER_FONT_SIZE=18p > $PS
@@ -14,6 +13,5 @@ sphinterpolate mars370.txt -Rg -I1 -Q3 -G$$.nc -V
 grdimage $$.nc -J -B30g30:."-Q3": -C$$.cpt -X4.9i -O -K  --HEADER_OFFSET=0i --HEADER_FONT_SIZE=18p >> $PS
 psxy -Rg -J -O -K mars370.txt -Sc0.05i -G0 -B30g30 -X-2.45 -Y2.5i >> $PS
 psxy -Rg -J -O /dev/null >> $PS
-ps2raster -Tf $PS
-open $PDF
-rm -f $PS *$$*
+gv $PS &
+rm -f *$$*
