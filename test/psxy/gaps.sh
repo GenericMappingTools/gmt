5ab9e056a51ace31c4b7b6671eb96c8fdb583097
Paul Wessel <pwessel@hawaii.edu>
2009-03-28 01:07:20 +0000

    *** empty log message ***

--- test/psxy/gaps.sh
+++ test/psxy/gaps.sh
@@ -21,7 +21,7 @@ cat << EOF > $$.txt
 13	2
 14	1
 EOF
-gmtset NAN_RECORDS bad
+gmtset NAN_RECORDS skip
 # Must redirect sample1d's stderr messages to avoid seeing them for the 3 bad records
 (psxy $$.txt -R-1/15/-3/3 -JX6i/3i -Sc0.1i -W0.25p -Ggreen -P -K -Y6i > $ps) 2> /dev/null
 awk '{if ($2 == "NaN") {print $1, -3}}' $$.txt | psxy -R -J -O -K -St0.2i -Gblack -W0.25p >> $ps
@@ -29,7 +29,7 @@ awk '{if ($2 == "NaN") {print $1, -3}}' $$.txt | psxy -R -J -O -K -St0.2i -Gblac
 (sample1d $$.txt -I0.1 -Fc | psxy -R -J -O -K -W0.25p,- >> $ps) 2> /dev/null
 (sample1d $$.txt -I0.1 -Fa | psxy -R -J -O -K -W0.25p -B5f1g1:".Skipping NaNs and interpolating through": --HEADER_FONT_SIZE=18 >> $ps) 2> /dev/null
 # New behavior with upper case switches
-gmtset NAN_RECORDS gap
+gmtset NAN_RECORDS pass
 psxy $$.txt -R -J -Sc0.1i -W0.25p -Ggreen -O -K -Y-4.5i >> $ps
 awk '{if ($2 == "NaN") {print $1, -3}}' $$.txt | psxy -R -J -O -K -St0.2i -Gblack -W0.25p >> $ps
 sample1d $$.txt -I0.1 -Fl | psxy -R -J -O -K -W0.25p,. >> $ps
