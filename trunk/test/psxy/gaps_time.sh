0e231faa3f2a8788a037d0567623a2fdbf49c9b5
Remko Scharroo <remko.scharroo@eumetsat.int>
2009-04-12 15:30:15 +0000

    - No more intermediate file

--- trunk/test/psxy/gaps_time.sh
+++ trunk/test/psxy/gaps_time.sh
@@ -6,7 +6,7 @@ ps=gaps_time.ps
 header "Test plotting time series with NaNs indicating line gaps"
 
 gmtset NAN_RECORDS pass TIME_UNIT y TIME_EPOCH 2000-01-01 PLOT_DATE_FORMAT mm
-gmtmath "gtec_tx_daily.nc?time/gtec" -C0 1 MOD = > k
-psxy k -f0t -R2000-01-01T/2001-01-01T/5/65 -Fx0.5 -JX6i/3i -W0.5p,blue -P -B1O:Month:/20f5:GTEC: > $ps
+gmtmath "gtec_tx_daily.nc?time/gtec" -C0 1 MOD = | \
+	psxy -f0t -R2000-01-01T/2001-01-01T/5/65 -Fx0.5 -JX6i/3i -W0.5p,blue -P -B1O:Month:/20f5:GTEC: > $ps
 
 pscmp
