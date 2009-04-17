f8f3db5b0bfdb0ac07886472e468347e65dfa79c
Remko Scharroo <remko.scharroo@eumetsat.int>
2009-04-17 00:41:43 +0000

    - Changed location of bullseye.def

--- trunk/doc/scripts/GMT_volcano.sh
+++ trunk/doc/scripts/GMT_volcano.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#	$Id: GMT_volcano.sh,v 1.5 2007-02-08 21:43:12 remko Exp $
+#	$Id: GMT_volcano.sh,v 1.6 2009-04-17 00:41:43 remko Exp $
 
-ln -s ../../examples/ex20/bullseye.def .
+ln -s ../../share/doc/gmt/examples/ex20/bullseye.def .
 echo "0 0" | psxy -R-0.5/0.5/-0.5/0.5 -JX2i -P -Ba0.25g0.05WSne -Wthick -Skvolcano/2i -K > GMT_volcano.ps
 echo "0 0" | psxy -R -J -N -Ba0.25g0.05wSnE -Wthick -Skbullseye/2i -O -X2.5i >> GMT_volcano.ps
 rm -f bullseye.def
