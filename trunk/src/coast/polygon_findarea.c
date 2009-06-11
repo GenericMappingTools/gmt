e47b334c6f1d86e64188d8568bced653e08be59d
Paul Wessel <pwessel@hawaii.edu>
2009-06-11 21:07:58 +0000

    *** empty log message ***

--- trunk/src/coast/polygon_findarea.c
+++ trunk/src/coast/polygon_findarea.c
@@ -1,25 +1,24 @@
 /*
- *	$Id: polygon_findarea.c,v 1.2 2009-06-11 05:42:09 guru Exp $
+ *	$Id: polygon_findarea.c,v 1.3 2009-06-11 21:07:58 guru Exp $
  * Based on polygon_findlevel but limited to just compute polygon areas.
  */
 #include "wvs.h"
 
 int main (int argc, char **argv) {
-	int k, ID, sign, n_alloc = 0;
-	double size;
+	int k, sign, n_alloc = 0;
+	double size, f, cut = 0.0;
 	double *flon = NULL, *flat = NULL;
 	FILE *fp;
 	struct LONGPAIR p;
 	struct GMT3_POLY h;
 	
 	if (argc == 1) {
-		fprintf (stderr, "usage: polygon_findarea final_dbase.b [ID]\n");
-		fprintf (stderr, "Computes all areas unless a specific ID is given\n");
-		fprintf (stderr, "Will write areas to stdout.\n");
+		fprintf (stderr, "usage: polygon_findarea final_dbase.b cut\n");
+		fprintf (stderr, "Reports polygons whose area is less that cut %% of full res area\n");
 		exit (-1);
 	}
 
-	ID = (argc == 3) ? atoi (argv[2]) : -1;
+	cut = atof (argv[2]);
 	
 	fprintf (stderr, "Read headers\n");
 	fp = fopen (argv[1], "r");
@@ -29,14 +28,13 @@ int main (int argc, char **argv) {
 	area_init ();
 
 	while (pol_readheader (&h, fp) == 1) {
-		if (ID >= 0 && h.id != ID) {	/* Skip this polygon */
-			fseek (fp, h.n * sizeof(struct LONGPAIR), 1);
-			continue;
-		}
 		if (h.n > n_alloc) {
 			n_alloc = h.n;
 			flon = (double *) GMT_memory ((void *)flon, n_alloc, sizeof(double), "polygon_findarea");
 			flat = (double *) GMT_memory ((void *)flat, n_alloc, sizeof(double), "polygon_findarea");
+		}
+		if (h.id == 315) {
+			k = 9;
 		}
 		for (k = 0; k < h.n; k++) {
 			if (pol_fread (&p, 1, fp) != 1) {
@@ -48,7 +46,8 @@ int main (int argc, char **argv) {
 			flat[k] = p.y * 1.0e-6;
 		}
 		size = 1.0e-6 * area_size (flon, flat, h.n, &sign); /* in km^2 */
-		printf ("%d\t%.12g\n", ID, size * sign);
+		f = 100.0 * (size / h.area);
+		if (f < cut) printf ("%d\t%.12g vs %.12g\n", h.id, size, h.area);
 	}
 	
 	fclose (fp);
