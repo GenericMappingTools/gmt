e5dbd7de4b142e64831827de7eac1a27c2cfb780
Paul Wessel <pwessel@hawaii.edu>
2009-06-17 22:44:03 +0000

    *** empty log message ***

--- src/coast/SWBD/swbd_stitch.sh
+++ src/coast/SWBD/swbd_stitch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Make coastline polygons from SRTM's SWBD files
-#	$Id: swbd_stitch.sh,v 1.12 2009-06-17 22:09:04 guru Exp $
+#	$Id: swbd_stitch.sh,v 1.13 2009-06-17 22:42:36 guru Exp $
 #
 # Usage: swbd_stitch.sh w e s n JOBDIR
 #
@@ -82,9 +82,9 @@ if [ $extract -eq 1 ]; then
 		unzip -q $file
 		ogr2ogr -f "GMT" $name.d $name.shp
 		rearranger < $name.gmt > tmp
-		gmtconvert -m -fg -S"BA040" -F0,1 --D_FORMAT=%.6f tmp > raw_c/$name.gmt
-		gmtconvert -m -fg -S"BH080" -F0,1 --D_FORMAT=%.6f tmp > raw_l/$name.gmt
-		gmtconvert -m -fg -S"BH140" -F0,1 --D_FORMAT=%.6f tmp > raw_r/$name.gmt
+		gmtconvert -m -fg -S"BA040" -F0,1 --D_FORMAT=%.8f --OUTPUT_DEGREE_FORMAT=D tmp > raw_c/$name.gmt
+		gmtconvert -m -fg -S"BH080" -F0,1 --D_FORMAT=%.8f --OUTPUT_DEGREE_FORMAT=D tmp > raw_l/$name.gmt
+		gmtconvert -m -fg -S"BH140" -F0,1 --D_FORMAT=%.8f --OUTPUT_DEGREE_FORMAT=D tmp > raw_r/$name.gmt
 		rm -f tmp $name.* *.zip
 	done < files.lis
 fi
@@ -99,7 +99,9 @@ if [ $stitch -eq 1 ]; then
 		s=`echo $name | awk '{if (substr($1,5,1) == "s") {printf "-%d\n", substr($1, 6, 2)} else {printf "%d\n", substr($1, 6, 2)}}'`
 		n=`expr $s + 1`
 		for type in c l r; do
-			gmtpoly -L0/2.8e-4/2.8e-4 -m -fg -R$w/$e/$s/$n raw_${type}/$name.gmt --D_FORMAT=%.6f >> SWBD.raw_${type}.d
+			gmtpoly -L0/2.8e-4/2.8e-4 -m -fg -R$w/$e/$s/$n raw_${type}/$name.gmt --D_FORMAT=%.6f --OUTPUT_DEGREE_FORMAT=D >> SWBD.raw_${type}.d
+			echo stopper
+			read
 		done
 	done < files.lis
 	rm -rf raw_[clr]
@@ -107,7 +109,7 @@ if [ $stitch -eq 1 ]; then
 	for type in c l r; do
 		if [ -f SWBD.raw_${type}.d ]; then
 			mkdir -p pol_${type}
-			gmtstitch -fg -T0 -Dpol_${type}/srtm_pol_%c_%8.8d.txt -L -m SWBD.raw_${type}.d -Qlist_${type}_%c.lis --D_FORMAT=%.6f
+			gmtstitch -fg -T0 -Dpol_${type}/srtm_pol_%c_%8.8d.txt -L -m SWBD.raw_${type}.d -Qlist_${type}_%c.lis --D_FORMAT=%.6f --OUTPUT_DEGREE_FORMAT=D
 			while read file; do
 				echo "> $type segment" >> SWBD_open_${type}.d
 				cat $file >> SWBD_open_${type}.d
@@ -127,7 +129,7 @@ if [ $combine -eq 1 ]; then
 	for type in c l r; do
 		if [ -f SWBD_open_${type}.d ]; then
 			mkdir -p polc_${type}
-			gmtstitch -fg -T0 -Dpolc_${type}/srtm_pol_%c_%8.8d.txt -L -m SWBD_open_${type}.d -Qlist_${type}_%c.lis --D_FORMAT=%.6f
+			gmtstitch -fg -T0 -Dpolc_${type}/srtm_pol_%c_%8.8d.txt -L -m SWBD_open_${type}.d -Qlist_${type}_%c.lis --D_FORMAT=%.6f --OUTPUT_DEGREE_FORMAT=D
 			rm -f SWBD_open_${type}.d
 			while read file; do
 				echo "> $type segment" >> SWBD_open_${type}.d
