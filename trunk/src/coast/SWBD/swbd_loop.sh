bcfbead40ad4548ad7745b3503e3aa77b6c22476
Paul Wessel <pwessel@hawaii.edu>
2009-06-18 23:02:15 +0000

    *** empty log message ***

--- trunk/src/coast/SWBD/swbd_loop.sh
+++ trunk/src/coast/SWBD/swbd_loop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Make coastline polygons from SRTM's SWBD files
-#	$Id: swbd_loop.sh,v 1.1 2009-06-15 20:15:49 guru Exp $
+#	$Id: swbd_loop.sh,v 1.2 2009-06-18 23:02:15 guru Exp $
 
 WEST=$1
 EAST=$2
@@ -19,3 +19,18 @@ while [ $w -lt $EAST ]; do
 	done
 	w=$e
 done
+closed=""
+open=""
+for type in c l r; do
+	nc=0
+	no=0
+	if [ -f ${TILE}/SWBD_${TILE}_open_${type}.d ]; then
+		nc=`grep '^>' ${TILE}/SWBD_${TILE}_closed_${type}.d | wc -l`
+	fi
+	if [ -f ${TILE}/SWBD_${TILE}_open_${type}.d ]; then
+		no=`grep '^>' ${TILE}/SWBD_${TILE}_open_${type}.d | wc -l`
+	fi
+	closed="$closed $nc"
+	open="$open $no"
+done
+printf "\n%17s : C L R polygons Closed: %s Open: %s\n"  "$WEST/$EAST/$SOUTH/$NORTH" "$closed" "$open"
