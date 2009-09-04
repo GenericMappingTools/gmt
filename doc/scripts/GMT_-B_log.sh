c8d03da5f2899f66322bb55dd2804f616d44a5c8
Remko Scharroo <remko.scharroo@eumetsat.int>
2009-09-04 22:02:01 +0000

    - Stuff used to reset the BoundingBox removed

--- doc/scripts/GMT_-B_log.sh
+++ doc/scripts/GMT_-B_log.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
-#	$Id: GMT_-B_log.sh,v 1.5 2007-02-08 21:46:27 remko Exp $
+#	$Id: GMT_-B_log.sh,v 1.6 2009-09-04 22:02:01 remko Exp $
 #
 
 gmtset GRID_PEN_PRIMARY thinnest,.
-psbasemap -R1/1000/0/1 -JX3l/0.25 -Ba1f2g3p:"Axis Label":S -K -P > GMT_-B_log.ps
-psbasemap -R -J -Ba1f2g3l:"Axis Label":S -O -K -Y0.85 >> GMT_-B_log.ps
-psbasemap -R -J -Ba1f2g3:"Axis Label":S -O -Y0.85 >> GMT_-B_log.ps
-#
-# Mess because the 10^x annotations sticks outside but gets clipped
-#
-sed '/%%Trailer/q' GMT_-B_log.ps > $$.ps
-cat << EOF >> $$.ps
-%%BoundingBox: 60 0 300 216
-S 0 -255 T 4.16667 4.16667 scale showpage
-
-end
-EOF
-mv -f $$.ps GMT_-B_log.ps
+psbasemap -R1/1000/0/1 -JX3il/0.25i -B1f2g3p:"Axis Label":S -K -P > GMT_-B_log.ps
+psbasemap -R -J -B1f2g3l:"Axis Label":S -O -K -Y0.85 >> GMT_-B_log.ps
+psbasemap -R -J -B1f2g3:"Axis Label":S -O -Y0.85 >> GMT_-B_log.ps
