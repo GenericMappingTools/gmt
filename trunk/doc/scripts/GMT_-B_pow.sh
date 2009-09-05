7576266afdda6fbaa8248719a116ff5327f6c67c
Remko Scharroo <remko.scharroo@eumetsat.int>
2009-09-05 01:31:01 +0000

    - Specify inch sizes

--- trunk/doc/scripts/GMT_-B_pow.sh
+++ trunk/doc/scripts/GMT_-B_pow.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#	$Id: GMT_-B_pow.sh,v 1.5 2007-02-08 21:46:27 remko Exp $
+#	$Id: GMT_-B_pow.sh,v 1.6 2009-09-05 01:31:01 remko Exp $
 #
 
 gmtset GRID_PEN_PRIMARY thinnest,.
-psbasemap -R0/100/0/0.9 -JX3p0.5/0.25 -Ba3f2g1p:"Axis Label":S -K -P > GMT_-B_pow.ps
-psbasemap -R -J -Ba20f10g5:"Axis Label":S -O -Y0.85 >> GMT_-B_pow.ps
+psbasemap -R0/100/0/0.9 -JX3ip0.5/0.25i -Ba3f2g1p:"Axis Label":S -K -P > GMT_-B_pow.ps
+psbasemap -R -J -Ba20f10g5:"Axis Label":S -O -Y0.85i >> GMT_-B_pow.ps
