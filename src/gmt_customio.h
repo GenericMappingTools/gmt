a78b5e9d15bff4f8380b27e91926df3ad3b47f86
Joaquim Luis <jmfluis@gmail.com>
2009-09-07 15:12:31 +0000

    Added GDAL interface as format 22

--- src/gmt_customio.h
+++ src/gmt_customio.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_customio.h,v 1.30 2009-05-13 21:06:41 guru Exp $
+ *	$Id: gmt_customio.h,v 1.31 2009-09-07 15:12:31 jluis Exp $
  *
  *	Copyright (c) 1991-2009 by P. Wessel and W. H. F. Smith
  *	See COPYING file for copying and redistribution conditions.
@@ -80,6 +80,13 @@ EXTERN_MSC GMT_LONG GMT_agc_update_grd_info (struct GRD_HEADER *header);
 EXTERN_MSC GMT_LONG GMT_agc_write_grd_info (struct GRD_HEADER *header);
 EXTERN_MSC GMT_LONG GMT_agc_read_grd (struct GRD_HEADER *header, float *grid, double w, double e, double s, double n, GMT_LONG *pad, BOOLEAN complex);
 EXTERN_MSC GMT_LONG GMT_agc_write_grd (struct GRD_HEADER *header, float *grid, double w, double e, double s, double n, GMT_LONG *pad, BOOLEAN complex);
+
+/* Format # 22 */
+EXTERN_MSC GMT_LONG GMT_gdal_read_grd_info (struct GRD_HEADER *header);
+EXTERN_MSC GMT_LONG GMT_gdal_update_grd_info (struct GRD_HEADER *header);
+EXTERN_MSC GMT_LONG GMT_gdal_write_grd_info (struct GRD_HEADER *header);
+EXTERN_MSC GMT_LONG GMT_gdal_read_grd (struct GRD_HEADER *header, float *grid, double w, double e, double s, double n, GMT_LONG *pad, BOOLEAN complex);
+EXTERN_MSC GMT_LONG GMT_gdal_write_grd (struct GRD_HEADER *header, float *grid, double w, double e, double s, double n, GMT_LONG *pad, BOOLEAN complex);
 
 #define GRD_HEADER_SIZE	892
 
