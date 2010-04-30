7933e3d7ef08053633c5ce93a9a6b5ec9a5c8b43
Paul Wessel <pwessel@hawaii.edu>
2010-04-30 02:50:08 +0000

    *** empty log message ***

--- trunk/test/grdcontour/grdcontour.sh
+++ trunk/test/grdcontour/grdcontour.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
-#	$Id: grdcontour.sh,v 1.8 2007-11-15 04:20:41 remko Exp $
+#	$Id: grdcontour.sh,v 1.9 2010-04-30 02:50:08 guru Exp $
 
 . ../functions.sh
 header "Test grdcontour for a problem on region selection"
 ps=grdcontour.ps
-grdcontour="grdcontour -A200 -C100 -Gd4 xz-temp.grd -Jx0.4c/0.4c -Ba5f1/a5f1WNse -Gd4 -Wa3/grey -Wc1/grey"
+grdcontour="grdcontour -A200 -C100 -Gd4 xz-temp.grd -Jx0.4c/0.4c -Ba5f1/a5f1WNse -Gd4 -Wa3,grey -Wc1,grey"
 $grdcontour -R-100/-60/3/21.02 -P -K > $ps
 $grdcontour -R-100/-60/3/20 -O -Y10c >> $ps
 
