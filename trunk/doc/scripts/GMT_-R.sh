4bb125a006fe9c85815a3c598327cd1bf715af34
Remko Scharroo <remko.scharroo@eumetsat.int>
2010-10-03 22:06:36 +0000

    Changed penwidths -W1 to -Wthinnest

--- trunk/doc/scripts/GMT_-R.sh
+++ trunk/doc/scripts/GMT_-R.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#	$Id: GMT_-R.sh,v 1.9 2009-04-17 00:16:11 remko Exp $
+#	$Id: GMT_-R.sh,v 1.10 2010-10-03 22:06:36 remko Exp $
 #
 
 gmtset BASEMAP_TYPE PLAIN ANNOT_FONT_PRIMARY 0 ANNOT_FONT_SIZE_PRIMARY 8 TICK_LENGTH 0.05i \
 	CHAR_ENCODING ISOLatin1+
-pscoast -R-90/-70/18/35.819 -JM2 -P -Dl -Glightgray -W1 -Ba10g5WSEn -K > GMT_-R.ps
+pscoast -R-90/-70/18/35.819 -JM2 -P -Dl -Glightgray -Wthinnest -Ba10g5WSEn -K > GMT_-R.ps
 pstext -R0/2/-0.5/2 -Jx1 -O -K -N -Y-0.5 << EOF >> GMT_-R.ps
 1	-0.375	9	0	2	CT	@%0%a)@%% @%1%\035R@%%xmin/xmax/ymin/ymax
 EOF
@@ -29,7 +29,7 @@ psxy -R -J -O -K -N -m -Wthinner << EOF >> GMT_-R.ps
 1.05	-0.35
 EOF
 #
-pscoast -R-90/20/-65.5327/29.4248r -JOc280/20/22/69/2 -O -Dl -Glightgray -W1 -Ba10g5WSEn -K -X2.75 -Y0.5 >> GMT_-R.ps
+pscoast -R-90/20/-65.5327/29.4248r -JOc280/20/22/69/2 -O -Dl -Glightgray -Wthinnest -Ba10g5WSEn -K -X2.75 -Y0.5 >> GMT_-R.ps
 pstext -R0/2/-0.5/2 -Jx1 -O -K -N -Y-0.5 << EOF >> GMT_-R.ps
 1	-0.375	9	0	2	CT	@%0%b)@%% @%1%\035R@%%xlleft/ylleft/xuright/yuright @%1%r@%%
 EOF
