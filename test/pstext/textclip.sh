2bddc7203e35ebbfdf230bf9abb2d27e51932779
Paul Wessel <pwessel@hawaii.edu>
2011-03-22 18:41:32 +0000

    *** empty log message ***

--- test/pstext/textclip.sh
+++ test/pstext/textclip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: textclip.sh,v 1.1 2011-03-21 21:37:35 guru Exp $
+# $Id: textclip.sh,v 1.2 2011-03-22 18:41:32 guru Exp $
 # Test pstext with text path clipping
 . ../functions.sh
 ps=textclip.ps
@@ -9,8 +9,8 @@ FONT=16p,Helvetica
 psbasemap -R0/10/0/10 -JX15c/10c -Ggray70 -K -P > $ps
 
 pstext -R -J -F+f${FONT}+jBL -Gc -C0 -O -K << EOF >> $ps
-1 3 This text should NOT be covered up by the yellow box
-1 8 Neither should this jolly text
+1 3 This text should NOT be covered up by the box
+1 8 Neither should this jolly text message
 EOF
 
 # Overlay big polygon
@@ -21,6 +21,6 @@ psxy -R -J -L -Gkhaki1 -W0.5p -O -K << EOF >> $ps
 3 9
 EOF
 
-psclip -Ct -O >> $ps
+psclip -Cs -O >> $ps
 
 pscmp
