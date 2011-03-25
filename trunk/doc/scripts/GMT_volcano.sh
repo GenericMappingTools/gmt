bcb19807939522217899c66f01b2aa4d07dbf525
Remko Scharroo <remko.scharroo@eumetsat.int>
2011-03-25 00:45:46 +0000

    - Moved volcano.def

--- trunk/doc/scripts/GMT_volcano.sh
+++ trunk/doc/scripts/GMT_volcano.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#	$Id: GMT_volcano.sh,v 1.8 2011-03-15 02:06:29 guru Exp $
+#	$Id: GMT_volcano.sh,v 1.9 2011-03-25 00:45:46 remko Exp $
 . functions.sh
 
-ln -s ../../share/doc/gmt/examples/ex20/bullseye.def .
+ln -s ../examples/ex20/bullseye.def .
 echo "0 0" | psxy -R-0.5/0.5/-0.5/0.5 -JX2i -P -Ba0.25g0.05WSne -Wthick -Skvolcano/2i -K > GMT_volcano.ps
 echo "0 0" | psxy -R -J -N -Ba0.25g0.05wSnE -Wthick -Skbullseye/2i -O -X2.5i >> GMT_volcano.ps
 rm -f bullseye.def
