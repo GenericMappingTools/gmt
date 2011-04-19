cb669ae764c010f1dceff2a00f43954b5659ac05
Paul Wessel <pwessel@hawaii.edu>
2011-04-19 17:35:07 +0000

    *** empty log message ***

--- trunk/guru/nightly_gmt_cvsupdate.sh
+++ trunk/guru/nightly_gmt_cvsupdate.sh
@@ -1,11 +1,11 @@
 #/bin/sh
-# $Id: nightly_gmt_cvsupdate.sh,v 1.6 2011-03-15 02:06:31 guru Exp $
+# $Id: nightly_gmt_cvsupdate.sh,v 1.7 2011-04-19 17:35:06 guru Exp $
 # This script is used to get the latest GMT CVS changes and compile and install everything.
 # We do that by first getting the changes and then build and install executables.
 # First set some environment parameters since cron will not process login settings
 export NETCDFHOME=/sw
 export MATLAB=/Applications/MATLAB_R2010a.app
-export GMTHOME=/Users/pwessel/UH/RESEARCH/PROJECTS/GMTdev/GMT5
+export GMTHOME=/Users/pwessel/UH/RESEARCH/CVSPROJECTS/GMTdev/GMT5
 export AWK=awk
 export RGBDEF=/usr/X11/share/X11/rgb.txt
 export PATH=${PATH}:/sw/bin:$GMTHOME/bin
