226660ba073290c7553ce0e15c4c572a510afda4
Paul Wessel <pwessel@hawaii.edu>
2011-04-23 00:54:51 +0000

    Added fancy meter annotation to the UTM meter part

--- trunk/doc/examples/ex28/job28.sh
+++ trunk/doc/examples/ex28/job28.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #		GMT EXAMPLE 28
-#		$Id: job28.sh,v 1.9 2011-03-15 02:06:31 guru Exp $
+#		$Id: job28.sh,v 1.10 2011-04-23 00:54:51 guru Exp $
 #
 # Purpose:	Illustrates how to mix UTM data and UTM projection
 # GMT progs:	makecpt, grdgradient, grdimage, grdinfo, pscoast, pstext, mapproject
@@ -19,15 +19,19 @@ LL=`mapproject tmp.txt -Ju5Q/1:1 -F -C -I --FORMAT_GEO_OUT=ddd:mm:ss.x -i1,3 -o0
 	$AWK '{printf "%s/%s\n", $1, $2}'`
 UR=`mapproject tmp.txt -Ju5Q/1:1 -F -C -I --FORMAT_GEO_OUT=ddd:mm:ss.x -i2,4 -o0,1 | \
 	$AWK '{printf "%s/%s\n", $1, $2}'`
-# Lay down the UTM topo grid using a 1:17,000 scale
-grdimage Kilauea.utm.nc -IKilauea.utm_i.nc -CKilauea.cpt -Jx1:170000 -P -K -B5000g5000WSne \
+# Lay down the UTM topo grid using a 1:16,000 scale
+grdimage Kilauea.utm.nc -IKilauea.utm_i.nc -CKilauea.cpt -Jx1:160000 -P -K \
 	-U"Example 28 in Cookbook" --FORMAT_FLOAT_OUT=%.10g --FONT_ANNOT_PRIMARY=9p \
-	--MAP_GRID_CROSS_SIZE_PRIMARY=0.1i > $ps
+	> $ps
 # Overlay geographic data and coregister by using correct region and projection with the same scale
-pscoast -R${LL}/${UR}r -Ju5Q/1:170000 -O -K -Df+ -Slightblue -W0.5p -B5mg5mNE \
+pscoast -R${LL}/${UR}r -Ju5Q/1:160000 -O -K -Df+ -Slightblue -W0.5p -B5mg5mNE \
 	--FONT_ANNOT_PRIMARY=12p --FORMAT_GEO_MAP=ddd:mmF >> $ps
-psbasemap -R -J -O -K --FONT_ANNOT_PRIMARY=9p -Lf155:07:30W/19:15:40N/19:23N/5k+l1:17,000+u \
+echo 155:16:20W 19:26:20N KILAUEA | pstext -R -J -O -K -F+f12p,Helvetica-Bold+jCB >> $ps
+psbasemap -R -J -O -K --FONT_ANNOT_PRIMARY=9p -Lf155:07:30W/19:15:40N/19:23N/5k+l1:16,000+u \
 	--FONT_LABEL=10p >> $ps
-echo 155:16:20W 19:26:20N KILAUEA | pstext -R -J -O -F+f12p,Helvetica-Bold+jCB >> $ps
+# Annotate in km but append ,000m to annotations to get customized meter labels
+Rkm=`grdinfo Kilauea.utm.nc -C | cut -f2,3,4,5 | gmtconvert -i0-3s0.001 --IO_COL_SEPARATOR=/`
+psbasemap -R$Rkm -Jx1:160 -B5g5:,"-@:8:,000m":WSne -O --FONT_ANNOT_PRIMARY=10p \
+	--MAP_GRID_CROSS_SIZE_PRIMARY=0.1i --FONT_LABEL=10p >> $ps
 # Clean up
 rm -f Kilauea.utm_i.nc Kilauea.cpt tmp.txt
