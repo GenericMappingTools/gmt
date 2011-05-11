19a668317893c673a6338814ac2f8c06f1d0b730
Joaquim Luis <jmfluis@gmail.com>
2011-05-11 01:23:07 +0000

    Include also a one band gray scale image. Test if GDAL is available before running

--- trunk/test/grdimage/readwrite_withgdal.sh
+++ trunk/test/grdimage/readwrite_withgdal.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
-#	$Id: readwrite_withgdal.sh,v 1.2 2011-05-10 03:06:48 guru Exp $
+#	$Id: readwrite_withgdal.sh,v 1.3 2011-05-11 01:23:07 jluis Exp $
 
 . ../functions.sh
+GDAL=`grdreformat 2>&1 | grep -c gd`
 if [ $GDAL -eq 0 ]; then exit; fi
 	
 header "Test grdimage for reading and writting images with GDAL"
@@ -10,19 +11,22 @@ header "Test grdimage for reading and writting images with GDAL"
 ps=readwrite_withgdal.ps
 
 # RGB image
-grdimage -D needle.jpg -R1/430/1/300 -JX7c/0 -P -Y22c -K > $ps
+grdimage -D needle.jpg -JX7c/0 -P -Y20c -K > $ps
 
 # Same image as above but as idexed
-grdimage -D needle.png -R1/430/1/300 -JX7c/0 -X7.5c -O -K >> $ps
+grdimage -D needle.png -JX7c/0 -X7.5c -O -K >> $ps
 
 # Projected
-grdimage -Dr needle.jpg -Rd -JW12c -Bg30/g15 -X-6.0c -Y-7c -O -K >> $ps
+grdimage -Dr needle.jpg -Rd -JW10c -Bg30/g15 -X-5.0c -Y-7c -O -K >> $ps
 
 # Illuminated
 grdmath -R-15/15/-15/15 -I0.2 X Y HYPOT DUP 2 MUL PI MUL 8 DIV COS EXCH NEG 10 DIV EXP MUL = somb.nc
 grdgradient somb.nc -A225 -Gillum.nc -Nt0.75
-grdimage -D needle.jpg -Iillum.nc -JM10c -Y-12c -O >> $ps
+grdimage -D needle.jpg -Iillum.nc -JM8c -Y-10c -X-2c -O -K >> $ps
+
+# A gray image (one band, no color map)
+grdimage -D vader.jpg -JX4c/0 -X9c -O >> $ps
 
 rm -f somb.nc illum.nc
 
-pscmp
+#pscmp
