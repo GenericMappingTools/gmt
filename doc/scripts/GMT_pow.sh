3a1b354651024db7008ecd4e51ba0e155a06ff3d
Paul Wessel <pwessel@hawaii.edu>
2011-05-17 03:02:06 +0000

    Cleaning up some scripts and docs.

--- doc/scripts/GMT_pow.sh
+++ doc/scripts/GMT_pow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#	$Id: GMT_pow.sh,v 1.6 2011-05-01 18:06:37 remko Exp $
+#	$Id: GMT_pow.sh,v 1.7 2011-05-17 03:02:06 guru Exp $
 #
 . ./functions.sh
 
 psxy -R0/100/0/10 -Jx0.3ip0.5/0.15i -Ba1p/a2f1WSne -Wthick -P -K sqrt.d > GMT_pow.ps
-psxy -R -J -Sc0.075i -Gwhite -W -O sqrt.d10 >> GMT_pow.ps
+psxy -R -J -Sc0.075i -Ggreen -W -O sqrt.d10 >> GMT_pow.ps
