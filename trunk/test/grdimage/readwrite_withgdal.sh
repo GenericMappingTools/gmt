4ef6a7c008c0e0d1e4c82de1a4450f997d6fe35f
Remko Scharroo <remko.scharroo@eumetsat.int>
2011-05-19 17:11:52 +0000

    - Shortened header

--- trunk/test/grdimage/readwrite_withgdal.sh
+++ trunk/test/grdimage/readwrite_withgdal.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #
-#	$Id: readwrite_withgdal.sh,v 1.4 2011-05-18 00:52:05 jluis Exp $
+#	$Id: readwrite_withgdal.sh,v 1.5 2011-05-19 17:11:52 remko Exp $
 
 . ../functions.sh
 GDAL=`grdreformat 2>&1 | grep -c gd`
 if [ $GDAL -eq 0 ]; then exit; fi
 	
-header "Test grdimage for reading and writting images with GDAL"
+header "Test grdimage with images through GDAL"
 
 ps=readwrite_withgdal.ps
 
 # RGB image
-grdimage -D needle.jpg -JX7c/0 -P -Y20c -K > $ps
+grdimage -D needle.jpg -JX7c -P -Y20c -K > $ps
 
 # Same image as above but as idexed
-grdimage -D needle.png -JX7c/0 -X7.5c -O -K >> $ps
+grdimage -D needle.png -JX7c -X7.5c -O -K >> $ps
 
 # Projected
 grdimage -Dr needle.jpg -Rd -JW10c -Bg30/g15 -X-5.0c -Y-7c -O -K >> $ps
