69a64dbe85cb01b603a4b16959f61cde36cb6c1e
Joaquim Luis <jmfluis@gmail.com>
2011-05-21 15:14:01 +0000

    *** empty log message ***

--- test/psimage/withgdal.sh
+++ test/psimage/withgdal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#	$Id: withgdal.sh,v 1.1 2011-05-18 00:40:06 jluis Exp $
+#	$Id: withgdal.sh,v 1.2 2011-05-21 15:14:01 jluis Exp $
 
 . ../functions.sh
 GDAL=`grdreformat 2>&1 | grep -c gd`
@@ -11,18 +11,18 @@ header "Test psimage for reading images with GDAL"
 ps=withgdal.ps
 
 # RGB image
-psimage ../grdimage/needle.jpg -W7c -P -Y15c -K > $ps
+psimage ../grdimage/gdal/needle.jpg -W7c -P -Y15c -K > $ps
 
 # Same image as above but as idexed
-psimage ../grdimage/needle.png -W7c -X7.5c -O -K >> $ps
+psimage ../grdimage/gdal/needle.png -W7c -X7.5c -O -K >> $ps
 
 # Convert RGB to YIQ
-psimage ../grdimage/needle.jpg -M -W7c -X-7.5c -Y-7c -O -K >> $ps
+psimage ../grdimage/gdal/needle.jpg -M -W7c -X-7.5c -Y-7c -O -K >> $ps
 
 # Convert Indexed to YIQ
-psimage ../grdimage/needle.png -M -W7c -X7.5c -O -K >> $ps
+psimage ../grdimage/gdal/needle.png -M -W7c -X7.5c -O -K >> $ps
 
 # A gray image (one band, no color map)
-psimage ../grdimage/vader.jpg -W4c -X-2.5c -Y4.5c -O >> $ps
+psimage ../grdimage/gdal/vader.jpg -W4c -X-2.5c -Y4.5c -O >> $ps
 
 pscmp
