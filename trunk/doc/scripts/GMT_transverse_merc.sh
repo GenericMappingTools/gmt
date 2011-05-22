e85a1a6392811d39fdd8a5aec4399ffd7f159ab8
Paul Wessel <pwessel@hawaii.edu>
2011-05-22 22:14:06 +0000

    *** empty log message ***

--- trunk/doc/scripts/GMT_transverse_merc.sh
+++ trunk/doc/scripts/GMT_transverse_merc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#	$Id: GMT_transverse_merc.sh,v 1.7 2011-05-01 18:06:37 remko Exp $
+#	$Id: GMT_transverse_merc.sh,v 1.8 2011-05-22 22:14:06 guru Exp $
 #
 . ./functions.sh
 
-pscoast -R20/30/50/45r -Jt35/0.18i -B10g5 -Dl -A250 -Glightgray -Wthinnest -P \
-	> GMT_transverse_merc.ps
+pscoast -R20/30/50/45r -Jt35/0.18i -B10g5 -Dl -A250 -Glightbrown -Wthinnest -P \
+	-Sseashell > GMT_transverse_merc.ps
