696b0d779a82c429755b30865e4e9672ae884dc9
Paul Wessel <pwessel@hawaii.edu>
2011-05-22 22:14:06 +0000

    *** empty log message ***

--- doc/scripts/GMT_TM.sh
+++ doc/scripts/GMT_TM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#	$Id: GMT_TM.sh,v 1.5 2011-05-01 18:06:37 remko Exp $
+#	$Id: GMT_TM.sh,v 1.6 2011-05-22 22:14:05 guru Exp $
 #
 . ./functions.sh
 
-pscoast -R0/360/-80/80 -JT330/-45/3.5i -B30g15/15g15WSne -Dc -A2000 -Gblack -P > GMT_TM.ps
+pscoast -R0/360/-80/80 -JT330/-45/3.5i -B30g15/15g15WSne -Dc -A2000 -Slightblue -G0 -P > GMT_TM.ps
--- doc/scripts/GMT_mercator.sh
+++ doc/scripts/GMT_mercator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#	$Id: GMT_mercator.sh,v 1.8 2011-05-01 18:06:37 remko Exp $
+#	$Id: GMT_mercator.sh,v 1.9 2011-05-22 22:14:06 guru Exp $
 #
 . ./functions.sh
 
 gmtset MAP_FRAME_TYPE fancy
-pscoast -R0/360/-70/70 -Jm1.2e-2i -Ba60f30/a30f15 -Dc -A5000 -Gblack -P > GMT_mercator.ps
+pscoast -R0/360/-70/70 -Jm1.2e-2i -Ba60f30/a30f15 -Dc -A5000 -Gred -P > GMT_mercator.ps
