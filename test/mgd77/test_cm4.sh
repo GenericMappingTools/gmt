bd197b16d6b5087129134630841830e83bb1eefe
Paul Wessel <pwessel@hawaii.edu>
2011-05-27 01:10:56 +0000

    *** empty log message ***

--- test/mgd77/test_cm4.sh
+++ test/mgd77/test_cm4.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
-#	$Id: test_cm4.sh,v 1.2 2011-05-26 19:35:43 jluis Exp $
+#	$Id: test_cm4.sh,v 1.3 2011-05-27 01:10:56 guru Exp $
 #
 # Tests mgd77magref against the values of the original FORTRAN version 
 # Because the second term (lithospheric) does not agree it is not included in the comparison
 
 . ../functions.sh
 header "Test mgd77magref against original CM4 Fortran version"
-
-log=test_cm4.log
 
 data=2000.08700533
 
@@ -28,17 +26,20 @@ echo -30 45 0 $data | mgd77magref -A+y -Fxyz/7 -Sc1/15 | gmtconvert --FORMAT_FLO
 #Ion_ind B_xyz     7.9738793E-01 -2.2101940E+00 -3.7272951E+00
 #Tor     B_xyz    -3.4804584E+00 -6.0774586E+00  1.1689001E-02
 
-
-echo     2.1577205E+04	-5.4288317E+03	4.1624773E+04	>  cm4_f.dat
 #echo    -1.2747567E+01	-9.8955432E+00 -1.1668323E+01	>> cm4_f.dat
-echo    -1.5506497E+01	4.4450796E+00	1.9214288E+01	>> cm4_f.dat
-echo    -8.4923656E-01	5.5134285E-01	-1.3137803E+00	>> cm4_f.dat
-echo    -5.1975837E+00	-4.5903891E+00	2.9385937E+00	>> cm4_f.dat
-echo     7.9738793E-01	-2.2101940E+00	-3.7272951E+00	>> cm4_f.dat
-echo    -3.4804584E+00	-6.0774586E+00	1.1689001E-02	>> cm4_f.dat
+cat << EOF > cm4_f.dat
+2.1577205E+04	-5.4288317E+03	4.1624773E+04
+-1.5506497E+01	4.4450796E+00	1.9214288E+01
+-8.4923656E-01	5.5134285E-01	-1.3137803E+00
+-5.1975837E+00	-4.5903891E+00	2.9385937E+00
+7.9738793E-01	-2.2101940E+00	-3.7272951E+00
+-3.4804584E+00	-6.0774586E+00	1.1689001E-02
+EOF
 
-gmtmath -Ca cm4_c.dat cm4_f.dat SUB = | awk '{print $1+$2+$3}' | gmtmath -S -T STDIN MEAN = | awk '($1 == 0)' > $log
-
-rm -f cm4_c.dat cm4_f.dat
-
-passfail test_cm4
+diff cm4_f.dat cm4_c.dat > fail
+if [ ! -s fail ]; then
+	passfail test_cm4
+else
+	cat fail
+fi
+rm -f cm4_f.dat cm4_c.dat
