e75c2375698c996e18799ee767dd77b153abcb3d
Remko Scharroo <remko.scharroo@eumetsat.int>
2011-05-27 12:41:35 +0000

    - Proper use of passfail

--- trunk/test/mgd77/test_cm4.sh
+++ trunk/test/mgd77/test_cm4.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#	$Id: test_cm4.sh,v 1.6 2011-05-27 04:03:34 guru Exp $
+#	$Id: test_cm4.sh,v 1.7 2011-05-27 12:41:35 remko Exp $
 #
 # Tests mgd77magref against the values of the original FORTRAN version 
 # Because the second term (lithospheric) does not agree it is not included in the comparison
@@ -9,13 +9,10 @@ header "Test mgd77magref against original CM4 Fortran version"
 
 data=2000.08700533
 
-echo -30 45 0 $data | mgd77magref -A+y -Fxyz/1 -Sc1/15 | gmtconvert --FORMAT_FLOAT_OUT=%.7E  > cm4_c.dat
-#echo -30 45 0 $data | mgd77magref -A+y -Fxyz/2 -Sc1/15 | gmtconvert --FORMAT_FLOAT_OUT=%.7E >> cm4_c.dat
-echo -30 45 0 $data | mgd77magref -A+y -Fxyz/3 -Sc1/15 | gmtconvert --FORMAT_FLOAT_OUT=%.7E >> cm4_c.dat
-echo -30 45 0 $data | mgd77magref -A+y -Fxyz/4 -Sc1/15 | gmtconvert --FORMAT_FLOAT_OUT=%.7E >> cm4_c.dat
-echo -30 45 0 $data | mgd77magref -A+y -Fxyz/5 -Sc1/15 | gmtconvert --FORMAT_FLOAT_OUT=%.7E >> cm4_c.dat
-echo -30 45 0 $data | mgd77magref -A+y -Fxyz/6 -Sc1/15 | gmtconvert --FORMAT_FLOAT_OUT=%.7E >> cm4_c.dat
-echo -30 45 0 $data | mgd77magref -A+y -Fxyz/7 -Sc1/15 | gmtconvert --FORMAT_FLOAT_OUT=%.7E >> cm4_c.dat
+rm -f test_cm4.dat
+for val in 1 2 3 4 5 6 7; do
+	echo -30 45 0 $data | mgd77magref -A+y -Fxyz/$val -Sc1/15 | gmtconvert --FORMAT_FLOAT_OUT=%20.10f >> test_cm4.dat
+done
 
 # Output from the Fortran version
 #C/L 1   B_xyz     2.1577205E+04 -5.4288317E+03  4.1624773E+04
@@ -26,21 +23,8 @@ echo -30 45 0 $data | mgd77magref -A+y -Fxyz/7 -Sc1/15 | gmtconvert --FORMAT_FLO
 #Ion_ind B_xyz     7.9738793E-01 -2.2101940E+00 -3.7272951E+00
 #Tor     B_xyz    -3.4804584E+00 -6.0774586E+00  1.1689001E-02
 
-#-1.2747567E+01	-9.8955432E+00	-1.1668323E+01
-cat << EOF > cm4_f.dat
-2.1577205E+04	-5.4288317E+03	4.1624773E+04
--1.5506497E+01	4.4450796E+00	1.9214288E+01
--8.4923656E-01	5.5134285E-01	-1.3137803E+00
--5.1975837E+00	-4.5903891E+00	2.9385937E+00
-7.9738793E-01	-2.2101940E+00	-3.7272951E+00
--3.4804584E+00	-6.0774586E+00	1.1689001E-02
-EOF
+diff test_cm4.dat orig/test_cm4.dat > fail
 
-X=`gmtmath -Ca cm4_c.dat cm4_f.dat SUB = | awk '{print $1+$2+$3}' | gmtmath -S -T STDIN MEAN =`
+rm -f test_cm4.dat
 
-if [ "X$X" = "X0" ]; then
-	passfail test_cm4
-else
-	echo "Fortran and C CM4 results differ. Check log"
-fi
-#rm -f cm4_f.dat cm4_c.dat
+passfail test_cm4
