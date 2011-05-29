32bfd20be240c1188251d48c12b5444bdaf0073f
Remko Scharroo <remko.scharroo@eumetsat.int>
2011-05-29 17:28:59 +0000

    - Fixed typo

--- src/gmt_support.c
+++ src/gmt_support.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt_support.c,v 1.523 2011-05-29 03:32:50 guru Exp $
+ *	$Id: gmt_support.c,v 1.524 2011-05-29 17:28:59 remko Exp $
  *
  *	Copyright (c) 1991-2011 by P. Wessel, W. H. F. Smith, R. Scharroo, and J. Luis
  *	See LICENSE.TXT file for copying and redistribution conditions.
@@ -141,15 +141,13 @@ GMT_LONG gmt_memtrack_find (struct GMT_CTRL *C, struct MEMORY_TRACKER *M, void *
 #endif
 #endif
 
-#ifdef HAVE_QSORT_R
+#ifndef HAVE_QSORT_R
+double *GMT_x2sys_Y;	/* Must use global variable if there is no qsort_r on this system */
+#elif defined(__APPLE__) || defined(__FreeBSD__)
 /* Wonderful news: BSD and GLIBC has different argument order in qsort_r */
-#if defined(__APPLE__) || defined(__FreeBSD__)
 void qsort_r(void *base, size_t nel, size_t width, void *thunk, int (*compar)(void *, const void *, const void *));
 #else
 void qsort_r(void *base, size_t nel, size_t width, int (*compar)(void *, const void *, const void *), void *thunk);
-#endif
-#else
-double *GMT_x2sys_Y;	/* Must use global variable if there is no qsort_r on this system */
 #endif
 
 /*----------------------------------------------------------------------------- */
@@ -5834,7 +5832,7 @@ GMT_LONG GMT_BC_init (struct GMT_CTRL *C, struct GRD_HEADER *h)
 GMT_LONG GMT_grd_BC_set (struct GMT_CTRL *C, struct GMT_GRID *G)
 {
 	/* Set two rows of padding (pad[] can be larger) around data according
-	   to desired boundary condition info in thet header.
+	   to desired boundary condition info in that header.
 	   Returns -1 on problem, 0 on success.
 	   If either x or y is periodic, the padding is entirely set.
 	   However, if neither is true (this rules out geographical also)
@@ -7525,17 +7523,15 @@ GMT_LONG GMT_init_track (struct GMT_CTRL *C, double y[], GMT_LONG n, struct GMT_
 
 	/* Sort on minimum y-coordinate, if tie then on 2nd coordinate */
 
-#ifdef HAVE_QSORT_R
-/* Wonderful news: BSD and GLIBC has different argument order in qsort_r */
-#if defined(__APPLE__) || defined(__FreeBSD__)
-	qsort_r ((void *)L, (size_t)nl, sizeof (struct GMT_XSEGMENT), (void *)y, GMT_ysort);
-#else
-	qsort_r ((void *)L, (size_t)nl, sizeof (struct GMT_XSEGMENT), GMT_ysort, (void *)y);
-#endif
-#else
+#ifndef HAVE_QSORT_R
 	GMT_x2sys_Y = y;	/* Sort routine needs this global variable pointer */
 	qsort ((void *)L, (size_t)nl, sizeof (struct GMT_XSEGMENT), GMT_ysort);
 	GMT_x2sys_Y = (double *)NULL;	/* Set to NULL to indicate not in use */
+#elif defined(__APPLE__) || defined(__FreeBSD__)
+	/* Wonderful news: BSD and GLIBC has different argument order in qsort_r */
+	qsort_r ((void *)L, (size_t)nl, sizeof (struct GMT_XSEGMENT), (void *)y, GMT_ysort);
+#else
+	qsort_r ((void *)L, (size_t)nl, sizeof (struct GMT_XSEGMENT), GMT_ysort, (void *)y);
 #endif
 
 	*S = L;
