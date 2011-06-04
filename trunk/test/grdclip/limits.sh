e9d6ab445a3aa181d02e1dc328b72d9399dcdb24
Paul Wessel <pwessel@hawaii.edu>
2011-06-04 02:31:01 +0000

    *** empty log message ***

--- trunk/test/grdclip/limits.sh
+++ trunk/test/grdclip/limits.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: limits.sh,v 1.1 2011-06-03 20:37:26 guru Exp $
+#	$Id: limits.sh,v 1.2 2011-06-04 02:31:01 guru Exp $
 # Testing grdclip
 
 . ../functions.sh
@@ -13,7 +13,7 @@ grdmath -R$Rg -I1 0 0 CDIST 0.1 MUL KEI 0 0 CDIST 0.1 MUL SQRT ADD DUP UPPER DIV
 makecpt -Crainbow -T-6/10/1 > t.cpt
 # Draw all nodes as open circles
 grd2xyz tmp.nc | psxy -R$Rp -JX4.5i -Sc0.25c -W0.25p -Ct.cpt -P -K -B10f5WSne -Xc -Y0.5i > $ps
-psscale -D5i/4.75i/6i/0.15i -O -K -Ct.cpt -E >> $ps
+psscale -D5i/4.75i/6i/0.15i -O -K -Ct.cpt -E+n >> $ps
 # Set values higher than 7 to NaN and values lower than 0 to -6
 grdclip tmp.nc -Sa7/NaN -Sb0/-6 -Gout.nc
 grd2xyz out.nc | psxy -R -J -Sc0.25c -W0.25p -Ct.cpt -O -K -B10f5WSne -Y5i >> $ps
