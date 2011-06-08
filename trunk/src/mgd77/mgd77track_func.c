5257cb5f04002926baa470ffa062e1bd57efab82
Paul Wessel <pwessel@hawaii.edu>
2011-06-08 03:43:39 +0000

    *** empty log message ***

--- trunk/src/mgd77/mgd77track_func.c
+++ trunk/src/mgd77/mgd77track_func.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: mgd77track_func.c,v 1.11 2011-06-07 21:38:29 guru Exp $
+ *	$Id: mgd77track_func.c,v 1.12 2011-06-08 03:43:39 guru Exp $
  *
  *    Copyright (c) 2004-2011 by P. Wessel
  *    See README file for copying and redistribution conditions.
@@ -592,6 +592,7 @@ GMT_LONG GMT_mgd77track (struct GMTAPI_CTRL *API, struct GMT_OPTION *options)
 	if (GMT_err_pass (GMT, GMT_map_setup (GMT, GMT->common.R.wesn), "")) Return (GMT_RUNTIME_ERROR);
 	
 	GMT_plotinit (GMT, options);
+	GMT_plotcanvas (GMT);	/* Fill canvas if requested */
 	
 	GMT_map_clip_on (GMT, GMT->session.no_rgb, 3);
 	GMT_setpen (GMT, &Ctrl->W.pen);
