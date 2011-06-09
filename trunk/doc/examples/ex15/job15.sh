8947ad8f467bc7c5a7a34973d8853d68e9021679
Remko Scharroo <remko.scharroo@eumetsat.int>
2011-06-09 16:41:18 +0000

    - Removed needless duplication of argument of -B

--- trunk/doc/examples/ex15/job15.sh
+++ trunk/doc/examples/ex15/job15.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #		GMT EXAMPLE 15
-#		$Id: job15.sh,v 1.13 2011-03-15 02:06:31 guru Exp $
+#		$Id: job15.sh,v 1.14 2011-06-09 16:41:18 remko Exp $
 #
 # Purpose:	Gridding and clipping when data are missing
 # GMT progs:	blockmedian, gmtconvert, grdclip, grdcontour, grdinfo, minmax
@@ -18,14 +18,14 @@ grdcontour ship.nc -JM3i -P -B2WSne -C250 -A1000 -Gd2i -K -U"Example 15 in Cookb
 blockmedian $region -I10m ship.b -bi3 -bo > ship_10m.b
 surface $region -I10m ship_10m.b -Gship.nc -bi3
 psmask $region -I10m ship.b -J -O -K -T -Glightgray -bi3 -X3.6i >> $ps
-grdcontour ship.nc -J -B2WSne -C250 -L-8000/0 -A1000 -Gd2i -O -K >> $ps
+grdcontour ship.nc -J -B -C250 -L-8000/0 -A1000 -Gd2i -O -K >> $ps
 #
-psmask $region -I10m ship_10m.b -bi3 -J -B2WSne -O -K -X-3.6i -Y3.75i >> $ps
+psmask $region -I10m ship_10m.b -bi3 -J -B -O -K -X-3.6i -Y3.75i >> $ps
 grdcontour ship.nc -J -C250 -A1000 -L-8000/0 -Gd2i -O -K >> $ps
 psmask -C -O -K >> $ps
 #
 grdclip ship.nc -Sa-1/NaN -Gship_clipped.nc
-grdcontour ship_clipped.nc -J -B2WSne -C250 -A1000 -L-8000/0 -Gd2i -O -K -X3.6i >> $ps
+grdcontour ship_clipped.nc -J -B -C250 -A1000 -L-8000/0 -Gd2i -O -K -X3.6i >> $ps
 pscoast $region -J -O -K -Ggray -Wthinnest >> $ps
 grdinfo -C -M ship.nc | psxy -R -J -O -K -Sa0.15i -Wthick -i11,12 >> $ps
 echo "-0.3 3.6 Gridding with missing data" | pstext -R0/3/0/4 -Jx1i \
--- trunk/doc/examples/ex16/job16.sh
+++ trunk/doc/examples/ex16/job16.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #		GMT EXAMPLE 16
-#		$Id: job16.sh,v 1.15 2011-06-02 20:18:33 guru Exp $
+#		$Id: job16.sh,v 1.16 2011-06-09 16:41:18 remko Exp $
 #
 # Purpose:	Illustrates interpolation methods using same data as Example 12.
 # GMT progs:	gmtset, grdview, grdfilter, pscontour, psscale, pstext, surface, triangulate
@@ -16,16 +16,16 @@ pscontour -R0/6.5/-0.2/6.5 -Jx0.45i -P -K -Y5.5i -Ba2f1WSne table_5.11 -Cex16.cp
 echo "3.25 7 pscontour (triangulate)" | pstext -R -J -O -K -N -F+f18p,Times-Roman+jCB >> $ps
 #
 surface table_5.11 -R -I0.1 -Graws0.nc
-grdview raws0.nc -R -J -Ba2f1WSne -Cex16.cpt -Qs -O -K -X3.5i >> $ps
+grdview raws0.nc -R -J -B -Cex16.cpt -Qs -O -K -X3.5i >> $ps
 echo "3.25 7 surface (tension = 0)" | pstext -R -J -O -K -N -F+f18p,Times-Roman+jCB >> $ps
 #
 surface table_5.11 -R -I0.1 -Graws5.nc -T0.5
-grdview raws5.nc -R -J -Ba2f1WSne -Cex16.cpt -Qs -O -K -Y-3.75i -X-3.5i >> $ps
+grdview raws5.nc -R -J -B -Cex16.cpt -Qs -O -K -Y-3.75i -X-3.5i >> $ps
 echo "3.25 7 surface (tension = 0.5)" | pstext -R -J -O -K -N -F+f18p,Times-Roman+jCB >> $ps
 #
 triangulate table_5.11 -Grawt.nc -R -I0.1 > /dev/null
 grdfilter rawt.nc -Gfiltered.nc -D0 -Fc1
-grdview filtered.nc -R -J -Ba2f1WSne -Cex16.cpt -Qs -O -K -X3.5i >> $ps
+grdview filtered.nc -R -J -B -Cex16.cpt -Qs -O -K -X3.5i >> $ps
 echo "3.25 7 triangulate @~\256@~ grdfilter" | pstext -R -J -O -K -N -F+f18p,Times-Roman+jCB >> $ps
 echo "3.2125 7.5 Gridding of Data" | pstext -R0/10/0/10 -Jx1i -O -K -N \
 	-F+f32p,Times-Roman+jCB -X-3.5i >> $ps
