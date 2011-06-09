7b3849f69ff92206e88efbb3090e0376be49c5b6
Remko Scharroo <remko.scharroo@eumetsat.int>
2011-06-09 16:09:35 +0000

    - No need to duplicate argument of -p

--- trunk/doc/examples/ex08/job08.bat
+++ trunk/doc/examples/ex08/job08.bat
@@ -1,6 +1,6 @@
 REM		GMT EXAMPLE 08
 REM
-REM		$Id: job08.bat,v 1.15 2011-06-09 04:12:31 guru Exp $
+REM		$Id: job08.bat,v 1.16 2011-06-09 16:09:35 remko Exp $
 REM
 REM Purpose:	Make a 3-D bar plot
 REM GMT progs:	grd2xyz, pstext, psxyz
@@ -9,5 +9,5 @@ REM
 echo GMT EXAMPLE 08
 set ps=..\example_08.ps
 grd2xyz guinea_bay.nc | psxyz -B1/1/1000:"Topography (m)"::.ETOPO5:WSneZ+ -R-0.1/5.1/-0.1/5.1/-5000/0 -JM5i -JZ6i -p200/30 -So0.0833333ub-5000 -P -U"Example 8 in Cookbook" -Wthinnest -Glightgreen -K > %ps%
-echo 0.1 4.9 This is the surface of cube | pstext -R -J -JZ -Z0 -F+f24p,Helvetica-Bold+jTL -p200/30 -O >> %ps%
+echo 0.1 4.9 This is the surface of cube | pstext -R -J -JZ -Z0 -F+f24p,Helvetica-Bold+jTL -p -O >> %ps%
 del .gmt*
