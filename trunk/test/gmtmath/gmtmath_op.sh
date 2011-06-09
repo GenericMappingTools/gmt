5aacb8711c94a2246f605557bd364eb5c3c070b6
Remko Scharroo <remko.scharroo@eumetsat.int>
2011-06-09 18:02:11 +0000

    Updates because of DPI

--- trunk/test/gmtmath/gmtmath_op.sh
+++ trunk/test/gmtmath/gmtmath_op.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-#	$Id: gmtmath_op.sh,v 1.1 2011-06-05 18:36:35 jluis Exp $
+#	$Id: gmtmath_op.sh,v 1.2 2011-06-09 18:02:10 remko Exp $
 # Testing gmtmath for the DIFF, MEAN & -S Ops
 
 . ../functions.sh
 header "Test gmtmath for DIFF "
 
-echo 1 | awk '{printf "1 10\n2 10\n3 10\n4 10\n"}' | gmtmath STDIN DIFF MEAN -S -o1 = fail 
+awk 'BEGIN{printf "1 10\n2 10\n3 10\n4 10\n"}' | gmtmath STDIN DIFF MEAN -S -o1 = out
+echo 0 > in
+diff out in > fail
 
 passfail gmtmath_op
