cda6f694c34b4c654401bad6bccb5c84f56f7c0c
Joaquim Luis <jmfluis@gmail.com>
2011-06-10 23:17:46 +0000

    Make diff EOL insensitive

--- trunk/test/gmtmath/gmtmath_op.sh
+++ trunk/test/gmtmath/gmtmath_op.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#	$Id: gmtmath_op.sh,v 1.2 2011-06-09 18:02:10 remko Exp $
+#	$Id: gmtmath_op.sh,v 1.3 2011-06-10 23:15:36 jluis Exp $
 # Testing gmtmath for the DIFF, MEAN & -S Ops
 
 . ../functions.sh
@@ -7,6 +7,8 @@ header "Test gmtmath for DIFF "
 
 awk 'BEGIN{printf "1 10\n2 10\n3 10\n4 10\n"}' | gmtmath STDIN DIFF MEAN -S -o1 = out
 echo 0 > in
-diff out in > fail
+diff out in --strip-trailing-cr > fail
 
 passfail gmtmath_op
+
+rm -f in out
