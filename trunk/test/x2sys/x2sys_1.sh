85e0ba343a74e0d8d0834e2aa70b29aee0ec1560
Paul Wessel <pwessel@hawaii.edu>
2011-06-13 05:16:10 +0000

    *** empty log message ***

--- trunk/test/x2sys/x2sys_1.sh
+++ trunk/test/x2sys/x2sys_1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: x2sys_1.sh,v 1.3 2011-06-13 05:01:42 guru Exp $
+# $Id: x2sys_1.sh,v 1.4 2011-06-13 05:16:10 guru Exp $
 #
 # This is original Figure 1 script from
 # Wessel, P. (2010), Tools for analyzing intersecting tracks: the x2sys package,
@@ -15,12 +15,13 @@ R=181/185/0/3
 Re=181/185/0/3.01527387994
 D=grav.16.1.B.img
 makecpt -Crainbow -T-80/80/10 -Z > faa.cpt
-img2grd $D -R$R -T1 -E -Gss_faa.nc -S
+#img2grd $D -R$R -T1 -E -Gss_faa.nc -S
+#Placed ss_faa.nc in CVS to make sure script will run
 grdgradient -Ne0.75 -A65 -fg ss_faa.nc -Gss_faa_int.nc
 
 grdimage ss_faa.nc -Iss_faa_int.nc -JM5.5i -P -K -Cfaa.cpt -X1.75i -Y2.5i > $ps
 psxy -R$R -J data/*.xyg -W0.25p -O -K -B1WSne --MAP_FRAME_WIDTH=3p --FORMAT_GEO_MAP=dddF >> $ps
 psscale -Cfaa.cpt -D2.5i/-0.5i/4.5i/0.15ih -O -E -B20f10/:"mGal": >> $ps
-rm -f faa.cpt ss_faa_int.nc ss_faa.nc
+rm -f faa.cpt ss_faa_int.nc
 
 pscmp
