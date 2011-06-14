d067c291e79071f06df7eb04f8fae2bd6c6c3e4f
Remko Scharroo <remko.scharroo@eumetsat.int>
2011-06-14 02:54:20 +0000

    Some updates

--- trunk/doc/scripts/GMT_Defaults_1c.sh
+++ trunk/doc/scripts/GMT_Defaults_1c.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#	$Id: GMT_Defaults_1c.sh,v 1.15 2011-05-01 18:06:37 remko Exp $
+#	$Id: GMT_Defaults_1c.sh,v 1.16 2011-06-14 02:54:20 remko Exp $
 #
 . ./functions.sh
 gmtset MAP_FRAME_TYPE plain FORMAT_DATE_MAP "o dd" FORMAT_CLOCK_MAP hh FONT_ANNOT_PRIMARY +8p
 psbasemap -R2001-9-11T/2001-9-13T/0.01/100 -JX2.25T/2.25l -Bpa6Hf1hg6h:"x-axis label":/a1g3p:"y-axis label":WSne \
-	-X2i -Bsa1D -P -K -U/-0.75i/-0.85i/"Dazed and Confused" --TIME_LANGUAGE=pt \
-	--FORMAT_TIME_LOGO="2010 Apr 16 15:07:02" > GMT_Defaults_1c.ps
+	-X2i -Bsa1D/ -P -K -U/-0.75i/-0.85i/"Dazed and Confused" --TIME_LANGUAGE=pt \
+	--FORMAT_TIME_LOGO="2011 Jun 13 22:10:02" > GMT_Defaults_1c.ps
 pstext -R0/2.25/0/2.25 -Jx1i -O -K -N -F+f7p,Helvetica-Bold,blue+j << EOF >> GMT_Defaults_1c.ps
 0.6    2.4 RB X_AXIS_LENGTH
 -0.4   2.1 RM MAP_ANNOT_ORTHO
@@ -14,11 +14,11 @@ pstext -R0/2.25/0/2.25 -Jx1i -O -K -N -F+f7p,Helvetica-Bold,blue+j << EOF >> GMT
 -0.9  -0.3 LB MAP_LOGO_POS
 -0.7 -0.45 LB MAP_LOGO
 0.0   -0.6 CB FORMAT_TIME_LOGO
-2    -0.55 LM FONT_LABEL
+2.1   -0.5 LM FONT_LABEL
 2.35   0.9 LB FORMAT_DATE_MAP
 2.35   0.6 LB FORMAT_CLOCK_MAP
 2.35   0.3 LB TIME_LANGUAGE
-2    -0.35 LM FONT_ANNOT_SECONDARY
+2.1   -0.3 LM FONT_ANNOT_SECONDARY
 EOF
 psxy -R -J -O -Svs0.005i/0.04i/0.03i -N -Gblue << EOF >> GMT_Defaults_1c.ps
 0.65 2.4 0.9 2.3
@@ -28,9 +28,9 @@ psxy -R -J -O -Svs0.005i/0.04i/0.03i -N -Gblue << EOF >> GMT_Defaults_1c.ps
 -0.9 -0.3 -0.75 -0.85
 -0.7 -0.47 -0.6 -0.66
 0.0 -0.62 0.2 -0.75
-1.95 -0.55 1.7 -0.55
+2.05 -0.5 1.8 -0.5
 2.3 0.9 0.66 -0.2
 2.3 0.6 1.2 -0.1
 2.3 0.3 1.8 -0.2
-1.95 -0.35 1.7 -0.35
+2.05 -0.3 1.8 -0.3
 EOF
