efb6e2f6c7293d8e3b7a9602122e1fc1bc32dbd3
Paul Wessel <pwessel@hawaii.edu>
2011-06-17 18:55:48 +0000

    Added script to keep track of how often a GMT program is used in various scripts

---
+++ prog_use.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# $Id: prog_use.sh,v 1.1 2011-06-17 18:55:48 guru Exp $
+# Returns a count of how many time a GMT program is used in our test scripts
+# including doc and examples.  Give -l to see which scripts [Default returns the count]
+#
+. gmt_shell_functions.sh
+# Determines in which test and example scripts each GMT program is used
+(cd src; ls *_func.c | awk -F_ '{print $1}' > /tmp/t.lis )
+while read prog; do
+	find . -name '*.sh' -exec grep -H $prog {} \; | sed -e 'sB/share/doc/gmtB/docBg' | awk -F: '{print $1}' | sort -u > /tmp/list
+	n=`gmt_nrecords /tmp/list`
+	if [ $# -gt 0 ]; then
+		echo " "
+		echo "-------------------"
+	fi
+	echo "===> $prog [$n]"
+	if [ $# -gt 0 ]; then
+		echo "-------------------"
+		cat /tmp/list
+	fi
+done < /tmp/t.lis
+
