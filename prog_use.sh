8605581d21f237b5faa4d85c710ddc6c59deceb4
Paul Wessel <pwessel@hawaii.edu>
2011-06-17 18:59:45 +0000

    *** empty log message ***

--- prog_use.sh
+++ prog_use.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# $Id: prog_use.sh,v 1.1 2011-06-17 18:55:48 guru Exp $
+# $Id: prog_use.sh,v 1.2 2011-06-17 18:59:45 guru Exp $
 # Returns a count of how many time a GMT program is used in our test scripts
 # including doc and examples.  Give -l to see which scripts [Default returns the count]
 #
 . gmt_shell_functions.sh
-# Determines in which test and example scripts each GMT program is used
-(cd src; ls *_func.c | awk -F_ '{print $1}' > /tmp/t.lis )
+# Determines in which test and example scripts each GMT module (incl supplement) is used
+find . -name '*_func.c' -print | tr '/' ' ' | awk '{print $NF}' | sed -e s/_func.c//g > /tmp/t.lis
 while read prog; do
 	find . -name '*.sh' -exec grep -H $prog {} \; | sed -e 'sB/share/doc/gmtB/docBg' | awk -F: '{print $1}' | sort -u > /tmp/list
 	n=`gmt_nrecords /tmp/list`
