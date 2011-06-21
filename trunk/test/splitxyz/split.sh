2db79aa2cd2becb7d5ce6ef828632e43020e670e
Remko Scharroo <remko.scharroo@eumetsat.int>
2011-06-21 17:30:42 +0000

    Added pscmp

--- trunk/test/splitxyz/split.sh
+++ trunk/test/splitxyz/split.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#	$Id: split.sh,v 1.1 2011-06-18 15:33:13 jluis Exp $
+#	$Id: split.sh,v 1.2 2011-06-21 17:30:42 remko Exp $
 #
 # Split a polyline into chunks
 
@@ -10,4 +10,4 @@ ps=split.ps
 psxy data.dat -R-15/-12/35/37.5 -JM10c -W5p,gray -B1 -P -K > $ps
 splitxyz data.dat -C45 -A45/15 -gd6k -fg | psxy -R-15/-12/35/38 -JM10c -W1p -O >> $ps
 
-#pscmp
+pscmp
