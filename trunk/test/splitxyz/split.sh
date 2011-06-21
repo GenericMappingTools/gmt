5deaa2c434cd43634ca9f52ec9eeb8ba101d14d2
Joaquim Luis <jmfluis@gmail.com>
2011-06-21 21:55:46 +0000

    *** empty log message ***

--- trunk/test/splitxyz/split.sh
+++ trunk/test/splitxyz/split.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#	$Id: split.sh,v 1.2 2011-06-21 17:30:42 remko Exp $
+#	$Id: split.sh,v 1.3 2011-06-21 21:55:46 jluis Exp $
 #
 # Split a polyline into chunks
 
@@ -8,6 +8,6 @@ header "Test splitxyz for splitting a polyline"
 
 ps=split.ps
 psxy data.dat -R-15/-12/35/37.5 -JM10c -W5p,gray -B1 -P -K > $ps
-splitxyz data.dat -C45 -A45/15 -gd6k -fg | psxy -R-15/-12/35/38 -JM10c -W1p -O >> $ps
+splitxyz data.dat -C45 -A45/15 -fg | psxy -R-15/-12/35/38 -JM10c -W1p -O >> $ps
 
 pscmp
