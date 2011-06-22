ef52d595c5f8e3487528408a3718abf83bea72ac
Paul Wessel <pwessel@hawaii.edu>
2011-06-22 21:29:04 +0000

    *** empty log message ***

--- trunk/doc/scripts/GMT_albers.sh
+++ trunk/doc/scripts/GMT_albers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#	$Id: GMT_albers.sh,v 1.7 2011-05-01 18:06:37 remko Exp $
+#	$Id: GMT_albers.sh,v 1.8 2011-06-22 21:29:04 guru Exp $
 #
 . ./functions.sh
 
 gmtset MAP_GRID_CROSS_SIZE_PRIMARY 0
-pscoast -R110/140/20/35 -JB125/20/25/45/5i -B10g5 -Dl -Ggreen -Wthinnest -A250 -P > GMT_albers.ps
+pscoast -R110/140/20/35 -JB125/20/25/45/5i -Bag -Dl -Ggreen -Wthinnest -A250 -P > GMT_albers.ps
