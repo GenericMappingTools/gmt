0f4832cafe4ff8bcccdf9421aea7a81c0137b2ba
Paul Wessel <pwessel@hawaii.edu>
2011-06-29 06:33:30 +0000

    Fixed porting bug; all sph tests run

--- src/sph/sphdistance_func.c
+++ src/sph/sphdistance_func.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: sphdistance_func.c,v 1.17 2011-06-29 02:55:28 guru Exp $
+ *	$Id: sphdistance_func.c,v 1.18 2011-06-29 06:33:30 guru Exp $
  *
  *	Copyright (c) 2008-2011 by P. Wessel, W. H. F. Smith, R. Scharroo, and J. Luis
  *	See LICENSE.TXT file for copying and redistribution conditions.
@@ -26,10 +26,12 @@
  * Renka, R, J,, 1997, Algorithm 772: STRIPACK: Delaunay Triangulation
  *  and Voronoi Diagram on the Surface of a Sphere, AMC Trans. Math.
  *  Software, 23 (3), 416-434.
- * We translate to C using f2c -r8 and link with -lf2c
+ * We translated to C using f2c -r8 and and manually edited the code
+ * so that f2c libs were not needed.
  *
  * Author:	Paul Wessel
- * Date:	16-FEB-2008
+ * Date:	16-FEB-2011
+ * Version:	5
  *
  */
  
@@ -70,19 +72,20 @@ struct SPHDISTANCE_CTRL {
 
 void prepare_polygon (struct GMT_CTRL *C, struct GMT_LINE_SEGMENT *P)
 {
+	/* Set the min/max extent of this polygon and determine if it
+	 * is a polar cap; if so set the required metadata flags */
 	GMT_LONG i;
 	double lon_sum = 0.0, lat_sum = 0.0, dlon;
 	
-	GMT_get_lon_minmax (C, P->coord[GMT_X], P->n_rows, &(P->min[GMT_X]), &(P->max[GMT_X]));
+	GMT_set_seg_minmax (C, P);	/* Set the domain of the segment */
 	
-	P->min[GMT_Y] = P->max[GMT_Y] = P->coord[GMT_Y][0];
+	/* Then loop over points to accumulate sums */
+	
 	for (i = 1; i < P->n_rows; i++) {	/* Start at i = 1 since (a) 0'th point is repeated at end and (b) we are doing differences */
 		dlon = P->coord[GMT_X][i] - P->coord[GMT_X][i-1];
 		if (fabs (dlon) > 180.0) dlon = copysign (360.0 - fabs (dlon), -dlon);
 		lon_sum += dlon;
 		lat_sum += P->coord[GMT_Y][i];
-		if (P->coord[GMT_Y][i] < P->min[GMT_Y]) P->min[GMT_Y] = P->coord[GMT_Y][i];
-		if (P->coord[GMT_Y][i] > P->max[GMT_Y]) P->max[GMT_Y] = P->coord[GMT_Y][i];
 	}
 	P->pole = 0;
 	if (GMT_360_RANGE (lon_sum, 0.0)) {	/* Contains a pole */
@@ -102,7 +105,7 @@ void *New_sphdistance_Ctrl (struct GMT_CTRL *GMT) {	/* Allocate and initialize a
 	struct SPHDISTANCE_CTRL *C;
 	
 	C = GMT_memory (GMT, NULL, 1, struct SPHDISTANCE_CTRL);
-	C->L.unit = 'E';	/* Default is meter distances */
+	C->L.unit = 'e';	/* Default is meter distances */
 	return ((void *)C);
 }
 
@@ -134,11 +137,10 @@ GMT_LONG GMT_sphdistance_usage (struct GMTAPI_CTRL *C, GMT_LONG level)
 	GMT_message (GMT, "\t<table> is one or more data file (in ASCII, binary, netCDF) with (x,y,z[,w]).\n");
 	GMT_message (GMT, "\t   If no files are given, standard input is read (but see -Q).\n");
 	GMT_message (GMT, "\t-C Conserve memory (Converts lon/lat <--> x/y/z when needed) [store both in memory]. Not used with -Q.\n");
-	GMT_message (GMT, "\t-D Skip repeated input vertex at the end of a closed segment\n");
-	GMT_message (GMT, "\t-E Assign to grid nodes the Voronoi polygon ID [Calculate distances]\n");
-	GMT_message (GMT, "\t-L Specify distance unit as (f)eet, m(e)ter, (k)m, (M)ile, (n)autical mile, or (d)egree.\n");
-	GMT_message (GMT, "\t   Calculations uses spherical approximations.  Default unit is meters.\n");
-	GMT_message (GMT, "\t   Set PROJ_ELLIPSOID to WGS-84 to get geodesic distances.\n");
+	GMT_message (GMT, "\t-D Skip repeated input vertex at the end of a closed segment.\n");
+	GMT_message (GMT, "\t-E Assign to grid nodes the Voronoi polygon ID [Calculate distances].\n");
+	GMT_message (GMT, "\t-L Set distance unit arc (d)egree, m(e)ter, (f)eet, (k)m, arc (m)inute, (M)ile, (n)autical mile, or arc (s)econd [e].\n");
+	GMT_message (GMT, "\t   PROJ_ELLIPSOID determines if geodesic or gerat-circle distances are used.\n");
 	GMT_message (GMT, "\t-N Specify node filename for the Voronoi polygons (sphtriangulate -N output)\n");
 	GMT_message (GMT, "\t-Q Specify table with Voronoi polygons in sphtriangulate -Qv format\n");
 	GMT_message (GMT, "\t   [Default performs Voronoi construction on input data first]\n");
@@ -191,8 +193,8 @@ GMT_LONG GMT_sphdistance_parse (struct GMTAPI_CTRL *C, struct SPHDISTANCE_CTRL *
 				break;
 			case 'L':
 				Ctrl->L.active = TRUE;
-				if (!(opt->arg && strchr ("defkMn", opt->arg[0]))) {
-					GMT_report (GMT, GMT_MSG_FATAL, "Syntax error: Expected -Ld|e|f|k|M|n]\n");
+				if (!(opt->arg && strchr (GMT_LEN_UNITS, opt->arg[0]))) {
+					GMT_report (GMT, GMT_MSG_FATAL, "Syntax error: Expected -L%s\n", GMT_LEN_UNITS_DISPLAY);
 					n_errors++;
 				}
 				else
@@ -279,7 +281,7 @@ GMT_LONG GMT_sphdistance (struct GMTAPI_CTRL *API, struct GMT_OPTION *options)
 	if (Ctrl->Q.active) {	/* Expect a single file with Voronoi polygons */
 		if ((error = GMT_Begin_IO (API, GMT_IS_DATASET, GMT_IN, GMT_BY_SET))) Return (error);				/* Enables data input and sets access mode */
 		GMT_report (GMT, GMT_MSG_NORMAL, "Read Volonoi polygons from %s ...", Ctrl->Q.file);
-		if (GMT_Get_Data (API, GMT_IS_DATASET, GMT_IS_FILE, GMT_IS_POINT, NULL, 0, (void **)&Ctrl->Q.file, (void **)&Qin)) Return ((error = GMT_DATA_READ_ERROR));
+		if (GMT_Get_Data (API, GMT_IS_DATASET, GMT_IS_FILE, GMT_IS_POLY, NULL, 0, (void **)&Ctrl->Q.file, (void **)&Qin)) Return ((error = GMT_DATA_READ_ERROR));
 		Table = Qin->table[0];	/* Only one table in a file */
 		GMT_report (GMT, GMT_MSG_NORMAL, "Found %ld segments\n", Table->n_segments);
 	 	lon = GMT_memory (GMT, NULL, Table->n_segments, double);
@@ -306,7 +308,7 @@ GMT_LONG GMT_sphdistance (struct GMTAPI_CTRL *API, struct GMT_OPTION *options)
 			GMT_Destroy_Data (API, GMT_ALLOCATED, (void **)&Nin);
 			GMT_report (GMT, GMT_MSG_NORMAL, "Found %ld records\n", NTable->n_records);
 		}
-		else {
+		else {	/* Get them from the segment header */
 			for (node = 0; node < Table->n_segments; node++) {
 				sscanf (Table->segment[node]->header, "%*s %*d %lf %lf", &lon[node], &lat[node]);
 			}
@@ -315,7 +317,7 @@ GMT_LONG GMT_sphdistance (struct GMTAPI_CTRL *API, struct GMT_OPTION *options)
 	else {	/* Must process input point/line data */
 		if ((error = GMT_set_cols (GMT, GMT_IN, 2))) Return (error);
 		if ((error = GMT_Init_IO (API, GMT_IS_DATASET, GMT_IS_POINT, GMT_IN, GMT_REG_DEFAULT, options))) Return (error);	/* Registers default input sources, unless already set */
-		if ((error = GMT_Begin_IO (API, GMT_IS_DATASET, GMT_IN, GMT_BY_REC))) Return (error);				/* Enables data input and sets access mode */
+		if ((error = GMT_Begin_IO (API, GMT_IS_DATASET, GMT_IN, GMT_BY_REC))) Return (error);	/* Enables data input and sets access mode */
 
 		if (!Ctrl->C.active) (void)GMT_malloc2 (GMT, lon, lat, 0, 0, double);
 		n_alloc = GMT_malloc3 (GMT, xx, yy, zz, 0, 0, double);
@@ -339,7 +341,7 @@ GMT_LONG GMT_sphdistance (struct GMTAPI_CTRL *API, struct GMT_OPTION *options)
 			}
 			/* Convert lon,lat in degrees to Cartesian x,y,z triplets */
 			GMT_geo_to_cart (GMT, in[GMT_Y], in[GMT_X], X, TRUE);
-			xx[n] = X[0];	yy[n] = X[1];	zz[n] = X[2];
+			xx[n] = X[GMT_X];	yy[n] = X[GMT_Y];	zz[n] = X[GMT_Z];
 			if (!Ctrl->C.active) {
 				lon[n] = in[GMT_X];
 				lat[n] = in[GMT_Y];
@@ -362,7 +364,7 @@ GMT_LONG GMT_sphdistance (struct GMTAPI_CTRL *API, struct GMT_OPTION *options)
 		GMT_memset (&T, 1, struct STRIPACK);
 		T.mode = VORONOI;
 		stripack_lists (GMT, n, xx, yy, zz, &T);	/* Do the basic triangulation */
-		GMT_free (GMT, T.D.tri);
+		GMT_free (GMT, T.D.tri);	/* Don't need the triangulation */
 		if (Ctrl->C.active) {	/* Recompute lon,lat and set pointers */
 			cart_to_geo (GMT, n, xx, yy, zz, xx, yy);	/* Revert to lon, lat */
 			lon = xx;
@@ -370,7 +372,7 @@ GMT_LONG GMT_sphdistance (struct GMTAPI_CTRL *API, struct GMT_OPTION *options)
 		}
 		GMT_free (GMT,  zz);
 	}
-	if ((error = GMT_End_IO (API, GMT_IN, 0))) Return (error);				/* Disables further data input */
+	if ((error = GMT_End_IO (API, GMT_IN, 0))) Return (error);	/* Disables further data input */
 	
 	/* OK, time to work on the distance grid */
 	
@@ -391,8 +393,8 @@ GMT_LONG GMT_sphdistance (struct GMTAPI_CTRL *API, struct GMT_OPTION *options)
 	else {	/* Need a single polygon structure that we reuse for each polygon */
 		P = GMT_memory (GMT, NULL, 1, struct GMT_LINE_SEGMENT);	/* Needed as pointer below */
 		P->coord = GMT_memory (GMT, NULL, 2, double *);	/* Needed as pointers below */
-		P->min = GMT_memory (GMT, NULL, 2, double);		/* Needed to hold min lon/lat */
-		P->max = GMT_memory (GMT, NULL, 2, double);		/* Needed to hold max lon/lat */
+		P->min = GMT_memory (GMT, NULL, 2, double);	/* Needed to hold min lon/lat */
+		P->max = GMT_memory (GMT, NULL, 2, double);	/* Needed to hold max lon/lat */
 
 		p_alloc = GMT_malloc2 (GMT, P->coord[GMT_X], P->coord[GMT_Y], GMT_TINY_CHUNK, 0, double);
 	
@@ -480,7 +482,7 @@ GMT_LONG GMT_sphdistance (struct GMTAPI_CTRL *API, struct GMT_OPTION *options)
 	
 	if ((error = GMT_Begin_IO (API, GMT_IS_GRID, GMT_OUT, GMT_BY_SET))) Return (error);	/* Enables data output and sets access mode */
 	if (GMT_Put_Data (API, GMT_IS_GRID, GMT_IS_FILE, GMT_IS_SURFACE, NULL, GMT_GRID_ALL, (void **)&Ctrl->G.file, (void *)Grid)) Return (GMT_DATA_WRITE_ERROR);
-	if ((error = GMT_End_IO (API, GMT_OUT, 0))) Return (error);				/* Disables further data output */
+	if ((error = GMT_End_IO (API, GMT_OUT, 0))) Return (error);	/* Disables further data output */
 
 	GMT_report (GMT, GMT_MSG_NORMAL, "Gridding completed, %ld nodes visited (at least once)\n", n_set);
 	
