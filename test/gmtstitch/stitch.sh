0615d47461acabe536c18244208398e058ddc363
Joaquim Luis <jmfluis@gmail.com>
2011-07-03 18:55:59 +0000

    Reduce number of pieces to avoid mysterious bug in Msys bash

--- test/gmtstitch/stitch.sh
+++ test/gmtstitch/stitch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#	$Id: stitch.sh,v 1.4 2011-06-30 02:20:01 guru Exp $
+#	$Id: stitch.sh,v 1.5 2011-07-03 18:55:45 jluis Exp $
 #
 # Basic stitching of Cartesian and geographic data
 
@@ -10,7 +10,7 @@ ps=stitch.ps
 
 # Make a polygon file
 gmtmath -T0/360/5 T -C0 COSD -C1 SIND -Ca T 5 MUL COSD 3 ADD MUL = t.txt
-split -l 10 t.txt piece
+split -l 12 t.txt piece
 psxy t.txt -R-5/5/-5/5 -JX3i -P -W1p -Gorange -B2g1WSne -Y5i -K > $ps
 psxy piece?? -R -J -O -W1p -B2g1WSne -X3.5i -K >> $ps
 gmtstitch piece?? -T0.5 | psxy -R -J -O -W1p -Gorange -B2g1WSne -X-3.5i -Y-3.5i -K >> $ps
