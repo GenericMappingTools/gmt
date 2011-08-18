063e10d1e204c51554ae4e532151f5217fa0ded9
Joaquim Luis <jmfluis@gmail.com>
2011-08-18 20:01:16 +0000

    Format output of x2sys_binlist so that the test doesn't fail due to minute differences in distance calculation.

--- trunk/test/x2sys/x2sys_5.sh
+++ trunk/test/x2sys/x2sys_5.sh
@@ -10,7 +10,7 @@ OLDX=$X2SYS_HOME
 export X2SYS_HOME=`pwd`
 x2sys_init TEST -D../../share/x2sys/mgd77 -Emgd77 -F -Gd -Rd -I15m
 echo "$X2SYS_HOME" >> $X2SYS_HOME/TEST/TEST_paths.txt
-x2sys_binlist -TTEST 01010221.mgd77 -D > bin.txt
+x2sys_binlist -TTEST 01010221.mgd77 -D --FORMAT_FLOAT_OUT=%.3f > bin.txt
 rm -rf TEST
 diff -q --strip-trailing-cr bin.txt orig/bin.txt >> fail
 passfail x2sys_5
