0e3d9605221e1e9ae2fe037ad7e5d5fb8be92593
Florian Wobbe <florian@eprofs.de>
2011-09-26 04:14:12 +0000

    ConfigUserTemplate.cmake more verbose now

--- branches/cmake-build/cmake/ConfigUserTemplate.cmake
+++ branches/cmake-build/cmake/ConfigUserTemplate.cmake
@@ -7,32 +7,41 @@
 # $LastChangedDate$
 #
 
-# Installation path:
+#
+# Note: CMake considers an empty string, "FALSE", "OFF", "NO", or any string ending
+# in "-NOTFOUND" to be false. (This happens to be case-insensitive, so "False",
+# "off", "no", and "something-NotFound" are all false.) Other values are true. Thus
+# it matters not whether you use TRUE and FALSE, ON and OFF, or YES and NO for your
+# booleans.
+#
+
+# Installation path [auto]:
 #set(CMAKE_INSTALL_PREFIX "prefix_path")
 
-# Set build type can be: empty, Debug, Release, relwithdebinfo or minsizerel (default is Release)
+# Set build type can be: empty, Debug, Release, relwithdebinfo or minsizerel [Release]:
 #set(CMAKE_BUILD_TYPE Debug)
 
-# set location of netcdf (can be root directory, path to header file or path to nc-config):
+# Set location of NetCDF (can be root directory, path to header file or path to nc-config) [auto]:
 #set(NETCDF_DIR "netcdf_install_prefix")
 
-# set location of gdal (can be root directory, path to header file or path to gdal-config):
-# set(GDAL_DIR "gdal_install_prefix")
+# Set location of GDAL (can be root directory, path to header file or path to gdal-config) [auto]:
+#set(GDAL_DIR "gdal_install_prefix")
 
-# Enable compatibility mode:
-#set(GMT_COMPAT "yes")
+# Enable compatibility mode [FALSE]:
+#set(GMT_COMPAT yes)
 
-# Enable file locking:
-#set(FLOCK "enabled")
+# Enable file locking [FALSE]:
+#set(FLOCK yes)
 
-# Use fast, non-GPL triangulation routine by J. Shewchuk:
-#set(TRIANGLE_D "yes")
+# Use fast, non-GPL triangulation routine by J. Shewchuk [FALSE]:
+#set(TRIANGLE_D yes)
 
 # Extra debugging for developers:
 #add_definitions(-DDEBUG)
+#set(CMAKE_C_FLAGS_DEBUG -ggdb3)
+#set(CMAKE_C_FLAGS_RELEASE -ggdb3)
 
 # Extra compiler Flags:
-#set(CMAKE_C_FLAGS_DEBUG -ggdb3)
 #add_definitions(
 #  -Wall
 #  -Wdeclaration-after-statement
