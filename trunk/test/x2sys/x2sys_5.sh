d60f84c82b276123bd731acf7e2215dd58f45f10
Remko Scharroo <remko.scharroo@eumetsat.int>
2011-09-27 13:48:01 +0000

    - Changed to 2 decimals

--- trunk/test/x2sys/x2sys_5.sh
+++ trunk/test/x2sys/x2sys_5.sh
@@ -10,12 +10,12 @@ OLDX=$X2SYS_HOME
 export X2SYS_HOME=`pwd`
 x2sys_init TEST -D../../share/x2sys/mgd77 -Emgd77 -F -Gd -Rd -I15m
 echo "$X2SYS_HOME" >> $X2SYS_HOME/TEST/TEST_paths.txt
-x2sys_binlist -TTEST 01010221.mgd77 -D --FORMAT_FLOAT_OUT=%.3f > bin.txt
+x2sys_binlist -TTEST 01010221.mgd77 -D --FORMAT_FLOAT_OUT=%.2f > x2sys_5.txt
 rm -rf TEST
-diff -q --strip-trailing-cr bin.txt orig/bin.txt > fail
+diff --strip-trailing-cr x2sys_5.txt orig/x2sys_5.txt > fail
 passfail x2sys_5
 
 if [ ! "X$OLDX" = "X" ]; then	# Reset prior setting
 	export X2SYS_HOME=$OLDX
 fi
-rm -f 01010221.mgd77 bin.txt
+rm -f 01010221.mgd77 x2sys_5.txt
