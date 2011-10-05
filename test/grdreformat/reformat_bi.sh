d8e124f6f99ceed1eab7eee581d06ce54d0da8f7
Joaquim Luis <jmfluis@gmail.com>
2011-10-05 19:13:41 +0000

    Fix error of bad GMT usage

--- test/grdreformat/reformat_bi.sh
+++ test/grdreformat/reformat_bi.sh
@@ -12,16 +12,16 @@ grdmath -R-10/10/-10/10 -I1 X = lixo.nc
 
 # First conver to int
 grdreformat lixo.nc lixo.bi=bi
-grdmath lixo.nc lixo.bi SUB = lixo_dif.nc
+grdmath lixo.nc lixo.bi=bi SUB = lixo_dif.nc
 grd2xyz lixo_dif.nc -ZTLa > $log
 
 # Now convert back to .nc
-grdreformat lixo.bi lixo.nc
-grdmath lixo.nc lixo.bi SUB = lixo_dif.nc
+grdreformat lixo.bi=bi lixo.nc
+grdmath lixo.nc lixo.bi=bi SUB = lixo_dif.nc
 grd2xyz lixo_dif.nc -ZTLa >> $log
 
 res=`minmax -C $log`
-echo ${res[0]} ${res[1]} | awk '{if(($1 != 0) || ($2 != 0)) print $0}' $log > fail
+echo ${res[0]} ${res[1]} | awk '{if($1 != 0 || $2 != 0) print 1}' > fail
 
 rm -f lixo*
 
