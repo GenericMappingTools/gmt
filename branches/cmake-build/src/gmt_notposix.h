ce293a6f0628aa2182058a83ce0151305e55a627
Florian Wobbe <florian@eprofs.de>
2011-11-02 00:08:59 +0000

    Silence MSVC warnings.

--- branches/cmake-build/src/gmt_notposix.h
+++ branches/cmake-build/src/gmt_notposix.h
@@ -315,7 +315,7 @@
 
 	/* Suppress Visual Studio deprecation warnings */
 #	ifdef _MSC_VER
-		/* #		pragma warning( disable : 4996 ) */
+#		pragma warning( disable : 4996 )
 #	endif
 
 #endif /* defined _WIN32 || defined WIN32 */
