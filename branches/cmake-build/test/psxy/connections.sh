57253e73a8eb6070733e4da776098423c81965d4
Florian Wobbe <florian@eprofs.de>
2011-11-27 23:28:14 +0000

    Make CTest Windows compatibile.

--- branches/cmake-build/test/psxy/connections.sh
+++ branches/cmake-build/test/psxy/connections.sh
@@ -10,9 +10,9 @@ ps=connections.ps
 
 pscoast -R-71.6/-57.4/37.4/46.2 -JM20c -Dh -Glightgreen -Wdarkgreen -K > $ps
 echo ; echo -n "- test with >  "
-cat connections.dat | time psxy -A -R -J -O -K -Wred >> $ps 
+time cat connections.dat | psxy -A -R -J -O -K -Wred >> $ps
 echo -n "- test with NaN"
-sed 's:>:NaN NaN:' connections.dat | time psxy -A -R -J -O -K -Wblue >> $ps 
+time sed 's:>:NaN NaN:' connections.dat | psxy -A -R -J -O -K -Wblue >> $ps
 psbasemap -R -J -O -Ba2f1g2 >> $ps
 
 printf "%-72s" "- PostScript result"
