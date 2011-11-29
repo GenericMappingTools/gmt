850578ccaf0b4c4fec690709ccd88859eb4f8bbb
Florian Wobbe <florian@eprofs.de>
2011-11-29 21:23:03 +0000

    Fixed bug gshhs_version.

--- src/gshhs_version.c
+++ src/gshhs_version.c
@@ -8,11 +8,20 @@
 #include <string.h>
 #include <netcdf.h>
 
+#define BUF_SIZE 64
+#define VERSION_ATT_NAME "version"
+
 int main (int argc, char *argv[])
 {
-	int  status;               /* error status */
-	int  ncid;                 /* netCDF ID */
-	char version[16];          /* GSHHS version */
+	int    status;               /* error status */
+	int    ncid;                 /* netCDF ID */
+	size_t v_len;                /* version length */
+	char   version[BUF_SIZE];    /* GSHHS version */
+
+	if (argc < 2) {
+		fprintf(stderr, "usage: gshhs_version file\n");
+		exit (EXIT_FAILURE);
+	}
 
 	/* open shoreline file */
 	status = nc_open(argv[1], NC_NOWRITE, &ncid);
@@ -21,8 +30,19 @@ int main (int argc, char *argv[])
 		exit (status);
 	}
 
+	/* get length of version string */
+	status = nc_inq_attlen (ncid, NC_GLOBAL, VERSION_ATT_NAME, &v_len);
+	if (status != NC_NOERR) {
+		fprintf(stderr, "cannot inquire version attribute length from file \"%s\".\n", argv[1]);
+		exit (status);
+	}
+	if (v_len == 0 || v_len > BUF_SIZE) {
+		fprintf(stderr, "invalid version attribute length: %d\n", v_len);
+		exit (EXIT_FAILURE);
+	}
+
 	/* get version string */
-	nc_get_att_text (ncid, NC_GLOBAL, "version", version);
+	nc_get_att_text (ncid, NC_GLOBAL, VERSION_ATT_NAME, version);
 	if (status != NC_NOERR || strlen(version) == 0 ) {
 		fprintf(stderr, "cannot read version attribute from file \"%s\".\n", argv[1]);
 		if (status == NC_NOERR)
@@ -30,7 +50,11 @@ int main (int argc, char *argv[])
 		exit (status);
 	}
 
+	/* null-terminate version string */
+	version[v_len] = '\0';
+
 	/* return version string */
 	fprintf(stdout, "%s\n", version);
+
 	return(0);
 }
