123aafb6e48a74e1ee80738391126c89cf50f82a
Florian Wobbe <florian@eprofs.de>
2012-01-13 11:06:24 +0000

    Make psdemo a test and remove the example files instead.

--- branches/cmake-build/src/psldemo.h.cmake
+++ branches/cmake-build/src/psldemo.h.cmake
@@ -1,4 +1,4 @@
 /* $Id$ */
 
-#define PSL_IMAGE   "@GMT_BINARY_DIR@/doc/examples/pslib/vader.ras"
-#define PSL_PATTERN "@GMT_BINARY_DIR@/doc/examples/ex16/circuit.ras"
+#define PSL_IMAGE   "@GMT_BINARY_DIR@/test/pslib/vader.ras"
+#define PSL_PATTERN "@GMT_BINARY_DIR@/test/pstext/circuit.ras"
