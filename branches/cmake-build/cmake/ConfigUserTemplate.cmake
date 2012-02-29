7c18603047155645b6c33329995e0d87080d5ed9
Florian Wobbe <florian@eprofs.de>
2012-02-29 21:45:21 +0000

    Monolithic install per default

--- branches/cmake-build/cmake/ConfigUserTemplate.cmake
+++ branches/cmake-build/cmake/ConfigUserTemplate.cmake
@@ -31,8 +31,8 @@
 #set (CMAKE_INSTALL_PREFIX "prefix_path")
 
 # Install monolithic tree instead of distribution type layout (doc and share
-# separated) [off]:
-#set (GMT_INSTALL_MONOLITHIC ON)
+# separated) [on]:
+#set (GMT_INSTALL_MONOLITHIC OFF)
 
 # Set share installation path [${CMAKE_INSTALL_PREFIX}/share/gmt-<version>]:
 #set (GMT_SHARE_PATH "share_path")
