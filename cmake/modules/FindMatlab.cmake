12475b8c1f55fc2db42c024a9efb4bab3efc5f4c
Florian Wobbe <florian@eprofs.de>
2012-03-13 09:09:14 +0000

    Undo changes in r9848; make build system CMake 2.8.5 compatible.

--- cmake/modules/FindMatlab.cmake
+++ cmake/modules/FindMatlab.cmake
@@ -18,7 +18,7 @@
 #
 # This software is distributed WITHOUT ANY WARRANTY; without even the
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
+# See COPYING-CMAKE-SCRIPTS for more information.
 #=============================================================================
 
 if (CMAKE_SIZEOF_VOID_P EQUAL 4)
