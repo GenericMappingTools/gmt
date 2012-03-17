b1c1c1468f61e506f08052ce1d5cbb1edb7a3f9e
Remko Scharroo <remko.scharroo@eumetsat.int>
2012-03-17 17:32:05 +0000

    Work better with "fail" files

--- trunk/test/gmtspatial/measure.sh
+++ trunk/test/gmtspatial/measure.sh
@@ -9,11 +9,10 @@ cat << EOF > area.txt
 0	1
 0	0
 EOF
-rm -f fail
 # Cartesian centroid and area
 echo "0.5	0.5	1" > answer
 gmtspatial -Q area.txt > result
-diff -q --strip-trailing-cr answer result >> fail
+diff -q --strip-trailing-cr answer result > fail
 # Geographic centroid and area
 echo "0.5	0.500019038226	12308.3096995" > answer
 gmtspatial -Q area.txt  -fg > result
