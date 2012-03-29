ab0fed5483d966814abc2c0a7e7f100e7dcde771
Florian Wobbe <florian@eprofs.de>
2012-03-29 12:52:43 +0000

    Move Matlab config to advanced section

--- cmake/ConfigUserTemplate.cmake
+++ cmake/ConfigUserTemplate.cmake
@@ -70,12 +70,6 @@
 # Enable file locking [FALSE]:
 #set (FLOCK TRUE)
 
-# Enable Matlab API [FALSE]:
-#set (GMT_MATLAB TRUE)
-# If Matlab is not found, point MATLAB_ROOT to its installation path, e.g.:
-#set (MATLAB_ROOT /Applications/MATLAB_R2010a.app)  # MacOSX
-#set (MATLAB_ROOT /opt/matlab-7sp1)                 # Linux
-
 ## Advanced configuration begins here. Usually it is not necessary to edit any
 ## settings below. You should know what you are doing if you do though.
 
@@ -111,6 +105,12 @@
 ##
 ## Section 4: Advanced tweaking
 ##
+
+# Enable Matlab API [FALSE]:
+#set (GMT_MATLAB TRUE)
+# If Matlab is not found, point MATLAB_ROOT to its installation path, e.g.:
+#set (MATLAB_ROOT /Applications/MATLAB_R2010a.app)  # MacOSX
+#set (MATLAB_ROOT /opt/matlab-7sp1)                 # Linux
 
 # Enable running examples/tests with "make check" (out-of-source)
 # Need to set either DO_EXAMPLES, DO_TESTS or both and uncomment
