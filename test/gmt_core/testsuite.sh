467a59712331a61912724c29f2fd8332fb2795ba
Florian Wobbe <florian@eprofs.de>
2012-05-23 14:32:30 +0000

    MinGW test compatibility.

---
+++ test/gmt_core/testsuite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# $Id$
+
+# test if testsuite is up to date and setup correctly
+
+# compare actual dirs with expected values
+GMT_PATH=$(fix_mingw_path "${GMT_BINARY_DIR}/src/gmt")
+echo -n "gmt location: "
+which gmt           | grep -E "^${GMT_PATH}(|[.]exe)$"
+echo -n "sharedir    : "
+gmt --show-sharedir | grep "^${GMT_SHAREDIR}$"
+echo -n "userdir     : "
+gmtget DIR_USER     | grep "^${GMT_USERDIR}$"
+echo -n "gshhg dir   : "
+gmtget DIR_GSHHG    | grep "^${GSHHG_DIR}$"
+
+# comapre actual version with expected version
+echo -n "gmt version : "
+gmt --version       | grep "${GMT_VERSION}$"
