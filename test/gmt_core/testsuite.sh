71958450313b822919f6a76644737796613ec2db
Florian Wobbe <florian@eprofs.de>
2012-06-01 23:17:48 +0000

    testsuite.sh also from Xcode and Visual Studio.

--- test/gmt_core/testsuite.sh
+++ test/gmt_core/testsuite.sh
@@ -5,7 +5,7 @@
 # test if testsuite is up to date and setup correctly
 
 # compare actual dirs with expected values
-GMT_PATH=$(fix_mingw_path "${GMT_BINARY_DIR}/src/gmt")
+GMT_PATH=$(fix_mingw_path "${GMT_BINARY_DIR}/src/${CMAKE_CONFIG_TYPE:-.}/gmt")
 echo -n "gmt location: "
 which gmt           | grep -E "^${GMT_PATH}(|[.]exe)$"
 echo -n "sharedir    : "
