7071314b117f9a6550f6bcc0ce1646901cd78e18
Remko Scharroo <remko.scharroo@eumetsat.int>
2012-06-28 22:14:48 +0000

    GPL -> LGPL Resolves issue #45

--- trunk/cmake/modules/CreateDebugSym.cmake
+++ trunk/cmake/modules/CreateDebugSym.cmake
@@ -6,17 +6,17 @@
 #
 #  TARGETS - list of targets
 #
-# Copyright (c) 1991-2011 by P. Wessel, W. H. F. Smith, R. Scharroo, J. Luis and F. Wobbe
+# Copyright (c) 1991-2012 by P. Wessel, W. H. F. Smith, R. Scharroo, J. Luis and F. Wobbe
 # See LICENSE.TXT file for copying and redistribution conditions.
 #
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 or any later version.
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3 or any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
 # Contact info: gmt.soest.hawaii.edu
 #-------------------------------------------------------------------------------
