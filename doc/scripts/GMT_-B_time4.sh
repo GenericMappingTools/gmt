944d56d982edd2661fbf41854777a010d6b1b98c
Paul Wessel <pwessel@hawaii.edu>
2012-07-09 01:49:35 +0000

    Update GMT_linear_cal PS and set TIME_UNIT=d in GMT_-B_time4

--- doc/scripts/GMT_-B_time4.sh
+++ doc/scripts/GMT_-B_time4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #	$Id$
 #
-gmtset FORMAT_CLOCK_MAP=-hham FONT_ANNOT_PRIMARY +9p
+gmtset FORMAT_CLOCK_MAP=-hham FONT_ANNOT_PRIMARY +9p TIME_UNIT d
 psbasemap -R0.2t/0.35t/0/1 -JX-5i/0.2i -Bpa15mf5m -Bsa1HS -P > GMT_-B_time4.ps
