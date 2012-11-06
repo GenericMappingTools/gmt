543a96746bdc1a3208599459097b212af650b3c1
Paul Wessel <pwessel@hawaii.edu>
2012-11-06 16:18:40 +0000

    Update GMT5 website

--- sandbox/registration/GMT_usage_map.sh
+++ sandbox/registration/GMT_usage_map.sh
@@ -48,7 +48,7 @@ GMT4=/Volumes/MacNutHD3/UH/RESEARCH/CVSPROJECTS/GMTdev/GMT4
 
 if [ "X$GMTHOME" = "X" ]; then	# Running crontab and environment is not set
 	. /sw/bin/init.sh
-	GS_LIB=/sw/share/ghostscript/9.02/lib
+	GS_LIB=/sw/share/ghostscript/9.05/lib
 	GMTHOME4=/Volumes/MacNutHD3/UH/RESEARCH/CVSPROJECTS/GMTdev/gmt4
 	GMTHOME5=/Volumes/MacNutHD3/UH/RESEARCH/CVSPROJECTS/GMTdev/gmt5
 	PATH=$GMTHOME5/trunk/build/gmt5/bin:$PATH
