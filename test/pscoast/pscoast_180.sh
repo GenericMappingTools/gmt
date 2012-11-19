0d9514da267b210d1f830f5f1e76b289f461cb3e
Paul Wessel <pwessel@hawaii.edu>
2012-11-19 18:51:14 +0000

    added new test script to help fix issue #175

---
+++ test/pscoast/pscoast_180.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#	$Id$
+#
+
+ps=pscoast_180.ps
+
+pscoast -R-10/180/20/50 -JM6i -Ba20g20/a20g20+10 -Dc -G221/204/170 -S153/221/255 -A0/0/1 -P -Xc -K > $ps
+pscoast -R0/180/20/50 -JM6i -Ba20g20/a20g20+10 -Dc -G221/204/170 -S153/221/255 -A0/0/1 -O -Y2.5i >> $ps
