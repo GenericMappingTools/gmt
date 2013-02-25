f27756480a4e9fa0f09e0a680a72a5deefe684d0
Joaquim Luis <jmfluis@gmail.com>
2013-02-25 02:31:12 +0000

    Update test extended grdclip's -Si option

--- test/grdclip/limits.sh
+++ test/grdclip/limits.sh
@@ -10,10 +10,12 @@ Rg=-20/20/-20/20
 grdmath -R$Rg -I1 0 0 CDIST 0.1 MUL KEI 0 0 CDIST 0.1 MUL SQRT ADD DUP UPPER DIV 10 MUL = tmp.nc
 makecpt -Crainbow -T-6/10/1 > t.cpt
 # Draw all nodes as open circles
-grd2xyz tmp.nc | psxy -R$Rp -JX4.5i -Sc0.25c -W0.25p -Ct.cpt -P -K -B10f5WSne -Xc -Y0.5i > $ps
-psscale -D5i/4.75i/6i/0.15i -O -K -Ct.cpt -E+n >> $ps
+grd2xyz tmp.nc | psxy -R$Rp -JX3.25i -Sc0.15c -W0.25p -Ct.cpt -P -K -B10f5WSne -Xc -Y0.5i > $ps
+psscale -D4i/4.75i/6i/0.15i -O -K -Ct.cpt -E+n >> $ps
 # Set values higher than 7 to NaN and values lower than 0 to -6
 grdclip tmp.nc -Sa7/NaN -Sb0/-6 -Gout.nc
-grd2xyz out.nc | psxy -R -J -Sc0.25c -W0.25p -Ct.cpt -O -K -B10f5WSne -Y5i >> $ps
-psxy -R$Rp -J -O -T >> $ps
-
+grd2xyz out.nc | psxy -R$Rp -J -Sc0.15c -W0.25p -Ct.cpt -O -K -B10f5Wsne -Y3.5i >> $ps
+# Set values between 0 and 5 to NaN
+grdclip tmp.nc -Si0/5/NaN -Gout.nc
+grd2xyz out.nc | psxy -R$Rp -J -Sc0.15c -W0.25p -Ct.cpt -O -K -B10f5Wsne -Y3.5i >> $ps
+psxy -R$Rp -J -T -O >> $ps
