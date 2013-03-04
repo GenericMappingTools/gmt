dec5ddc808bcd06aa7c7b293f08f46e0a6f4180f
Joaquim Luis <jmfluis@gmail.com>
2013-03-04 14:10:09 +0000

    Add theoretical admittance curve

--- test/potential/admittance.sh
+++ test/potential/admittance.sh
@@ -11,28 +11,36 @@ grdseamount -R0/512/0/512 -I4 -r -Gz.nc -Z-5000 << EOF
 290 205 50 4000
 210 280 60 2500
 EOF
+
 # 2. LL Map: Bathymetry load + flexural contours
 makecpt -Crainbow -T-5000/-1000/500 -Z > z.cpt
 grdimage z.nc -Cz.cpt -JX3i -P -BaWSne -K -Y0.75i > $ps
 makecpt -Crainbow -T-50/250/10 -Z > g.cpt
+
 # Compute flexure and overlay on bathymetry
 gravfft z.nc+uk -T12000/2800/3300/1000 -Q -Z12000 -N1024/1024+w -L -Gmoho_flex.nc
 grdcontour moho_flex.nc -J -O -K -C100 -A500 >> $ps
+
 # 3. LR Map: Gravity from flexure of Moho only
 gravfft moho_flex.nc+uk -Ff -E$order -D500 -Gfaa_flex.nc
 grdimage faa_flex.nc -Cg.cpt -J -O -K -BaWSne -X3.5i >> $ps
+
 # 4. ML Map: Gravity from seamounts only
 gravfft z.nc+uk -Ff -E$order -D1800 -Gfaa_z.nc
 grdimage faa_z.nc -Cg.cpt -J -O -K -BaWsne -X-3.5i -Y3.25i >> $ps
+
 # 5. MR Map: Total gravity model
 grdmath faa_flex.nc faa_z.nc ADD = faa_total.nc
 grdimage faa_total.nc -Cg.cpt -J -O -K -BaWsne -X3.5i >> $ps
-# Compute admittance and coherence between topo and gravity
-#gravfft z.nc+uk faa_total.nc+uk -Iwk > adm.txt
+
+# Compute admittance, both data and theoretical, and coherence between topo and gravity
+gravfft z.nc+uk faa_total.nc+uk -Iwk -Ff -Z12000 -T12000/2800/3300/1000 > adm_t.txt
 grdfft z.nc+uk faa_total.nc+uk -Erwk -h+c > adm.txt
-# coherence in red, admittance in green
-psxy adm.txt -i0,15,16 -R8/512/0/1 -JX-6il/2.75i -O -K -Ba2g3:,km:/af:"Coherence":WSn -Sc0.05i -Gred -Ey0.2c/0.5p,red -X-3.25i -Y3.5i >> $ps
+
+# coherence in red, admittance in blue, theoretical admittance in thick lightgray
+psxy adm.txt -i0,15,16 -R8/512/0/1 -JX-6il/2.75i -O -K -Ba2g3:,km://af:"Coherence":WSn -Sc0.05i -Gred -Ey0.2c/0.5p,red -X-3.25i -Y3.5i >> $ps
 psxy adm.txt -i0,15 -R -J -O -K -W0.5p,red >> $ps
-psxy adm.txt -R8/512/0/70 -J -O -Ba2g3/af:"Admittance (mGal/km)":E -Sc0.05i -Ey0.2c/0.5p,green -Ggreen -K -i0,11s1000,12s1000 >> $ps
-psxy adm.txt -R -J -O -K  -W0.5p,green -i0,11s1000 >> $ps
+psxy adm_t.txt -R8/512/0/70 -J -O -K -W3.0p,lightgray -i0,1s1000 >> $ps
+psxy adm.txt -R8/512/0/70 -J -O -Ba2g3/af:"Admittance (mGal/km)":E -Sc0.05i -Ey0.2c/0.5p,blue -Gblue -K -i0,11s1000,12s1000 >> $ps
+psxy adm.txt -R -J -O -K  -W0.5p,blue -i0,11s1000 >> $ps
 psxy -R -J -O -T >> $ps
