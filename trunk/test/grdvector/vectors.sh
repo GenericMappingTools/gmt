11cd6a892a6675d8b931f3e4be874afda35e2ecc
Paul Wessel <pwessel@hawaii.edu>
2013-03-22 03:54:04 +0000

    grdvector did not reject vectors on the back side of -JG maps

---
+++ trunk/test/grdvector/vectors.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ps=vectors.ps
+grdmath -Rg -I30 -r 0.5 Y COSD ADD = r.nc
+grdmath -Rg -I30 -r X = az.nc
+makecpt -T0.5/1.5.0.1 -Z > t.cpt
+
+grdvector r.nc az.nc -A -Ct.cpt -JG45/45/4.5i -Q0.15i+e+n0.5 -W1p -S3i -P -K -B30g30 -Xc -Y0.75i > $ps
+grdvector r.nc az.nc -A -Ct.cpt -J -W1p -S3i -O -B30g30 -Y5i >> $ps
+gv $ps &
