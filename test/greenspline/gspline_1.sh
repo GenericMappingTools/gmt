df8541af8f1f5d6a6d133bca2961acd0e3311d1c
Paul Wessel <pwessel@hawaii.edu>
2013-03-27 19:40:28 +0000

    Added greenspline linear spline to test suite

--- test/greenspline/gspline_1.sh
+++ test/greenspline/gspline_1.sh
@@ -12,7 +12,7 @@ R=-2000/25000/0/11
 T=Table_4.2.txt
 psbasemap -R-2/25/0/11 -JX6i/3i -P -K -B5f1:"Distance (km)":/2f1:"Mg (ppm)":WSne -X1.25i -Y2i --FONT_LABEL=18p > $ps
 psxy -R$R -J -O -K $T -Sc0.075i -Gblack >> $ps
-sample1d $T -S0 -I100 -Fl | psxy -R -J -O -K -Wthin,. >> $ps
+greenspline -R-2000/25000 -I100 $T -Sl -D0 | psxy -R$R -J -O -K -Wthin,. >> $ps
 greenspline -R-2000/25000 -I100 $T -Sc -D0 | psxy -R$R -J -O -K -Wthin,- >> $ps
 greenspline -R-2000/25000 -I100 $T -St0.25 -D0 | psxy -R$R -J -O -K -Wthin >> $ps
 pslegend -R$R -J -O -K -F -Dx5.9i/2.9i/2.05i/TR --FONT_ANNOT_PRIMARY=12p << EOF >> $ps
