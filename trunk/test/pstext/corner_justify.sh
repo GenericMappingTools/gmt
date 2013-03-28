7f82c695b6c391e56687923f8f4d3938a35100b7
Paul Wessel <pwessel@hawaii.edu>
2013-03-28 20:09:46 +0000

    Adding PS

--- trunk/test/pstext/corner_justify.sh
+++ trunk/test/pstext/corner_justify.sh
@@ -3,17 +3,21 @@
 #
 # Test pstext with coordinates extracted from -R
 
+function plot {
+	pstext -R -J --FONT=24p,Helvetica-Bold -O -K $*
+}
+
 ps=corner_justify.ps
 
-
 psbasemap -R0/16/0/12 -Jx1c -B5g1WSne -P -K > $ps
-echo TopLeft | pstext -R -J --FONT=24p,Helvetica-Bold -F+cTL -O -K >> $ps
-echo MidLeft | pstext -R -J --FONT=24p,Helvetica-Bold -F+cML -O -K >> $ps
-echo BotLeft | pstext -R -J --FONT=24p,Helvetica-Bold -F+cBL -O -K >> $ps
-echo TopCenter | pstext -R -J --FONT=24p,Helvetica-Bold -F+cTC -O -K >> $ps
-echo MidCenter | pstext -R -J --FONT=24p,Helvetica-Bold -F+cMC -O -K >> $ps
-echo MidCenter | pstext -R -J --FONT=24p,Helvetica-Bold -D1 -F+cMC -O -K >> $ps
-echo BotCenter | pstext -R -J --FONT=24p,Helvetica-Bold -F+cBC -O -K >> $ps
-echo TopRight | pstext -R -J --FONT=24p,Helvetica-Bold -F+cTR -O -K >> $ps
-echo MidRight | pstext -R -J --FONT=24p,Helvetica-Bold -F+cMR -O -K >> $ps
-echo BotRight | pstext -R -J --FONT=24p,Helvetica-Bold -F+cBR -O >> $ps
+echo TopLeft   | plot -F+cTL >> $ps
+echo MidLeft   | plot -F+cML >> $ps
+echo BotLeft   | plot -F+cBL >> $ps
+echo TopCenter | plot -F+cTC >> $ps
+echo MidCenter | plot -F+cMC >> $ps
+echo MidCenter | plot -D1c -F+cMC >> $ps
+echo BotCenter | plot -F+cBC >> $ps
+echo TopRight  | plot -F+cTR >> $ps
+echo MidRight  | plot -F+cMR >> $ps
+echo BotRight  | plot -F+cBR >> $ps
+psxy -R -J -O -T >> $ps
