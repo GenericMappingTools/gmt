3abf75efeabbe1cb4ad23896c720dc789aaeb917
Joaquim Luis <jmfluis@gmail.com>
2013-03-30 17:05:57 +0000

    Explicitly set -R in some scripts to prevent errors of the type "Found no history for option ..."

--- test/gmtspatial/intersect.sh
+++ test/gmtspatial/intersect.sh
@@ -28,13 +28,13 @@ EOF
 # Find intersections between A and B
 R=-R-0.5/1.6/-0.5/1.5
 psxy $R -Jx1.5i -P -Ba1g1WSne A.txt -W3p,red -K -X0.75i > $ps
-psxy -R -J -O B.txt -W3p,blue -K >> $ps
-gmtspatial A.txt B.txt -Ie | psxy -R -J -O -K -Sc0.15i -W0.25p >> $ps
+psxy $R -J -O B.txt -W3p,blue -K >> $ps
+gmtspatial A.txt B.txt -Ie | psxy $R -J -O -K -Sc0.15i -W0.25p >> $ps
 # Truncate A given B
-gmtspatial A.txt -TB.txt | psxy -R -J -O -K -W0.5p,green >> $ps
+gmtspatial A.txt -TB.txt | psxy $R -J -O -K -W0.5p,green >> $ps
 # Clip A and B to a smaller region
-psxy -R -J -O -K -Ba1g1:.Cartesian:WSne -W0.25p,. [AB].txt -Y4i >> $ps
-psxy -R -J -O -K -L -W0.5p,- << EOF >> $ps
+psxy $R -J -O -K -Ba1g1:.Cartesian:WSne -W0.25p,. [AB].txt -Y4i >> $ps
+psxy $R -J -O -K -L -W0.5p,- << EOF >> $ps
 -0.2	-0.4
 1.1	-0.4
 1.1	0.9
@@ -49,13 +49,13 @@ gmtmath -T B.txt 10 MUL = Bg.txt
 # Find intersections between Ag and Bg
 R=-R-5/16/-5/15
 psxy $R -Jm0.15i -O -Ba5g5WSne Ag.txt -W3p,red -K -Y-4i -X3.75i >> $ps
-psxy -R -J -O Bg.txt -W3p,blue -K >> $ps
-gmtspatial Ag.txt Bg.txt -Ie -fg | psxy -R -J -O -K -Sc0.15i -W0.25p >> $ps
+psxy $R -J -O Bg.txt -W3p,blue -K >> $ps
+gmtspatial Ag.txt Bg.txt -Ie -fg | psxy $R -J -O -K -Sc0.15i -W0.25p >> $ps
 # Truncate Ag given Bg
-gmtspatial Ag.txt -TBg.txt -fg | psxy -R -J -O -K -W0.5p,green >> $ps
+gmtspatial Ag.txt -TBg.txt -fg | psxy $R -J -O -K -W0.5p,green >> $ps
 # Clip Ag and Bg to a smaller region
-psxy -R -J -O -K -Ba5g5:.Geographic:WSne -W0.25p,. [AB]g.txt -Y4i >> $ps
-psxy -R -J -O -K -L -W0.5p,- << EOF >> $ps
+psxy $R -J -O -K -Ba5g5:.Geographic:WSne -W0.25p,. [AB]g.txt -Y4i >> $ps
+psxy $R -J -O -K -L -W0.5p,- << EOF >> $ps
 -2	-4
 11	-4
 11	9
--- test/greenspline/gspline_1.sh
+++ test/greenspline/gspline_1.sh
@@ -12,13 +12,13 @@ R=-2000/25000/0/11
 T=Table_4.2.txt
 psbasemap -R-2/25/0/11 -JX6i/3i -P -K -B5f1:"Distance (km)":/2f1:"Mg (ppm)":WSne -X1.25i -Y2i --FONT_LABEL=18p > $ps
 psxy -R$R -J -O -K $T -Sc0.075i -Gblack >> $ps
-greenspline -R-2000/25000 -I100 $T -Sl -D0 | psxy -R$R -J -O -K -Wthin,. >> $ps
-greenspline -R-2000/25000 -I100 $T -Sc -D0 | psxy -R$R -J -O -K -Wthin,- >> $ps
+greenspline -R-2000/25000 -I100 $T -Sl -D0 | psxy -R$R -JX -O -K -Wthin,. >> $ps
+greenspline -R-2000/25000 -I100 $T -Sc -D0 | psxy -R$R -JX -O -K -Wthin,- >> $ps
 greenspline -R-2000/25000 -I100 $T -St0.25 -D0 | psxy -R$R -J -O -K -Wthin >> $ps
-pslegend -R$R -J -O -K -F -Dx5.9i/2.9i/2.05i/TR --FONT_ANNOT_PRIMARY=12p << EOF >> $ps
+pslegend -R$R -JX -O -K -F -Dx5.9i/2.9i/2.05i/TR --FONT_ANNOT_PRIMARY=12p << EOF >> $ps
 S 0.2i - 0.35i - 0.5p 0.5i Tension (@%6%t@%% = 0.25)
 S 0.2i - 0.35i - 0.5p,- 0.5i No tension
 S 0.2i - 0.35i - 0.5p,. 0.5i Linear interpolation
 EOF
-psxy -R$R -J -O -T >> $ps
+psxy -R$R -JX -O -T >> $ps
 
