0acaef1886a82d8a8984cf9141d5071e858aee28
Remko Scharroo <remko.scharroo@eumetsat.int>
2013-04-21 16:09:08 +0000

    Check more variations of -G and -F

--- test/pslegend/fill_frame.sh
+++ test/pslegend/fill_frame.sh
@@ -9,12 +9,13 @@ color=yellow
 psbasemap -R0/15/0/25 -Jx1c -P -B0 -K -Y1c > $ps
 pslegend -R -J -O -K -Dx1c/24c/8c/1c/TL -G$color >> $ps <<< "L - - C $color fill, no frame"
 pslegend -R -J -O -K -Dx1c/22c/8c/1c/TL -G$color -F >> $ps <<< "L - - C $color fill, black frame"
-pslegend -R -J -O -K -Dx1c/20c/8c/1c/TL -G$color -F+p2p,blue@50 >> $ps <<< "L - - C $color fill, blue@@50 frame"
-pslegend -R -J -O -K -Dx1c/18c/8c/TL -G$color -F >> $ps <<%
+pslegend -R -J -O -K -Dx1c/20c/8c/1c/TL -G${color}@50 -F+p2p,blue >> $ps <<< "L - - C $color@@50 fill, blue frame"
+pslegend -R -J -O -K -Dx1c/18c/8c/1c/TL -G$color -F+p2p,blue@50 >> $ps <<< "L - - C $color fill, blue@@50 frame"
+pslegend -R -J -O -K -Dx1c/15c/8c/TL -G$color -F >> $ps <<%
 P
 T This text wraps to the next line. It uses all standard paragraph modes. But what if you want to use left alignment?
 %
-pslegend -R -J -O -K -Dx1c/15c/8c/TL -G$color -F >> $ps <<%
+pslegend -R -J -O -K -Dx1c/12c/8c/TL -G$color -F >> $ps <<%
 P - - - - - - - l
 T Same trying to do left alignment. That seems to work nicely but needs 8 parameters on P line.
 %
