35ac6d4f71f3cf61b9c7041be86c963226417374
Paul Wessel <pwessel@hawaii.edu>
2013-05-25 21:37:17 +0000

    Converted two more test scripts to new examples 37 and 38

--- doc/examples/ex35/example_35.sh
+++ doc/examples/ex35/example_35.sh
@@ -16,7 +16,7 @@ gmt sphtriangulate gshhs_c.txt -Qv -D > tt.pol
 gmt sphdistance -Rg -I1 -Qtt.pol -Gtt.nc -Lk
 makecpt -Chot -T0/3500/500 -Z > t.cpt
 # Make a basic image plot and overlay contours, Voronoi polygons and coastlines
-gmt grdimage tt.nc -JG-140/30/7i -P -K -Ct.cpt -X0.75i -Y2i > $ps
+gmt grdimage tt.nc -JG-140/30/7i -P -K -Ct.cpt -X0.75i -Y2i -U/-0.5i/-1.75i/"Example 35 in Cookbook" > $ps
 gmt grdcontour tt.nc -J -O -K -C500 -A1000+f10p,Helvetica,white -L500 -GL0/90/203/-10,175/60/170/-30,-50/30/220/-5 -Wa0.75p,white -Wc0.25p,white >> $ps
 gmt psxy -R -J -O -K tt.pol -W0.25p,green,. >> $ps
 gmt pscoast -R -J -O -W1p -Gsteelblue -A0/1/1 -B30g30 -B+t"Distances from GSHHG crude coastlines" >> $ps
