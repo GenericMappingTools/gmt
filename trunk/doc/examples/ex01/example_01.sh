ccbc5cf1963a54c0f6b44bfb61145bb14dece23a
Paul Wessel <pwessel@hawaii.edu>
2013-05-25 19:18:15 +0000

    Colorize example 3

--- trunk/doc/examples/ex01/example_01.sh
+++ trunk/doc/examples/ex01/example_01.sh
@@ -9,11 +9,11 @@
 ps=example_01.ps
 gmt gmtset MAP_GRID_CROSS_SIZE_PRIMARY 0 FONT_ANNOT_PRIMARY 10p
 gmt psbasemap -R0/6.5/0/9 -Jx1i -B0 -P -K -U"Example 1 in Cookbook" > $ps
-gmt pscoast -Rg -JH0/6i -X0.25i -Y0.5i -O -K -Bg30 -Dc -Glightgray >> $ps
+gmt pscoast -Rg -JH0/6i -X0.25i -Y0.5i -O -K -Bg30 -Dc -Glightbrown -Slightblue >> $ps
 gmt grdcontour osu91a1f_16.nc -J -C10 -A50+f7p -Gd4i -L-1000/-1 -Wcthinnest,- -Wathin,- -O -K \
 	-T0.1i/0.02i >> $ps
 gmt grdcontour osu91a1f_16.nc -J -C10 -A50+f7p -Gd4i -L-1/1000 -O -K -T0.1i/0.02i >> $ps
-gmt pscoast -Rg -JH6i -Y4i -O -K -B+t"Low Order Geoid" -Bg30 -Dc -Glightgray >> $ps
+gmt pscoast -Rg -JH6i -Y4i -O -K -B+t"Low Order Geoid" -Bg30 -Dc -Glightbrown -Slightblue >> $ps
 gmt grdcontour osu91a1f_16.nc -J -C10 -A50+f7p -Gd4i -L-1000/-1 -Wcthinnest,- -Wathin,- -O -K \
 	-T0.1i/0.02i:-+ >> $ps
 gmt grdcontour osu91a1f_16.nc -J -C10 -A50+f7p -Gd4i -L-1/1000 -O -T0.1i/0.02i:-+ >> $ps
