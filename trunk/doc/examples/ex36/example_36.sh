4c9b26d48f33190e92859c27555796531965c0da
Paul Wessel <pwessel@hawaii.edu>
2013-05-25 21:37:17 +0000

    Converted two more test scripts to new examples 37 and 38

--- trunk/doc/examples/ex36/example_36.sh
+++ trunk/doc/examples/ex36/example_36.sh
@@ -11,7 +11,7 @@ ps=example_36.ps
 gmt makecpt -Crainbow -T-7000/15000/1000 -Z > tt.cpt
 # Piecewise linear interpolation; no tension
 gmt sphinterpolate mars370.txt -Rg -I1 -Q0 -Gtt.nc
-gmt grdimage tt.nc -JH0/6i -Bag -Ctt.cpt -P -Xc -Y7.25i -K > $ps
+gmt grdimage tt.nc -JH0/6i -Bag -Ctt.cpt -P -Xc -Y7.25i -K -U/-1i/-7i/"Example 36 in Cookbook" > $ps
 gmt psxy -Rg -J -O -K mars370.txt -Sc0.05i -G0 -B30g30 -Y-3.25i >> $ps
 # Smoothing
 gmt sphinterpolate mars370.txt -Rg -I1 -Q3 -Gtt.nc
--- trunk/doc/examples/ex37/example_37.sh
+++ trunk/doc/examples/ex37/example_37.sh
@@ -1,13 +1,19 @@
-#!/bin/sh -xv
-#	$Id: coherence.sh 11495 2013-05-16 19:40:41Z pwessel $
+#!/bin/bash
+#               GMT EXAMPLE 37
+#               $Id$
+#
+# Purpose:      Illustrate 2-D FFT and coherence between gravity and bathymetry grids
+# GMT progs:    psbasemap, psxy, makecpt, grdfft, grdimage, grdinfo, grdgradient
+# Unix progs:   rm
+#
+ps=example_37.ps
+
 # Testing gmt grdfft coherence calculation with Karen Marks example data
-
-ps=coherence.ps
-
 # Prefix of two .nc files
+
 G=grav.V18.par.surf.1km.sq
 T=mb.par.surf.1km.sq
-gmt gmtset FONT_TITLE 12p
+gmt gmtset FONT_TITLE 14p
 
 gmt makecpt -Crainbow -T-5000/-3000/100 -Z > z.cpt
 gmt makecpt -Crainbow -T-50/25/5 -Z > g.cpt
@@ -16,7 +22,7 @@ gmt grdgradient $G.nc -A0 -Nt1 -G${G}_int.nc
 gmt grdgradient $T.nc -A0 -Nt1 -G${T}_int.nc
 scl=1.4e-5
 sclkm=1.4e-2
-gmt grdimage $T.nc -I${T}_int.nc -Jx${scl}i -Cz.cpt -P -K -X1.474i > $ps
+gmt grdimage $T.nc -I${T}_int.nc -Jx${scl}i -Cz.cpt -P -K -X1.474i -Y1i -U/-1.224i/-0.75i/"Example 37 in Cookbook"> $ps
 gmt psbasemap -R-84/75/-78/81 -Jx${sclkm}i -O -K -Ba -BWSne+t"Multibeam bathymetry" >> $ps
 gmt grdimage $G.nc -I${G}_int.nc -Jx${scl}i -Cg.cpt -O -K -X3.25i >> $ps
 gmt psbasemap -R-84/75/-78/81 -Jx${sclkm}i -O -K -Ba -BWSne+t"Satellite gravity" >> $ps
@@ -36,6 +42,8 @@ gmt grdimage ${G}_tmp.nc -I${G}_tmp_int.nc -Jx${scl}i -Cg.cpt -O -K -X3.25i >> $
 gmt psxy -R${G}_tmp.nc -J bbox -O -K -L -W0.5p,- >> $ps
 gmt psbasemap -R-100/91/-94/97 -Jx${sclkm}i -O -K -Ba -BWSne+t"Detrended and extended" >> $ps
 
-gmt psxy -R2/160/0/1 -JX-6il/2.5i -Bxa2f3g3+u" km" -Byafg0.5+l"Coherency@+2@+" -BWsNe -O -K -X-3.25i -Y3.5i cross.txt -i0,15 -W0.5p >> $ps
+gmt gmtset FONT_TITLE 24p
+gmt psxy -R2/160/0/1 -JX-6il/2.5i -Bxa2f3g3+u" km" -Byafg0.5+l"Coherency@+2@+" -BWsNe+t"Coherency between gravity and bathymetry" -O -K -X-3.25i -Y3.3i cross.txt -i0,15 -W0.5p >> $ps
 gmt psxy -R -J cross.txt -O -K -i0,15,16 -Sc0.075i -Gred -W0.25p -Ey >> $ps
 gmt psxy -R -J -O -T >> $ps
+rm -f cross.txt *_tmp.nc *_int.nc ?.cpt bbox
