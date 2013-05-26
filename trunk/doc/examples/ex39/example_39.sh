f72731d16ad4440f22adb0e4237d9987764c74e1
Paul Wessel <pwessel@hawaii.edu>
2013-05-26 17:59:39 +0000

    Fix bugs in new scripts 35 and 39

--- trunk/doc/examples/ex39/example_39.sh
+++ trunk/doc/examples/ex39/example_39.sh
@@ -18,15 +18,15 @@ gmt sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv1.nc -F1/1/25/30
 gmt sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv2.nc -F1/1/85/90
 gmt sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv3.nc -F1/1/170/180
 gmt grd2cpt v3.nc -Crainbow -E16 -Z > t.cpt
-grdgradient v1.nc -Nt0.75 -A45 -Gvint.nc
+gmt grdgradient v1.nc -Nt0.75 -A45 -Gvint.nc
 gmt grdimage v1.nc -Ivint.nc -JG90/30/5i -P -K -Bg -Ct.cpt -X3i -Y1.1i -U/-2.75i/-0.85i/"Example 39 in Cookbook" > $ps
 echo 4 4.5 L = 30 | gmt pstext -R0/6/0/6 -Jx1i -O -K -Dj0.2i -F+f16p+jLM -N >> $ps
-psscale -Ct.cpt -O -K -D1.25i/-0.2i/5.5i/0.1ih -Bxaf -By+lm >> $ps
-grdgradient v2.nc -Nt0.75 -A45 -Gvint.nc
+gmt psscale -Ct.cpt -O -K -D1.25i/-0.2i/5.5i/0.1ih -Bxaf -By+lm >> $ps
+gmt grdgradient v2.nc -Nt0.75 -A45 -Gvint.nc
 gmt grdimage v2.nc -Ivint.nc -JG -O -K -Bg -Ct.cpt -X-1.25i -Y1.9i >> $ps
 echo 4 4.5 L = 90 | gmt pstext -R0/6/0/6 -Jx1i -O -K -Dj0.2i -F+f16p+jLM -N >> $ps
 gmt sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv3.nc -V -F1/1/170/180
-grdgradient v3.nc -Nt0.75 -A45 -Gvint.nc
+gmt grdgradient v3.nc -Nt0.75 -A45 -Gvint.nc
 gmt grdimage v3.nc -Ivint.nc -JG -O -K -Bg -Ct.cpt -X-1.25i -Y1.9i >> $ps
 echo 4 4.5 L = 180 | gmt pstext -R0/6/0/6 -Jx1i -O -K -Dj0.2i -F+f16p+jLM -N >> $ps
 echo 3.75 5.4 Venus Spherical Harmonic Model | gmt pstext -R0/6/0/6 -Jx1i -O -F+f24p+jCM -N >> $ps
