1375674f57616273ec75e02cf2367cf28d5b1548
Joaquim Luis <jmfluis@gmail.com>
2013-06-07 14:39:13 +0000

    Fix mistake commit

--- doc/rst/source/conf.py
+++ doc/rst/source/conf.py
@@ -15,8 +15,6 @@ import sys, os
 
 sys.path.append(os.path.abspath('.'))
 
-#extensions = ['numfig']
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -29,7 +27,7 @@ sys.path.append(os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.pngmath', 'numfig']
+extensions = ['sphinx.ext.pngmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
