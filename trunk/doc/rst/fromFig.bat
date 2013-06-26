bb667b269066b7917474a1d9216386029eb539e2
Joaquim Luis <jmfluis@gmail.com>
2013-06-26 18:33:44 +0000

    Helper batch files to build the documentation on Windows

---
+++ trunk/doc/rst/fromFig.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM	$Id$
+REM
+REM %1 -> %name%
+REM %2 -> %pato%
+
+REM set W=gmt grdinfo -C ../fig/%1% | gawk "print $3"
+REM set H=gmt grdinfo -C ../fig/%1% | gawk "print $5"
+REM FOR /F %%I IN ('gmt grdinfo -C ../fig/%1% | gawk "print $3"') DO SET W=%%I
+REM FOR /F %%I IN ('gmt grdinfo -C ../fig/%1% | gawk "print $5"') DO SET H=%%I
+echo .. figure:: %~dp0../fig/%1 > %2/fig_%1.rst_
+REM echo    :height: %H% px >> %2/fig_%1.rst_
+REM echo    :width: %W% px >> %2/fig_%1.rst_
+echo    :align: center >> %2/fig_%1.rst_
+echo    :scale: 50 ^%% >> %2/fig_%1.rst_
+echo.   >> %2/fig_%1.rst_
---
+++ trunk/doc/rst/fromScripts.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM	$Id$
+REM
+REM %1 -> %name%
+REM %2 -> %ext%
+REM %3 -> %frmt%
+REM %4 -> %path_build%
+REM %5 -> %pato%
+
+IF "%2"=="pdf" (
+	gmt ps2raster %~dp0..\scripts/%1.ps -A -P -T%3 -D%4
+) ELSE (
+	gmt ps2raster %~dp0..\scripts/%1.ps -A -P -T%3 -D%4 -E150 -Qt4
+)
+echo .. figure:: %4/%1.%2 > %5/fig_%1.rst_
+echo    :width: 500 px >> %5/fig_%1.rst_
+echo    :align: center >> %5/fig_%1.rst_
+echo.   >> %5/fig_%1.rst_
