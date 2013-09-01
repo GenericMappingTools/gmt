5e6c38c8f167c984c03ab6f6af8ea66decee43f0
Paul Wessel <pwessel@hawaii.edu>
2013-09-01 21:14:06 +0000

    Consider single counter for mem arrays, place tmp mem array info in GMT_INTERNAL struct. Now ready to try implementing new mem use for reading segments

--- trunk/src/gmt_memory.c
+++ trunk/src/gmt_memory.c
@@ -22,21 +22,6 @@
  * Date:	1-SEPT-2013
  * Version:	5.x
  *
- * To avoid lots of alloc and realloc calls we prefer to allocate a sizeable array
- * per coordinate axes once, then use that temprary space for reading and
- * calculations, and then alloc permanent space elsewhere and call memcpy to
- * place the final memory there.  We assume that for most purposes we will
- * need GMT_INITIAL_MEM_COL_ALLOC columns [3] and allocate GMT_INITIAL_MEM_ROW_ALLOC
- * [1048576U] rows for each column.  This is 24 Mb for double precision data.
- * These arrays are expected to hardly ever beeing reallocated as that would
- * only happen for very long segments, a rare occurance. For most typical data
- * we may have lots of smaller segments but rarely do any segment exceed the
- * 1048576U length initialized above.  Thus, reallocs are generally avoided.
- * Note: (1) Each column maintains its own n_alloc counter and the code belows will
- *           check each length whenever things need to be extended.
- *	 (2) We chose to maintain a small set of column vectors rather than a single
- *	     item since GMT tends to use columns vectors and thus the book-keeping is
- *	     simpler and the number of columns is typically very small (2-3).
  */
 
 /* Two functions called elsewhere:
@@ -365,20 +350,35 @@ void GMT_memtrack_report (struct GMT_CTRL *GMT) {
 }
 #endif
 
+/* To avoid lots of alloc and realloc calls we prefer to allocate a sizeable array
+ * per coordinate axes once, then use that temporary space for reading and
+ * calculations, and then alloc permanent space elsewhere and call memcpy to
+ * place the final memory there.  We assume that for most purposes we will
+ * need GMT_INITIAL_MEM_COL_ALLOC columns [3] and allocate GMT_INITIAL_MEM_ROW_ALLOC
+ * [1048576U] rows for each column.  This is 24 Mb for double precision data.
+ * These arrays are expected to hardly ever beeing reallocated as that would
+ * only happen for very long segments, a rare occurance. For most typical data
+ * we may have lots of smaller segments but rarely do any segment exceed the
+ * 1048576U length initialized above.  Thus, reallocs are generally avoided.
+ * Note: (1) All columns share a signle n_alloc counter and the code belows will
+ *           check whenever arrays need to be extended.
+ *	 (2) We chose to maintain a small set of column vectors rather than a single
+ *	     item since GMT tends to use columns vectors and thus the book-keeping is
+ *	     simpler and the number of columns is typically very small (2-3).
+ */
+
 void gmt_init_tmp_arrays (struct GMT_CTRL *GMT, size_t n_cols)
 {
 	/* Initialization of GMT coordinate temp arrays - this is called at most once per GMT session  */
 
 	size_t col;
 
-	if (n_cols < GMT_INITIAL_MEM_COL_ALLOC) n_cols = GMT_INITIAL_MEM_COL_ALLOC;	/* Allocate at least this many */
-	GMT->current.io.mem_coord  = GMT_memory (GMT, NULL, n_cols, double *);		/* These are all NULL */
-	GMT->current.io.mem_rows = GMT_memory (GMT, NULL, n_cols, size_t);		/* These are all 0 */
-	GMT->current.io.mem_cols = n_cols;		/* How many columns we have initialized */
-	for (col = 0; col < n_cols; col++) {	/* For each column, reallocate space for n_rows */
-		GMT->current.io.mem_coord[col] = GMT_memory (GMT, NULL, GMT_INITIAL_MEM_ROW_ALLOC, double);
-		GMT->current.io.mem_rows[col] = GMT_INITIAL_MEM_ROW_ALLOC;
-	}
+	if (n_cols == 0) n_cols = GMT_INITIAL_MEM_COL_ALLOC;	/* Allocate at least this many */
+	GMT->hidden.mem_coord  = GMT_memory (GMT, NULL, n_cols, double *);	/* These are all NULL */
+	GMT->hidden.mem_cols = n_cols;	/* How many columns we have initialized */
+	for (col = 0; col < n_cols; col++)	/* For each column, reallocate space for n_rows */
+		GMT->hidden.mem_coord[col] = GMT_memory (GMT, NULL, GMT_INITIAL_MEM_ROW_ALLOC, double);
+	GMT->hidden.mem_rows = GMT_INITIAL_MEM_ROW_ALLOC;
 }
 
 void GMT_free_tmp_arrays (struct GMT_CTRL *GMT)
@@ -386,10 +386,10 @@ void GMT_free_tmp_arrays (struct GMT_CTRL *GMT)
 	/* Free temporary coordinate memory used by this session */
 	size_t col;
 
-	for (col = 0; col < GMT->current.io.mem_cols; col++)	/* For each column, free an array */
-		GMT_free (GMT, GMT->current.io.mem_coord[col]);
-	GMT_free (GMT, GMT->current.io.mem_coord);
-	GMT_free (GMT, GMT->current.io.mem_rows);
+	for (col = 0; col < GMT->hidden.mem_cols; col++)	/* For each column, free an array */
+		GMT_free (GMT, GMT->hidden.mem_coord[col]);
+	GMT_free (GMT, GMT->hidden.mem_coord);
+	GMT->hidden.mem_rows = 0;
 }
 
 void GMT_prep_tmp_arrays (struct GMT_CTRL *GMT, size_t row, size_t n_cols)
@@ -397,28 +397,27 @@ void GMT_prep_tmp_arrays (struct GMT_CTRL *GMT, size_t row, size_t n_cols)
 	size_t col;
 
 	/* Check if this is the very first time, if so we initialize the arrays */
-	if (GMT->current.io.mem_coord == NULL)
+	if (GMT->hidden.mem_coord == NULL)
 		gmt_init_tmp_arrays (GMT, n_cols);	/* First time we get here */
 
 	/* Check if we are exceeding our column count so far, if so we must allocate more columns */
-	else if (n_cols > GMT->current.io.mem_cols) {	/* Must allocate more columns, this is expected to happen rarely */
-		GMT->current.io.mem_coord = GMT_memory (GMT, GMT->current.io.mem_coord, n_cols, double *);	/* New ones are NOT NULL */
-		GMT->current.io.mem_rows  = GMT_memory (GMT, GMT->current.io.mem_rows, n_cols, size_t);		/* New ones are NOT 0 */
-		for (col = GMT->current.io.mem_cols; col < n_cols; col++) {	/* Explicitly zero out the new additions */
-			GMT->current.io.mem_coord[col] = NULL;
-			GMT->current.io.mem_rows[col] = 0;
-		}
-		GMT->current.io.mem_cols = n_cols;		/* Updated count */
+	else if (n_cols > GMT->hidden.mem_cols) {	/* Must allocate more columns, this is expected to happen rarely */
+		GMT->hidden.mem_coord = GMT_memory (GMT, GMT->hidden.mem_coord, n_cols, double *);	/* New ones are NOT NULL */
+		for (col = GMT->hidden.mem_cols; col < n_cols; col++)	/* Explicitly zero out the new additions */
+			GMT->hidden.mem_coord[col] = NULL;
+		GMT->hidden.mem_cols = n_cols;		/* Updated column count */
 	}
 
 	/* Check if we are exceeding our allocated count for this column.  If so allocate more rows */
 
-	for (col = 0; col < n_cols; col++) {	/* Handle each column separately */
-		if (row >= GMT->current.io.mem_rows[col]) {	/* Must allocate more rows, this is expected to happen rarely given the large initial allocation */
-			while (row > GMT->current.io.mem_rows[col]) GMT->current.io.mem_rows[col] <<= 1;	/* Double up */
-			GMT->current.io.mem_coord[col] = GMT_memory (GMT, GMT->current.io.mem_coord[col], GMT->current.io.mem_rows[col], double);
-		}
-	}
+	if (row < GMT->hidden.mem_rows) return;	/* Nothing to do */
+	
+	/* Here we must allocate more rows, this is expected to happen rarely given the large initial allocation */
+
+	while (row > GMT->hidden.mem_rows) GMT->hidden.mem_rows <<= 1;	/* Double up until enough */
+	for (col = 0; col < GMT->hidden.mem_cols; col++)	/* Add more memory via realloc */
+		GMT->hidden.mem_coord[col] = GMT_memory (GMT, GMT->hidden.mem_coord[col], GMT->hidden.mem_rows, double);
+
 	/* Note: Any additions to these arrays are not guaranteed to be set to zero */
 }
 
