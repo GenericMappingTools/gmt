4459884235dddd56018d99459f524b1f0e23e571
Joaquim Luis <jmfluis@gmail.com>
2013-09-11 19:26:05 +0000

    Fix install of debug symbol files on Windows

--- trunk/cmake/modules/CreateDebugSym.cmake
+++ trunk/cmake/modules/CreateDebugSym.cmake
@@ -87,7 +87,7 @@ elseif (MSVC AND DEBUG_BUILD)
 			# clean target
 			get_target_property (_location ${target} LOCATION)
 			get_filename_component (_path ${_location} PATH)
-			get_filename_component (_name ${_location} NAME)
+			get_filename_component (_name ${_location} NAME_WE)
 			set (_pdb_file "${_path}/${_name}.pdb")
 			add_custom_target (_pdb_clean_${target}
 				COMMAND ${CMAKE_COMMAND} remove -f ${_pdb_file}
