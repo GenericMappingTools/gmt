487a5f1ed6b2fc796f9765ac29a2679aa9b1bfd5
Paul Wessel <pwessel@hawaii.edu>
2013-09-28 23:05:36 +0000

    Add lower-case dem?.cpt and update App M to have two cpt figs

--- doc/scripts/GMT_App_M_1b.sh
+++ doc/scripts/GMT_App_M_1b.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #	$Id$
 #
-#	Makes the insert for Appendix M(cpt)
+#	Makes the inserts for Appendix M(cpt)
 #
+# Use the knowledge that we need 2 pages.
+
 grep -v '#' "${GMT5_SHAREDIR:-$GMT_SHAREDIR}"/conf/gmt_cpt.conf | cut -d: -f1 | sort -r > tt.lis
 
-ps=GMT_App_M_1.ps
+ps=GMT_App_M_1b.ps
 n=`cat tt.lis | wc -l`
-
+let n2=n/2
 # dy is line spacing and y0 is total box height
 
 gmt gmtset MAP_FRAME_PEN thinner FONT_ANNOT_PRIMARY 8p MAP_TICK_LENGTH_PRIMARY 0.1i MAP_ANNOT_OFFSET_PRIMARY 0.04i
@@ -16,8 +18,8 @@ gmt psbasemap -R0/6.1/0/6.9 -Jx1i -P -K -B0 > $ps
 i=1
 y=0.475
 y2=0.35
-dy=0.52
-while [ $i -le 26 ]
+dy=0.75
+while [ $i -le $n2 ]
 do
 	j=`expr $i + 1`
 	left=`sed -n ${j}p tt.lis`
@@ -26,11 +28,11 @@ do
 	gmt makecpt -C$left -T-1/1/0.25 > tt.left2.cpt
 	gmt makecpt -C$right -Z > tt.right.cpt
 	gmt makecpt -C$right -T-1/1/0.25 > tt.right2.cpt
-	gmt psscale -D1.55/$y/2.70/0.125h -Ctt.left.cpt -B0 -O -K >> $ps
-	gmt psscale -D4.50/$y/2.70/0.125h -Ctt.right.cpt -B0 -O -K >> $ps
-	gmt psscale -D1.55/$y2/2.70/0.125h -Ctt.left2.cpt -Bf0.25 -O -K >> $ps
-	gmt psscale -D4.50/$y2/2.70/0.125h -Ctt.right2.cpt -Bf0.25 -O -K >> $ps
-	gmt pstext -R -J -O -K -D0/0.05 -F+f9p,Helvetica-Bold+jBC >> $ps <<- END
+	gmt psscale -D1.55i/${y}i/2.70i/0.125ih -Ctt.left.cpt -B0 -O -K >> $ps
+	gmt psscale -D4.50i/${y}i/2.70i/0.125ih -Ctt.right.cpt -B0 -O -K >> $ps
+	gmt psscale -D1.55i/${y2}i/2.70i/0.125ih -Ctt.left2.cpt -Bf0.25 -O -K >> $ps
+	gmt psscale -D4.50i/${y2}i/2.70i/0.125ih -Ctt.right2.cpt -Bf0.25 -O -K >> $ps
+	gmt pstext -R -J -O -K -D0/0.05i -F+f9p,Helvetica-Bold+jBC >> $ps <<- END
 	1.55 $y ${left}
 	4.50 $y ${right}
 	END
