b9b909f9283eb983df4aa82a8864b001e3693c88
Florian Wobbe <florian@eprofs.de>
2013-10-21 10:28:12 +0000

    Make style IDs positive integers and prefix with "st"; add tests for gmt2kml -C.

--- trunk/test/kml/gmt2kml.sh
+++ trunk/test/kml/gmt2kml.sh
@@ -7,3 +7,12 @@ gmt gmt2kml coast.txt -Fl -W1p,red > coast.kml
 # reduce exponent digits (e-015 -> e-15):
 sed -i -e 's/e-0\([0-9][0-9]\)/e-\1/g' coast.kml
 diff --strip-trailing-cr coast.kml "${src:-.}"/coast.kml > fail
+
+# test -C option
+makecpt -Ccategorical > categories.cpt
+
+gmt gmt2kml pointsets.txt -Ccategories.cpt > pointsets.kml
+#todo: diff --strip-trailing-cr pointsets.kml "${src:-.}"/pointsets.kml > fail
+
+gmt gmt2kml lines.txt -Fl -Ccategories.cpt > lines.kml
+#todo: diff --strip-trailing-cr lines.kml "${src:-.}"/lines.kml > fail
