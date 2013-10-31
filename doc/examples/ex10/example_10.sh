10d35b85a8348f90453bfb772e5791dbeb3fe232
Paul Wessel <pwessel@hawaii.edu>
2013-10-31 18:29:07 +0000

    Update example 10 for world languages

--- doc/examples/ex10/example_10.sh
+++ doc/examples/ex10/example_10.sh
@@ -7,9 +7,9 @@
 # Unix progs:	$AWK, rm
 #
 ps=example_10.ps
-gmt pscoast -Rd -JX8id/5id -Dc -Slightblue -Glightbrown -Wfaint -A1000 -p200/40 -K > $ps
-$AWK '{print $1, $2, $3}' agu2008.d \
-	| gmt pstext -R -J -O -K -p -D-0.2i/0 -F+f20p,Helvetica-Bold,blue=thinner+jRM >> $ps
-gmt psxyz agu2008.d -R-180/180/-90/90/1.01/100000 -J -JZ2.5il -So0.3ib1 -Gdarkgreen -Wthinner \
-	--FONT_TITLE=30p,Times-Bold --MAP_TITLE_OFFSET=-0.7i -O -p \
-	-Bx60g60 -By30g30 -Bza1p+lMemberships -BWSneZ+t"AGU 2008 Membership Distribution" >> $ps
+gmt pscoast -Rd -JX8id/5id -Dc -Sazure2 -Gwheat -Wfaint -A5000 -p200/40 -K > $ps
+$AWK '{print $1, $2, $3}' languages.txt \
+	| gmt pstext -R -J -O -K -p -D-0.2i/0 -F+f30p,Helvetica-Bold,firebrick=thinner+jRM >> $ps
+gmt psxyz languages.txt -R-180/180/-90/90/0/2500 -J -JZ2.5i -So0.3i -Gdarkgreen -Wthinner \
+	--FONT_TITLE=30p,Times-Bold --MAP_TITLE_OFFSET=-0.7i -O -p --FORMAT_GEO_MAP=dddF \
+	-Bx60 -By30 -Bza500+lLanguages -BWSneZ+t"World Languages By Continent" >> $ps
