ac42a2b3925c8ef4922cdd8a13d12f0b5032ce4d
Remko Scharroo <remko.scharroo@eumetsat.int>
2013-10-31 21:03:41 +0000

    Few more tweaks to example 10

--- doc/examples/ex10/example_10.sh
+++ doc/examples/ex10/example_10.sh
@@ -9,7 +9,7 @@
 ps=example_10.ps
 gmt pscoast -Rd -JX8id/5id -Dc -Sazure2 -Gwheat -Wfaint -A5000 -p200/40 -K > $ps
 $AWK '{print $1, $2, $3+$4+$5+$6+$7}' languages.txt \
-	| gmt pstext -R -J -O -K -p -D-0.2i/0 -F+f30p,Helvetica-Bold,firebrick=thinner+jRM >> $ps
+	| gmt pstext -R -J -O -K -p -Gwhite@30 -D-0.2i/0 -F+f30p,Helvetica-Bold,firebrick=thinner+jRM >> $ps
 gmt psxyz languages.txt -R-180/180/-90/90/0/2500 -J -JZ2.5i -So0.3i -Gpurple -Wthinner \
 	--FONT_TITLE=30p,Times-Bold --MAP_TITLE_OFFSET=-0.7i -O -K -p --FORMAT_GEO_MAP=dddF \
 	-Bx60 -By30 -Bza500+lLanguages -BWSneZ+t"World Languages By Continent" >> $ps
@@ -21,15 +21,4 @@ $AWK '{print $1, $2, $3+$4+$5+$6, $3+$4+$5}' languages.txt \
 	| gmt psxyz -R -J -JZ -So0.3ib -Gyellow -Wthinner -O -K -p >> $ps
 $AWK '{print $1, $2, $3+$4+$5+$6+$7, $3+$4+$5+$6}' languages.txt \
 	| gmt psxyz -R -J -JZ -So0.3ib -Gred -Wthinner -O -K -p >> $ps
-gmt pslegend -R -J -JZ -D-170/-80/1.5i/0/BL -O --FONT=Helvetica-Bold -F+glightgrey+pthinner -p >> $ps <<END
-C red
-L - - L dying
-C yellow
-L - - L in trouble
-C darkgreen
-L - - L vigorous
-C blue
-L - - L developing
-C purple
-L - - L institutional
-END
+gmt pslegend -R -J -JZ -D-170/-80/1.2i/0/BL -O --FONT=Helvetica-Bold -F+glightgrey+pthinner+s-4p/-6p/grey20@40 -p legend.txt >> $ps
