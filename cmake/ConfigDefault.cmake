01b283e4ff03bb9be4e78314692887b16eb5e00d
Florian Wobbe <florian@eprofs.de>
2013-11-05 00:05:25 +0000

    Freeze release 5.1.0.

--- cmake/ConfigDefault.cmake
+++ cmake/ConfigDefault.cmake
@@ -44,14 +44,14 @@ set (GMT_PACKAGE_VERSION_PATCH "0")
 # This is manually set when making GMT *public* releases.
 # However, when making internal releases or just an ordinary developer build, leave it
 # empty; if it is empty, the revision number is automatically populated for you on build.
-#set (GMT_SOURCE_CODE_CONTROL_VERSION_STRING "9047")
+set (GMT_SOURCE_CODE_CONTROL_VERSION_STRING "12452")
 
 # The GMT package version.
 set (GMT_PACKAGE_VERSION "${GMT_PACKAGE_VERSION_MAJOR}.${GMT_PACKAGE_VERSION_MINOR}.${GMT_PACKAGE_VERSION_PATCH}")
 
 # GMT_VERSION_YEAR set to current date in cmake/modules/ConfigCMake.cmake
 # if not specified here:
-#set (GMT_VERSION_YEAR "2013")
+set (GMT_VERSION_YEAR "2013")
 
 # The GMT wiki location
 set (GMT_TRAC_WIKI "http://gmt.soest.hawaii.edu/")
