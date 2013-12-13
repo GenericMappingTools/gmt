1d732e4ac7517cdbf1901f400b96fb020bffc8f8
Paul Wessel <pwessel@hawaii.edu>
2013-12-13 21:20:46 +0000

    Eliminate gmt_core_module.h and gmt_supplements_module.h

--- trunk/src/gmt_core_module.c
+++ trunk/src/gmt_core_module.c
@@ -17,7 +17,86 @@
 
 #include "gmt_dev.h"
 #ifndef BUILD_SHARED_LIBS
-#include "gmt_core_module.h"
+EXTERN_MSC int GMT_blockmean (void *API, int mode, void *args);
+EXTERN_MSC int GMT_blockmedian (void *API, int mode, void *args);
+EXTERN_MSC int GMT_blockmode (void *API, int mode, void *args);
+EXTERN_MSC int GMT_filter1d (void *API, int mode, void *args);
+EXTERN_MSC int GMT_fitcircle (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmt2kml (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtconnect (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtconvert (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtdefaults (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtget (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtinfo (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtmath (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtselect (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtset (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtsimplify (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtspatial (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtvector (void *API, int mode, void *args);
+EXTERN_MSC int GMT_gmtwhich (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grd2cpt (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grd2rgb (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grd2xyz (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdblend (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdclip (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdcontour (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdcut (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdedit (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdfft (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdfilter (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdgradient (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdhisteq (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdimage (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdinfo (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdlandmask (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdmask (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdmath (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdpaste (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdproject (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdraster (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdreformat (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdsample (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdtrack (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdtrend (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdvector (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdview (void *API, int mode, void *args);
+EXTERN_MSC int GMT_grdvolume (void *API, int mode, void *args);
+EXTERN_MSC int GMT_greenspline (void *API, int mode, void *args);
+EXTERN_MSC int GMT_kml2gmt (void *API, int mode, void *args);
+EXTERN_MSC int GMT_makecpt (void *API, int mode, void *args);
+EXTERN_MSC int GMT_mapproject (void *API, int mode, void *args);
+EXTERN_MSC int GMT_nearneighbor (void *API, int mode, void *args);
+EXTERN_MSC int GMT_project (void *API, int mode, void *args);
+EXTERN_MSC int GMT_ps2raster (void *API, int mode, void *args);
+EXTERN_MSC int GMT_psbasemap (void *API, int mode, void *args);
+EXTERN_MSC int GMT_psclip (void *API, int mode, void *args);
+EXTERN_MSC int GMT_pscoast (void *API, int mode, void *args);
+EXTERN_MSC int GMT_pscontour (void *API, int mode, void *args);
+EXTERN_MSC int GMT_pshistogram (void *API, int mode, void *args);
+EXTERN_MSC int GMT_psimage (void *API, int mode, void *args);
+EXTERN_MSC int GMT_pslegend (void *API, int mode, void *args);
+EXTERN_MSC int GMT_psmask (void *API, int mode, void *args);
+EXTERN_MSC int GMT_psrose (void *API, int mode, void *args);
+EXTERN_MSC int GMT_psscale (void *API, int mode, void *args);
+EXTERN_MSC int GMT_pstext (void *API, int mode, void *args);
+EXTERN_MSC int GMT_pswiggle (void *API, int mode, void *args);
+EXTERN_MSC int GMT_psxyz (void *API, int mode, void *args);
+EXTERN_MSC int GMT_psxy (void *API, int mode, void *args);
+EXTERN_MSC int GMT_read (void *API, int mode, void *args);
+EXTERN_MSC int GMT_sample1d (void *API, int mode, void *args);
+EXTERN_MSC int GMT_spectrum1d (void *API, int mode, void *args);
+EXTERN_MSC int GMT_sph2grd (void *API, int mode, void *args);
+EXTERN_MSC int GMT_sphdistance (void *API, int mode, void *args);
+EXTERN_MSC int GMT_sphinterpolate (void *API, int mode, void *args);
+EXTERN_MSC int GMT_sphtriangulate (void *API, int mode, void *args);
+EXTERN_MSC int GMT_splitxyz (void *API, int mode, void *args);
+EXTERN_MSC int GMT_surface (void *API, int mode, void *args);
+EXTERN_MSC int GMT_trend1d (void *API, int mode, void *args);
+EXTERN_MSC int GMT_trend2d (void *API, int mode, void *args);
+EXTERN_MSC int GMT_triangulate (void *API, int mode, void *args);
+EXTERN_MSC int GMT_write (void *API, int mode, void *args);
+EXTERN_MSC int GMT_xyz2grd (void *API, int mode, void *args);
 #endif
 
 static inline struct GMTAPI_CTRL * gmt_get_api_ptr (struct GMTAPI_CTRL *ptr) {return (ptr);}
