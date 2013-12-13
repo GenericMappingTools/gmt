2f26b1bb87afda0aff16ffdb4c79b61c494d6a69
Paul Wessel <pwessel@hawaii.edu>
2013-12-13 21:30:08 +0000

    Eliminate gmt_compat.h

--- src/gmt_compat.c
+++ src/gmt_compat.c
@@ -31,7 +31,7 @@ int GMT_gmtdp (void *V_API, int mode, void *args)
 	struct GMTAPI_CTRL *API = GMT_get_API_ptr (V_API);	/* Cast from void to GMTAPI_CTRL pointer */
 	if (GMT_compat_check (API->GMT, 4)) {
 		GMT_Report (API, GMT_MSG_COMPAT, "Warning: module gmtdp is deprecated; use gmtsimplify.\n");
-		return (GMT_gmtsimplify (V_API, mode, args));
+		return (GMT_Call_Module (API, "gmtsimplify", mode, args));
 	}
 	GMT_Report (API, GMT_MSG_NORMAL, "Shared GMT module not found: gmtdpi\n");
 	return (GMT_NOT_A_VALID_MODULE);
@@ -42,7 +42,7 @@ int GMT_minmax (void *V_API, int mode, void *args)
 	struct GMTAPI_CTRL *API = GMT_get_API_ptr (V_API);	/* Cast from void to GMTAPI_CTRL pointer */
 	if (GMT_compat_check (API->GMT, 4)) {
 		GMT_Report (API, GMT_MSG_COMPAT, "Warning: module minmax is deprecated; use gmtinfo.\n");
-		return (GMT_gmtinfo (V_API, mode, args));
+		return (GMT_Call_Module (API, "gmtinfo", mode, args));
 	}
 	GMT_Report (API, GMT_MSG_NORMAL, "Shared GMT module not found: minmax\n");
 	return (GMT_NOT_A_VALID_MODULE);
@@ -53,7 +53,7 @@ int GMT_gmtstitch (void *V_API, int mode, void *args)
 	struct GMTAPI_CTRL *API = GMT_get_API_ptr (V_API);	/* Cast from void to GMTAPI_CTRL pointer */
 	if (GMT_compat_check (API->GMT, 4)) {
 		GMT_Report (API, GMT_MSG_COMPAT, "Warning: module gmtstitch is deprecated; use gmtconnect.\n");
-		return (GMT_gmtconnect (V_API, mode, args));
+		return (GMT_Call_Module (API, "gmtconnect", mode, args));
 	}
 	GMT_Report (API, GMT_MSG_NORMAL, "Shared GMT module not found: gmtstitch\n");
 	return (GMT_NOT_A_VALID_MODULE);
