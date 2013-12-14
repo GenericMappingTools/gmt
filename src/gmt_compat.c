e92d38cf98e98fc9f27b67cb83b4ae6fca2e4b3c
Paul Wessel <pwessel@hawaii.edu>
2013-12-14 20:52:08 +0000

    gmt_compat.h meat inserted into gmt_compat.c works for Windows also

--- src/gmt_compat.c
+++ src/gmt_compat.c
@@ -26,6 +26,10 @@
 
 #include "gmt_dev.h"
 
+EXTERN_MSC int GMT_gmtdp (void *V_API, int mode, void *args);
+EXTERN_MSC int GMT_minmax (void *V_API, int mode, void *args);
+EXTERN_MSC int GMT_gmtstitch (void *V_API, int mode, void *args);
+
 int GMT_gmtdp (void *V_API, int mode, void *args)
 {	/* This was the GMT4 name */
 	struct GMTAPI_CTRL *API = GMT_get_API_ptr (V_API);	/* Cast from void to GMTAPI_CTRL pointer */
