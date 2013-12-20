5aadbfaa40aa30320e6ab49baaf9eafe916992d7
Paul Wessel <pwessel@hawaii.edu>
2013-12-20 03:31:56 +0000

    Finished testing grdseamount

--- test/potential/growth.sh
+++ test/potential/growth.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 #	$Id$
 #
-# Test the output of gmt grdseamount for Gaussian shapes
+# Test the output of gmt grdseamount for evolving Gaussian shapes
 ps=growth.ps
-m=c
-f=0.25
+m=g
+f=0.2
 gmt gmtset MAP_FRAME_TYPE plain
 cat << EOF > t.txt
-#lon lat azimuth, semi-major, semi-minor, height
-50	75	110	60	30	5000	6	2
+#lon lat azimuth, semi-major, semi-minor, height tstart tend
+50	75	-20	60	30	5000	6	2
 150	75	30	50	40	4000	10	4
 EOF
-#echo "100 75 0 50 50 5000 10 0" > t.txt
-#echo "100 75 50 5000 10 0" > t.txt
-#grdseamount -Rk0/200/0/150 -I1000 -Gsmt_%3.1f.nc t.txt -T10/0/1 -V -Cg -Qc/g -Dk
-grdseamount -Rk0/200/0/150 -I1000 -Gsmt_%4.2f.nc t.txt -T10/0/0.1 -V -Qc/g -Dk -E -F$f -C$m
-grdseamount -Rk0/200/0/150 -I1000 -Gsmt.nc t.txt -C$m -Dk -E -F$f
-#grdseamount -Rk0/200/0/150 -I1000 -Gsmt_0.0.nc t.txt -Vl -Cg -Dk -E
-grdcontour smt_0.00.nc+Uk -Jx0.03i -Xc -P -C250 -A1000 -Bafg -K > $ps
-grdtrack -Gsmt_0.00.nc+Uk -E0/75/200/75 -o0,1 -nn | psxy -R0/200/0/150 -J -O -K -W1p >> $ps
-grdtrack -Gsmt_0.00.nc+Uk -E0/75/200/75 -o0,2 -nn | psxy -R0/200/0/5050 -JX6i/4i -O -K -Y5i -Bafg >> $ps
-grdtrack -Gsmt.nc+Uk -E0/75/200/75 -o0,2 -nn | psxy -R -J -O -K -W0.25p,red >> $ps
+grdseamount -Rk0/200/0/150 -I1000 -Gsmt_%05.2f.nc t.txt -T10/0/0.25 -Qc/l -Dk -E -F$f -C$m -Z-1
+grdseamount -Rk0/200/0/150 -I1000 -Gsmt.nc t.txt -C$m -Dk -E -F$f -Z-1
+grdcontour smt_00.00.nc+Uk -Jx0.03i -Xc -P -C500 -A1000 -GlLM/RM -Bafg -K > $ps
+grdcontour smt.nc+Uk -J -O -K -C500 -A1000 -Gl50/20/50/130,150/20/150/130 -Wc0.25p,red -Wa0.75p,red --FONT_ANNOT_PRIMARY=12p,Helvetica,red >> $ps
+grdtrack -Gsmt_00.00.nc+Uk -E0/75/200/75 -o0,1 -nn | psxy -R0/200/20/130 -J -O -K -W1p >> $ps
+ls smt_*.*.nc > t.lis
+makecpt -Cjet -T0/41/1 -N > t.cpt
+makecpt -Cjet -T0/41/1 -N -I> t2.cpt
+psbasemap -R0/200/0/5100 -JX6i/3i -O -K -Y6i -Bafg >> $ps
+let k=1
+while read file; do
+	rgb=`sed -n ${k}p t.cpt | awk '{print $2}'`
+	if [ $k -eq 23 ]; then
+		rgb=black
+	fi
+	grdtrack -G${file}+Uk -E0/75/200/75 -o0,2 -nn | psxy -R -J -O -K -G$rgb -Wfaint >> $ps
+	let k=k+1
+done < t.lis
+grdtrack -Gsmt.nc+Uk -E0/75/200/75 -o0,2 -nn | psxy -R -J -O -K -W1p >> $ps
+psscale -Ct2.cpt -D3i/-0.4i/5.5i/0.15ih -O -K -Baf -Bx+l"Time of emplacement" >> $ps
 psxy -R -J -O -T >> $ps
 ps2raster -Tf $ps
 open growth.pdf
-rm -f smt_*.??.nc
+rm -f smt_*.??.nc smt.nc t.lis t.cpt t2.cpt t.txt
