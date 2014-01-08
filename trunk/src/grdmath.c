2f03f442dcc2a84ab618f8572e03687ded902d69
Remko Scharroo <remko.scharroo@eumetsat.int>
2014-01-08 20:15:44 +0000

    Removed LDIST1 again from this version. Will be put in 5.2.0 instead.

--- trunk/src/grdmath.c
+++ trunk/src/grdmath.c
@@ -1862,74 +1862,19 @@ void grd_LDIST (struct GMT_CTRL *GMT, struct GRDMATH_INFO *info, struct GRDMATH_
 {
 	uint64_t node, row, col;
 	double d;
-	struct GMT_DATATABLE *T = NULL;
+	struct GMT_DATATABLE *line = NULL;
 	struct GMT_DATASET *D = NULL;
 
 	if ((D = ASCII_read (GMT, info, GMT_IS_LINE, "LDIST")) == NULL) return;
-	T = D->table[0];	/* Only one table in a single file */
+	line = D->table[0];	/* Only one table in a single file */
 
 	GMT_grd_padloop (GMT, info->G, row, col, node) {	/* Visit each node */
-		if (col == 0) GMT_Report (GMT->parent, GMT_MSG_LONG_VERBOSE, "Row %d\n", row);
-		(void) GMT_near_lines (GMT, info->d_grd_x[col], info->d_grd_y[row], T, true, &d, NULL, NULL);
+		(void) GMT_near_lines (GMT, info->d_grd_x[col], info->d_grd_y[row], line, true, &d, NULL, NULL);
 		stack[last]->G->data[node] = (float)d;
+		if (col == 0) GMT_Report (GMT->parent, GMT_MSG_LONG_VERBOSE, "Row %d\n", row);
 	}
 
 	ASCII_free (GMT, info, &D, "LDIST");	/* Free memory used for line */
-}
-
-void grd_LDIST1 (struct GMT_CTRL *GMT, struct GRDMATH_INFO *info, struct GRDMATH_STACK *stack[], unsigned int last)
-/*OPERATOR: LDIST1 1 1 Compute distance (in degrees if -fg) from lines in multi-segment ASCII file A; Optimized for coastlines.  */
-{
-	uint64_t node, row, col, seg;
-	int i, old_i = 999999, old_row = 999999;
-	double d;
-	struct GMT_DATATABLE *T = NULL;
-	struct GMT_DATASET *D = NULL;
-	double lon, lon1, lat, x, y, hor, *dist, bin_size = 1.0;
-
-	if ((D = ASCII_read (GMT, info, GMT_IS_LINE, "LDIST1")) == NULL) return;
-	T = D->table[0];	/* Only one table in a single file */
-
-	/* Check if this is indeed a coastline data set */
-	if (!strncmp(T->header[0], "# Coastlines extracted from the full resolution", 47))
-		bin_size = 1.0;
-	else if (!strncmp(T->header[0], "# Coastlines extracted from the high resolution", 47))
-		bin_size = 2.0;
-	else if (!strncmp(T->header[0], "# Coastlines extracted from the intermediate resolution", 55))
-		bin_size = 5.0;
-	else if (!strncmp(T->header[0], "# Coastlines extracted from the low resolution", 46))
-		bin_size = 10.0;
-	else if (!strncmp(T->header[0], "# Coastlines extracted from the crude resolution", 48))
-		bin_size = 20.0;
-	else
-		GMT_Report (GMT->parent, GMT_MSG_NORMAL, "Error: input is not coast line data set. May lead to erroneous results.\n");
-	
-	dist = GMT_memory (GMT, NULL, T->n_segments, double);
-
-	GMT_grd_padloop (GMT, info->G, row, col, node) {	/* Visit each node */
-		if (col == 0) GMT_Report (GMT->parent, GMT_MSG_LONG_VERBOSE, "Row %d\n", row);
-		lon = info->d_grd_x[col], lat = info->d_grd_y[row];
-		i = floor(lon/bin_size);
-		if (i != old_i || row != old_row) {
-			lon1 = (i + 0.5) * bin_size;
-			for (seg = 0, hor = DBL_MAX; seg < T->n_segments; seg++) {
-				x = 0.5 * (T->segment[seg]->min[GMT_X] + T->segment[seg]->max[GMT_X]);
-				y = 0.5 * (T->segment[seg]->min[GMT_Y] + T->segment[seg]->max[GMT_Y]);
-				dist[seg] = GMT_distance (GMT, lon1, lat, x, y);
-				if (dist[seg] < hor) hor = dist[seg];
-			}
-			hor = hor + 1.5 * bin_size;
-			old_i = i, old_row = row;
-		}
-
-		for (seg = 0, d = DBL_MAX; seg < T->n_segments; seg++) {	/* Loop over each line segment */
-			if (dist[seg] < hor) (void) GMT_near_a_line (GMT, lon, lat, seg, T->segment[seg], true, &d, NULL, NULL);
-		}
-		stack[last]->G->data[node] = (float)d;
-	}
-
-	GMT_free (GMT, dist);
-	ASCII_free (GMT, info, &D, "LDIST1");	/* Free memory used for line */
 }
 
 void grd_LDIST2 (struct GMT_CTRL *GMT, struct GRDMATH_INFO *info, struct GRDMATH_STACK *stack[], unsigned int last)
@@ -1938,21 +1883,21 @@ void grd_LDIST2 (struct GMT_CTRL *GMT, struct GRDMATH_INFO *info, struct GRDMATH
 	uint64_t node, row, col;
 	unsigned int prev;
 	double d;
-	struct GMT_DATATABLE *T = NULL;
+	struct GMT_DATATABLE *line = NULL;
 	struct GMT_DATASET *D = NULL;
 
 	if ((D = ASCII_read (GMT, info, GMT_IS_LINE, "LDIST2")) == NULL) return;
-	T = D->table[0];	/* Only one table in a single file */
+	line = D->table[0];	/* Only one table in a single file */
 	prev = last - 1;
 
 	GMT_grd_padloop (GMT, info->G, row, col, node) {	/* Visit each node */
-		if (col == 0) GMT_Report (GMT->parent, GMT_MSG_LONG_VERBOSE, "Row %d\n", row);
 		if (stack[prev]->G->data[node] == 0.0)
 			stack[prev]->G->data[node] = GMT->session.f_NaN;
 		else {
-			(void) GMT_near_lines (GMT, info->d_grd_x[col], info->d_grd_y[row], T, true, &d, NULL, NULL);
+			(void) GMT_near_lines (GMT, info->d_grd_x[col], info->d_grd_y[row], line, true, &d, NULL, NULL);
 			stack[prev]->G->data[node] = (float)d;
 		}
+		if (col == 0) GMT_Report (GMT->parent, GMT_MSG_LONG_VERBOSE, "Row %d\n", row);
 	}
 
 	ASCII_free (GMT, info, &D, "LDIST2");	/* Free memory used for line */
