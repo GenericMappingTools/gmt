f83f26d4db6cb87f75e8639ebc3ee6740d51dac6
Paul Wessel <pwessel@hawaii.edu>
2014-04-21 22:46:55 +0000

    Set portraint

--- test/grdcontour/grdtxtclip.sh
+++ test/grdcontour/grdtxtclip.sh
@@ -2,5 +2,5 @@
 ps=grdtxtclip.ps
 grdcontour test.grd -R-74.25/-63.75/-44.0/-39.5 \
        -JB-69/-41.75/-40.625/-42.875/6i -Ba1 \
-       -C500 -A1000+e+p+ggreen@50 -K > $ps 
+       -C500 -A1000+e+p+ggreen@50 -K -P > $ps 
 psclip -Cs -O >> $ps
