1f6db979cb99f37d71ba2424cf4b5d69b758f75e
Paul Wessel <pwessel@hawaii.edu>
2014-05-30 04:59:50 +0000

    Add gmt calls

--- test/psxy/repeat1.sh
+++ test/psxy/repeat1.sh
@@ -1,9 +1,9 @@
 #	$Id$
 # Test various repeating/clip mode for periodic and straight boundary
 function map { #  $1 = -N option, $2 is text, $3 is any -Y option
-        psxy -R -J t.txt -Ct.cpt -Sc -Bafg -BWsne -O -K --MAP_FRAME_TYPE=plain $1 $3
-        psxy -R -J -O -K t.txt -Gblack -Sc0.1c
-	echo 180 0 $2 | pstext -R -J -O -K -F+f16p -Gwhite >> $ps
+        gmt psxy -R -J t.txt -Ct.cpt -Sc -Bafg -BWsne -O -K --MAP_FRAME_TYPE=plain $1 $3
+        gmt psxy -R -J -O -K t.txt -Gblack -Sc0.1c
+	echo 180 0 $2 | gmt pstext -R -J -O -K -F+f16p -Gwhite >> $ps
 }
 ps=repeat1.ps
 cat << EOF > t.txt
@@ -16,9 +16,9 @@ cat << EOF > t.cpt
 1	blue	2	blue
 2	green	3	green
 EOF
-psxy -R0/360/-60/60 -JM5i -T -P -K -Xc > $ps
+gmt psxy -R0/360/-60/60 -JM5i -T -P -K -Xc > $ps
 map "" Default >> $ps
 map -N -N  -Y2.3i >> $ps
 map -Nr -Nr -Y2.3i >> $ps
 map -Nc -Nc -Y2.3i >> $ps
-psxy -R -J -O -T >> $ps
+gmt psxy -R -J -O -T >> $ps
--- test/psxy/repeat2.sh
+++ test/psxy/repeat2.sh
@@ -2,9 +2,9 @@
 #	$Id$
 # Test various repeating/clip mode for periodic and non-straight boundary
 function map { #  $1 = -N option, $2 is text, $3 is any -Y option
-        psxy -R -J t.txt -Ct.cpt -Sc -Bafg -BWsne -O -K --MAP_FRAME_TYPE=plain $1 $3
-        psxy -R -J -O -K t.txt -Gblack -Sc0.1c
-	echo 180 0 $2 | pstext -R -J -O -K -F+f16p -Gwhite >> $ps
+        gmt psxy -R -J t.txt -Ct.cpt -Sc -Bafg -BWsne -O -K --MAP_FRAME_TYPE=plain $1 $3
+        gmt psxy -R -J -O -K t.txt -Gblack -Sc0.1c
+	echo 180 0 $2 | gmt pstext -R -J -O -K -F+f16p -Gwhite >> $ps
 }
 ps=repeat2.ps
 cat << EOF > t.txt
@@ -17,9 +17,9 @@ cat << EOF > t.cpt
 1	blue	2	blue
 2	green	3	green
 EOF
-psxy -Rg -JH4.4i -T -P -K -Xc > $ps
+gmt psxy -Rg -JH4.4i -T -P -K -Xc > $ps
 map "" Default >> $ps
 map -N -N  -Y2.3i >> $ps
 map -Nr -Nr -Y2.3i >> $ps
 map -Nc -Nc -Y2.3i >> $ps
-psxy -R -J -O -T >> $ps
+gmt psxy -R -J -O -T >> $ps
