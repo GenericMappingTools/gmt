7af5c999da2687962cbc9a7a3b35707c1977f775
Paul Wessel <pwessel@hawaii.edu>
2014-06-05 02:02:15 +0000

    Add gmt command to new time_testing_9.sh script

--- test/time/time_testing_9.sh
+++ test/time/time_testing_9.sh
@@ -12,13 +12,13 @@ cat << EOF > tt9.answer
 9.00136798906
 9.00136798906
 EOF
-echo "2000-10-20T12:00:00  0" | mapproject -R1999T/2001T/0/1 -JX10cT/10c -o0      > tt9.result
-echo "2000-OCT-20T12:00:00 0" | mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-o-dd  >> tt9.result
-echo "2000-20-OCTT12:00:00 0" | mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-dd-o  >> tt9.result
-echo "2000-10-20T 	   0" | mapproject -R -J -o0                             >> tt9.result
-echo "2000-10-20	   0" | mapproject -R -J -o0                             >> tt9.result
-echo "2000-OCT-20T	   0" | mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-o-dd  >> tt9.result
-echo "2000-OCT-20	   0" | mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-o-dd  >> tt9.result
-echo "2000-20-OCT	   0" | mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-dd-o  >> tt9.result
-echo "2000-20-10	   0" | mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-dd-mm >> tt9.result
+echo "2000-10-20T12:00:00  0" | gmt mapproject -R1999T/2001T/0/1 -JX10cT/10c -o0      > tt9.result
+echo "2000-OCT-20T12:00:00 0" | gmt mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-o-dd  >> tt9.result
+echo "2000-20-OCTT12:00:00 0" | gmt mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-dd-o  >> tt9.result
+echo "2000-10-20T 	   0" | gmt mapproject -R -J -o0                             >> tt9.result
+echo "2000-10-20	   0" | gmt mapproject -R -J -o0                             >> tt9.result
+echo "2000-OCT-20T	   0" | gmt mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-o-dd  >> tt9.result
+echo "2000-OCT-20	   0" | gmt mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-o-dd  >> tt9.result
+echo "2000-20-OCT	   0" | gmt mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-dd-o  >> tt9.result
+echo "2000-20-10	   0" | gmt mapproject -R -J -o0 --FORMAT_DATE_IN=yyyy-dd-mm >> tt9.result
 diff tt9.result tt9.answer --strip-trailing-cr > fail
