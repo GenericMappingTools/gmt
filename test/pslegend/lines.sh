2465c0a621b48bc9063df1026ff682b48acd9bec
Paul Wessel <pwessel@hawaii.edu>
2014-07-19 03:49:34 +0000

    Update lines.sh

--- test/pslegend/lines.sh
+++ test/pslegend/lines.sh
@@ -18,6 +18,6 @@ S 1.3i f+c	2i/0.4i/0.1i	white	1.0p		2.6i	Some path goes here
 S 1.3i f+c+o0.2i+p0.25p,yellow	2i/0.4i/0.1i	gray	1.0p,.		2.6i	Another path is offset
 S 1.3i qn1:+lPerimeter	2i 		-	2.5p,orange	2.6i	A boundary between things
 S 1.3i qn2:+l5m	2i		-	1.5p		2.6i	5-m contour line
-S 1.3i qn8:+l\031	2i	-	1.5p,red		2.6i	Frontier between angry folk
+S 1.3i qn8:+l\267	2i	-	1.5p,red		2.6i	Frontier between angry folk
 S 1.3i qN2:+lA+x	2i	-	1.5p,brown		2.6i	A geologic crossection
 EOF
