70d21e99eb5e7b5dd266fa01abc2bf7604aecf56
Paul Wessel <pwessel@hawaii.edu>
2014-08-21 23:28:14 +0000

    Fix script

--- test/psxy/lineclip.sh
+++ test/psxy/lineclip.sh
@@ -10,10 +10,10 @@ cat <<END > t.txt
 -131.69480 9.80915
 END
 
-mapproject -R-131.74249/9.8087739/-131.65578/9.883111r -Ju9/1:90000 t.txt -Di > t.xy
-psxy -R-1/10/-1/10 -Jx1i t.xy -W0.25p,- -K -P -X0 -Y0 > $ps
-psxy -A -R-131.74249/9.8087739/-131.65578/9.883111r -Ju9/1:90000 -L t.txt -W3p -O -K -X1i -Y1i >> $ps
-psxy -R -J -O -K -N -Sc0.25i -Gyellow -W0.25p t.txt -Baf >> $ps
+gmt mapproject -R-131.74249/9.8087739/-131.65578/9.883111r -Ju9/1:90000 t.txt -Di > t.xy
+gmt psxy -R-1/10/-1/10 -Jx1i t.xy -W0.25p,- -K -P -X0 -Y0 > $ps
+gmt psxy -A -R-131.74249/9.8087739/-131.65578/9.883111r -Ju9/1:90000 -L t.txt -W3p -O -K -X1i -Y1i >> $ps
+gmt psxy -R -J -O -K -N -Sc0.25i -Gyellow -W0.25p t.txt -Baf >> $ps
 awk '{print $1, $2, NR-1}' t.txt | pstext -R -J -O -K -N -F+f12p+jCM >> $ps
 
 # Change first y coordinate from 9.882795 to 9.882794
@@ -24,8 +24,8 @@ cat <<END > t.txt
 -131.69480 9.80915
 END
 
-mapproject -R-131.74249/9.8087739/-131.65578/9.883111r -Ju9/1:90000 t.txt -Di > t.xy
-psxy -R-1/10/-1/10 -Jx1i t.xy -W0.25p,- -K -O -Y4i -X-1i >> $ps
-psxy -A -R-131.74249/9.8087739/-131.65578/9.883111r -Ju9/1:90000 -L t.txt -W3p -O -K -X1i -Y1i >> $ps
-psxy -R -J -O -K -N -Sc0.25i -Gyellow -W0.25p t.txt -Baf >> $ps
+gmt mapproject -R-131.74249/9.8087739/-131.65578/9.883111r -Ju9/1:90000 t.txt -Di > t.xy
+gmt psxy -R-1/10/-1/10 -Jx1i t.xy -W0.25p,- -K -O -Y4i -X-1i >> $ps
+gmt psxy -A -R-131.74249/9.8087739/-131.65578/9.883111r -Ju9/1:90000 -L t.txt -W3p -O -K -X1i -Y1i >> $ps
+gmt psxy -R -J -O -K -N -Sc0.25i -Gyellow -W0.25p t.txt -Baf >> $ps
 awk '{print $1, $2, NR-1}' t.txt | pstext -R -J -O -N -F+f12p+jCM >> $ps
