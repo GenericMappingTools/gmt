2ad28de2d392e4083437bd368d6c62edc38e2b98
Paul Wessel <pwessel@hawaii.edu>
2014-10-20 23:16:50 +0000

    Fix bug report via forum post 1088

--- src/meca/utilmeca.c
+++ src/meca/utilmeca.c
@@ -1243,6 +1243,8 @@ int trace_cross (struct GMT_CTRL *GMT, double slon, double slat, double eps1, do
 	/* local */
 	double dx, dy, x1, x2, y1, y2, hl, hw, vw, s, c, dim[PSL_MAX_DIMS];
 
+	GMT_setpen (GMT, &pen);		/* Pen for segment line */
+	PSL_setfill (GMT->PSL, pen.rgb, 0);	/* Same color for arrow head fill with no outline */
 	sincosd (theta, &s, &c);
 
 	/*  extension component */
@@ -1272,8 +1274,7 @@ int trace_cross (struct GMT_CTRL *GMT, double slon, double slat, double eps1, do
 	dim[0] = x2, dim[1] = y2;
 	dim[2] = vw, dim[3] = hl, dim[4] = hw;
 	dim[5] = vector_shape, dim[6] = GMT_VEC_END | GMT_VEC_FILL;
-	PSL_setcolor (GMT->PSL, pen.rgb, PSL_IS_STROKE);
-	PSL_plotsymbol (GMT->PSL, x1, x2, dim, PSL_VECTOR);
+	PSL_plotsymbol (GMT->PSL, x1, y1, dim, PSL_VECTOR);
 
 	/* second, extensional arrow in opposite direction */
 
@@ -1298,13 +1299,12 @@ int trace_cross (struct GMT_CTRL *GMT, double slon, double slat, double eps1, do
 
 	dim[0] = x2, dim[1] = y2;
 	dim[2] = vw, dim[3] = hl, dim[4] = hw;
-	PSL_setcolor (GMT->PSL, pen.rgb, PSL_IS_STROKE);
 	PSL_plotsymbol (GMT->PSL, x1, y1, dim, PSL_VECTOR);
 
 	/* compression component */
 	dx = eps2 * s;
 	dy = eps2 * c;
-
+	dim[6] = GMT_VEC_BEGIN | GMT_VEC_FILL;
 	trace_arrow (GMT, slon, slat, dx, dy, sscale, &x1, &y1, &x2, &y2);
 
 	if (eps2 > 0.0) {
@@ -1327,7 +1327,6 @@ int trace_cross (struct GMT_CTRL *GMT, double slon, double slat, double eps1, do
 
 	dim[0] = x2, dim[1] = y2;
 	dim[2] = vw, dim[3] = hl, dim[4] = hw;
-	PSL_setcolor (GMT->PSL, pen.rgb, PSL_IS_STROKE);
 	PSL_plotsymbol (GMT->PSL, x1, y1, dim, PSL_VECTOR);
 
 	/* second, compressional arrow in opposite direction */
@@ -1355,7 +1354,6 @@ int trace_cross (struct GMT_CTRL *GMT, double slon, double slat, double eps1, do
 
 	dim[0] = x2, dim[1] = y2;
 	dim[2] = vw, dim[3] = hl, dim[4] = hw;
-	PSL_setcolor (GMT->PSL, pen.rgb, PSL_IS_STROKE);
 	PSL_plotsymbol (GMT->PSL, x1, y1, dim, PSL_VECTOR);
 
 	return 0;
