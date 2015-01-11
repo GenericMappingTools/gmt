de9e82e1d70221b65366f0d3506297495f398bd1
Paul Wessel <pwessel@hawaii.edu>
2015-01-11 01:53:13 +0000

    Update invalidate_modules with new items

--- test/invalidate_modules.sh
+++ test/invalidate_modules.sh
@@ -12,10 +12,10 @@
 #
 
 gmt_modules="backtracker blockmean blockmedian blockmode dimfilter filter1d \
-fitcircle gmt2kml gmtconvert gmtdefaults gmtget gmtgravmag3d gmtinfo gmtmath \
-gmtselect gmtset gmtsimplify gmtspatial gmtstitch gmtvector gmtwhich gravfft \
+fitcircle gmt2kml gmtconvert gmtdefaults gmtflexure gmtget gmtgravmag3d gmtinfo gmtmath \
+gmtregress gmtselect gmtset gmtsimplify gmtspatial gmtstitch gmtvector gmtwhich gravfft \
 grd2cpt grd2rgb grd2xyz grdblend grdclip grdcontour grdcut grdedit grdfft \
-grdfilter grdgradient grdgravmag3d grdhisteq grdimage grdinfo grdlandmask \
+grdflexure grdfilter grdgradient grdgravmag3d grdhisteq grdimage grdinfo grdlandmask \
 grdmask grdmath grdpaste grdpmodeler grdproject grdraster grdredpol grdconvert \
 grdrotater grdsample grdseamount grdspotter grdtrack grdtrend grdvector \
 grdview grdvolume greenspline gshhg hotspotter img2grd kml2gmt makecpt \
