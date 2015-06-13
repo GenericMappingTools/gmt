bb1a175bf83f58b564610576412d2c70659c8eae
Paul Wessel <pwessel@hawaii.edu>
2015-06-13 19:18:46 +0000

    Adding cyltest.ps file

--- test/potential/cyltest.sh
+++ test/potential/cyltest.sh
@@ -6,6 +6,9 @@
 ps=cyltest.ps
 # cylinder.mod was made thus:
 #cylinder 25 10 10 1333 5084
+# Make a 1st order correction for the fact that the cubes extend 12.5 meter outside given limits.
+# This works out to a factor of 0.992386602589:
+corr=`gmt math -Q 25000 2 POW 5084 1333 SUB MUL 25000 25 2 DIV ADD 2 POW 5084 1333 SUB 25 ADD MUL DIV =`
 gmt math -T-100/100/1 0 = trk
 gmt talwani3d cylinder.mod -D1670 -Mh -Ntrk -o0,2 > faa.txt
 gmt talwani3d cylinder.mod -D1670 -Mh -Ntrk -o0,2 -Fp > vgg_p.txt
@@ -13,9 +16,9 @@ gmt talwani3d cylinder.mod -D1670 -Mh -Ntrk -o0,2 -Fv > vgg_s.txt
 gmt math -T-100/250/350 -25 = > tmp
 gmt math -T-100/250/350 -I 25  = >> tmp
 gmt psxy -R-100/100/-100/250 -JX6i/6i -P -Glightgray tmp -: -K -Xc -Y4i > $ps
-awk '{print $1, $3}' cylinder25.txt | gmt psxy -R-100000/100000/-100/250 -J -Wfaint,blue -O -K >> $ps
-awk '{print $1, $3}' cylinder25.txt | gmt psxy -R -J -Sc0.1c -Gblue -O -K >> $ps
-awk '{print $1, $2}' cylinder25.txt | gmt psxy -R -J -Sc0.1c -Gred -O -K >> $ps
+awk '{print $1, $3*'"$corr"'}' cylinder25.txt | gmt psxy -R-100000/100000/-100/250 -J -Wfaint,blue -O -K >> $ps
+awk '{print $1, $3*'"$corr"'}' cylinder25.txt | gmt psxy -R -J -Sc0.1c -Gblue -O -K >> $ps
+awk '{print $1, $2*'"$corr"'}' cylinder25.txt | gmt psxy -R -J -Sc0.1c -Gred -O -K >> $ps
 gmt psxy -R-100/100/-100/250 -J faa.txt -W0.25p,red -O -K -Bxafg1000 -Byafg1000 -BWsne+t"Testing FAA and VGG over cylinder" >> $ps
 # Exact value over top at x = 0 only
 gmt math -T-100/100/200 2 PI MUL 6.673e-6 MUL 1670 MUL 5084 1333 SUB 1333 25000 HYPOT ADD 5084 25000 HYPOT SUB MUL = tmp
@@ -26,7 +29,7 @@ cat << EOF > legend.txt
 S 0.2i - 0.3i - 0.5p,- 0.5i FAA (at x = 0)
 S 0.2i c 0.05i red - 0.5i FAA (brute force)
 S 0.2i c 0.05i blue - 0.5i VGG (brute force)
-S 0.2i - 0.3i - 1.5p,red 0.5i FAA
+S 0.2i - 0.3i - 1.5p,red 0.5i FAA (Talwani)
 S 0.2i - 0.3i - 1.5p,darkgreen 0.5i VGG [SSK eq.]
 S 0.2i - 0.3i - 1.5p,orange,2_2:0 0.5i VGG [PW eq.]
 EOF
@@ -38,4 +41,11 @@ gmt psxy -R-100/100/0/5084 -JX6i/-2.5i -O -K -Y-2.75i -Gblack -Bxafg1000+u" km" 
 25	1333
 -25	1333
 EOF
+echo 30 1333 30 5084 | gmt psxy -R -J -O -K -Sv0.2i+bt+et+s -W0.5p >> $ps
+echo -25 1000 25 1000 | gmt psxy -R -J -O -K -Sv0.2i+bt+et+s -W0.5p >> $ps
+echo 0 2500 @~Dr = 1670@~ | gmt pstext -R -J -O -K -F+f18p,Helvetica,white+jCM >> $ps
+echo 0 1000 d = 50 km | gmt pstext -R -J -O -K -F+f14p,Times-Italic+jCM -Gwhite >> $ps
+echo 30 3208.5 3751 m | gmt pstext -R -J -O -K -F+f14p,Times-Italic+jCM+a90 -Gwhite >> $ps
+echo 32 1333 z@-1@- = 1333 m | gmt pstext -R -J -O -K -F+f14p,Times-Italic+jLM -Dj0.1i/0.1i >> $ps
+echo 32 5084 z@-2@- = 5084 m | gmt pstext -R -J -O -K -F+f14p,Times-Italic+jLB -Dj0.1i/0.1i >> $ps
 gmt psxy -R -J -O -T >> $ps
