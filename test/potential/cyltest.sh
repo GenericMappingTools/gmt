7560cfc8b876d8e641e489af0ea31621e1a12ee5
Paul Wessel <pwessel@hawaii.edu>
2015-06-18 21:49:16 +0000

    Update test scripts

--- test/potential/cyltest.sh
+++ test/potential/cyltest.sh
@@ -10,15 +10,15 @@ ps=cyltest.ps
 # This works out to a factor of 0.992386602589:
 corr=`gmt math -Q 25000 2 POW 5084 1333 SUB MUL 25000 25 2 DIV ADD 2 POW 5084 1333 SUB 25 ADD MUL DIV =`
 gmt math -T-100/100/1 0 = trk
-gmt talwani3d cylinder.mod -D1670 -Mh -Ntrk -o0,2 > faa.txt
-gmt talwani3d cylinder.mod -D1670 -Mh -Ntrk -o0,2 -Fp > vgg_p.txt
-gmt talwani3d cylinder.mod -D1670 -Mh -Ntrk -o0,2 -Fv > vgg_s.txt
+gmt talwani3d "${src:-.}"/cylinder.mod -D1670 -Mh -Ntrk -o0,2 > faa.txt
+gmt talwani3d "${src:-.}"/cylinder.mod -D1670 -Mh -Ntrk -o0,2 -Fp > vgg_p.txt
+gmt talwani3d "${src:-.}"/cylinder.mod -D1670 -Mh -Ntrk -o0,2 -Fv > vgg_s.txt
 gmt math -T-100/250/350 -25 = > tmp
 gmt math -T-100/250/350 -I 25  = >> tmp
 gmt psxy -R-100/100/-100/250 -JX6i/6i -P -Glightgray tmp -: -K -Xc -Y4i > $ps
-awk '{print $1, $3*'"$corr"'}' cylinder25.txt | gmt psxy -R-100000/100000/-100/250 -J -Wfaint,blue -O -K >> $ps
-awk '{print $1, $3*'"$corr"'}' cylinder25.txt | gmt psxy -R -J -Sc0.1c -Gblue -O -K >> $ps
-awk '{print $1, $2*'"$corr"'}' cylinder25.txt | gmt psxy -R -J -Sc0.1c -Gred -O -K >> $ps
+awk '{print $1, $3*'"$corr"'}' "${src:-.}"/cylinder25.txt | gmt psxy -R-100000/100000/-100/250 -J -Wfaint,blue -O -K >> $ps
+awk '{print $1, $3*'"$corr"'}' "${src:-.}"/cylinder25.txt | gmt psxy -R -J -Sc0.1c -Gblue -O -K >> $ps
+awk '{print $1, $2*'"$corr"'}' "${src:-.}"/cylinder25.txt | gmt psxy -R -J -Sc0.1c -Gred -O -K >> $ps
 gmt psxy -R-100/100/-100/250 -J faa.txt -W0.25p,red -O -K -Bxafg1000 -Byafg1000 -BWsne+t"Testing FAA and VGG over cylinder" >> $ps
 # Exact value over top at x = 0 only
 gmt math -T-100/100/200 2 PI MUL 6.673e-6 MUL 1670 MUL 5084 1333 SUB 1333 25000 HYPOT ADD 5084 25000 HYPOT SUB MUL = tmp
