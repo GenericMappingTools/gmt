9f2fbdbf95d1451f242d4c6c78c90e28dc8e9895
Paul Wessel <pwessel@hawaii.edu>
2015-06-24 21:59:30 +0000

    Update map frame

--- doc/examples/ex44/example_44.sh
+++ doc/examples/ex44/example_44.sh
@@ -18,7 +18,7 @@ gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
 # Top map of Spain/Portugal
 gmt mapproject -R15W/35E/30N/48N -JM2i -W > tmp
 read w h < tmp
-gmt pscoast -R10W/5E/35N/44N -JM6i -Baf -BWSne -EES+gbisque -Gbrown -Wfaint -N1/1p -Sazure1 -Df -O -K -Y4.5i >> $ps
+gmt pscoast -R10W/5E/35N/44N -JM6i -Baf -BWSne -EES+gbisque -Gbrown -Wfaint -N1/1p -Sazure1 -Df -O -K -Y4.5i --FORMAT_GEO_MAP=dddF >> $ps
 gmt psbasemap -R -J -O -K -DjTR/$w/$h/TR/0.15i/0.1i+stmp -F+gwhite+p1p+c0.1c+s >> $ps
 read x0 y0 w h < tmp
 gmt pscoast -R15W/35E/30N/48N -JM$w -Da -Gbrown -B0 -EES+gbisque -O -K -X$x0 -Y$y0 --MAP_FRAME_TYPE=plain >> $ps
