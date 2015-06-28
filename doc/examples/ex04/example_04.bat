94c2e7352fd307007928aec176c81e895f612573
Paul Wessel <pwessel@hawaii.edu>
2015-06-28 10:43:45 +0000

    A few fixes

--- doc/examples/ex04/example_04.bat
+++ doc/examples/ex04/example_04.bat
@@ -15,7 +15,7 @@ echo 0       100     10      100 >> zero.cpt
 gmt grdcontour HI_geoid4.nc -R195/210/18/25 -Jm0.45i -p60/30 -C1 -A5+o -Gd4i -K -P -X1.5i -Y1.5i > %ps%
 gmt pscoast -R -J -p -B2 -BNEsw -Gblack -O -K -T209/19.5/1i >> %ps%
 gmt grdview HI_topo4.nc -R195/210/18/25/-6/4 -J -Jz0.34i -p -Czero.cpt -N-6+glightgray -Qsm -O -K -B2 -Bz2+l"Topo (km)" -BneswZ -Y2.2i >> %ps%
-echo 3.25 5.75 H@#awaiian@# R@#idge | gmt pstext -R0/10/0/10 -Jx1i -F+f60p,ZapfChancery-MediumItalic+jCB -O >> %ps%
+echo 3.25 5.75 H@#awaiian@# R@#idge@# | gmt pstext -R0/10/0/10 -Jx1i -F+f60p,ZapfChancery-MediumItalic+jCB -O >> %ps%
 del zero.cpt
 REM
 echo GMT EXAMPLE 4 (color)
@@ -28,6 +28,6 @@ gmt pscoast -R -J -p -B2 -BNEsw -Gblack -O -K >> %ps%
 gmt psscale -R -J -p -D3.375i/4.3i/5i/0.3ih -Cgeoid.cpt -I -O -K -A -Bx2+l"Geoid (m)" >> %ps%
 gmt psbasemap -R -J -p -O -K -T209/19.5/1i --COLOR_BACKGROUND=red --MAP_TICK_PEN_PRIMARY=thinner,red --FONT=red >> %ps%
 gmt grdview HI_topo4.nc -It_intens.nc -R195/210/18/25/-6/4 -J -JZ3.4i -p -Ctopo.cpt -N-6+glightgray -Qc100 -O -K -Y2.2i -B2 -Bz2+l"Topo (km)" -BneswZ >> %ps%
-echo 3.25 5.75 H@#awaiian@# R@#idge | gmt pstext -R0/10/0/10 -Jx1i -F+f60p,ZapfChancery-MediumItalic+jCB -O >> %ps%
+echo 3.25 5.75 H@#awaiian@# R@#idge@# | gmt pstext -R0/10/0/10 -Jx1i -F+f60p,ZapfChancery-MediumItalic+jCB -O >> %ps%
 del *_intens.nc
 del .gmt*
--- doc/examples/ex04/example_04.sh
+++ doc/examples/ex04/example_04.sh
@@ -14,7 +14,7 @@ gmt grdcontour HI_geoid4.nc -R195/210/18/25 -Jm0.45i -p60/30 -C1 -A5+o -Gd4i -K 
 gmt pscoast -R -J -p -B2 -BNEsw -Gblack -O -K -Tdg209/19.5+w1i >> $ps
 gmt grdview HI_topo4.nc -R195/210/18/25/-6/4 -J -Jz0.34i -p -Czero.cpt -O -K \
 	-N-6+glightgray -Qsm -B2 -Bz2+l"Topo (km)" -BneswZ -Y2.2i >> $ps
-echo '3.25 5.75 H@#awaiian@# R@#idge' | gmt pstext -R0/10/0/10 -Jx1i \
+echo '3.25 5.75 H@#awaiian@# R@#idge@#' | gmt pstext -R0/10/0/10 -Jx1i \
 	-F+f60p,ZapfChancery-MediumItalic+jCB -O >> $ps
 rm -f zero.cpt
 #
@@ -29,6 +29,6 @@ gmt psbasemap -R -J -p -O -K -Td209/19.5+w1i --COLOR_BACKGROUND=red --FONT=red \
 gmt psscale -R -J -p240/30 -Dx3.375i/-0.5i+w5i/0.3i+h -Cgeoid.cpt -I -O -K -Bx2+l"Geoid (m)" >> $ps
 gmt grdview HI_topo4.nc -It_intens.nc -R195/210/18/25/-6/4 -J -JZ3.4i -p60/30 -Ctopo.cpt \
 	-O -K -N-6+glightgray -Qc100 -B2 -Bz2+l"Topo (km)" -BneswZ -Y2.2i >> $ps
-echo '3.25 5.75 H@#awaiian@# R@#idge' | gmt pstext -R0/10/0/10 -Jx1i \
+echo '3.25 5.75 H@#awaiian@# R@#idge@#' | gmt pstext -R0/10/0/10 -Jx1i \
 	-F+f60p,ZapfChancery-MediumItalic+jCB -O >> $ps
 rm -f *_intens.nc
