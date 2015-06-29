4f5b563f598029fe5a42c6f9023141c0cf8c85a3
Paul Wessel <pwessel@hawaii.edu>
2015-06-29 02:37:34 +0000

    Update some scripts

--- doc/scripts/GMT_mapscale.sh
+++ doc/scripts/GMT_mapscale.sh
@@ -2,7 +2,7 @@
 #	$Id$
 #
 ps=GMT_mapscale.ps
-gmt psbasemap -R0/40/50/56 -JM5i -Baf -P -K -LjML+c53+w1000k+f+l"Scale at 53\\312N" -F+glightcyan+c0+p > $ps
+gmt psbasemap -R0/40/50/56 -JM5i -Baf -P -K -LjML+c53+w1000k+f+l"Scale at 53\\232N" -F+glightcyan+c0+p > $ps
 gmt psbasemap -R -J -O -K -LjBR+c53+w1000k+l+f -F+p1p+i+gwhite+c0.1i >> $ps
 h=`gmt mapproject -R -J -Wh -Di`
 h=`gmt math -Q $h 2 DIV =`
