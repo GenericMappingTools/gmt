9c8c42bf9b38ae8824adc6e5c4db7d66ebabebe4
Paul Wessel <pwessel@hawaii.edu>
2015-07-15 03:45:11 +0000

    Fix scripts

--- doc/scripts/GMT_tut_16.sh
+++ doc/scripts/GMT_tut_16.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #	$Id$
 #
+gmt makecpt -Crainbow -T1000/5000/500 -Z > topo.cpt
 gmt grdgradient ../tutorial/us.nc -Ne0.8 -A100 -fg -Gus_i.nc
 gmt grdimage ../tutorial/us.nc -Ius_i.nc -JM6i -P -Ba -Ctopo.cpt -K > GMT_tut_16.ps
 gmt psscale -D3i/8.5i/5i/0.25ih -Ctopo.cpt -I0.4 -By+lm -O >> GMT_tut_16.ps
--- doc/scripts/GMT_tut_9.sh
+++ doc/scripts/GMT_tut_9.sh
@@ -2,4 +2,5 @@
 #	$Id$
 #
 gmt pscoast -R130/150/35/50 -JM6i -B5 -P -Ggray -K > GMT_tut_9.ps
-gmt psxy -R -J -O ../tutorial/quakes.ngdc -Wfaint -i4,3,5,6s0.1 -h3 -Scc -C../tutorial/quakes.cpt >> GMT_tut_9.ps
+cp ../tutorial/quakes.cpt .
+gmt psxy -R -J -O ../tutorial/quakes.ngdc -Wfaint -i4,3,5,6s0.1 -h3 -Scc -Cquakes.cpt >> GMT_tut_9.ps
