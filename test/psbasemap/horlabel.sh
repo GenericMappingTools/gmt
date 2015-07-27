54462e7eec574b28c6293890db65c7cfeb629c48
Paul Wessel <pwessel@hawaii.edu>
2015-07-27 07:54:40 +0000

    typos

--- test/psbasemap/horlabel.sh
+++ test/psbasemap/horlabel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#	$Id: annotations4.sh 13355 2014-07-15 00:25:34Z jluis $
-# Demonstrates that the inside annotations gets clipped by boundary; what should we do here?
+#	$Id$
+# Demonstrates +L for horizontal labels for y-axis
 ps=horlabel.ps
 
 gmt psbasemap -R-30/30/-20/20 -Baf -Bx+l"Standard horizontal label" -By+l"Long vertical label @~Y(q)@~" -JX5i/3.5i -P -K -Xc > $ps
