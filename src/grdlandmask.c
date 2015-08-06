5513d79574961e7ad5580d4164dcf04b69eb9714
Paul Wessel <pwessel@hawaii.edu>
2015-08-06 08:47:56 +0000

    typo

--- src/grdlandmask.c
+++ src/grdlandmask.c
@@ -477,7 +477,7 @@ int GMT_grdlandmask (void *V_API, int mode, void *args)
 		Grid->header->wesn[XHI] -= 360.0;
 	}
 
-	sprintf (line, "Derived from the %s resolution shorelinen", shore_resolution[base]);
+	sprintf (line, "Derived from the %s resolution shorelines", shore_resolution[base]);
 	if (GMT_Set_Comment (API, GMT_IS_GRID, GMT_COMMENT_IS_REMARK, line, Grid)) return (API->error);
 	if (GMT_Set_Comment (API, GMT_IS_GRID, GMT_COMMENT_IS_OPTION | GMT_COMMENT_IS_COMMAND, options, Grid)) Return (API->error);
 	if (GMT_Write_Data (API, GMT_IS_GRID, GMT_IS_FILE, GMT_IS_SURFACE, GMT_GRID_ALL, NULL, Ctrl->G.file, Grid) != GMT_OK) {
