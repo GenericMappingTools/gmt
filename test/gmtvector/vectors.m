a6bb9a632210351455262c5510ab0154336337d0
Joaquim Luis <jmfluis@gmail.com>
2015-09-18 16:02:19 +0000

    Porting vectors test to .m

---
+++ test/gmtvector/vectors.m
@@ -0,0 +1,21 @@
+function [ps, path] = vectors(out_path)
+% OUT_PATH -> Path to where the PS file will be written
+% PS       -> Full name of the created postscript file (built from OUT_PATH)
+% PATH     -> Path to where this file lives (usefull for gmtest.m)
+%
+%	$Id$
+
+	full = mfilename('fullpath');
+	[pato, fname] = fileparts(full);
+	ps = [out_path fname '.ps'];
+	path = [pato filesep];
+
+	gmt('destroy'),		gmt('gmtset -Du'),	gmt('destroy')		% Make sure we start with a clean session
+	Gr  = gmt('grdmath -Rg -I30 -r 0.5 Y COSD ADD =');
+	Gaz = gmt('grdmath -Rg -I30 -r X =');
+	t_cpt = gmt('makecpt -T0.5/1.5/0.1 -Z');
+
+	gmt(['grdvector $ $ -A -C -JG45/45/4.5i -Q0.2i+e -W2p -Si2500 -P -K -B30g30' ...
+		' -Xc -Y0.75i --MAP_VECTOR_SHAPE=0.5 > ' ps], t_cpt, Gr, Gaz)
+	gmt(['grdvector $ $ -A -C -J -W2p -Si2500 -O -B30g30 -Y5i >> ' ps], t_cpt, Gr, Gaz)
+	builtin('delete','gmt.conf');
