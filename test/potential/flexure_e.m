e25adda578cf84239b6829fc5b07376c05349b65
Joaquim Luis <jmfluis@gmail.com>
2015-09-23 23:42:05 +0000

    Add tests .m

---
+++ test/potential/flexure_e.m
@@ -0,0 +1,25 @@
+function [ps, path] = flexure_e(out_path)
+% OUT_PATH -> Path to where the PS file will be written
+% PS       -> Full name of the created postscript file (built from OUT_PATH)
+% PATH     -> Path to where this file lives (usefull for gmtest.m)
+%
+%	$Id$
+
+	full = mfilename('fullpath');
+	[pato, fname] = fileparts(full);
+	ps = [out_path fname '.ps'];
+	path = [pato filesep];
+
+	gmt('destroy'),		gmt('gmtset -Du')
+	gmt('gmtset MAP_FRAME_TYPE plain'),		gmt('destroy')		% Make sure we start with a clean session
+
+	%lon lat azimuth, semi-major, semi-minor, height
+	t = [300	200	70	70	30	5000];
+
+	Gsmt = gmt('grdseamount -Rk0/600/0/400 -I1000 -G -Dk -E -F0.2 -Cg', t);
+	% WE DONT HAVE SYNTAX FOR "smt.nc+Uk"
+gmt(['grdcontour smt.nc+Uk -Jx0.01i -Xc -P -A1 -GlLM/RM -Bafg -K -Z0.001 > ' ps])
+	Gflex_e = gmt('grdflexure -D3300/2700/2400/1030 -E5k -G', Gsmt);
+gmt(['grdcontour flex_e.nc+Uk -J -O -K -C0.2 -A1 -Z0.001 -GlLM/RM -Bafg -BWsNE+t"Elastic Plate Flexure, T@-e@- = 5 km" -Y4.4i >> ' ps])
+	gmt(['psxy -R -J -O -T >> ' ps])
+	builtin('delete','gmt.conf');
---
+++ test/psxy/clipping6.m
@@ -0,0 +1,23 @@
+function [ps, path] = clipping6(out_path)
+% OUT_PATH -> Path to where the PS file will be written
+% PS       -> Full name of the created postscript file (built from OUT_PATH)
+% PATH     -> Path to where this file lives (usefull for gmtest.m)
+%
+%	$Id$
+
+	full = mfilename('fullpath');
+	[pato, fname] = fileparts(full);
+	ps = [out_path fname '.ps'];
+	path = [pato filesep];
+
+	gmt('destroy'),		gmt('gmtset -Du'),	gmt('destroy')		% Make sure we start with a clean session
+	badpol = [-70	-55
+			-50	-30
+			-50	50
+			-120	67
+			-170	60
+			-80	0];
+	gmt(['psxy -R-90/270/-60/75 -JM5i -A -Ggreen -W0.25p,red -Bag -K -P -Xc > ' ps], badpol)
+	gmt(['psxy -R-180/180/-60/75 -JR90/5i -A -Ggreen -W0.25p,red -Bag -O -K -Y3.5i >> ' ps], badpol)
+	gmt(['psxy -R -JR-90/5i -A -Ggreen -W0.25p,red -Bag -O -Y3.5i >> ' ps], badpol)
+	builtin('delete','gmt.conf');
