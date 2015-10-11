b067d92ba76b4df164c919212a231f6f83e9cc32
Paul Wessel <pwessel@hawaii.edu>
2015-10-11 21:20:36 +0000

    Forgot data set for a test

--- test/psxy/bezierline.sh
+++ test/psxy/bezierline.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Test pen modifier +b for Bezier spline in psxy
 ps=bezierline.ps
+cat << EOF > t.txt
+0	0
+1	1
+2	1
+3	0.5
+2	0.25
+EOF
 # Linear
 gmt psxy -R-1/4/-1/3 -Jx1i -P -K -Baf -W0.25p -Gred -Sc0.2i t.txt -Xc > $ps
 gmt psxy -R -J -O -K -W1p t.txt >> $ps
