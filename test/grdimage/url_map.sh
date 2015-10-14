530e7a84c4c051e44aba344103c0e744b16f9be7
Paul Wessel <pwessel@hawaii.edu>
2015-10-14 18:58:34 +0000

    Add note so we remember why it may fail sometimes

--- test/grdimage/url_map.sh
+++ test/grdimage/url_map.sh
@@ -3,3 +3,4 @@ ps=url_map.ps
 gmt makecpt -Cglobe -Z > t.cpt
 gmt grdimage -Rd -JI15c -Dr http://larryfire.files.wordpress.com/2009/07/untooned_jessicarabbit.jpg -P -K -Xc > $ps
 gmt grdimage http://imina.soest.hawaii.edu/pwessel/etopo5m.nc -J -Ct.cpt -B0 -B+t"grdimage via URLs" -O -Y4i >> $ps
+echo "Note: Will fail if gdal is not built with libcurl"
