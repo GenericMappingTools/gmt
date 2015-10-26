130f22a158953756583cbd652d06f33a54b43661
Paul Wessel <pwessel@hawaii.edu>
2015-10-26 20:36:29 +0000

    Updated KEYS for spectrum1d to turn off required output with -T

--- src/spectrum1d.c
+++ src/spectrum1d.c
@@ -39,7 +39,7 @@
 #define THIS_MODULE_NAME	"spectrum1d"
 #define THIS_MODULE_LIB		"core"
 #define THIS_MODULE_PURPOSE	"Compute auto- [and cross-] spectra from one [or two] timeseries"
-#define THIS_MODULE_KEYS	"<DI,>DO"
+#define THIS_MODULE_KEYS	"<DI,>DO,T-o"
 
 #include "gmt_dev.h"
 
