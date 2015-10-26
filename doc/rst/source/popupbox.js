fdb81c09bde7266bf39ab484da745d356caaa693
Joaquim Luis <jmfluis@gmail.com>
2015-10-26 16:30:37 +0000

    Help files for nicely display html boxes in per options help.

---
+++ doc/rst/source/popupbox.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  document.getElementById("ExampleBtn").onclick = function(){
+        var overlay = document.getElementById("overlay");
+        var popup = document.getElementById("popup");
+        overlay.style.display = "block";
+        popup.style.display = "block";
+    };
+    
+  document.getElementById("CloseBtn").onclick = function(){
+        var overlay = document.getElementById("overlay");
+        var popup = document.getElementById("popup");
+        overlay.style.display = "none";
+        popup.style.display = "none";      
+  }
+};
