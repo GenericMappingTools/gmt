64427b2d9d493bdd5efcf65c730f525ad9396275
Paul Wessel <pwessel@hawaii.edu>
2015-10-31 00:17:38 +0000

    Update segy stuff

---
+++ src/segy/segyprogs_2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#	$Id$
+#
+# script to plot mendo wa1 combined data
+#
+# cdp = 40 * coordinate on line, 30km max depth
+#
+ps=segyprogs_2.ps
+
+area1=-R-35/6/0/30
+proj1="-Jx0.15i/-0.15i"
+
+gmt pssegy wa1_mig13.segy $area1 $proj1 -Qx0.1i -D0.35 -C8.0 -Qy0.1i -W -Fgreen -Qb-1.2 -Sc -K -Ttest.list -V -Z -Xc > $ps
+gmt psbasemap -R -J -Baf -O >> $ps
