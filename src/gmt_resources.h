4f74d1a1ca1b60fae29a792cdc934134f11f7ecc
Joaquim Luis <jmfluis@gmail.com>
2015-11-21 01:43:52 +0000

    Move GMT_IS_PS to the end of list again to not break Julia wrapper

--- src/gmt_resources.h
+++ src/gmt_resources.h
@@ -100,12 +100,12 @@ enum GMT_enum_family {
 	GMT_IS_DATASET = 0,	/* Entity is a data table */
 	GMT_IS_TEXTSET,		/* Entity is a Text table */
 	GMT_IS_GRID,		/* Entity is a GMT grid */
-	GMT_IS_CPT,		/* Entity is a CPT table */
+	GMT_IS_CPT,			/* Entity is a CPT table */
 	GMT_IS_IMAGE,		/* Entity is a 1- or 3-layer unsigned char image */
-	GMT_IS_PS,		/* Entity is a PostScript content struct */
 	GMT_IS_VECTOR,		/* Entity is set of user vectors */
 	GMT_IS_MATRIX,		/* Entity is user matrix */
 	GMT_IS_COORD,		/* Entity is a double coordinate array */
+	GMT_IS_PS,			/* Entity is a PostScript content struct */
 	GMT_N_FAMILIES};	/* Total number of families [API Developers only]  */
 
 /*! These are modes for handling comments */
