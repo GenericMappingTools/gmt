820da161416daa3041e177b77252e67023416c96
Remko Scharroo <remko.scharroo@eumetsat.int>
2015-12-22 13:58:07 +0000

    Added Id key to gmt_memory.h

--- src/gmt_memory.h
+++ src/gmt_memory.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id $
+ *	$Id$
  *
  *	Copyright (c) 1991-2015 by P. Wessel, W. H. F. Smith, R. Scharroo, J. Luis and F. Wobbe
  *	See LICENSE.TXT file for copying and redistribution conditions.
