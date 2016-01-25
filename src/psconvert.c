6c196e1c1d2394d08c23ec6707a02462c0ccf215
Paul Wessel <pwessel@hawaii.edu>
2016-01-25 19:11:49 +0000

    Implement 15476 fix in 5.2.2 branch after the fact ...

--- src/psconvert.c
+++ src/psconvert.c
@@ -747,7 +747,7 @@ int GMT_psconvert (void *V_API, int mode, void *args) {
 	size_t len, line_size = 0U;
 	bool got_BB, got_HRBB, file_has_HRBB, got_end, landscape, landscape_orig, set_background = false;
 	bool excessK, setup, found_proj = false, isGMT_PS = false, return_image = false;
-	bool transparency = false, look_for_transparency, BeginPageSetup_here = false;
+	bool transparency = false, look_for_transparency, BeginPageSetup_here = false, add_grestore = false;
 
 	double xt, yt, xt_bak, yt_bak, w, h, x0 = 0.0, x1 = 612.0, y0 = 0.0, y1 = 828.0;
 	double west = 0.0, east = 0.0, south = 0.0, north = 0.0;
@@ -1173,7 +1173,7 @@ int GMT_psconvert (void *V_API, int mode, void *args) {
 
 		/* To produce non-PDF output from PS with transparency we must determine if transparency is requested in the PS */
 		look_for_transparency = Ctrl->T.device != GS_DEV_PDF && Ctrl->T.device != -GS_DEV_PDF;
-		transparency = false;
+		transparency = add_grestore = false;
 		set_background = (Ctrl->A.paint || Ctrl->A.outline);
 
 		while (line_reader (GMT, &line, &line_size, fp) != EOF) {
@@ -1353,6 +1353,7 @@ int GMT_psconvert (void *V_API, int mode, void *args) {
 					else
 						fprintf (fpo, "gsave %g %g scale\n", new_scale_x, new_scale_y);
 					set_background = false;
+					add_grestore = true;
 					possibly_fill_or_outline_BoundingBox (GMT, &(Ctrl->A), fpo);
 				}
 			}
@@ -1436,6 +1437,8 @@ int GMT_psconvert (void *V_API, int mode, void *args) {
 #endif
 			fprintf (fpo, "%s\n", line);
 		}
+		if (add_grestore)
+			fprintf (fpo, "grestore\n");	/* Since we started with gsave to change size */
 
 		/* Recede a bit to test the contents of last line. -7 for when PS has CRLF endings */
 		if (fseek (fp, (off_t)-7, SEEK_END))
