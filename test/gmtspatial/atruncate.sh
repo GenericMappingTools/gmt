035175f5b125e638fd3ee678f86d0c663b4c9cb2
Paul Wessel <pwessel@hawaii.edu>
2016-02-10 21:57:17 +0000

    Address issue #851

---
+++ test/gmtspatial/atruncate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Test gmtspatial truncating a polygon to a -R setting that
+# is entirely inside the polygon, so should result in the -R
+# pollygon outline as answer.  Cartesian case.  Issue #851,a
+ps=atruncate.ps
+cat << EOF > tpoly.xy
+-300 -3500
+-200 -800
+400 -780
+500 -3400
+-300 -3500
+EOF
+gmtspatial tpoly.xy -C -R0/100/-3100/-3000 > clipped.txt
+
+gmt psbasemap -R-500/600/-3600/-700 -JX6i/9i -P -Baf -D0/100/-3100/-3000 -F+p3p,gray -K -Xc > $ps
+gmt psxy -R -J tpoly.xy -W1p -O -K >> $ps
+gmt psxy -R -J clipped.txt -W0.25p,red -O >> $ps
+gv $ps &
---
+++ test/gmtspatial/btruncate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Test gmtspatial truncating a polygon to a -R setting that
+# is tangent to the polygon at W/S.  Fails when polygon exceeds
+# x = 190 which is odd since not geographic (?). Issue #851, b
+ps=btruncate.ps
+cat << EOF > tpoly.xy
+0 0
+200 0
+200 200
+0 200
+0 0
+EOF
+gmtspatial tpoly.xy -C -R0/10/0/10 > clipped.txt
+
+gmt psbasemap -R-5/205/-5/205 -JX6i -P -Baf -D0/10/0/10 -F+p3p,gray -K -Xc > $ps
+gmt psxy -R -J tpoly.xy -W1p -O -K >> $ps
+gmt psxy -R -J clipped.txt -W0.25p,red -O >> $ps
+gv $ps &
