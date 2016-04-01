b2c5500707107595d1d3ef20d0dd14d5a4669ff8
Joaquim Luis <jmfluis@gmail.com>
2016-04-01 00:04:42 +0000

    Fix CID 38968

--- src/potential/gravfft.c
+++ src/potential/gravfft.c
@@ -805,7 +805,10 @@ int GMT_gravfft (void *V_API, int mode, void *args) {
 		GMT_FFT_Destroy (API, &Rho_info);
 	gmt_M_free (GMT, raised);
 
-	if (GMT_Set_Comment (API, GMT_IS_GRID, GMT_COMMENT_IS_OPTION | GMT_COMMENT_IS_COMMAND, options, Grid[0])) Return (API->error);
+	if (GMT_Set_Comment (API, GMT_IS_GRID, GMT_COMMENT_IS_OPTION | GMT_COMMENT_IS_COMMAND, options, Grid[0])) {
+		gmt_M_free (GMT, topo);
+		Return (API->error);
+	}
 	if (GMT_Write_Data (API, GMT_IS_GRID, GMT_IS_FILE, GMT_IS_SURFACE, GMT_GRID_DATA_ONLY |
                         GMT_GRID_IS_COMPLEX_REAL, NULL, Ctrl->G.file, Grid[0]) != GMT_OK) {
 		gmt_M_free (GMT, topo);
