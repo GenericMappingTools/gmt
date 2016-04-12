758c35e3c516d80db1a303ffe7f612f45844bd02
Joaquim Luis <jmfluis@gmail.com>
2016-04-12 23:02:31 +0000

    Fix "Printf arg type mismatch" CIDs

--- src/triangle.c
+++ src/triangle.c
@@ -3716,61 +3716,49 @@ struct otri *t;
   if (printtri.tri == m->dummytri) {
     printf("    [0] = Outer space\n");
   } else {
-    printf("    [0] = x%lx  %d\n", (unsigned LONG) printtri.tri,
-           printtri.orient);
+    printf("    [0] = x%" PRIu "  %d\n", (unsigned LONG) printtri.tri, printtri.orient);
   }
   decode(t->tri[1], printtri);
   if (printtri.tri == m->dummytri) {
     printf("    [1] = Outer space\n");
   } else {
-    printf("    [1] = x%lx  %d\n", (unsigned LONG) printtri.tri,
-           printtri.orient);
+    printf("    [1] = x%" PRIu "  %d\n", (unsigned LONG) printtri.tri, printtri.orient);
   }
   decode(t->tri[2], printtri);
   if (printtri.tri == m->dummytri) {
     printf("    [2] = Outer space\n");
   } else {
-    printf("    [2] = x%lx  %d\n", (unsigned LONG) printtri.tri,
-           printtri.orient);
+    printf("    [2] = x%" PRIu "  %d\n", (unsigned LONG) printtri.tri, printtri.orient);
   }
 
   org(*t, printvertex);
   if (printvertex == (vertex) NULL)
     printf("    Origin[%d] = NULL\n", (t->orient + 1) % 3 + 3);
   else
-    printf("    Origin[%d] = x%lx  (%.12g, %.12g)\n",
-           (t->orient + 1) % 3 + 3, (unsigned LONG) printvertex,
-           printvertex[0], printvertex[1]);
-  dest(*t, printvertex);
+    printf("    Origin[%d] = x%" PRIu "  (%.12g, %.12g)\n",
+           (t->orient + 1) % 3 + 3, (unsigned LONG) printvertex, printvertex[0], printvertex[1]); dest(*t, printvertex);
   if (printvertex == (vertex) NULL)
     printf("    Dest  [%d] = NULL\n", (t->orient + 2) % 3 + 3);
   else
-    printf("    Dest  [%d] = x%lx  (%.12g, %.12g)\n",
-           (t->orient + 2) % 3 + 3, (unsigned LONG) printvertex,
-           printvertex[0], printvertex[1]);
-  apex(*t, printvertex);
+    printf("    Dest  [%d] = x%" PRIu "  (%.12g, %.12g)\n",
+           (t->orient + 2) % 3 + 3, (unsigned LONG) printvertex, printvertex[0], printvertex[1]); apex(*t, printvertex);
   if (printvertex == (vertex) NULL)
     printf("    Apex  [%d] = NULL\n", t->orient + 3);
   else
-    printf("    Apex  [%d] = x%lx  (%.12g, %.12g)\n",
-           t->orient + 3, (unsigned LONG) printvertex,
-           printvertex[0], printvertex[1]);
+    printf("    Apex  [%d] = x%" PRIu "  (%.12g, %.12g)\n", t->orient + 3, (unsigned LONG) printvertex, printvertex[0], printvertex[1]);
 
   if (b->usesegments) {
     sdecode(t->tri[6], printsh);
     if (printsh.ss != m->dummysub) {
-      printf("    [6] = x%lx  %d\n", (unsigned LONG) printsh.ss,
-             printsh.ssorient);
+      printf("    [6] = x%" PRIu "  %d\n", (unsigned LONG) printsh.ss, printsh.ssorient);
     }
     sdecode(t->tri[7], printsh);
     if (printsh.ss != m->dummysub) {
-      printf("    [7] = x%lx  %d\n", (unsigned LONG) printsh.ss,
-             printsh.ssorient);
+      printf("    [7] = x%" PRIu "  %d\n", (unsigned LONG) printsh.ss, printsh.ssorient);
     }
     sdecode(t->tri[8], printsh);
     if (printsh.ss != m->dummysub) {
-      printf("    [8] = x%lx  %d\n", (unsigned LONG) printsh.ss,
-             printsh.ssorient);
+      printf("    [8] = x%" PRIu "  %d\n", (unsigned LONG) printsh.ss, printsh.ssorient);
     }
   }
 
@@ -3804,67 +3792,57 @@ struct osub *s;
   struct otri printtri;
   vertex printvertex;
 
-  printf("subsegment x%lx with orientation %d and mark %d:\n",
+  printf("subsegment x" PRIu "% with orientation %d and mark %d:\n",
          (unsigned LONG) s->ss, s->ssorient, mark(*s));
   sdecode(s->ss[0], printsh);
   if (printsh.ss == m->dummysub) {
     printf("    [0] = No subsegment\n");
   } else {
-    printf("    [0] = x%lx  %d\n", (unsigned LONG) printsh.ss,
-           printsh.ssorient);
+    printf("    [0] = x%" PRIu "  %d\n", (unsigned LONG) printsh.ss, printsh.ssorient);
   }
   sdecode(s->ss[1], printsh);
   if (printsh.ss == m->dummysub) {
     printf("    [1] = No subsegment\n");
   } else {
-    printf("    [1] = x%lx  %d\n", (unsigned LONG) printsh.ss,
-           printsh.ssorient);
+    printf("    [1] = x%" PRIu "  %d\n", (unsigned LONG) printsh.ss, printsh.ssorient);
   }
 
   sorg(*s, printvertex);
   if (printvertex == (vertex) NULL)
     printf("    Origin[%d] = NULL\n", 2 + s->ssorient);
   else
-    printf("    Origin[%d] = x%lx  (%.12g, %.12g)\n",
-           2 + s->ssorient, (unsigned LONG) printvertex,
-           printvertex[0], printvertex[1]);
+    printf("    Origin[%d] = x%" PRIu "  (%.12g, %.12g)\n", 2 + s->ssorient, (unsigned LONG) printvertex, printvertex[0], printvertex[1]);
   sdest(*s, printvertex);
   if (printvertex == (vertex) NULL)
     printf("    Dest  [%d] = NULL\n", 3 - s->ssorient);
   else
-    printf("    Dest  [%d] = x%lx  (%.12g, %.12g)\n",
-           3 - s->ssorient, (unsigned LONG) printvertex,
-           printvertex[0], printvertex[1]);
+    printf("    Dest  [%d] = x%" PRIu "  (%.12g, %.12g)\n", 3 - s->ssorient, (unsigned LONG) printvertex, printvertex[0], printvertex[1]);
 
   decode(s->ss[6], printtri);
   if (printtri.tri == m->dummytri) {
     printf("    [6] = Outer space\n");
   } else {
-    printf("    [6] = x%lx  %d\n", (unsigned LONG) printtri.tri,
-           printtri.orient);
+    printf("    [6] = x%" PRIu "  %d\n", (unsigned LONG) printtri.tri, printtri.orient);
   }
   decode(s->ss[7], printtri);
   if (printtri.tri == m->dummytri) {
     printf("    [7] = Outer space\n");
   } else {
-    printf("    [7] = x%lx  %d\n", (unsigned LONG) printtri.tri,
-           printtri.orient);
+    printf("    [7] = x%" PRIu "  %d\n", (unsigned LONG) printtri.tri, printtri.orient);
   }
 
   segorg(*s, printvertex);
   if (printvertex == (vertex) NULL)
     printf("    Segment origin[%d] = NULL\n", 4 + s->ssorient);
   else
-    printf("    Segment origin[%d] = x%lx  (%.12g, %.12g)\n",
-           4 + s->ssorient, (unsigned LONG) printvertex,
-           printvertex[0], printvertex[1]);
+    printf("    Segment origin[%d] = x%" PRIu "  (%.12g, %.12g)\n",
+           4 + s->ssorient, (unsigned LONG) printvertex, printvertex[0], printvertex[1]);
   segdest(*s, printvertex);
   if (printvertex == (vertex) NULL)
     printf("    Segment dest  [%d] = NULL\n", 5 - s->ssorient);
   else
-    printf("    Segment dest  [%d] = x%lx  (%.12g, %.12g)\n",
-           5 - s->ssorient, (unsigned LONG) printvertex,
-           printvertex[0], printvertex[1]);
+    printf("    Segment dest  [%d] = x%" PRIu "  (%.12g, %.12g)\n",
+           5 - s->ssorient, (unsigned LONG) printvertex, printvertex[0], printvertex[1]);
 }
 
 /**                                                                         **/
