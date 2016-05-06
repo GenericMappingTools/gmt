1f394c6368647d1545531012a9f32363c6935344
Paul Wessel <pwessel@hawaii.edu>
2016-05-06 06:39:59 +0000

    Fix grdvolume test

--- test/grdvolume/grdvolume.sh
+++ test/grdvolume/grdvolume.sh
@@ -15,8 +15,8 @@ gmt xyz2grd -I1 -Gt.nc -Z $1 <<%
 0
 %
 echo
-echo gmt xyz2grd $1 \; gmt grdvolume $2 -Sk:
-gmt grdvolume t.nc $2 -Sk
+echo gmt xyz2grd $1 \; gmt grdvolume $2 -Sk -fg:
+gmt grdvolume t.nc $2 -Sk -fg
 echo gmt xyz2grd $1 \; gmt grdvolume $2:
 gmt grdvolume t.nc $2
 echo gmt xyz2grd $1 \; gmt grdvolume $2 -L-1:
@@ -31,7 +31,7 @@ makegrd "-R0/3/0/3 -r -di0"
 makegrd -R0/2/0/2 "-C0/0.8/0.4"
 }
 
-testcase > gmt grdvolume.log
+testcase > grdvolume.log
 
 # Note: a non-zero exit status of diff is not an error
-diff gmt grdvolume.log gmt grdvolume.out --strip-trailing-cr > log || true
+diff grdvolume.log "${src:-.}"/grdvolume.out --strip-trailing-cr > log || true
