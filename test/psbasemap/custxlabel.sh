a3fcef6a7c818c5b03372a3a42b9cda4942f77cb
Paul Wessel <pwessel@hawaii.edu>
2016-05-16 21:02:45 +0000

    Update custxlabel.sh script so it fails showing the bug

--- test/psbasemap/custxlabel.sh
+++ test/psbasemap/custxlabel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Test Cartesian, relative and absolute time axis custom annotations
 # As of 6/16/2016 the relative time example fails.  I faked the orig PS
-# by running the absolute time part twice.  Once the bug is fixed we will
-# revert to the commented-out version of psbasemap
+# by running the absolute time part twice (the commented out commands).
+# Once the bug is fixed we will remove the commented-out versions
+
 ps=custxlabel.ps
 # Cartesian
 cat << EOF > m.txt
@@ -15,18 +16,18 @@ cat << EOF > m.txt
 EOF
 gmt psbasemap -R-1/7/-1/7 -JX6i/2.75i -P -Bxcm.txt -Byafg -BWSne -K -Xc > $ps
 # Time axis relative time
-#cat << EOF > m.txt
-#10 a p@-1@-
-#20 a p@-2@-
-#EOF
-#gmt psbasemap -R2013-06-01T/2013-08-31T/-10/10 -JX6it/2.75i --TIME_EPOCH=2013-05-31T --TIME_UNIT=d -Byafg -Bxcm.txt -BWSne -O -K -Y3.25i >> $ps
 cat << EOF > m.txt
-2013-06-10T a p@-1@-
-2013-07-10T afg p@-2@-
-2013-07-20T f
-2013-08-20T a p@-3@-
+10 a p@-1@-
+20 a p@-2@-
 EOF
-gmt psbasemap -R2013-06-01T/2013-08-31T/-10/10 -JX6iT/2.75i -Byafg -Bxcm.txt -BWSne -O -K -Y3.25i  >> $ps
+gmt psbasemap -R2013-06-01T/2013-08-31T/-10/10 -JX6it/2.75i --TIME_EPOCH=2013-05-31T --TIME_UNIT=d -Byafg -Bxcm.txt -BWSne -O -K -Y3.25i >> $ps
+#at << EOF > m.txt
+#013-06-10T a p@-1@-
+#013-07-10T afg p@-2@-
+#013-07-20T f
+#013-08-20T a p@-3@-
+#OF
+#mt psbasemap -R2013-06-01T/2013-08-31T/-10/10 -JX6iT/2.75i -Byafg -Bxcm.txt -BWSne -O -K -Y3.25i  >> $ps
 # Time axis absolute time
 cat << EOF > m.txt
 2013-06-10T a p@-1@-
