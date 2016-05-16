ecf144722fa3dfc825e1c4b7994b36aafe5609e4
Paul Wessel <pwessel@hawaii.edu>
2016-05-16 19:28:28 +0000

    Add test for custom labels with time

---
+++ test/psbasemap/custxlabel.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Test Cartesian, relative and absolute time axis custom annotations
+# As of 6/16/2016 the relative time example fails.  I faked the orig PS
+# by running the absolute time part twice.  Once the bug is fixed we will
+# revert to the commented-out version of psbasemap
+ps=custxlabel.ps
+# Cartesian
+cat << EOF > m.txt
+0		afg	zero
+1.61803		afg	@~j@~
+2.71828		afg	e
+3.1415926 	afg	@~p@~
+5	 	f	
+6.283186	afg	@~2p@~
+EOF
+gmt psbasemap -R-1/7/-1/7 -JX6i/2.75i -P -Bxcm.txt -Byafg -BWSne -K -Xc > $ps
+# Time axis relative time
+#cat << EOF > m.txt
+#10 a p@-1@-
+#20 a p@-2@-
+#EOF
+#gmt psbasemap -R2013-06-01T/2013-08-31T/-10/10 -JX6it/2.75i --TIME_EPOCH=2013-05-31T --TIME_UNIT=d -Byafg -Bxcm.txt -BWSne -O -K -Y3.25i >> $ps
+cat << EOF > m.txt
+2013-06-10T a p@-1@-
+2013-07-10T afg p@-2@-
+2013-07-20T f
+2013-08-20T a p@-3@-
+EOF
+gmt psbasemap -R2013-06-01T/2013-08-31T/-10/10 -JX6iT/2.75i -Byafg -Bxcm.txt -BWSne -O -K -Y3.25i  >> $ps
+# Time axis absolute time
+cat << EOF > m.txt
+2013-06-10T a p@-1@-
+2013-07-10T afg p@-2@-
+2013-07-20T f
+2013-08-20T a p@-3@-
+EOF
+gmt psbasemap -R2013-06-01T/2013-08-31T/-10/10 -JX6iT/2.75i -Byafg -Bxcm.txt -BWSne -O -Y3.25i  >> $ps
