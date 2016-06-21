79972e801fbced15195971680d45f335708bc875
Joaquim Luis <jmfluis@gmail.com>
2016-06-21 01:06:47 +0000

    Shrink PSL_BUFSIZ to 256 chars. Change growing array rate to 1.75

--- src/postscriptlight.h
+++ src/postscriptlight.h
@@ -142,7 +142,7 @@ enum PSL_enum_const {PSL_CM	= 0,
 	PSL_MAX_EPS_FONTS	= 6,
 	PSL_MAX_DIMS		= 12,		/* Max number of dim arguments to PSL_plot_symbol */
 	PSL_N_PATTERNS		= 91,		/* Current number of predefined patterns + 1, # 91 is user-supplied */
-	PSL_BUFSIZ		= 4096U};
+	PSL_BUFSIZ		= 256U};
 
 /* PSL codes for pen movements (used by PSL_plotpoint, PSL_plotline, PSL_plotarc) */
 
