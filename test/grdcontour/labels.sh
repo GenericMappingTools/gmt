ab08120d535f1a67b98495f224800b85a38cc0f2
Paul Wessel <pwessel@hawaii.edu>
2016-06-22 03:41:57 +0000

    Add new test plus update keys

---
+++ test/grdcontour/labels.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#	$Id$
+#
+# Test dumping of contour labels and replotting in pstext
+
+ps=labels.ps
+
+grdmath -R0/10/0/10 -I1 X Y MUL = t.nc
+gmt grdcontour t.nc -JX4i -P -Baf -A10+tlabels.txt -GlBL/TR -K -Xc > $ps
+gmt pstext -R -J -O -Baf -F+A+f12p labels.txt -Y4.75i >> $ps
