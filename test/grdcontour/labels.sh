e785e17f0c2a50e67aec8a142869b9d93037e552
Paul Wessel <pwessel@hawaii.edu>
2016-06-23 20:11:34 +0000

    Fix lack of gmt in labels.sh

--- test/grdcontour/labels.sh
+++ test/grdcontour/labels.sh
@@ -6,6 +6,6 @@
 
 ps=labels.ps
 
-grdmath -R0/10/0/10 -I1 X Y MUL = t.nc
+gmt grdmath -R0/10/0/10 -I1 X Y MUL = t.nc
 gmt grdcontour t.nc -JX4i -P -Baf -A10+tlabels.txt -GlBL/TR -K -Xc > $ps
 gmt pstext -R -J -O -Baf -F+A+f12p labels.txt -Y4.75i >> $ps
