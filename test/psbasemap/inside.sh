f0fd3e7d44d6d40a3b2006949415c122bc0388a8
Paul Wessel <pwessel@hawaii.edu>
2016-07-18 20:45:58 +0000

    Added new test for inside annotations

---
+++ test/psbasemap/inside.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#	$Id$
+# Thest inside labeling for Cartesian and geographic maps
+ps=inside.ps
+
+gmt psbasemap -R0/13/0/10 -Ba2f0.5 -P -Jm0.4i -K --MAP_FRAME_TYPE=inside -Xc  > $ps
+gmt psbasemap -R -Ba2f0.5 -O -Jx0.4i --MAP_FRAME_TYPE=inside  -Y5i >> $ps
