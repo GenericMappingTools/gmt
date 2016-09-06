0b0331af72910c424f045edce9c53b2429e153fa
Joaquim Luis <jmfluis@gmail.com>
2016-09-06 15:53:26 +0000

    Apply Dongdong patch to pssac tests

--- test/pssac/pssac_W.sh
+++ test/pssac/pssac_W.sh
@@ -8,8 +8,8 @@ J=X15c/5c
 B=1
 PS=pssac_W.ps
 
-gmt pssac seis.sac -R$R -J$J -B$B -W2p,red,- -K -P > $PS
-gmt pssac seis.sac -R$R -J$J -B$B -W1p -K -O -Y7c >> $PS
-gmt pssac seis.sac -R$R -J$J -B$B -Wblue -K -O -Y7c >> $PS
-gmt pssac seis.sac -R$R -J$J -B$B -W. -K -O -Y7c >> $PS
+gmt pssac seis.sac -R$R -J$J -B$B -BWSen -W2p,red,- -K -P > $PS
+gmt pssac seis.sac -R$R -J$J -B$B -BWsen -W1p -K -O -Y6c >> $PS
+gmt pssac seis.sac -R$R -J$J -B$B -BWsen -Wblue -K -O -Y6c >> $PS
+gmt pssac seis.sac -R$R -J$J -B$B -BWsen -W. -K -O -Y6c >> $PS
 gmt psxy -R$R -J$J -T -K -O >> $PS
