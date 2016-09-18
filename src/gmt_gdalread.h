da17cd281cc514da96a5e95730cf63af422816bb
Joaquim Luis <jmfluis@gmail.com>
2016-09-18 21:38:10 +0000

    Correct a comment.

--- src/gmt_gdalread.h
+++ src/gmt_gdalread.h
@@ -77,7 +77,7 @@ struct GMT_GDALREAD_IN_CTRL {
 		int active;
 	} M;
 	struct GD_O {	/* Three chars code to specify the array layout in memory */
-		/* first char T(op)|B(ot), second R(ow)|C(ol), third L(eft)|R(ight), fourth P(ix)|L(ine)|S(equencial) */
+		/* first char T(op)|B(ot), second R(ow)|C(ol), third P(ix)|L(ine)|S(equencial) */
 		char mem_layout[4];
 	} O;
 	struct GD_N {	/* For floats, replace this value by NaN */
