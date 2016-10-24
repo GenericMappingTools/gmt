00fe7ba7d2b2f06aff1383f9fe0c1626492ac16d
Remko Scharroo <remko.scharroo@eumetsat.int>
2016-10-24 07:59:43 +0000

    Revert patch version (should be 0) for version 5.4.0

--- cmake/ConfigDefault.cmake
+++ cmake/ConfigDefault.cmake
@@ -42,7 +42,7 @@ set (GMT_PACKAGE_DESCRIPTION_SUMMARY "The Generic Mapping Tools")
 # The GMT package version.
 set (GMT_PACKAGE_VERSION_MAJOR 5)
 set (GMT_PACKAGE_VERSION_MINOR 4)
-set (GMT_PACKAGE_VERSION_PATCH 2)
+set (GMT_PACKAGE_VERSION_PATCH 0)
 
 # The subversion revision of the GMT source code.
 # This is manually set when making GMT *public* releases.
