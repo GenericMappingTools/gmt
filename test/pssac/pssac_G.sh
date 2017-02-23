e8dcaae3bd1acbd7ef5aec23a3230bb19134ff3b
Joaquim Luis <jmfluis@gmail.com>
2017-02-23 23:14:46 +0000

    Apply patches from #1052

--- test/pssac/pssac_G.sh
+++ test/pssac/pssac_G.sh
@@ -8,11 +8,12 @@ J=X15c/5c
 B=1
 PS=pssac_G.ps
 
-gmt set PS_MEDIA 21cx33c
+gmt set PS_MEDIA 21cx40c
 gmt pssac seis.sac -R$R -J$J -B$B -BWSen -W0p -K -P -G > $PS
 gmt pssac seis.sac -R$R -J$J -B$B -BWsen -W0p -K -O -Y6c -G+gblue+z-0.2 >> $PS
 gmt pssac seis.sac -R$R -J$J -B$B -BWsen -W0p -K -O -Y6c -Gn+gred+z0.2 >> $PS
 gmt pssac seis.sac -R$R -J$J -B$B -BWsen -W0.1p -K -O -Y6c -Gp+z0.2+t10/13+gblue -Gn+z-0.2+t12/18+gred >> $PS
 gmt pssac seis.sac -R$R -J$J -B$B -BWsen -W0.1p -K -O -Y6c -Gp+z-0.2+t10/13+gblue -Gn+z0.2+t12/18+gred >> $PS
+gmt pssac seis.sac -J$J -R10.2/15/-1.6/1.6 -B$B -BWSen -W0p -K -O -Y6c -Gn+gred >> $PS
 
 gmt psxy -R$R -J$J -O -T >> $PS
