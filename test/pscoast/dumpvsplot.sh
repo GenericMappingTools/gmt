93e772e0eb88155bbbeb5b3d121d42e803492f9b
Paul Wessel <pwessel@hawaii.edu>
2017-03-07 20:55:46 +0000

    Added new test for pscoast -M -E dump

---
+++ test/pscoast/dumpvsplot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Plot Poland directly or via a data dump
+ps=dumpvsplot.ps
+
+gmt pscoast -EPL -M > t.txt
+gmt psxy -R13/26/48/56 -JM4i -P -W0.25p t.txt -Baf -K -Xc > $ps
+gmt pscoast -R -J -EPL+p0.25p -Baf -O -Y5i >> $ps
