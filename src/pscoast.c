a8d16cf9a24eeb0153442879542e692bbc7c6735
Paul Wessel <pwessel@hawaii.edu>
2017-03-07 20:45:58 +0000

    Address lack of pscoast -M output with -E

--- src/pscoast.c
+++ src/pscoast.c
@@ -497,6 +497,9 @@ GMT_LOCAL int parse (struct GMT_CTRL *GMT, struct PSCOAST_CTRL *Ctrl, struct GMT
 		}
 	}
 
+	/* If no -E modes were selected then we default to dump provided -M was given */
+	if (Ctrl->E.info.mode == 0 && Ctrl->M.active) Ctrl->E.info.mode = GMT_DCW_DUMP;
+	
 	if (gmt_DCW_list (GMT, Ctrl->E.info.mode)) return 1;
 
 	if (Ctrl->C.active && !(Ctrl->G.active || Ctrl->S.active || Ctrl->W.active)) {	/* Just lakes, fix -A */
