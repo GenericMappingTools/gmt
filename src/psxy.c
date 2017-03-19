2d280e9b40dd028b999b4985607403cee256a3bf
Joaquim Luis <jmfluis@gmail.com>
2017-03-19 22:53:56 +0000

    Fix #1067

--- src/psxy.c
+++ src/psxy.c
@@ -1085,8 +1085,10 @@ int GMT_psxy (void *V_API, int mode, void *args) {
 				text_rec = (char *)record;
 
 				/* First establish the symbol type given at the end of the record */
-				gmt_chop (text_rec);	/* Get rid of \n \r */
-				i = (unsigned int)strlen (text_rec) - 1;
+				gmt_chop (text_rec);		/* Get rid of \n \r */
+				i = (unsigned int)strlen (text_rec);
+				if (i == 0) continue;		/* Empty line */
+				i -= 1;
 				while (text_rec[i] && !strchr (" \t", (int)text_rec[i])) i--;
 				if (S.read_symbol_cmd == 1) gmt_parse_symbol_option (GMT, &text_rec[i+1], &S, 0, false);
 				for (j = n_cols_start; j < 6; j++) GMT->current.io.col_type[GMT_IN][j] = GMT_IS_DIMENSION;		/* Since these may have units appended */
