5ddc7779eb48d4e328004f078b2d003fac70171d
Joaquim Luis <jmfluis@gmail.com>
2017-03-22 18:50:05 +0000

    Prepare for 5.3.3 patch release

--- cmake/ConfigDefault.cmake
+++ cmake/ConfigDefault.cmake
@@ -48,7 +48,7 @@ set (GMT_PACKAGE_VERSION_PATCH 3)
 # This is manually set when making GMT *public* releases.
 # However, when making internal releases or just an ordinary developer build, leave it
 # empty; if it is empty, the revision number is automatically populated for you on build.
-set (GMT_SOURCE_CODE_CONTROL_VERSION_STRING "")
+set (GMT_SOURCE_CODE_CONTROL_VERSION_STRING "17743")
 
 # The GMT package version.
 set (GMT_PACKAGE_VERSION "${GMT_PACKAGE_VERSION_MAJOR}.${GMT_PACKAGE_VERSION_MINOR}.${GMT_PACKAGE_VERSION_PATCH}")
