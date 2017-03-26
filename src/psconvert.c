771d692080f0092adbe9b2edd6f107756bee7008
Joaquim Luis <jmfluis@gmail.com>
2017-03-26 13:10:44 +0000

    Apply r17783 (implementing TCP in pipe_ghost())

--- src/psconvert.c
+++ src/psconvert.c
@@ -998,7 +998,7 @@ GMT_LOCAL int pipe_ghost (struct GMTAPI_CTRL *API, struct PS2RASTER_CTRL *Ctrl, 
 	      2. If it holds a file name plus the settings for that driver, than we save the result in a file.
 	*/
 	char      cmd[1024] = {""}, buf[GMT_LEN128], t[16] = {""};
-	int       fh, n, pix_w, pix_h;
+	int       fh, n, k, pix_w, pix_h;
 	uint64_t  dim[GMT_DIM_SIZE], nXY, row, col, band, nCols, nRows, nBands;
 	unsigned char *tmp;
 	unsigned int nopad[4] = {0, 0, 0, 0};
@@ -1114,13 +1114,20 @@ GMT_LOCAL int pipe_ghost (struct GMTAPI_CTRL *API, struct PS2RASTER_CTRL *Ctrl, 
 
 	nCols = dim[GMT_X];		nRows = dim[GMT_Y];		nBands = dim[2];	nXY = nRows * nCols;
 	tmp   = gmt_M_memory(API->GMT, NULL, nCols * nBands, char);
-	for (row = 0; row < nRows; row++) {
-		int ios;
-		ios = read (fh, tmp, (unsigned int)(nCols * nBands));	/* Read a row of nCols by nBands bytes of data */
-		for (col = 0; col < nCols; col++) {
-			for (band = 0; band < nBands; band++) {
-				I->data[row + col*nRows + band*nXY] = tmp[band + col*nBands];	/* This is Band interleaved. The best for MEX. */
-			}
+	if (!strncmp(I->header->mem_layout, "TCP", 3)) {		/* Images.jl in Julia wants this */
+		for (row = 0; row < nRows; row++) {
+			k = read (fh, tmp, (unsigned int)(nCols * nBands));	/* Read a row of nCols by nBands bytes of data */
+			for (col = n = 0; col < nCols; col++)
+				for (band = 0; band < nBands; band++)
+					I->data[row*nBands + col*nBands*nRows + band] = tmp[n++];
+		}
+	}
+	else {
+		for (row = 0; row < nRows; row++) {
+			k = read (fh, tmp, (unsigned int)(nCols * nBands));	/* Read a row of nCols by nBands bytes of data */
+			for (col = n = 0; col < nCols; col++)
+				for (band = 0; band < nBands; band++)
+					I->data[row + col*nRows + band*nXY] = tmp[n++];	/* Band interleaved, the best for MEX. */
 		}
 	}
 	gmt_M_free (API->GMT, tmp);
