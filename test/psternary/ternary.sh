f258c3e3d49c9d5d038f00fa0633dc76ee99fdc0
Paul Wessel <pwessel@hawaii.edu>
2017-04-03 22:16:23 +0000

    Fix ternary script

--- test/psternary/ternary.sh
+++ test/psternary/ternary.sh
@@ -4,4 +4,4 @@ ps=ternary.ps
 gmt makecpt -T0/70 -Cjet > t.cpt
 gmt psternary ${src:-.}/ternary.txt -R0/100/0/100/0/100 -JX6i -P -Xc -Baafg+l"Water component"+u" %" -Bbafg+l"Air component"+u" %" \
 -Bcagf+l"Limestone component"+u" %" -B+givory+t"Example data from MATLAB Central" -Sc0.1c -Ct.cpt -K -Y2i -LWater/Air/Limestone > $ps
-psscale -R0/100/0/100 -J -O -Ct.cpt -DJCB+o0/0.75i -Baf >> $ps
+gmt psscale -R0/100/0/100 -J -O -Ct.cpt -DJCB+o0/0.75i -Baf >> $ps
