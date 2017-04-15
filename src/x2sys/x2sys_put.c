6fef663caca3be30cd9e1f5b25fce0326fff6b5e
Joaquim Luis <jmfluis@gmail.com>
2017-04-15 16:22:21 +0000

    Fix "Resource leak" CID

--- src/x2sys/x2sys_put.c
+++ src/x2sys/x2sys_put.c
@@ -361,8 +361,10 @@ int GMT_x2sys_put (void *V_API, int mode, void *args) {
 	x2sys_path (GMT, old_track_file, old_track_path);
 	x2sys_path (GMT, old_index_file, old_index_path);
 
-	if (gmt_remove_file (GMT, old_track_path))	/* First delete old file */
+	if (gmt_remove_file (GMT, old_track_path)) {	/* First delete old file */
+		x2sys_end (GMT, s);
 		Return (GMT_RUNTIME_ERROR);
+	}
 	if (gmt_rename_file (GMT, track_path, old_track_path)) {
 		GMT_Report (API, GMT_MSG_NORMAL, "Rename failed for %s\t%s. Aborting %d!\n", track_path, old_track_path, i);
 		x2sys_end (GMT, s);
