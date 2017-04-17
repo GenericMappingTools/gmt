4b835f298f2761768a953c32492d0d8ee29def18
Joaquim Luis <jmfluis@gmail.com>
2017-04-17 12:39:10 +0000

    Fix CIDs 74414,15

--- src/potential/gpsgridder.c
+++ src/potential/gpsgridder.c
@@ -1136,6 +1136,7 @@ int GMT_gpsgridder (void *V_API, int mode, void *args) {
 				sprintf (file, Ctrl->G.file, tag[k]);
 				if (GMT_Set_Comment (API, GMT_IS_GRID, GMT_COMMENT_IS_OPTION | GMT_COMMENT_IS_COMMAND, options, Out[k])) Return (API->error);
 				if (GMT_Write_Data (API, GMT_IS_GRID, GMT_IS_FILE, GMT_IS_SURFACE, GMT_GRID_ALL, NULL, file, Out[k]) != GMT_NOERROR) {
+					gmt_M_free (GMT, xp);	gmt_M_free (GMT, yp);
 					Return (API->error);
 				}
 			}
