5541bb761c5fa242dcca071312d9b02fb0999e4d
Paul Wessel <pwessel@hawaii.edu>
2017-04-27 06:43:17 +0000

    Added F test script

---
+++ test/grdmath/F.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# $Id$
+ps=F.ps
+gmt grdmath -R1/10/1/15 -I1 0.95 X Y FCRIT = F.nc
+gmt psbasemap -R0.5/10.5/0.5/15.5 -JX6i/-9i -Bg1+0.5 -P -K -X1.5i -Y0.5i > $ps
+gmt grd2xyz F.nc --FORMAT_FLOAT_OUT=%.2f | gmt pstext -R -J -F+f12p -O -Ba1 \
+	-Bx+l"Degrees of freedom for numerator" -By+l"Degrees of freedom for denomitor" -BWN+t"Critical F table (@~a@~ = 0.05)" >> $ps
