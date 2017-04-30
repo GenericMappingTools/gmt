10632c52ce16a8799e788a7719e53e539214d7a6
Paul Wessel <pwessel@hawaii.edu>
2017-04-30 17:55:15 +0000

    Finalize PS for new tests

--- test/grdview/denver.sh
+++ test/grdview/denver.sh
@@ -7,10 +7,10 @@ ps=denver.ps
 radius_of_spaceship=100
 gmt makecpt -Cjet -T1000/4000 > t.cpt
 gmt grdgradient ${GMT_SOURCE_DIR:-../..}/doc/tutorial/us.nc -A225 -Nt1 -Gint.nc
-gmt grdmath -Rus.nc 104.9903W 39.7392N SDIST = r.nc
+gmt grdmath -Rint.nc 104.9903W 39.7392N SDIST = r.nc
 gmt grdmath r.nc $radius_of_spaceship LT = inside.nc
-gmt grdgradient us.nc -A90 -Nt1 -Gint1.nc
-gmt grdgradient us.nc -A-45 -Nt1 -Gint2.nc
+gmt grdgradient ${GMT_SOURCE_DIR:-../..}/doc/tutorial/us.nc -A90 -Nt1 -Gint1.nc
+gmt grdgradient ${GMT_SOURCE_DIR:-../..}/doc/tutorial/us.nc -A-45 -Nt1 -Gint2.nc
 gmt grdmath int1.nc int2.nc 0.5 MUL ADD inside.nc ADD 0.5 SUB = int.nc
-gmt grdview us.nc -Iint.nc -JM5i -JZ0.25i -p155/25 -Ct.cpt -P -K -Baf -Qi200 -X0.75i -N1000+glightgray > $ps
-gmt psscale -Ct.cpt -Rus.nc -J -DJCB+w4i/0.2i+h+o0/0.5i -p -O -Baf >> $ps
+gmt grdview ${GMT_SOURCE_DIR:-../..}/doc/tutorial/us.nc -Iint.nc -JM5i -JZ0.25i -p155/25 -Ct.cpt -P -K -Baf -Qi200 -X0.75i -N1000+glightgray > $ps
+gmt psscale -Ct.cpt -Rint.nc -J -DJCB+w4i/0.2i+h+o0/0.5i -p -O -Baf >> $ps
