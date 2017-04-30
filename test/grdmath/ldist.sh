760d44a727a5f297e222588d7cebc756bb176948
Paul Wessel <pwessel@hawaii.edu>
2017-04-30 08:38:15 +0000

    Added 3 more examples from GG675 as tests

---
+++ test/grdmath/ldist.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# $Id$
+#
+# Purpose:  Compute Cartesian distances from square and triangular polygons
+ps=ldist.ps
+# Triangle
+cat << EOF > p.txt
+-1	-1
+1	-1
+0	1
+-1	-1
+EOF
+gmt grdmath -R-10/10/-10/10 -I0.1 p.txt LDIST = d.nc
+gmt grdcontour d.nc -JX4i -P -A5 -C0.25 -Bafg -K -Xc > $ps
+gmt psxy -R -J p.txt -W2p,red -O -K >> $ps
+# Square
+cat << EOF > p.txt
+-1	-1
+1	-1
+1	1
+-1	1
+-1	-1
+EOF
+gmt grdmath -R-10/10/-10/10 -I0.1 p.txt LDIST = d.nc
+gmt grdcontour d.nc -J -O -A5 -C0.25 -Bafg -K -Y4.75i >> $ps
+gmt psxy -R -J p.txt -W2p,red -O >> $ps
---
+++ test/psxy/japquakes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# $Id$
+# Plot data reusing a columm
+ps=japquakes.ps
+
+# Plot seismicity map on top, specify a 500 km scalebar kinda in the lower-right corner where there are no quakes
+gmt makecpt -Cred,green,blue -T0,70,300,10000 > quakes.cpt
+gmt pscoast -R130/150/35/50 -JM6i -P -Baf -BWsNe+t"Seismicity of Japan" -Sazure1 -Gburlywood -K -X1.5i -Y3.5i > $ps
+gmt psxy quakes.ngdc -R -J -Sci -Cquakes.cpt -O -K -i4,3,5,6+s0.02 -hi3 >> $ps
+# Along bottom plot seismicity in a depth vs longitude Cartesian plot
+gmt psbasemap -R130/150/0/600 -JX6id/-2.5i -Bxaf -Byaf+l"Depth (km)" -O -K -BWSne+gazure1 -Y-2.75i >> $ps
+gmt psxy -R -J -O -K -W0.5p,orange,- << EOF >> $ps
+130	410
+150	410
+EOF
+# Plot color-coded, size-challenged symbols of longitude/depth
+gmt psxy quakes.ngdc -R -J -Sci -Cquakes.cpt -O -i4,5,5,6+s0.02 -hi3 >> $ps
