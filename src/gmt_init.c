4941aa948747b5c0be52bd5043799dce4635af7b
Paul Wessel <pwessel@hawaii.edu>
2017-05-27 22:44:12 +0000

    Better parsing to address #message-5334

--- src/gmt_init.c
+++ src/gmt_init.c
@@ -6894,7 +6894,7 @@ int gmt_parse_R_option (struct GMT_CTRL *GMT, char *item) {
 
 	strncpy (GMT->common.R.string, item, GMT_LEN256-1);	/* Verbatim copy */
 
-	if (strchr ("LCRlcr", item[0]) && strchr ("TMBtmb", item[1])) {	/* Extended -R option using coordinate codes and grid increments */
+	if ((strchr ("LCRlcr", item[0]) && strchr ("TMBtmb", item[1])) || (strchr ("LCRlcr", item[1]) && strchr ("TMBtmb", item[0]))) {	/* Extended -R option using coordinate codes and grid increments */
 		char X[2][GMT_LEN64] = {"", ""}, code[3] = {""};
 		double xdim, ydim, orig[2];
 		int n_columns, n_rows, just, part;
