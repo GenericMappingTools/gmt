bea61790c6d826d7e30da6571ca65ce7f0ec2514
Paul Wessel <pwessel@hawaii.edu>
2017-06-01 06:13:08 +0000

    Added another bad polygon wrap script

---
+++ test/psxy/antwrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#	$Id$
+#
+# Test plotting of Antarctica polygon in different projections.
+
+ps=antwrap.ps
+
+gmt psxy -R110/-79/-55/0r -JN6i -Gred -Bafg GSHHS_l_Antarctica.txt -P -K > $ps
+gmt psxy -R0/360/-90/-55 -JA0/-90/5i -O -Gred GSHHS_l_Antarctica.txt -Bafg -X0.5i -Y4.25i >> $ps
