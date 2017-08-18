ff44f1d8284ff3ff566a2a4775f3fc5f17e01fa6
Paul Wessel <pwessel@hawaii.edu>
2017-08-18 15:56:24 +0000

    Update radians

--- test/psbasemap/radians.sh
+++ test/psbasemap/radians.sh
@@ -6,5 +6,5 @@ gmt math -T-12pi/12pi/200+n T 4 DIV COS 20 MUL 25 ADD = cos.txt
 
 gmt psbasemap -R-12pi/12pi/0/100 -JX6i/7i -P -Bxa4pi -Byaf -K -Xc -Y1.5i > $ps
 gmt psxy -R -J -O -K -W2p cos.txt >> $ps
-gmt psscale -R -J -DjRM+w4i/0.2i+o0.5i/0 -Ct.cpt -Bxapi -O -K >> $ps
+gmt psscale -R -J -DjRM+w4i/0.2i+o0.5i/0 -Ct.cpt -Bxapi -O -K -Vd >> $ps
 gmt psscale -R -J -DJCB+w5i/0.2i+o0/0.5i -Ct.cpt -Bxapi2 -O >> $ps
