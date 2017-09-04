9fdbd803b016719f95c5976dfa5fab13599c964f
Paul Wessel <pwessel@hawaii.edu>
2017-09-04 02:55:13 +0000

    Revise img test to use smaller 2m grid

--- test/img/imgmap.sh
+++ test/img/imgmap.sh
@@ -3,21 +3,16 @@
 #	$Id$
 
 ps=imgmap.ps
-IMG=topo.15.1.img
-OK=`gmt which -C $IMG`
-if [ $OK = N ]; then
-	echo "File $IMG not available for testing imgmap.sh"
-	exit
-fi
+IMG=@topo.8.2.img
 # Get merc grid
-gmt img2grd $IMG -R180/200/-5/5 -I1m -T1 -D -S1 -Gimg.nc -M
+gmt img2grd $IMG -R180/200/-5/5 -T1 -S1 -Gimg.nc -M
 gmt makecpt -Crainbow -T-8000/0 > t.cpt
 gmt grdimage img.nc -Jx0.25i -Ct.cpt -P -K -Xc > $ps
 gmt psbasemap -R -Jm0.25i -Ba -BWSne -O -K >> $ps
 # Get geo grid
-gmt img2grd $IMG -R -I1m -T1 -D -S1 -Gimg.nc
+gmt img2grd $IMG -R -T1 -S1 -Gimg.nc
 gmt grdimage img.nc -Jm -Ct.cpt -O -K -Ba -BWSne -Y3.25i >> $ps
 # Get resampled geo grid
-gmt img2grd $IMG -R -I1m -T1 -D -S1 -Gimg.nc -E
+gmt img2grd $IMG -R -T1 -S1 -Gimg.nc -E
 gmt grdimage img.nc -Jm -Ct.cpt -O -K -Ba -BWSne -Y3.25i >> $ps
 gmt psxy -R -J -O -T >> $ps
