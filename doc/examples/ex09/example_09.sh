61fca1c68eca0345b1dfc4dc9976998b2831d7e1
Paul Wessel <pwessel@hawaii.edu>
2017-10-26 19:49:17 +0000

    Use -D in ex 9

--- doc/examples/ex09/example_09.sh
+++ doc/examples/ex09/example_09.sh
@@ -8,7 +8,7 @@
 #
 ps=example_09.ps
 gmt pswiggle @tracks_09.txt -R185/250/-68/-42 -K -Jm0.13i -Ba10f5 -BWSne+g240/255/240 -G+red \
-	-G-blue -Z2000 -Wthinnest -S240/-67/500/@~m@~rad --FORMAT_GEO_MAP=dddF > $ps
+	-G-blue -Z2000 -Wthinnest -DjBR+l500+u@~m@~rad+o0.2i --FORMAT_GEO_MAP=dddF > $ps
 gmt psxy -R -J -O -K @ridge_09.txt -Wthicker >> $ps
 gmt psxy -R -J -O -K @fz_09.txt -Wthinner,- >> $ps
 # Take label from segment header and plot near coordinates of last record of each track
