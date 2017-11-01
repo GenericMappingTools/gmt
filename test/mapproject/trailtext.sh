2fa91eed836750b8642c31b45f438cec10e0522e
Paul Wessel <pwessel@hawaii.edu>
2017-11-01 06:53:48 +0000

    Fix a test

--- test/mapproject/trailtext.sh
+++ test/mapproject/trailtext.sh
@@ -7,7 +7,7 @@ cat << EOF > line.txt
 1 2
 3 4
 EOF
-echo -e "0\t0\t246419.226859\t1\t2\tnot important text" > input.txt
+echo -e "0\t0\tnot important text" > input.txt
 
 gmt mapproject -Lline.txt input.txt > result.txt
 gmt mapproject -Lline.txt input.txt -on >> result.txt
@@ -15,8 +15,8 @@ gmt mapproject -Lline.txt input.txt -ot >> result.txt
 gmt mapproject -Lline.txt input.txt -o2,4 >> result.txt
 gmt mapproject -Lline.txt input.txt -o2,4,t >> result.txt
 cat << EOF > answer.txt
-0	0	246419.226859	1	2	246419.226859	1	2	not important text
-0	0	246419.226859	1	2	246419.226859	1	2
+0	0	246419.226859	1	2	not important text
+0	0	246419.226859	1	2
 not important text
 246419.226859	2
 246419.226859	2	not important text
