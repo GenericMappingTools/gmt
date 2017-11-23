fac81c8a6560b0c39271c7e0c71c2bcf95ad5a06
Paul Wessel <pwessel@hawaii.edu>
2017-11-23 02:16:58 +0000

    Remove +f from usage message

--- src/subplot.c
+++ src/subplot.c
@@ -154,7 +154,7 @@ GMT_LOCAL void Free_Ctrl (struct GMT_CTRL *GMT, struct SUBPLOT_CTRL *C) {	/* Dea
 GMT_LOCAL int usage (struct GMTAPI_CTRL *API, int level) {
 	gmt_show_name_and_purpose (API, THIS_MODULE_LIB, THIS_MODULE_NAME, THIS_MODULE_PURPOSE);
 	if (level == GMT_MODULE_PURPOSE) return (GMT_NOERROR);
-	GMT_Message (API, GMT_TIME_NONE, "usage: subplot begin <nrows>x<ncols> -F[f|s]<width(s)>/<height(s)>[:<wfracs/hfracs>][+f<fill>] [-A<autolabelinfo>]\n");
+	GMT_Message (API, GMT_TIME_NONE, "usage: subplot begin <nrows>x<ncols> -F[f|s]<width(s)>/<height(s)>[:<wfracs/hfracs>] [-A<autolabelinfo>]\n");
 	GMT_Message (API, GMT_TIME_NONE, "\t [-C<side><clearance>[u]] [-D[x][y]] [%s] [-SC<layout>][+<mods>] [-SR<layout>][+<mods>] [-M<margins>] [%s] [-T<title>] [%s]\n\n", GMT_J_OPT, GMT_Rgeo_OPT, GMT_V_OPT);
 	GMT_Message (API, GMT_TIME_NONE, "usage: subplot <row>,<col> [-A<fixedlabel>] [-C<side><clearance>[u]] [%s]\n\n", GMT_V_OPT);
 	GMT_Message (API, GMT_TIME_NONE, "usage: subplot end [%s]\n\n", GMT_V_OPT);
--- test/exmod/ex14.sh
+++ test/exmod/ex14.sh
@@ -19,7 +19,7 @@ gmt begin ex14 ps
   gmt grdtrack track -Gtrend.nc -o2,3 > trend.d
   gmt plot -Ra -JX6i/1.4i data.d -Wthick -Bx1 -By50 -BWSne -P
   gmt plot trend.d -Wthinner,-
-  gmt subplot begin 2x2 -M0.05i -Ff6i/6i+d -BWSne -Yh+0.4i
+  gmt subplot begin 2x2 -M0.05i -Ff6i/6i -BWSne -Yh+0.4i
     # First draw network and label the nodes
     gmt plot @Table_5_11.txt -R0/7/0/7 -Sc0.05i -Gblack -c1,1
     gmt text @Table_5_11.txt -D0.1c/0 -F+f6p+jLM -N
