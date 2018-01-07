7ecf414adcc6854c2c81f03bd40cb4b9194a6a93
Paul Wessel <pwessel@hawaii.edu>
2018-01-07 21:23:40 +0000

    Reset PS origin at end of completed plot

--- src/postscriptlight.c
+++ src/postscriptlight.c
@@ -3990,6 +3990,7 @@ int PSL_endplot (struct PSL_CTRL *PSL, int lastpage) {
 	if (!PSL_eq (PSL->current.rgb[PSL_IS_STROKE][3], 0.0)) PSL_command (PSL, "1 /Normal PSL_transp\n");
 
 	if (lastpage) {
+		PSL_command (PSL, "grestore\n");	/* End encapsulation of main body for this plot */
 		PSL_command (PSL, "%%PSL_Begin_Trailer\n");
 		PSL_command (PSL, "%%%%PageTrailer\n");
 		if (PSL->init.runmode) {
@@ -4203,6 +4204,7 @@ int PSL_beginplot (struct PSL_CTRL *PSL, FILE *fp, int orientation, int overlay,
 		i = PSL->internal.line_join;	PSL->internal.line_join = PSL_MITER_JOIN;	PSL_setlinejoin (PSL, i);
 		i = PSL->internal.miter_limit;	PSL->internal.miter_limit = PSL_MITER_DEFAULT;	PSL_setmiterlimit (PSL, i);
 		PSL_command (PSL, "%%PSL_End_Header\n");
+		PSL_command (PSL, "gsave\n");	/* Begin encapsulation of main body for this plot */
 	}
 
 	/* Set default line color and no-rgb */
