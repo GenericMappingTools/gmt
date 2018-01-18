f8d88691f0bb1c59c28515dfb4878c7c06751c6e
Joaquim Luis <jmfluis@gmail.com>
2018-01-18 16:47:52 +0000

    Increase size of 4 static vars

--- src/psconvert.c
+++ src/psconvert.c
@@ -1906,7 +1906,7 @@ int GMT_psconvert (void *V_API, int mode, void *args) {
 				continue;
 			}
 			else if (!found_proj && !strncmp (&line[2], "PROJ", 4)) {	/* Search for the PROJ tag in the ps file */
-				char *ptmp = NULL, xx1[20], xx2[20], yy1[20], yy2[20];
+				char *ptmp = NULL, xx1[32], xx2[32], yy1[32], yy2[32];
 				sscanf (&line[8], "%s %s %s %s %s %s %s %s %s",proj4_name,xx1,xx2,yy1,yy2,c1,c2,c3,c4);
 				west = atof (c1);		east = atof (c2);
 				south = atof (c3);		north = atof (c4);
