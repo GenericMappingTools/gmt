446a9365697c1de38c299a533d2c7ea6e2203c57
Paul Wessel <pwessel@hawaii.edu>
2018-01-20 00:46:59 +0000

    gmt-ize Erics NN code

--- src/grdfill.c
+++ src/grdfill.c
@@ -292,147 +292,128 @@ GMT_LOCAL unsigned int trace_the_hole (struct GMT_GRID *G, uint64_t node, unsign
 	return (n_nodes);
 }
 
-GMT_LOCAL int find_nearest (int i, int j, int *r2, int *is, int *js, int *xs, int *ys) {
-
-    // function to find the nearest point based on previous search, smallest distance ourside a radius
-    int ct=0,nx,ny,nx1,ii,k=0;
-    int rr;
+GMT_LOCAL int64_t find_nearest (int64_t i, int64_t j, int64_t *r2, int64_t *is, int64_t *js, int64_t *xs, int64_t *ys) {
+	/* function to find the nearest point based on previous search, smallest distance ourside a radius */
+	int64_t ct = 0, nx, ny, nx1, ii, k = 0, rr;
     
-    rr = *r2+1e7;
+	rr = *r2 + 1e7;
     
-    // starting with nx = ny
-    nx = (int)(sqrt((double)(*r2)/2.0));
-    // loop over possible nx, find smallest rr
-    for (nx1=nx;nx1<=(int)sqrt((double)(*r2))+1;nx1++) {
-        if (nx1*nx1 < *r2) ny = (int)(sqrt((*r2)-nx1*nx1));
-        else ny = 0;
-        while (nx1*nx1+ny*ny <= (*r2) && ny<=nx1) {
-            ny++;
-        }
-        if (ny<=nx1) {
-            if (rr>nx1*nx1+ny*ny) {
-                k = 0;
-                rr = nx1*nx1+ny*ny;
-                xs[k] = nx1;
-                ys[k] = ny;
-            }
-            else if (rr == nx1*nx1+ny*ny) {
-                k++;
-                xs[k] = nx1;
-                ys[k] = ny;
-            }
-        }
-    }
+	/* starting with nx = ny */
+	nx = (int64_t)(sqrt((double)(*r2)/2.0));
+	/* loop over possible nx, find smallest rr */
+	for (nx1 = nx; nx1 <= (int64_t)sqrt((double)(*r2))+1; nx1++) {
+		if (nx1*nx1 < *r2)
+			ny = (int64_t)(sqrt((*r2)-nx1*nx1));
+		else
+			ny = 0;
+		while (nx1*nx1+ny*ny <= (*r2) && ny<=nx1)
+			ny++;
+		if (ny <= nx1) {
+			if (rr > (nx1*nx1+ny*ny)) {
+				k = 0;
+				rr = nx1*nx1+ny*ny;
+				xs[k] = nx1;
+				ys[k] = ny;
+			}
+			else if (rr == (nx1*nx1+ny*ny)) {
+				k++;
+				xs[k] = nx1;
+				ys[k] = ny;
+			}
+		}
+	}
 
-    // return the grid index, changing the order may lead to different solution, but mainly because nearest-neighbor is non-unique
-    for (ii=0;ii<=k;ii++) {
-        nx = xs[ii];
-        ny = ys[ii];
+	/* Return the grid index, changing the order may lead to different solution, but mainly because nearest-neighbor is non-unique */
+	for (ii = 0; ii <= k; ii++) {
+		nx = xs[ii];
+		ny = ys[ii];
 
-        if (ny == 0 ) {
-            js[ct+0] = 0;  js[ct+1] = 0;   js[ct+2] = nx; js[ct+3] = -nx;
-            is[ct+0] = nx; is[ct+1] = -nx; is[ct+2] = 0;  is[ct+3] = 0;
-            ct = ct+4;
-        }
-        else if (nx != ny){
-            js[ct+0] = ny; js[ct+1] = ny;  js[ct+2] = -ny; js[ct+3] = -ny; js[ct+4] = nx; js[ct+5] = -nx; js[ct+6] = nx;  js[ct+7] = -nx;
-            is[ct+0] = nx; is[ct+1] = -nx; is[ct+2] = nx;  is[ct+3] = -nx; is[ct+4] = ny; is[ct+5] = ny;  is[ct+6] = -ny; is[ct+7] = -ny; 
-            ct = ct+8;
-        }
-        else {
-            js[ct+0] = nx; js[ct+1] = nx; js[ct+2] = -nx;  js[ct+3] = -nx;
-            is[ct+0] = nx; is[ct+1] = -nx;  is[ct+2] = nx; is[ct+3] = -nx;
-            ct = ct+4;
-        }
-    }
+		if (ny == 0) {
+			js[ct+0] = 0;  js[ct+1] = 0;   js[ct+2] = nx; js[ct+3] = -nx;
+			is[ct+0] = nx; is[ct+1] = -nx; is[ct+2] = 0;  is[ct+3] = 0;
+			ct += 4;
+		}
+		else if (nx != ny){
+			js[ct+0] = ny; js[ct+1] = ny;  js[ct+2] = -ny; js[ct+3] = -ny; js[ct+4] = nx; js[ct+5] = -nx; js[ct+6] = nx;  js[ct+7] = -nx;
+			is[ct+0] = nx; is[ct+1] = -nx; is[ct+2] = nx;  is[ct+3] = -nx; is[ct+4] = ny; is[ct+5] = ny;  is[ct+6] = -ny; is[ct+7] = -ny; 
+			ct += 8;
+		}
+		else {
+			js[ct+0] = nx; js[ct+1] = nx; js[ct+2] = -nx;  js[ct+3] = -nx;
+			is[ct+0] = nx; is[ct+1] = -nx;  is[ct+2] = nx; is[ct+3] = -nx;
+			ct += 4;
+		}
+ 	}
 
-    for (ii=0;ii<ct;ii++) {
-        is[ii] = is[ii]+i;
-        js[ii] = js[ii]+j;
-    }
+	for (ii = 0; ii < ct; ii++) {	/* Set absolute row/col values */
+		is[ii] = is[ii] + i;
+		js[ii] = js[ii] + j;
+	}
 
-    *r2 = rr;
+	*r2 = rr;	/* Update radius */
 
-    // return the indexes of the nearest neighbor
-    return(ct);
-
+	/* return the number of indexes of the nearest neighbor */
+	return (ct);
 }
 
-// GMT_LOCAL double nearest_interp(int nx,int ny, float *m, float *m_interp,int radius) {
-GMT_LOCAL void nearest_interp (struct GMT_CTRL *GMT, struct GMT_GRID *In, struct GMT_GRID *Out, uint64_t radius) {
-	uint64_t ij;
-	float *m = NULL, *m_interp = NULL;
-	int nx = In->header->n_columns;
-	int ny = In->header->n_rows;
- 	int i,j,flag,ct,k,kt,kk=1,recx=1,recy=1;
- 	int *is, *js, *xs, *ys;
- 	int cs=0;
- 	int rr;
-	gmt_M_unused(GMT);
+GMT_LOCAL void nearest_interp (struct GMT_CTRL *GMT, struct GMT_GRID *In, struct GMT_GRID *Out, int64_t radius) {
+	uint64_t ij, node;
+	int64_t nx = In->header->n_columns, ny = In->header->n_rows;
+ 	int64_t i, j, flag, ct, k, kt, kk = 1, recx = 1, recy = 1, cs = 0, rr;
+ 	int64_t *is = NULL, *js = NULL, *xs = NULL, *ys = NULL;
+	float *m = In->data, *m_interp = Out->data;	/* Short-hand for input and output grids */
  	
- 	// malloc memory for temporary indexes
- 	is = (int *)malloc(sizeof(int)*2048);
- 	js = (int *)malloc(sizeof(int)*2048);
- 	xs = (int *)malloc(sizeof(int)*512);
- 	ys = (int *)malloc(sizeof(int)*512);
-	m_interp = Out->data;
-	m = In->data;
+ 	/* Allocate memory for temporary indexes */
+ 	is = gmt_M_memory (GMT, NULL, 2048, int64_t);
+ 	js = gmt_M_memory (GMT, NULL, 2048, int64_t);
+ 	xs = gmt_M_memory (GMT, NULL, 512, int64_t);
+ 	ys = gmt_M_memory (GMT, NULL, 512, int64_t);
 
-	printf("Interpolating to nearest neighbour...\n  ");
-	fprintf(stderr,"Working on line  ");
-	// loop over each line
-	gmt_M_row_loop (GMT, In, i) {
-        for (k=0;k<kk;k++) fprintf(stderr,"\b");
-        fprintf(stderr,"%d ...",i+1);
-        kk = floor(log10 ((double)(i+1)))+1+4;
-        rr = 0; 
-        // loop over each row
-	gmt_M_col_loop (GMT, In, i, j, ij) {
-            // if it's not NaN, copy value
-            if (gmt_M_is_fnan (m[ij]) == false) {
-                //m_interp[ij] = m[i*nx+j];	ALready duplicated in main
-                rr = 0;
-            }
-            // if it's NaN, search nearest neighbor, use previous nearest distance to exclude certain search area.
-            else {
-                flag=0;
-                // set the starting search radius based on last nearest distance
-                if (rr >= 4 && recy > 0 && recx > 0) rr = (recx-1)*(recx-1)+(recy-1)*(recy-1)-1;
-                else if (rr >= 4 && recy == 0 && recx > 0) rr = (recx-1)*(recx-1)-1;
-                else if (rr >= 4 && recy > 0 && recx == 0) rr = (recy-1)*(recy-1)-1;
-                else rr = 0;
+	GMT_Report (GMT, GMT_MSG_LONG_VERBOSE, "Interpolating to nearest neighbour...\n");
+	gmt_M_row_loop (GMT, In, i) {	/* Loop over each row in grid */
+ 		GMT_Report (GMT, GMT_MSG_LONG_VERBOSE, "Working on row %" PRIi64 "\n", i);
+		kk = floor(log10 ((double)(i+1))) + 1 + 4;
+		rr = 0; 
+		gmt_M_col_loop (GMT, In, i, j, ij) {	/* Loop over all columns */
+			if (!gmt_M_is_fnan (m[ij]))	/* Already duplicated in the calling program */
+				rr = 0;
+			else {	/* search nearest neighbor, use previous nearest distance to exclude certain search area. */
+				flag = 0;
+				/* set the starting search radius based on last nearest distance */
+				if (rr >= 4 && recy > 0 && recx > 0) rr = (recx-1)*(recx-1)+(recy-1)*(recy-1)-1;
+				else if (rr >= 4 && recy == 0 && recx > 0) rr = (recx-1)*(recx-1)-1;
+				else if (rr >= 4 && recy > 0 && recx == 0) rr = (recy-1)*(recy-1)-1;
+				else rr = 0;
 
-                while (flag == 0 && rr <= (double)(radius*radius)) {
-                    ct = find_nearest(i,j,&rr,is,js,xs,ys);
-                    cs++;
-                    for (k=0;k<ct;k++) {
-                        if (is[k]>=0 && is[k]<ny && js[k]>=0 && js[k]<nx) {
-                            if (isnan(m[is[k]*nx+js[k]]) == 0) {
-                                m_interp[i*nx+j] = m[is[k]*nx+js[k]];
-                                flag = 1;
-                                kt = k;
-                                recx = abs(is[k]-i);
-                                recy = abs(js[k]-j);
-                                break;
-                            }
-                        }
-                    }
-                }
+				while (flag == 0 && rr <= (double)(radius*radius)) {
+					ct = find_nearest (i, j, &rr, is, js, xs, ys);
+					cs++;
+					for (k = 0; k < ct; k++) {
+						if (is[k] >= 0 && is[k] < ny && js[k] >=0 && js[k] < nx) {
+							node = gmt_M_ijp (In->header, is[k], js[k]);
+							if (!gmt_M_is_fnan (m[node])) {
+								m_interp[ij] = m[node];
+								flag = 1;
+								kt = k;
+								recx = labs (is[k]-i);
+								recy = labs (js[k]-j);
+								break;
+							}
+						}
+					}
+				}
+			}
+			if (i == 0 && rr == -1)
+				GMT_Report (GMT, GMT_MSG_DEBUG, "(%d %d %d %d)\n",j,rr,recx,recy);
+		}
+	}
 
-            }
-        if (i == 0 && rr == -1) fprintf(stderr,"(%d %d %d %d)\n",j,rr,recx,recy);
+	GMT_Report (GMT, GMT_MSG_LONG_VERBOSE, "%" PRIi64 " number of searches used\n", cs);
 
-        }
-
-    }
-    fprintf(stderr,"\n");
-
-    printf("%d number of searches used ...\n",cs);
-
-    free(is);
-    free(js);
-    free(xs);
-    free(ys);
+	gmt_M_free (GMT, is);
+	gmt_M_free (GMT, js);
+	gmt_M_free (GMT, xs);
+	gmt_M_free (GMT, ys);
 }
 
 #define bailout(code) {gmt_M_free_options (mode); return (code);}
