0aa79706fe03a1ab335b95f858855a966ec70e83
Paul Wessel <pwessel@hawaii.edu>
2018-02-22 22:16:44 +0000

    Address issue #1201

---
+++ test/psxy/arrowline.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Examine issue #1201
+ps=arrowline.ps
+
+cat << EOF > p.txt
+0.2 0 0 2
+0.4 0 0 1
+0.6 0 0 0.5
+0.8 0 0 0.25
+EOF
+gmt psxy -Jx10c -R0.1/0.9/-0.1/0.7 -SV1c+e+h0 -W0.5p,black -P -X2.5c -Y1.5c -K p.txt -Bx0 -Byg0.1 --PROJ_LENGTH_UNIT=cm > $ps
+echo CM | gmt pstext -R -J -O -K -F+f18p+cTL -Dj0.1i >> $ps
+gmt psxy -J -R -SV1c+e+h0 -W0.5p,black -O -K p.txt -Bx0 -Byg0.1 -X8.5c -PROJ_LENGTH_UNIT=inch >> $ps
+echo INCH | gmt pstext -R -J -O -K -F+f18p+cTL -Dj0.1i >> $ps
+gmt psxy -J -R -SV1c+e+h0+n1c -W0.5p,black -O -K -X-8.5c -Y8.5c p.txt -Bx0 -Byg0.1 --PROJ_LENGTH_UNIT=cm >> $ps
+echo CM | gmt pstext -R -J -O -K -F+f18p+cTL -Dj0.1i >> $ps
+gmt psxy -J -R -SV1c+e+h0+n1c -W0.5p,black -O -K p.txt -Bx0 -Byg0.1 -X8.5c -PROJ_LENGTH_UNIT=inch >> $ps
+echo INCH | gmt pstext -R -J -O -K -F+f18p+cTL -Dj0.1i >> $ps
+gmt psxy -J -R -SV1c+e+h0+n0.5c -W0.5p,black -Gblack -O -K -X-8.5c -Y8.5c p.txt -Bx0 -Byg0.1 --PROJ_LENGTH_UNIT=cm >> $ps
+echo CM | gmt pstext -R -J -O -K -F+f18p+cTL -Dj0.1i >> $ps
+gmt psxy -J -R -SV1c+e+h0+n0.5c -W0.5p,black -Gblack -O -K p.txt -Bx0 -Byg0.1 -X8.5c -PROJ_LENGTH_UNIT=inch >> $ps
+echo INCH | gmt pstext -R -J -O -F+f18p+cTL -Dj0.1i >> $ps
