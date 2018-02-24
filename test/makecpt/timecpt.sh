434aa347cbfdc6554da9a2b28b7f56d305d42748
Paul Wessel <pwessel@hawaii.edu>
2018-02-24 07:28:20 +0000

    Fix timecpt defaults

--- test/makecpt/timecpt.sh
+++ test/makecpt/timecpt.sh
@@ -3,6 +3,7 @@
 # Test makecpt and psscale with calendar time axis as x or y
 
 ps=timecpt.ps
+gmt set FORMAT_DATE_MAP "o" FORMAT_TIME_PRIMARY_MAP Abbrev
 gmt makecpt -T2017T/2018T/1o -Cjet > t.cpt
 gmt psxy -R2017-01-01T/2018-01-01T/10000/40000 @HI_arrivals_2017.txt -JX6iT/3.5i -Ct.cpt -i0,1,0 -Sc0.1c -BWsNe -P -Bxa1Og1o -Byafg -K -Xc -Y1.5i> $ps
 gmt psscale -Ct.cpt -DJBC -O -K -R -J -Bxa1Og1o >> $ps
