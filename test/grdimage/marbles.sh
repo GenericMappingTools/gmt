ccf02cbe26ac87109df5810f205a40d69feb3c7e
Paul Wessel <pwessel@hawaii.edu>
2018-03-05 20:04:11 +0000

    Needed a src check

--- test/grdimage/marbles.sh
+++ test/grdimage/marbles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Plot the NASA Blue Marble 1x1 degree geotiff in different projections
 ps=marbles.ps
-gmt grdimage -JG0/0/3i -D BlueMarble_60m.tiff -P -Y7i -K > $ps
-gmt grdimage -JG90/30/3i -D BlueMarble_60m.tiff -O -K -X3.5i >> $ps
-gmt grdimage -JH270/6.5i -D BlueMarble_60m.tiff -O -K -X-3.5i -Y-3.5i >> $ps
-gmt grdimage -JM6.5i -R0/360/-45/45 -D -Bag BlueMarble_60m.tiff -O -Y-2.5i >> $ps
+gmt grdimage -JG0/0/3i -D ${src:-.}/BlueMarble_60m.tiff -P -Y7i -K > $ps
+gmt grdimage -JG90/30/3i -D ${src:-.}/BlueMarble_60m.tiff -O -K -X3.5i >> $ps
+gmt grdimage -JH270/6.5i -D ${src:-.}/BlueMarble_60m.tiff -O -K -X-3.5i -Y-3.5i >> $ps
+gmt grdimage -JM6.5i -R0/360/-45/45 -D -Bag ${src:-.}/BlueMarble_60m.tiff -O -Y-2.5i >> $ps
