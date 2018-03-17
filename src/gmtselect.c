b2ff3f8a8e86eb9dde796819d2e926fe7cc5ac4c
Paul Wessel <pwessel@hawaii.edu>
2018-03-17 00:16:42 +0000

    Fix issue #1215

--- src/gmtselect.c
+++ src/gmtselect.c
@@ -865,8 +865,13 @@ int GMT_gmtselect (void *V_API, int mode, void *args) {
 
 		if (Ctrl->G.active) {	/* Check if we are in/out-side mask cell */
 			unsigned int row, col;
-			inside = !gmt_row_col_out_of_bounds (GMT, In->data, G->header, &row, &col);
-			if (inside) {	/* Inside -R so check the node value */
+			if (gmt_M_y_is_outside (GMT, In->data[GMT_Y], G->header->wesn[YLO], G->header->wesn[YHI]) ||	/* Outside y-range */
+				gmt_x_is_outside (GMT, &In->data[GMT_X], G->header->wesn[XLO], G->header->wesn[XHI])) {	/* Outside x-range */
+				inside = false;	/* Outside both y- and x-range (or periodic longitude) for this grid */
+			}
+			else
+				inside = !gmt_row_col_out_of_bounds (GMT, In->data, G->header, &row, &col);
+			if (inside) {	/* Inside grid so check the node value */
 				uint64_t node = gmt_M_ijp (G->header, row, col);
 				inside = !(gmt_M_is_fnan (G->data[node]) || G->data[node] == 0);
 			}
