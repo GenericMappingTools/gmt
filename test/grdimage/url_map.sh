4236dea04a654748ca3548b81402f3cb38a3a044
Paul Wessel <pwessel@hawaii.edu>
2018-03-23 21:34:11 +0000

    Let url_map just do an URL plot

--- test/grdimage/url_map.sh
+++ test/grdimage/url_map.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Actually, netCDF is not supported via the virtual file system so comment out for mow
 ps=url_map.ps
-gmt grdimage -Rd -JI15c /vsicurl/http://larryfire.files.wordpress.com/2009/07/untooned_jessicarabbit.jpg -P -Xc > $ps
+gmt grdimage -Rd -JI15c http://larryfire.files.wordpress.com/2009/07/untooned_jessicarabbit.jpg -P -Xc > $ps
 #gmt makecpt -Cglobe > t.cpt
 #gmt grdimage /vsicurl/http://imina.soest.hawaii.edu/pwessel/etopo5m.nc -J -Ct.cpt -B0 -B+t"grdimage via URLs" -O -Y4i >> $ps
 echo "Note: Will fail if gdal is not built with libcurl"
