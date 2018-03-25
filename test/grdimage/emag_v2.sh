14efa024396f5aee072c88dd1f2b89daeb1736bf
Paul Wessel <pwessel@hawaii.edu>
2018-03-25 02:56:54 +0000

    Use a smaller EMAG2 file

--- test/grdimage/emag_v2.sh
+++ test/grdimage/emag_v2.sh
@@ -4,8 +4,8 @@
 ps=emag_v2.ps
 
 gmt makecpt -Crainbow -T-200/200/50 > col.cpt
-gmt grdcut @EMAG2_V2.grd -Gtmp.nc -R-25/-10/60/67
-gmt grdimage @EMAG2_V2.grd -P -JM4i -R -Ccol.cpt -K -Baf -BWSne -Xc > $ps
+gmt grdcut @EMAG2_V2_1x1.grd -Gtmp.nc -R-25/-10/60/67
+gmt grdimage @EMAG2_V2_1x1.grd -P -JX4id -R -Ccol.cpt -K -Baf -BWSne -Xc > $ps
 echo ORIG | gmt pstext -R -J -O -K -F+cTR+f24p+jTR -Gwhite >> $ps
 gmt pscoast -Gblack -Dc -O -K -R -J >> $ps
 gmt grdimage tmp.nc -J -Ccol.cpt -K -O -Y5i -Baf -BWSne >> $ps
