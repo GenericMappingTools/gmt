bb941a5a5fea1a3b4b9d1248888a8e94d0e76854
Paul Wessel <pwessel@hawaii.edu>
2018-04-29 00:26:40 +0000

    Finalize new test PS

--- test/gmtregress/rls_on_rma.sh
+++ test/gmtregress/rls_on_rma.sh
@@ -11,8 +11,8 @@ gmt regress noisy_data.txt -Nr -Er > rma.txt
 gmt regress noisy_data.txt -Nr -Er -S > raw.txt
 gmt regress noisy_data.txt -Nw -Er > model.txt
 gmt psxy rma.txt -R-200/-30/200/800 -JX6i/9i -P -Baf -W2p,green -i0,2 -K -Xc > $ps
-psxy model.txt -R -J -O -K -W4p -i0,2 >> $ps
-gmt regress raw.txt -i0,1 -Er | psxy -R -J -O -K -W1p,orange -i0,2 >> $ps
+gmt psxy model.txt -R -J -O -K -W4p -i0,2 >> $ps
+gmt regress raw.txt -i0,1 -Er | gmt psxy -R -J -O -K -W1p,orange -i0,2 >> $ps
 gmt psxy model.txt -i0,1,6 -Ccode.cpt -R -J -O -Sc0.25c -K >> $ps
 gmt pslegend -R -J -O -DjBR+w3.5i+jBR+o0.1i -F+p1p << EOF >> $ps
 S 0.2i c 0.25c blue - 0.5i Good point according to @;green;RMA via LMS@;;
