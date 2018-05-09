0891098d64f650f6b74604916ea0d3675343dc73
Paul Wessel <pwessel@hawaii.edu>
2018-05-09 19:41:23 +0000

    Added simple version of Sabins problem in message #7065

---
+++ test/grdmask/twoblobs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Test script demonstrating problem discussed in message #7065 [Sabin].
+# grdmask does not create the mask for the polygon in the W hemisphere (?)
+ps=twoblobs.ps
+gmt makecpt -T0,1,2 -Cwhite,red > t.cpt
+cat << EOF > two.txt
+> I straddle Greenwhich
+2	40
+10	40
+10	35
+1	35
+-5	35
+-5	40
+2	40
+> I am in the south Pacific
+-135	-20
+-125	-20
+-125	-30
+-135	-30
+-135	-20
+EOF
+gmt psxy -R-180/180/-60/60 -JM6i -P -Bafg -B+t"Plot polygons with psxy" -K two.txt -Gred > $ps
+
+gmt grdmask -R two.txt -Gout.nc -N0/1/1 -V -I20k
+gmt grdimage -R -J -Ct.cpt out.nc -Bafg -B+t"Plot polygons via grdmask/image" -O -Y5i >> $ps
+gmt psconvert -Tf -P $ps
+open twoblobs.pdf
