d1f1b5134a165164e756430574896104e5e97e3b
Paul Wessel <pwessel@hawaii.edu>
2018-05-20 00:14:35 +0000

    Added test for issue #131

---
+++ test/psbasemap/poption.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Test script for -p perspective. Ancient #131 issue.
+# The p<az>/90 is wrongly positioned while th p180/90 is always correct.
+
+ps=poption.ps
+psbasemap -Vd -R0/360/-90/0 -JA0/-90/4c -Bg360a360f360 -Xc0c -Yc0c -P -K -p45/90+w0/-90 > $ps
+psbasemap -Vd -R -J -P -O -Bg360a360f360 -p180/90+w0/-90 --MAP_GRID_PEN_PRIMARY=thin,blue --MAP_FRAME_PEN=thin,blue --MAP_TICK_PEN_PRIMARY=thin,blue >> $ps
