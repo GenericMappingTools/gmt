1960ffb72c00fdfc460c291301a63cee91bfb4b0
Paul Wessel <pwessel@hawaii.edu>
2018-05-28 03:27:08 +0000

    Add another pssolar test since dumping via psxy works

--- test/pssolar/pssolar_fill.sh
+++ test/pssolar/pssolar_fill.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Documenting issue # 1155 [Andreas]
+# Documenting issue # 1155 [Andreas].  The problem is that the terminator
+# touches the N pole and what is inside and outside of the global polygon
+# becomes harder to decide.  The 11:30:58 time is ok but an hour later...
 
 ps=pssolar_fill.ps
 gmt pscoast -P -Rd -JKs0/6i -Dl -A5000 -W0.5p -N1/0.5p,gray -S175/210/255 -Bafg --MAP_FRAME_TYPE=plain -K -Xc > $ps
@@ -10,7 +12,7 @@ gmt pssolar -R  -J -Td+d$date -Wthick -Gnavy@95 -K -O >> $ps
 gmt pssolar -R  -J -Tc+d$date -Wthick -Gnavy@85 -K -O >> $ps
 gmt pssolar -R  -J -Tn+d$date -Wthick,red@80 -Gnavy@80 -K -O >> $ps
 gmt pssolar -R  -J -Ta+d$date -Wthick,blue@80 -Gnavy@80 -K -O >> $ps
-gmt pssolar -I+d$date -C | psxy -R -J -Sc1c -Gyellow -O -K -B+t"$date" >> $ps
+gmt pssolar -I+d$date -C | gmt psxy -R -J -Sc1c -Gyellow -O -K -B+t"$date" >> $ps
 
 gmt pscoast -R -J -Dl -A5000 -W0.5p -N1/0.5p,gray -S175/210/255 -Bafg --MAP_FRAME_TYPE=plain -O -K -Y4.5i >> $ps
 
@@ -20,4 +22,4 @@ gmt pssolar -R  -J -Td+d$date -Wthick -Gnavy@95 -K -O >> $ps
 gmt pssolar -R  -J -Tc+d$date -Wthick -Gnavy@85 -K -O >> $ps
 gmt pssolar -R  -J -Tn+d$date -Wthick,red@80 -Gnavy@80 -K -O >> $ps
 gmt pssolar -R  -J -Ta+d$date -Wthick,blue@80 -Gnavy@80 -K -O >> $ps
-gmt pssolar -I+d$date -C | psxy -R -J -Sc1c -Gyellow -O -B+t"$date" >> $ps
+gmt pssolar -I+d$date -C | gmt psxy -R -J -Sc1c -Gyellow -O -B+t"$date" >> $ps
---
+++ test/pssolar/pssolar_fill2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Documenting issue # 1155 [Andreas].  The problem is that the terminator
+# touches the N pole and what is inside and outside of the global polygon
+# becomes harder to decide.  This case shows that while pssolar selects the
+# wrong side, dumping it to file and plotting via psxy selects the correct side.
+# We are faking the orig PS by using psxy for both, then commenting that out
+ps=pssolar_fill2.ps
+gmt pscoast -Rd -JKs0/7i -Dl -A5000 -W0.5p -S175/210/255 -Bafg --MAP_FRAME_TYPE=plain -P -K -Xc > $ps
+
+date="2017-09-26T12:40:58"
+
+# Dump to file then plot
+gmt pssolar -Ta+d$date -M > t.txt
+gmt psxy -R  -J t.txt -Wthick -Gnavy@95 -K -O >> $ps
+gmt pssolar -I+d$date -C | gmt psxy -R -J -Sc1c -Gyellow -O -K -B+t"$date dump via psxy" >> $ps
+
+# Draw in pssolar instead
+gmt pscoast -R -J -Dl -A5000 -W0.5p -S175/210/255 -Bafg --MAP_FRAME_TYPE=plain -O -K -Y5i >> $ps
+gmt psxy -R  -J t.txt -Wthick -Gnavy@95 -K -O >> $ps
+#gmt pssolar -R -J -Ta+d$date -Wthick -Gnavy@95 -K -O >> $ps
+gmt pssolar -I+d$date -C | gmt psxy -R -J -Sc1c -Gyellow -O -B+t"$date directly via pssolar" >> $ps
