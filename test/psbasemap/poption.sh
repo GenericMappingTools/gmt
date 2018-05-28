f92149364fda3fbe1dbe706a1f6eae558668a4f4
Paul Wessel <pwessel@hawaii.edu>
2018-05-28 02:37:15 +0000

    Add new z rotation test

--- test/psbasemap/poption.sh
+++ test/psbasemap/poption.sh
@@ -3,5 +3,5 @@
 # The p<az>/90 is wrongly positioned while th p180/90 is always correct.
 
 ps=poption.ps
-psbasemap -Vd -R0/360/-90/0 -JA0/-90/4c -Bg360a360f360 -Xc0c -Yc0c -P -K -p45/90+w0/-90 > $ps
-psbasemap -Vd -R -J -P -O -Bg360a360f360 -p180/90+w0/-90 --MAP_GRID_PEN_PRIMARY=thin,blue --MAP_FRAME_PEN=thin,blue --MAP_TICK_PEN_PRIMARY=thin,blue >> $ps
+gmt psbasemap -Vd -R0/360/-90/0 -JA0/-90/4c -Bg360a360f360 -Xc0c -Yc0c -P -K -p45/90+w0/-90 > $ps
+gmt psbasemap -Vd -R -J -P -O -Bg360a360f360 -p180/90+w0/-90 --MAP_GRID_PEN_PRIMARY=thin,blue --MAP_FRAME_PEN=thin,blue --MAP_TICK_PEN_PRIMARY=thin,blue >> $ps
---
+++ test/psbasemap/zrotation.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Test script for plain -p<rot> about map center.
+# This should place 4 plots that differ by a rotation about the S pole
+# Currently fails.  Faked the orig by using -JA<rot>... instead
+
+ps=zrotation.ps
+gmt set MAP_FRAME_TYPE plain
+gmt psxy -R0/8.3/0/8.3 -Jx1i -P -K -W1p -X0.1i -Y1.35i -B0 -B+t"Rotation about S pole axis" << EOF > $ps
+> x midline
+4.15	0
+4.15	8.3
+> Y midline
+0	4.15
+8.3	4.15
+EOF
+gmt pstext -R -J -O -K -F+jTL -Dj0.2i << EOF >> $ps
+0.00 4.15 rot = 0
+4.15 4.15 rot = 45
+0.00 8.30 rot = 180
+4.15 8.30 rot = 245
+EOF
+# LL corner is standard, no rotation
+gmt pscoast -R0/360/-90/0 -JA0/-90/3.25i -Bg90a360f360 -O -K -Gred -Dc -X0.45i -Y0.45i >> $ps
+# LR corner is 45 degrees rotation
+gmt pscoast -R -JA45/-90/3.25i -Bg90a360f360 -Gred -Dc -X4.15i -O -K -p0+w0/-90 >> $ps
+# UL corner is 180 degrees rotation
+gmt pscoast -R -JA180/-90/3.25i -Bg90a360f360 -Gred -Dc -X-4.15i -Y4.15i -O -K -p0+w0/-90 >> $ps
+# UR corner is 245 degrees rotation
+gmt pscoast -R -JA245/-90/3.25i -Bg90a360f360 -Gred -Dc -X4.15i -O -p0+w0/-90 >> $ps
