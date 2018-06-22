1af23febd2c3e917ad0f073565ae21006c35109b
Paul Wessel <pwessel@hawaii.edu>
2018-06-22 00:21:25 +0000

    Fix filename

--- test/psxy/categorical.sh
+++ test/psxy/categorical.sh
@@ -11,7 +11,7 @@ Reykjanes	red
 Klitgord	green
 Dietmar		blue
 EOF
-gmt psxy -R-40/-25/30/45 -JM6i -P -Baf @RidgeTest.shp -aZ=NAME -Cridge.cpt -K -Xc > $ps
+gmt psxy -R-40/-25/30/45 -JM6i -P -Baf RidgeTest.shp -aZ=NAME -Cridge.cpt -K -Xc > $ps
 gmt psxy -R -J -O -Sc0.2i -Cridge.cpt << EOF >> $ps
 -37	33	Dietmar
 -32	38	Klitgord
