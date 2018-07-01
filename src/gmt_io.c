dc9ce311d4775d7e6784c27e3f8336d8278d0d57
Joaquim Luis <jmfluis@gmail.com>
2018-07-01 12:18:05 +0000

    Fix CID 186707

--- src/gmt_io.c
+++ src/gmt_io.c
@@ -3831,7 +3831,7 @@ GMT_LOCAL FILE *gmt_nc_fopen (struct GMT_CTRL *GMT, const char *filename, const 
 		nc_inq_varname (GMT->current.io.ncid, GMT->current.io.varid[i], varname);
 
 		/* Check number of dimensions */
-		nc_inq_varndims (GMT->current.io.ncid, GMT->current.io.varid[i], &ndims);
+		gmt_M_err_trap (nc_inq_varndims (GMT->current.io.ncid, GMT->current.io.varid[i], &ndims));
 		if (ndims > 5) {
 			GMT_Report (GMT->parent, GMT_MSG_NORMAL, "NetCDF variable %s has too many dimensions (%d)\n", varname, j);
 			GMT_exit (GMT, GMT_DIM_TOO_LARGE); return NULL;
