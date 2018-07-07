e121cd7d6dd7e69dd8010ef426368ed8f3c9ed43
Joaquim Luis <jmfluis@gmail.com>
2018-07-07 23:24:38 +0000

    Add earthtide test

---
+++ test/potential/test_earthtide.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#	$Id$
+#
+# Tests gmt earthtide against the values of the original FORTRAN version 
+
+
+rm -f test_earthtide.dat
+
+# Skip the first column with the time since the fortran version prints seconds since t0
+gmt earthtide -T2018-7-7T/2018-7-7T00:04:00/1m -L0/0  --FORMAT_FLOAT_OUT=%.6f -o1,2,3 > test_earthtide.dat
+
+# Output from the Fortran version
+#-0.015781 -0.012347 -0.027025
+#-0.015756 -0.012132 -0.027402
+#-0.015731 -0.011916 -0.027772
+#-0.015705 -0.011698 -0.028136
+#-0.015678 -0.011480 -0.028494
+
+diff test_earthtide.dat "${src:-.}"/test_earthtide.dat --strip-trailing-cr > fail
