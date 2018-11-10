e1c0d4cbd933bc5b95611fbed81b34025bb427a2
Dongdong Tian <seisman.info@gmail.com>
2018-11-09 23:24:31 -0500

    Move cmake parameters to configuration file

--- ci/travis-build.sh
+++ ci/travis-build.sh
@@ -4,13 +4,18 @@
 # To return a failure if any commands inside fail
 set -e
 
+cat << EOF >> cmake/ConfigUser.cmake
+set (CMAKE_INSTALL_PREFIX "$ENV{INSTALLDIR}")
+set (GMT_LIBDIR "$ENV{INSTALLDIR}/lib")
+set (DCW_ROOT "$ENV{COASTLINEDIR}")
+set (GSHHG_ROOT "$ENV{COASTLINEDIR}")
+EOF
+
+cat cmake/ConfigUser.cmake
+
 mkdir build && cd build
 
-cmake -D CMAKE_INSTALL_PREFIX=$INSTALLDIR \
-      -D GMT_LIBDIR=$INSTALLDIR/lib \
-      -D DCW_ROOT=$COASTLINEDIR \
-      -D GSHHG_ROOT=$COASTLINEDIR \
-      ..
+cmake ..
 
 make -j
 make check
