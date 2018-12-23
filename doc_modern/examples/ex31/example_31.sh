225359cd4dafb6f847ee441b70b73ab702d1fa52
Paul Wessel <pwessel@hawaii.edu>
2018-12-23 13:40:44 -1000

    Let -T option be a modifer under -C
    The -T and -C are dealing with the same issue: The text bounding box atrributes.  I have combined these with -T now being a modifer +t<shape> under -C.  Tests pass, documentation has been updated.

--- doc_modern/examples/ex31/example_31.sh
+++ doc_modern/examples/ex31/example_31.sh
@@ -50,7 +50,7 @@ gmt psxy -R -J -Sc0.15c -W1.25p -O -K >> $ps
 
 # label big EU cities
 $AWK 'BEGIN {FS=","} $4 > 1000000 {print $1, $2, $3}' $capitals | \
-gmt pstext -R -J -F+f7p,LinBiolinumOI+jBL -Dj0.1c -Gwhite -C5% -Qu -TO -O -K >> $ps
+gmt pstext -R -J -F+f7p,LinBiolinumOI+jBL -Dj0.1c -Gwhite -C5%+tO -Qu -O -K >> $ps
 
 # construct legend
 cat << EOF > legend.txt
--- test/potential/deflections.sh
+++ test/potential/deflections.sh
@@ -15,7 +15,7 @@ gmt grdimage smt.nc -R-100/100/-100/100 -JX3i -P -Bag -BWSne -Ct.cpt -K > $ps
 gmt grdtrack -Gsmt.nc -ELM/RM+d > smt.trk
 gmt psxy -R -J -O -K -W5p,white smt.trk >> $ps
 gmt psxy -R -J -O -K -W1p smt.trk >> $ps
-echo "-100 100 BATHYMETRY" | gmt pstext -R -J -O -K -F+jTL+f14p -Dj0.1i -Gwhite -TO >> $ps
+echo "-100 100 BATHYMETRY" | gmt pstext -R -J -O -K -F+jTL+f14p -Dj0.1i -Gwhite -C+tO >> $ps
 # 2. Compute the E-W deflection anomaly
 gmt gravfft smt.nc+uk -D1670 -Nf+a -Fe -E$order -Gdef_e.nc
 # ML plot the E-W deflection anomaly
@@ -24,7 +24,7 @@ gmt grdimage def_e.nc -R-100/100/-100/100 -JX3i -O -Bag -BWSne -Ct.cpt -K -X3.5i
 gmt grdtrack -Gdef_e.nc -ELM/RM+d > def_e.trk
 gmt psxy -R -J -O -K -W5p,white def_e.trk >> $ps
 gmt psxy -R -J -O -K -W1p,blue def_e.trk >> $ps
-echo "-100 100 @~h@~" | gmt pstext -R -J -O -K -F+jTL+f14p -Dj0.1i -Gwhite -TO >> $ps
+echo "-100 100 @~h@~" | gmt pstext -R -J -O -K -F+jTL+f14p -Dj0.1i -Gwhite -C+tO >> $ps
 # 3. Compute the VGG anomaly
 gmt gravfft smt.nc+uk -D1670 -Nf+a -Fv -E$order -Gvgg.nc
 # BR plot the VGG anomaly
@@ -33,7 +33,7 @@ gmt grdimage vgg.nc -R-100/100/-100/100 -JX3i -O -Bag -BWsne -Ct.cpt -K -X-3.5i 
 gmt grdtrack -Gvgg.nc -ELM/RM+d > vgg.trk
 gmt psxy -R -J -O -K -W5p,white vgg.trk >> $ps
 gmt psxy -R -J -O -K -W1p,red vgg.trk >> $ps
-echo "-100 100 VGG" | gmt pstext -R -J -O -K -F+jTL+f14p -Dj0.1i -Gwhite -TO >> $ps
+echo "-100 100 VGG" | gmt pstext -R -J -O -K -F+jTL+f14p -Dj0.1i -Gwhite -C+tO >> $ps
 # 4. Compute the N-S deflection anomaly anomaly
 gmt gravfft smt.nc+uk -D1670 -Nf+a -Fn -E$order -Gdef_n.nc
 # MR plot the N-S deflection anomaly
@@ -42,7 +42,7 @@ gmt grdimage def_n.nc -R-100/100/-100/100 -JX3i -O -Bag -BWsne -Ct.cpt -K -X3.5i
 gmt grdtrack -Gdef_n.nc -EBL/TR+d > def_n.trk
 gmt psxy -R -J -O -K -W5p,white def_n.trk >> $ps
 gmt psxy -R -J -O -K -W1p,orange def_n.trk >> $ps
-echo "-100 100 @~x@~" | gmt pstext -R -J -O -K -F+jTL+f14p -Dj0.1i -Gwhite -TO >> $ps
+echo "-100 100 @~x@~" | gmt pstext -R -J -O -K -F+jTL+f14p -Dj0.1i -Gwhite -C+tO >> $ps
 # 5 Plot crossections of bathy and vgg crossections
 # TL plot the bathy and vgg anomaly
 gmt psxy -R-100/100/-5100/1000 -JX3i/2.5i -O -K -W1p -i0,3 smt.trk -Baf -BWsN -X-3.5i -Y3.2i >> $ps
