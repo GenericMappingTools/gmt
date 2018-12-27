983bcf40249642947c8184b4b885e6d623ebcf01
Dongdong Tian <seisman.info@gmail.com>
2018-12-27 13:07:27 -0500

    pssac: don't draw line if -G is used and -W isn't

--- test/pssac/pssac_Q.sh
+++ test/pssac/pssac_Q.sh
@@ -5,8 +5,8 @@
 ps=pssac_Q.ps
 
 gmt pssac seis.sac -JX10c/5c -R9/20/-2/2 -Bx2 -By1 -BWSen -K -P \
-    -Fr -Gp+gblack -Gn+gblue > $ps
+    -Fr -Gp+gblack -Gn+gblue -W > $ps
 gmt pssac seis.sac -JX5c/-10c -R-2/2/9/20 -Bx1 -By2 -BWSen -K -O \
-    -Fr -Gp+gblack -Gn+gblue -Q -X12c >> $ps
+    -Fr -Gp+gblack -Gn+gblue -W -Q -X12c >> $ps
 
 gmt psxy -J -R -T -O >> $ps
--- test/pssac/pssac_W.sh
+++ test/pssac/pssac_W.sh
@@ -3,12 +3,13 @@
 # Description:
 
 R=9/20/-2/2
-J=X15c/5c
+J=X15c/3c
 B=1
 ps=pssac_W.ps
 
 gmt pssac seis.sac -R$R -J$J -B$B -BWSen -W2p,red,- -K -P > $ps
-gmt pssac seis.sac -R -J -B$B -BWsen -W1p -K -O -Y6c >> $ps
-gmt pssac seis.sac -R -J -B$B -BWsen -Wblue -K -O -Y6c >> $ps
-gmt pssac seis.sac -R -J -B$B -BWsen -W. -K -O -Y6c >> $ps
+gmt pssac seis.sac -R -J -B$B -BWsen -W1p -K -O -Y4c >> $ps
+gmt pssac seis.sac -R -J -B$B -BWsen -Wblue -K -O -Y4c >> $ps
+gmt pssac seis.sac -R -J -B$B -BWsen -W. -K -O -Y4c >> $ps
+gmt pssac seis.sac -R -J -B$B -BWsen -K -O -Y4c >> $ps
 gmt psxy -R -J -T -K -O >> $ps
