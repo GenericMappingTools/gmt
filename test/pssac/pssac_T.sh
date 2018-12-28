45b61102172a21338ac42390e249d05a011bd092
Dongdong Tian <seisman.info@gmail.com>
2018-12-27 19:31:12 -0500

    pssac: don't draw line if -G is used and -W isn't (#241)

--- test/pssac/pssac_T.sh
+++ test/pssac/pssac_T.sh
@@ -6,11 +6,11 @@ ps=pssac_T.ps
 SACFILEs="ntkl.z onkl.z"
 
 gmt set PS_MEDIA 21cx38c
-gmt pssac $SACFILEs -JX15c/4c -R200/1600/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -P -G+t700/900 > $ps
-gmt pssac $SACFILEs -J -R0/1400/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -O -Y6c -T+t-5 -G+t600/800>> $ps
-gmt pssac $SACFILEs -J -R-500/500/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -O -Y6c -T+t1 -G+t0/100 >> $ps
-gmt pssac $SACFILEs -J -R0/1400/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -O -Y6c -T+r10 -G+t300/400 >> $ps
-gmt pssac $SACFILEs -J -R200/1600/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -O -Y6c -T+s300 -G+t1000/1300>> $ps
+gmt pssac $SACFILEs -JX15c/4c -R200/1600/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -P -G+t700/900 -W > $ps
+gmt pssac $SACFILEs -J -R0/1400/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -O -Y6c -T+t-5 -G+t600/800 -W >> $ps
+gmt pssac $SACFILEs -J -R-500/500/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -O -Y6c -T+t1 -G+t0/100 -W >> $ps
+gmt pssac $SACFILEs -J -R0/1400/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -O -Y6c -T+r10 -G+t300/400 -W >> $ps
+gmt pssac $SACFILEs -J -R200/1600/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -O -Y6c -T+s300 -G+t1000/1300 -W >> $ps
 gmt pssac $SACFILEs -J -R-300/800/22/27 -Bx100 -By1 -BWSen -Ed -M1.5c -K -O -Y6c -T+t1+s100 >> $ps
 
 gmt psxy -J -R -O -T >> $ps
--- test/pssac/pssac_geo.sh
+++ test/pssac/pssac_geo.sh
@@ -11,19 +11,19 @@ SACFILEs="${src:-.}"/*.z
 
 gmt psxy -J$J -R$R -T -K > $ps
 # lower left
-gmt pssac $SACFILEs -J -R -BWSen -B$Bx -B$By -M0.5i -S1000i -G+gblue -K -O >> $ps
+gmt pssac $SACFILEs -J -R -BWSen -B$Bx -B$By -M0.5i -S1000i -G+gblue -W -K -O >> $ps
 gmt psxy station.list -J -R -St0.4c -Gblack -i1,2 -K -O >> $ps
 
 # lower right
-gmt pssac station.list -J -R -BWSen -B$Bx -B$By -M0.5i -S400c -Q -G+gblue -K -O -X13c >> $ps
+gmt pssac station.list -J -R -BWSen -B$Bx -B$By -M0.5i -S400c -Q -G+gblue -W -K -O -X13c >> $ps
 gmt psxy station.list -J -R -St0.4c -Gblack -i1,2 -K -O >> $ps
 
 # upper left
-gmt pssac $SACFILEs -J -R -BWSen -B$Bx -B$By -M0.5i -Si0.001i -G+gblue -K -O -X-13c -Y9.5c >> $ps
+gmt pssac $SACFILEs -J -R -BWSen -B$Bx -B$By -M0.5i -Si0.001i -G+gblue -W -K -O -X-13c -Y9.5c >> $ps
 gmt psxy station.list -J -R -St0.4c -Gblack -i1,2 -K -O >> $ps
 
 # upper right
-gmt pssac station.list -J -R -BWSen -B$Bx -B$By -M0.5i -Si0.0025c -Q -G+gblue -K -O -X13c >> $ps
+gmt pssac station.list -J -R -BWSen -B$Bx -B$By -M0.5i -Si0.0025c -Q -G+gblue -W -K -O -X13c >> $ps
 gmt psxy station.list -J -R -St0.4c -Gblack -i1,2 -K -O >> $ps
 
 gmt psxy -J -R -T -O >> $ps
