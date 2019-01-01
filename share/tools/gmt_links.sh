7c2f0d00eafd8307c3c0cf961b229efd03e384f5
Paul Wessel <pwessel@hawaii.edu>
2019-01-01 09:37:28 -1000

    Update copyright year in 5.4
    Same as was done to master.

--- share/tools/gmt_links.sh
+++ share/tools/gmt_links.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # $Id$
 #
-# Copyright (c) 1991-2018 by P. Wessel, W. H. F. Smith, R. Scharroo,
+# Copyright (c) 1991-2019 by P. Wessel, W. H. F. Smith, R. Scharroo,
 # J. Luis, and F. Wobbe
 # See LICENSE.TXT file for copying and redistribution conditions.
 #
--- src/sphtriangulate.c
+++ src/sphtriangulate.c
@@ -1,7 +1,7 @@
 /*--------------------------------------------------------------------
  *	$Id$
  *
- *	Copyright (c) 2008-2018 by P. Wessel, W. H. F. Smith, R. Scharroo, J. Luis and F. Wobbe
+ *	Copyright (c) 2008-2019 by P. Wessel, W. H. F. Smith, R. Scharroo, J. Luis and F. Wobbe
  *	See LICENSE.TXT file for copying and redistribution conditions.
  *
  *	This program is free software; you can redistribute it and/or modify
