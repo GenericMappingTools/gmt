6848d14b623e20d7d4d8567b0d1115c56b433852
Paul Wessel <pwessel@hawaii.edu>
2019-01-01 03:29:35 -1000

    Improve -Q option directives for sphinterpolate (#263)
    The old -Q option took numbers 0-3 which does not remind users what they stand for.  Replaced with character codes that at least has some mnemonic power.  Backwards compatible parsing.

--- test/sph/sph_1.sh
+++ test/sph/sph_1.sh
@@ -5,14 +5,14 @@ ps=sph_1.ps
 mars370=@mars370d.txt
 
 gmt makecpt -Crainbow -T-7000/15000 > tt.cpt
-gmt sphinterpolate ${mars370} -Rg -I1 -Q0 -Gtt.nc
-gmt grdimage tt.nc -JH0/4.5i -B30g30 -B+t"-Q0" -Ctt.cpt -X0.7i -Y5.5i -K --MAP_TITLE_OFFSET=0i --FONT_TITLE=18p > $ps
-gmt sphinterpolate ${mars370} -Rg -I1 -Q1 -Gtt.nc
-gmt grdimage tt.nc -J -B30g30 -B+t"-Q1" -Ctt.cpt -X5.1i -O -K  --MAP_TITLE_OFFSET=0i --FONT_TITLE=18p >> $ps
-gmt sphinterpolate ${mars370} -Rg -I1 -Q2 -Gtt.nc
-gmt grdimage tt.nc -J -B30g30 -B+t"-Q2" -Ctt.cpt -X-5.1i -Y-5i -O -K  --MAP_TITLE_OFFSET=0i --FONT_TITLE=18p >> $ps
-gmt sphinterpolate ${mars370} -Rg -I1 -Q3 -Gtt.nc
-gmt grdimage tt.nc -J -B30g30 -B+t"-Q3" -Ctt.cpt -X5.1i -O -K  --MAP_TITLE_OFFSET=0i --FONT_TITLE=18p >> $ps
+gmt sphinterpolate ${mars370} -Rg -I1 -Qp -Gtt.nc
+gmt grdimage tt.nc -JH0/4.5i -B30g30 -B+t"-Qp" -Ctt.cpt -X0.7i -Y5.5i -K --MAP_TITLE_OFFSET=0i --FONT_TITLE=18p > $ps
+gmt sphinterpolate ${mars370} -Rg -I1 -Ql -Gtt.nc
+gmt grdimage tt.nc -J -B30g30 -B+t"-Ql" -Ctt.cpt -X5.1i -O -K  --MAP_TITLE_OFFSET=0i --FONT_TITLE=18p >> $ps
+gmt sphinterpolate ${mars370} -Rg -I1 -Qg -Gtt.nc
+gmt grdimage tt.nc -J -B30g30 -B+t"-Qg" -Ctt.cpt -X-5.1i -Y-5i -O -K  --MAP_TITLE_OFFSET=0i --FONT_TITLE=18p >> $ps
+gmt sphinterpolate ${mars370} -Rg -I1 -Qs -Gtt.nc
+gmt grdimage tt.nc -J -B30g30 -B+t"-Qs" -Ctt.cpt -X5.1i -O -K  --MAP_TITLE_OFFSET=0i --FONT_TITLE=18p >> $ps
 gmt psxy -Rg -J -O -K ${mars370} -Sc0.05i -G0 -B30g30 -X-2.55i -Y2.5i >> $ps
 gmt psxy -Rg -J -O -T >> $ps
 
