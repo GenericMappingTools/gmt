12fda9ec6b2f909fc4f622b7e39f8826eab050eb
Dongdong Tian <seisman.info@gmail.com>
2019-01-02 18:11:04 -0500

    Add coverage reports from TravisCI (#277)
    We need to run the whole tests to generate the coverage reports.
    Because these tests take too long (~30 min on travis), we cannot build the 
    coverage reports for every commit. Currently, the tests and coverage are 
    only activated by cron jobs, so we don't need to wait the reports for every 
    commit, but still get latest coverage reports every day.
    Although there're some failing tests, we still get coverage reports.

--- ci/travis-build.sh
+++ ci/travis-build.sh
@@ -4,16 +4,34 @@
 # To return a failure if any commands inside fail
 set -e
 
+cat > cmake/ConfigUser.cmake << 'EOF'
+set (CMAKE_INSTALL_PREFIX "$ENV{INSTALLDIR}")
+set (GMT_LIBDIR "$ENV{INSTALLDIR}/lib")
+set (DCW_ROOT "$ENV{COASTLINEDIR}")
+set (GSHHG_ROOT "$ENV{COASTLINEDIR}")
+
+set (CMAKE_BUILD_TYPE Debug)
+enable_testing()
+set (DO_EXAMPLES TRUE)
+set (DO_TESTS TRUE)
+set (DO_ANIMATIONS TRUE)
+set (N_TEST_JOBS 2)
+EOF
+
+if [ "$COVERAGE" == "true" ]; then
+    cat >> cmake/ConfigUser.cmake << 'EOF'
+set (CMAKE_BUILD_TYPE Debug)
+set (CMAKE_C_FLAGS "-Wall -Wdeclaration-after-statement -coverage -O0")
+EOF
+fi
+
+cat cmake/ConfigUser.cmake
+
 mkdir build && cd build
 
-cmake -D CMAKE_INSTALL_PREFIX=$INSTALLDIR \
-      -D GMT_LIBDIR=$INSTALLDIR/lib \
-      -D DCW_ROOT=$COASTLINEDIR \
-      -D GSHHG_ROOT=$COASTLINEDIR \
-      ..
+cmake ..
 
 make -j
-make check
 make install
 
 # Turn off exit on failure.
