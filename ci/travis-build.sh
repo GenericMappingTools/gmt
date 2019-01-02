4e2887d9278a7627d43543d4f1047d06f811c91d
Dongdong Tian <seisman.info@gmail.com>
2019-01-02 14:00:57 -0500

    Enable tests

--- ci/travis-build.sh
+++ ci/travis-build.sh
@@ -4,16 +4,26 @@
 # To return a failure if any commands inside fail
 set -e
 
+cat > cmake/ConfigUser.cmake << 'EOF'
+set (CMAKE_INSTALL_PREFIX "$ENV{INSTALLDIR}")
+set (GMT_LIBDIR "$ENV{INSTALLDIR}/lib")
+set (DCW_ROOT "$ENV{COASTLINEDIR}")
+set (GSHHG_ROOT "$ENV{COASTLINEDIR}")
+
+set (CMAKE_BUILD_TYPE Debug)
+enable_testing()
+set (DO_EXAMPLES TRUE)
+set (DO_TESTS TRUE)
+set (DO_ANIMATIONS TRUE)
+EOF
+
+cat cmake/ConfigUser.cmake
+
 mkdir build && cd build
 
-cmake -D CMAKE_INSTALL_PREFIX=$INSTALLDIR \
-      -D GMT_LIBDIR=$INSTALLDIR/lib \
-      -D DCW_ROOT=$COASTLINEDIR \
-      -D GSHHG_ROOT=$COASTLINEDIR \
-      ..
+cmake ..
 
 make -j
-make check
 make install
 
 # Turn off exit on failure.
