da7613fe4c31aad5b8803421227a54d860fd2fb1
Paul Wessel <pwessel@hawaii.edu>
2019-01-12 19:11:27 -1000

    Modernize -G option is psimage
    LIke grdimage, psimage optino -G started with a letter b, f, or t possibly follwed by a color name, making parsing tricky.  Now, these letters are part of modifiers and color name follows -G immediately, unless not given at all for transparency.  Backwards compatible up to and including GMT 5.  Updated man pages and fixed formatting errors in them as well.

--- test/psimage/transparent_png.sh
+++ test/psimage/transparent_png.sh
@@ -9,14 +9,14 @@ ps=transparent_png.ps
 gmt psbasemap -R0/1/0/1 -JX7c -Y19c -B+glightblue+t"no option" -K -P > $ps
 gmt psimage @warning.png -Dx0.5c/0.5c+jBL+w6c -O -K >> $ps
 
-gmt psbasemap -Y-9c -R -J -B+glightblue+t"-Gtblack" -O -K >> $ps
-gmt psimage @warning.png -Gtblack -Dx0.5c/0.5c+jBL+w6c -O -K >> $ps
+gmt psbasemap -Y-9c -R -J -B+glightblue+t"-Gblack\053t" -O -K >> $ps
+gmt psimage @warning.png -Gblack+t -Dx0.5c/0.5c+jBL+w6c -O -K >> $ps
 
-gmt psbasemap -X8c -R -J -B+glightblue+t"-Gtwhite" -O -K >> $ps
-gmt psimage @warning.png -Gtwhite -Dx0.5c/0.5c+jBL+w6c -O -K >> $ps
+gmt psbasemap -X8c -R -J -B+glightblue+t"-Gwhite\053t" -O -K >> $ps
+gmt psimage @warning.png -Gwhite+t -Dx0.5c/0.5c+jBL+w6c -O -K >> $ps
 
-gmt psbasemap -X-8c -Y-9c -R -J -B+glightblue+t"-Gtred" -O -K >> $ps
-gmt psimage @warning.png -Gtred -Dx0.5c/0.5c+jBL+w6c -O -K >> $ps
+gmt psbasemap -X-8c -Y-9c -R -J -B+glightblue+t"-Gred\053t" -O -K >> $ps
+gmt psimage @warning.png -Gred+t -Dx0.5c/0.5c+jBL+w6c -O -K >> $ps
 
-gmt psbasemap -X8c -R -J -B+glightblue+t"-Gtblue" -O -K >> $ps
-gmt psimage @warning.png -Gtblue -Dx0.5c/0.5c+jBL+w6c -O >> $ps
+gmt psbasemap -X8c -R -J -B+glightblue+t"-Gblue\053t" -O -K >> $ps
+gmt psimage @warning.png -Gblue+t -Dx0.5c/0.5c+jBL+w6c -O >> $ps
