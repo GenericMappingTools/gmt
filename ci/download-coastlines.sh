35933c8526ccf65f603257ca6c185ceabe60dfb8
ldldr <ldldr@users.noreply.github.com>
2019-01-24 14:28:43 +0100

    Make coastline download more robust

--- ci/download-coastlines.sh
+++ ci/download-coastlines.sh
@@ -12,22 +12,28 @@ cd $HOME/pkg-gshhg-dcw
 EXT="tar.gz"
 GSHHG="gshhg-gmt-2.3.7"
 DCW="dcw-gmt-1.1.4"
+MD5_GSHHG=8ee2653f9daf84d49fefbf990bbfa1e7
+MD5_DCW=4f30857a8b12af0f910222fceb591538
 
 # GSHHG (coastlines, rivers, and political boundaries):
 echo ""
 echo "Downloading and unpacking GSHHG"
 echo "================================================================================"
-curl -L -O -C - "http://www.soest.hawaii.edu/pwessel/gshhg/$GSHHG.$EXT"
+curl -L -O -C - --retry 10 "https://mirrors.ustc.edu.cn/gmt/$GSHHG.$EXT" || true
+md5=$(md5sum $GSHHG.$EXT | cut -d ' ' -f 1)
+test "$md5" = "$MD5_GSHHG"
 tar xzf $GSHHG.$EXT
-cp $GSHHG/* $COASTLINEDIR/
+mv $GSHHG/* $COASTLINEDIR/
 
 # DCW (country polygons):
 echo ""
 echo "Downloading and unpacking DCW"
 echo "================================================================================"
-curl -L -O -C - "http://www.soest.hawaii.edu/pwessel/dcw/$DCW.$EXT"
+curl -L -O -C - --retry 10 "https://mirrors.ustc.edu.cn/gmt/$DCW.$EXT" || true
+md5=$(md5sum $DCW.$EXT | cut -d ' ' -f 1)
+test "$md5" = "$MD5_DCW"
 tar xzf $DCW.$EXT
-cp $DCW/* $COASTLINEDIR
+mv $DCW/* $COASTLINEDIR
 
 ls $COASTLINEDIR
 
