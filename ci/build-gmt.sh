fb9e6cd2b37dcf7cc4529dde994e19bd03172397
ldldr <ldldr@users.noreply.github.com>
2019-01-25 10:24:45 +0100

    Do animations in doc build

--- ci/build-gmt.sh
+++ ci/build-gmt.sh
@@ -11,14 +11,19 @@ set (GSHHG_ROOT "$ENV{COASTLINEDIR}")
 set (CMAKE_C_FLAGS "-Wall -Wdeclaration-after-statement ${CMAKE_C_FLAGS}")
 EOF
 
-if [ "$TEST" == "true" ]; then
+if [[ "$TEST" == "true" ]]; then
     cat >> cmake/ConfigUser.cmake << 'EOF'
 enable_testing()
 set (DO_EXAMPLES TRUE)
 set (DO_TESTS TRUE)
-set (DO_ANIMATIONS TRUE)
 set (N_TEST_JOBS 2)
 set (CMAKE_C_FLAGS "-Wextra -coverage ${CMAKE_C_FLAGS}")
+EOF
+fi
+
+if [[ "$TEST" == "true" || "$BUILD_DOCS" == "true" ]]; then
+    cat >> cmake/ConfigUser.cmake << 'EOF'
+set (DO_ANIMATIONS TRUE)
 EOF
 fi
 
