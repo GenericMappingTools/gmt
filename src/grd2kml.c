226560b874428fc139ee96934768bd3d8eb9b427
Brook <btozer@ucsd.edu>
2019-02-13 16:18:38 -0800

    Update grd2kml.c

--- src/grd2kml.c
+++ src/grd2kml.c
@@ -98,7 +98,7 @@ struct GRD2KML_CTRL {
 	struct GRD2KML_W {	/* -W[+w] */
 		bool active;
 		char *file;
-	} Q;
+	} W;
 };
 
 /* Structure used to keep track of which tile and its 4 possible underlings */
@@ -142,7 +142,7 @@ GMT_LOCAL int usage (struct GMTAPI_CTRL *API, int level) {
 	const char *name = gmt_show_name_and_purpose (API, THIS_MODULE_LIB, THIS_MODULE_NAME, THIS_MODULE_PURPOSE);
 	if (level == GMT_MODULE_PURPOSE) return (GMT_NOERROR);
 	GMT_Message (API, GMT_TIME_NONE, "usage: %s <grid> -N<name> [-C<cpt>] [-E<url>] [-F<filter>] [-H<factor>] [-I[<intensgrid>|<value>|<modifiers>]]\n", name);
-	GMT_Message (API, GMT_TIME_NONE, "	[-L<size>] [-Q] [-W|<value>|<file>] [-T<title>] [%s] [%s] [%s]\n\n", GMT_V_OPT, GMT_f_OPT, GMT_PAR_OPT);
+	GMT_Message (API, GMT_TIME_NONE, "	[-L<size>] [-Q] [-W[<value>|<file>]] [-T<title>] [%s] [%s] [%s]\n\n", GMT_V_OPT, GMT_f_OPT, GMT_PAR_OPT);
 
 	if (level == GMT_SYNOPSIS) return (GMT_MODULE_SYNOPSIS);
 
