509ee5c3c53a8e8baf6fe02454595784713c0dd6
Joaquim <jluis@ualg.pt>
2019-02-27 12:19:39 +0000

    Initialize to NULL
    This is in response to this compiler warning. But I'm not certain that's the only fix.
    ``c:\progs_cygw\gmtdev\gmt5\master\src\grdvector.c(650) : warning C4700: uninitialized local variable 'last_headpen' used``

--- src/grdvector.c
+++ src/grdvector.c
@@ -341,7 +341,7 @@ int GMT_grdvector (void *V_API, int mode, void *args) {
 
 	struct GMT_GRID *Grid[2] = {NULL, NULL};
 	struct GMT_PALETTE *P = NULL;
-	struct GMT_PEN last_headpen;
+	struct GMT_PEN last_headpen = NULL;
 	struct GRDVECTOR_CTRL *Ctrl = NULL;
 	struct GMT_CTRL *GMT = NULL, *GMT_cpy = NULL;	/* General GMT internal parameters */
 	struct GMT_OPTION *options = NULL;
