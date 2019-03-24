2ae9ed7c9298b6071619dc999e2c0fc7ccf984cb
Paul Wessel <pwessel@hawaii.edu>
2019-03-23 15:52:56 -1000

    Let subplot set and -c advance to next panel
    Instead of always require the row,col of the next panel, let a lack of specification simply advance to the next panel in the order set by -A.

--- test/modern/panels.sh
+++ test/modern/panels.sh
@@ -2,9 +2,9 @@
 # Test very minimal 2x2 basemap matrix
 gmt begin panels ps
   gmt subplot begin 2x2 -Fs3i -M5p -A -SCb -SRl -Bwstr
-    gmt psbasemap -R0/80/0/10 -c1,1
-    gmt psbasemap -c1,2
-    gmt psbasemap -c2,1
-    gmt psbasemap -c2,2
+    gmt psbasemap -R0/80/0/10 -c
+    gmt psbasemap -c
+    gmt psbasemap -c
+    gmt psbasemap -c
   gmt subplot end
 gmt end
