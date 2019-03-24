facd1b257a8bf32862aba7e5de3eae13aeb3c0df
Paul Wessel <pwessel@hawaii.edu>
2019-03-23 17:53:30 -1000

    Simplify use of subplot (#570)
    Only append row,col to -c when not marching along to next panel.

--- test/modern/vardims.sh
+++ test/modern/vardims.sh
@@ -3,11 +3,11 @@
 # This test uses a variable subplot dimensions
 gmt begin vardims ps
   gmt subplot begin 3x2 -Fs2i,4i/2.5i,5i,1.25i -SRl+p -SCb -BWSne -M0 -AA+v -T"Variable dimensions"
-  gmt basemap -R0/5/0/5 -B+gpink -c1,1
-  gmt basemap -R10/15/0/5 -B+gyellow -c1,2
-  gmt basemap -R0/5/10/15 -B+gcyan -c2,1
-  gmt basemap -R10/15/10/15 -B+gpurple -c2,2
-  gmt basemap -R0/5/10/15 -B+glightgray -c3,1
-  gmt basemap -R10/15/10/15 -B+gorange -c3,2
+  gmt basemap -R0/5/0/5 -B+gpink -c
+  gmt basemap -R10/15/0/5 -B+gyellow -c
+  gmt basemap -R0/5/10/15 -B+gcyan -c
+  gmt basemap -R10/15/10/15 -B+gpurple -c
+  gmt basemap -R0/5/10/15 -B+glightgray -c
+  gmt basemap -R10/15/10/15 -B+gorange -c
   gmt subplot end
 gmt end
