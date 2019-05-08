b25d8b5a03838f4b6cbf07e8b13bd1aa9b407463
Paul Wessel <pwessel@hawaii.edu>
2019-05-08 10:17:47 -1000

    Update psevents.c
    remove space.

--- src/psevents.c
+++ src/psevents.c
@@ -122,7 +122,7 @@ GMT_LOCAL int usage (struct GMTAPI_CTRL *API, int level) {
 	GMT_Message (API, GMT_TIME_NONE, "\t[-D[t|<duration>]] [-F[<transparency>][+c<transparency2]] [-G<color>]\n");
 	GMT_Message (API, GMT_TIME_NONE, "\t[-I[<intensity>][+c<intensity2]] [-Q<file>][-W[<pen>] [%s] [%s]\n", GMT_V_OPT, GMT_b_OPT);
 	GMT_Message (API, GMT_TIME_NONE, "\t[%s] [%s] [%s] [%s]\n\t[%s] [%s] [%s]\n\n",
-		GMT_d_OPT, GMT_e_OPT, GMT_f_OPT, GMT_h_OPT, GMT_i_OPT,  GMT_colon_OPT, GMT_PAR_OPT);
+		GMT_d_OPT, GMT_e_OPT, GMT_f_OPT, GMT_h_OPT, GMT_i_OPT, GMT_colon_OPT, GMT_PAR_OPT);
 
 	if (level == GMT_SYNOPSIS) return (GMT_MODULE_SYNOPSIS);
 
