db277d54aac4cd3aab5eab489238f5eb3612feab
Paul Wessel <pwessel@hawaii.edu>
2019-05-15 14:43:14 -1000

    Add test for psevents
    Added a single test (events.sh) to make sure psevents run.  Fixed a few minor issues related to outdated PS for ex28, old GMT4 syntax in waypoints.sh, and bad check in mapproject.c.

--- test/mapproject/waypoints.sh
+++ test/mapproject/waypoints.sh
@@ -8,16 +8,16 @@ gmt psxy -R -J -O -K -Sc0.2c -Gblue $data -: >> $ps
 awk '{if (NR == 2) print $2, $1}' $data | gmt psxy -R -J -O -K -Sa0.4c -Gred >> $ps
 awk '{if (NR > 2) print $2, $1, $3}' $data | gmt pstext -R -J -O -K -F+f9p+r100 -D0/0.4c >> $ps
 cat << EOF > legend
-H 20 Helvetica Map Legend
+H 20p,Helvetica Map Legend
 G 4p
 N 0.75 0.9 1.1 0.9
 V 0 0.25p
-L 16 0 C WPT#
-L 16 0 C Heading
-L 16 0 C Distance
-L 16 0 C Time
+L 16p,0 C WPT#
+L 16p,0 C Heading
+L 16p,0 C Distance
+L 16p,0 C Time
 D 0 0.5p
 EOF
-awk '{if (NR > 1) printf "L 16 0 C P-%d\nL 16 0 R %s@.\nL 16 0 R %s nm\nL 16 0 R %s hr\n", int($1), $2, $3, $4}' tmp >> legend
+awk '{if (NR > 1) printf "L 16p,0 C P-%d\nL 16p,0 R %s@.\nL 16p,0 R %s nm\nL 16p,0 R %s hr\n", int($1), $2, $3, $4}' tmp >> legend
 echo "V 0 0.25p" >> legend
 gmt pslegend -R -J -O -DjTL+w3.75i+o0.1i -F+p1p+gwhite+s legend >> $ps
