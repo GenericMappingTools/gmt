68fa91af8bbebaa38bdf733a07af516149361162
Paul Wessel <pwessel@hawaii.edu>
2019-05-26 09:24:08 -1000

    Re-fix fatline
    Forgot the point was to show the problem, not fix it.

--- doc/scripts/GMT_fatline.sh
+++ doc/scripts/GMT_fatline.sh
@@ -3,11 +3,11 @@
 # algorithm in ghostscript for fat lines.
 # https://github.com/GenericMappingTools/gmt/issues/431
 cat > gc.d << END
--85.5 85
--4.5  85
+-82 85
+-8  85
 END
 gmt begin GMT_fatline ps
-	gmt psxy -R-85/82/-5/87+r -JM-45/84.5/2.5i -W30p gc.d --PS_LINE_CAP=round --PS_LINE_JOIN=round
+	gmt psxy -R-90/82/0/87+r -JM-45/84.5/2.5i -W30p gc.d
 	gmt psxy -W1p,red gc.d
 	gmt psxy -X3.25i -W30p gc.d --PS_LINE_CAP=round --PS_LINE_JOIN=round
 	gmt psxy -W1p,red gc.d
