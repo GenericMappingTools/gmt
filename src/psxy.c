e825fea68d22b2c2a951bb544826c8e212abbe49
Paul Wessel <pwessel@hawaii.edu>
2019-05-26 19:55:43 -1000

    Let psxy -SE -JX/x work even if axes scalings differ
    This is a poor-mans geo ellipse wbere distortion is up to you.

--- src/psxy.c
+++ src/psxy.c
@@ -1420,7 +1420,7 @@ int GMT_psxy (void *V_API, int mode, void *args) {
 							dim[0] = 90.0 - in[ex1];	/* Cartesian azimuth */
 							gmt_flip_angle_d (GMT, &dim[0]);
 							dim[1] = in[ex2] * GMT->current.proj.scale[GMT_X];
-							dim[2] = in[ex3] * GMT->current.proj.scale[GMT_X];
+							dim[2] = in[ex3] * GMT->current.proj.scale[GMT_Y];
 							PSL_plotsymbol (PSL, xpos[item], plot_y, dim, S.symbol);
 						}
 						else if (S.symbol == PSL_ELLIPSE) {	/* Got axis in km */
