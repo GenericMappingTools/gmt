117449a129ffa80c3db777ed33e1340caa81b2d8
Paul Wessel <pwessel@hawaii.edu>
2019-06-07 00:20:33 -1000

    Finalize aborted test script and update some PS files (#857)
    * Complete missing script and update PS
    
    The test paintallzs.sh in grd2cpt was not completed so no PS was there and it was not listed in CMakeLists.txt.  I finished the script and added a PS .  Also the apimatgrd.sh script created a PS file by another name, so I renamed it to follow the pattern that scripts and plots have the same basename.  Finally, I deleted testapi_mixmatrix.ps as not referenced by anuthing.
    
    * Update paintallzs.ps

--- test/grd2cpt/paintallzs.sh
+++ test/grd2cpt/paintallzs.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 #
 # Check that gmt grd2cpt estimates correct upper/lower bounds <= z_min && >= z_max
+# We try lower and upper, then flip sign on grid and try min and high
 
 ps=paintallzs.ps
 echo "6	3	0	1" > t.txt
+gmt set MAP_FRAME_TYPE plain
 gmt sph2grd t.txt -I1 -Rg -Nm -Ggrid.nc
-gmt grd2cpt grid.nc -Cpolar -E7 -T+ -Z > t.cpt
-gmt grdimage grid.nc -Jx0.015id -P -Ct.cpt -K -Xc -Y0.75i > $ps
+gmt grdmath grid.nc 0.2 ADD = grid.nc
+gmt grd2cpt grid.nc -Cpolar -E7 -Z > t.cpt
+gmt grdimage grid.nc -Jx0.015id/0.01id -P -Ct.cpt -B0 -K -X1.5i -Y0.5i > $ps
+gmt psscale -DJRM -Ct.cpt -Rgrid.nc -Bxaf -J -O -K >> $ps
+gmt grd2cpt grid.nc -Cpolar -E7 -Sl -Z > t.cpt
+gmt grdimage grid.nc -J -Ct.cpt -B0 -O -K -Y2i >> $ps
+gmt psscale -DJRM -Ct.cpt -R -Bxaf -By+l"-Sl" -J -O -K >> $ps
+gmt grd2cpt grid.nc -Cpolar -E7 -Su -Z > t.cpt
+gmt grdimage grid.nc -J -Ct.cpt -B0 -O -K -Y2i >> $ps
+gmt psscale -DJRM -Ct.cpt -R -Bxaf -By+l"-Su" -J -O -K >> $ps
+gmt grdmath grid.nc NEG = grid.nc
+gmt grd2cpt grid.nc -Cpolar -E7 -Sm -Z > t.cpt
+gmt grdimage grid.nc -J -Ct.cpt -B0 -O -K -Y2i >> $ps
+gmt psscale -DJRM -Ct.cpt -R -Bxaf -By+l"-Sm" -J -O -K >> $ps
+gmt grd2cpt grid.nc -Cpolar -E7 -Sh -Z > t.cpt
+gmt grdimage grid.nc -J -Ct.cpt -B0 -O -K -Y2i >> $ps
+gmt psscale -DJRM -Ct.cpt -R -Bxaf -By+l"-Sh" -J -O >> $ps
