9b31b6d8a1a5deb84e4e14c651e23a68fe768953
Paul Wessel <pwessel@hawaii.edu>
2019-06-14 19:05:22 -1000

    Ensure that -p resets any rotation at the end of the layer (#935)
    The test script zrotation.sh failed because we never undid the rotation and reset the origin at the end of the layer, hence rotations and offsets were accumulating.  Adding the reset fixes the problem that the script was designed to test.

--- test/psbasemap/zrotation.sh
+++ test/psbasemap/zrotation.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-# GMT_KNOWN_FAILURE
 # Test script for plain -p<rot> about map center.
 # This should place 4 plots that differ by a rotation about the S pole
-# Currently fails.  Faked the orig by using -JA<rot>... instead [in brackets]
 
 ps=zrotation.ps
 gmt set MAP_FRAME_TYPE plain
@@ -22,9 +20,9 @@ gmt pstext -R -J -O -K -F+jTL -Dj0.2i << EOF >> $ps
 EOF
 # LL corner is standard, no rotation
 gmt pscoast -R0/360/-90/0 -JA0/-90/3.25i -Bg90a360f360 -O -K -Gred -Dc -X0.45i -Y0.45i >> $ps
-# LR corner is 45 degrees rotation [-JA45/-90/3.25i -p0+w0/-90]
+# LR corner is 45 degrees rotation
 gmt pscoast -R -J -Bg90a360f360 -Gred -Dc -X4.15i -O -K -p45+w0/-90 >> $ps
-# UL corner is 180 degrees rotation [-JA180/-90/3.25i -p0+w0/-90]
+# UL corner is 180 degrees rotation
 gmt pscoast -R -J -Bg90a360f360 -Gred -Dc -X-4.15i -Y4.15i -O -K -p180+w0/-90 >> $ps
-# UR corner is 245 degrees rotation [-JA245/-90/3.25i -p0+w0/-90]
+# UR corner is 245 degrees rotation
 gmt pscoast -R -J -Bg90a360f360 -Gred -Dc -X4.15i -O -p245+w0/-90 >> $ps
