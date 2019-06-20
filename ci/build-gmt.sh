72965e3551f9e9acd5186ced3b590d4b28fc25d4
Dongdong Tian <seisman.info@gmail.com>
2019-06-20 16:39:52 -0400

    Set COPY_GSHHG and COPY_DCW to TRUE
    To create standalone binary installers, we need copy GSHHG and DCW data.

--- ci/build-gmt.sh
+++ ci/build-gmt.sh
@@ -8,6 +8,8 @@ cat > cmake/ConfigUser.cmake << 'EOF'
 set (CMAKE_INSTALL_PREFIX "$ENV{INSTALLDIR}")
 set (DCW_ROOT "$ENV{COASTLINEDIR}")
 set (GSHHG_ROOT "$ENV{COASTLINEDIR}")
+set (COPY_GSHHG TRUE)
+set (COPY_DCW TRUE)
 set (CMAKE_C_FLAGS "-Wall -Wdeclaration-after-statement ${CMAKE_C_FLAGS}")
 EOF
 
