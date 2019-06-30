600500cab3076e789580352453afb4a6fc43e345
Paul Wessel <pwessel@hawaii.edu>
2019-06-30 10:21:45 -1000

    Clarify subplot usage in -SC|R and fix documentatino (#1060)
    * Clarify subplot usage in -SC|R and fix documentatino
    
    There was references to a colon separator for -Fs which is not true, and there were confusions regarding where +t goes (with -SC).
    
    * Update subplot.rst_
    
    Fix doc page
    
    * Be gentle with +t, fix somepanels.sh
    
    It used +tc to make space for top title but none was set.

--- test/modern/somepanels.sh
+++ test/modern/somepanels.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Test only giving a few subplots, Roman labeling, and -A override for one subplot
 gmt begin somepanels ps
-  gmt subplot begin 3x3 -F6i/8.5i -M5p -A1+c+r+o0.2i+jBR -SCb+lXLABEL+tc -SRl+lYLABEL -Bwstr -T"FIGURE HEADER"
+  gmt subplot begin 3x3 -F6i/8.5i -M5p -A1+c+r+o0.2i+jBR -SCb+lXLABEL -SRl+lYLABEL -Bwstr -T"FIGURE HEADER"
     gmt basemap -R0/100/0/10 -c1,1
     gmt subplot 2,2 -Apink
     gmt basemap -B+gpink
