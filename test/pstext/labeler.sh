050f9414db66094902dbe002dfef65a88b03fdd6
Paul Wessel <pwessel@hawaii.edu>
2019-07-13 09:49:35 -1000

    Let +t be last modifier in pstext -F (#1163)
    * Let pstext -F+t handle any plus signs in the static text
    
    Since no quotes are let into GMT, we cannot tell if a +x string following +t is part of the string or some other modifier.  Hence, if your string contains plus characters then +t must be the last modifier given in -F.
    
    * Fix script with +t
    
    Place at end of arguments to -F.

--- test/pstext/labeler.sh
+++ test/pstext/labeler.sh
@@ -6,5 +6,5 @@ gmt convert points.txt -o0,1 > p.txt
 gmt psxy -R0/7/-0.5/6.5 -JX6i -P -Baf -Sc0.25i -Gred -Wfaint points.txt -K > $ps
 gmt pstext -R -J -O -K -F+r+f10p,Helvetica-Bold,white p.txt >> $ps
 gmt pstext -R -J -O -K -F+z"z = %.1f@."+f9p,Times-Italic+jTC -Dj0/0.2i points.txt >> $ps
-gmt pstext -R -J -O -K -F+t"Some text label"+f24p,Courier-Bold t.txt >> $ps
+gmt pstext -R -J -O -K -F+f24p,Courier-Bold+t"Some text label" t.txt >> $ps
 gmt psxy -R -J -O -T >> $ps
