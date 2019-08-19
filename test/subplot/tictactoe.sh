38ef4e7a105aa428806ed41553fd2f230337038d
Dongdong Tian <seisman.info@gmail.com>
2019-08-19 01:16:13 -0400

    Fix a typo in subplot/tictactoe.sh (#1409)

--- test/subplot/tictactoe.sh
+++ test/subplot/tictactoe.sh
@@ -2,7 +2,7 @@
 # Test lines between subplot with -W
 gmt begin tictactoe ps
     gmt set FONT_ANNOT_PRIMARY 12p
-    gmt subplot begin 3x3 -Fs5c+w1p,1 -M0 -Blrbt
+    gmt subplot begin 3x3 -Fs5c+w1p,- -M0 -Blrbt
         gmt coast -Rg -JG30/30/?  -Gred -Bg -c0,0
         gmt coast -Rg -JG120/30/? -Gred -Bg -c1,1
         gmt coast -Rg -JG210/30/? -Gred -Bg -c2,2
