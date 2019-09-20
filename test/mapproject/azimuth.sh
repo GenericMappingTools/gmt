68b627b39af1aa8c8293f7404831e860bfbeee92
Paul Wessel <pwessel@hawaii.edu>
2019-09-20 10:50:25 -1000

    Update azimuth test with knowledge of back-azimuth (#1602)
    * UPdate aximuth test with knowledge of back-azimuth
    
    This failure was a user error (using forward instead of back azimuth at the wrong time).  Updated the script and added PS original.
    
    * remove omega and fix comments

--- test/mapproject/azimuth.sh
+++ test/mapproject/azimuth.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 #
 # Tests mapproject for azimuth calculation
-# This is failing since the vectors are not parallel to the oblique
-# meridians and parallels.  Yet the reported azimuth match calculations
-# in MATLAB with the same equations as well as some online azimuth calculator
-# Maybe the plotting of arrows are wrong?
+# Important to use backazimuth in this example!
 
 gmt begin azimuth ps
   plon=186.3
   plat=-60.1
-  omega=1.08
   lon=156.293957716
   lat=-8.06651671195
   # Get az from point to pole
-  az=`echo $lon $lat | gmt mapproject -Af${plon}/${plat} -o2 -fg`
+  az=`echo $lon $lat | gmt mapproject -Ab${plon}/${plat} -o2 -fg`
   # Add 90 to aziumth:
-  az2=`gmt math -Q $az 90 ADD 360 FMOD =`
+  az2=`gmt math -Q $az 90 SUB 360 FMOD =`
   gmt basemap -R140/210/-65/15 -JM6i -Bafg2 -BWEsn+o${plon}/${plat}+t"Parallel az = $az2"
   echo ${plon} ${plat} | gmt plot -Sc0.1i -Gred
   echo ${lon} ${lat}   | gmt plot -Sc0.1i -Ggreen
