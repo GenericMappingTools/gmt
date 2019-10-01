53f325de1829ae72de9d77562d1816b6558b5335
Dongdong Tian <seisman.info@gmail.com>
2019-10-01 17:13:20 -0400

    ex: Cleanup intermediate files after running examples

--- doc/examples/ex31/ex31.sh
+++ doc/examples/ex31/ex31.sh
@@ -57,5 +57,5 @@ gmt begin ex31
 	# plot legend
 	gmt legend -DjTR+o0.1c+w8.0c+l1.2 -C0.3c/0.4c -F+p+gwhite legend.txt
 
-	rm -f PSL_custom_fonts.txt legend.txt ex31CropNoLogo.eps
+	rm -f PSL_custom_fonts.txt legend.txt ex31CropNoLogo.eps europe-capitals-ru.csv
 gmt end show
