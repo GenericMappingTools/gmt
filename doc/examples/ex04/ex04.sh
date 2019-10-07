3d79ffc65111d688b33e992d572daca907e0a067
Dongdong Tian <seisman.info@gmail.com>
2019-10-07 10:29:05 -0400

    docs: Fix indentations of some scripts

--- doc/examples/ex04/ex04.sh
+++ doc/examples/ex04/ex04.sh
@@ -10,10 +10,8 @@ gmt begin
 	gmt makecpt -C255,100 -T-10/10/10 -N
 	gmt grdcontour @HI_geoid_04.nc -R195/210/18/25 -Jm0.45i -p60/30 -C1 -A5+o -Gd4i -X1.25i -Y1.25i
 	gmt coast -p -B2 -BNEsw -Gblack -TdjBR+o0.1i+w1i+l
-	gmt grdview @HI_topo_04.nc -R195/210/18/25/-6/4 -Jz0.34i -p -C \
-		-N-6+glightgray -Qsm -B2 -Bz2+l"Topo (km)" -BneswZ -Y2.2i
-	echo '3.25 5.75 H@#awaiian@# R@#idge@#' | gmt text -R0/10/0/10 -Jx1i \
-	-F+f60p,ZapfChancery-MediumItalic+jCB
+	gmt grdview @HI_topo_04.nc -R195/210/18/25/-6/4 -Jz0.34i -p -C -N-6+glightgray -Qsm -B2 -Bz2+l"Topo (km)" -BneswZ -Y2.2i
+	echo '3.25 5.75 H@#awaiian@# R@#idge@#' | gmt text -R0/10/0/10 -Jx1i -F+f60p,ZapfChancery-MediumItalic+jCB
 
 	gmt figure ex04c
 	gmt grdimage @HI_geoid_04.nc -I+a0+nt0.75 -R195/210/18/25 -JM6.75i -p60/30 -C@geoid_04.cpt -X1.25i -Y1.25i
@@ -22,6 +20,5 @@ gmt begin
 	gmt colorbar -p240/30 -DJBC+o0/0.5i+w5i/0.3i+h -C@geoid_04.cpt -I -Bx2+l"Geoid (m)"
 	gmt grdview @HI_topo_04.nc -I+a0+nt0.75 -R195/210/18/25/-6/4 -JZ3.4i -p60/30 -C@topo_04.cpt \
 		-N-6+glightgray -Qc100 -B2 -Bz2+l"Topo (km)" -BneswZ -Y2.2i
-	echo '3.25 5.75 H@#awaiian@# R@#idge@#' | gmt text -R0/10/0/10 -Jx1i \
-	-F+f60p,ZapfChancery-MediumItalic+jCB
+	echo '3.25 5.75 H@#awaiian@# R@#idge@#' | gmt text -R0/10/0/10 -Jx1i -F+f60p,ZapfChancery-MediumItalic+jCB
 gmt end show
--- doc/examples/ex19/ex19.sh
+++ doc/examples/ex19/ex19.sh
@@ -10,7 +10,7 @@ gmt begin ex19
 	gmt makecpt -Cwhite,blue -T0,1 -Z -N -H > lat.cpt
 	gmt makecpt -Crainbow -T-180/180 -H > lon.cpt
 	gmt subplot begin 3x1 -Fs6.5i/0 -M0 -Bbltr -Rd -JI0/6.5i
-#   First make a worldmap with graded blue oceans and rainbow continents
+		#   First make a worldmap with graded blue oceans and rainbow continents
 		gmt grdimage lat.nc -Clat.cpt -nl -c0,0
 		gmt coast -Dc -A5000 -Gc
 		gmt grdimage lon.nc -Clon.cpt -nl
@@ -19,12 +19,12 @@ gmt begin ex19
 		echo "0 20 15TH INTERNATIONAL" | gmt text -F+f32p,Helvetica-Bold,red=thinner
 		echo "0 -10 GMT CONFERENCE" | gmt text -F+f32p,Helvetica-Bold,red=thinner
 		echo "0 -30 Honolulu, Hawaii, April 1, 2018" | gmt text -F+f18p,Helvetica-Bold,green=thinnest
-#   Then show example of color patterns and placing a PostScript image
+		#   Then show example of color patterns and placing a PostScript image
 		gmt coast -Dc -A5000 -Gp86+fred+byellow+r100 -Sp@circuit.png+r100 -c1,0
 		echo "0 30 SILLY USES OF" | gmt text -F+f32p,Helvetica-Bold,lightgreen=thinner
 		echo "0 -30 COLOR PATTERNS" | gmt text -F+f32p,Helvetica-Bold,magenta=thinner
 		gmt image -DjCM+w3i @GMT_covertext.eps
-#   Finally repeat 1st plot but exchange the colors
+		#   Finally repeat 1st plot but exchange the colors
 		gmt grdimage lon.nc -Clon.cpt -nl -c2,0
 		gmt coast -Dc -A5000 -Gc
 		gmt grdimage lat.nc -Clat.cpt -nl
