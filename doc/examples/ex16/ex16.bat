a81aa71d3bdc1a0892f12332fa6a434c84a4ce34
Dongdong Tian <seisman.info@gmail.com>
2019-10-08 17:12:02 -0400

    Modernize batch scripts and run batch examples in Windows CI (#1755)

--- doc/examples/ex16/ex16.bat
+++ doc/examples/ex16/ex16.bat
@@ -1,35 +1,23 @@
 REM		GMT EXAMPLE 16
 REM
-REM
 REM Purpose:	Illustrates interpolation methods using same data as Example 12.
-REM GMT progs:	gmtset, grdview, grdfilter, pscontour, psscale, pstext, surface, triangulate
-REM DOS calls:	echo, del
+REM GMT modules:	gmtset, grdview, grdfilter, contour, colorbar, surface, triangulate
+REM DOS calls:	del
 REM
-REM First make a cpt file as in example 12:
-REM
-echo GMT EXAMPLE 16
-set ps=example_16.ps
-REM
-REM Illustrate various means of contouring, using triangulate and surface.
-REM
-gmt set FONT_ANNOT_PRIMARY 9p
-REM
-gmt pscontour -R0/6.5/-0.2/6.5 -Jx0.45i -P -K -Y5.5i -Ba2f1 -BWSne @table_5.11 -C@ex_16.cpt -I > %ps%
-echo 3.25 7 pscontour (triangulate) | gmt pstext -R -J -O -K -N -F+f18p,Times-Roman+jCB >> %ps%
-REM
-gmt surface @table_5.11 -R -I0.2 -Graws0.nc
-gmt grdview raws0.nc -R -J -B -C@ex_16.cpt -Qs -O -K -X3.5i >> %ps%
-echo 3.25 7 surface (tension = 0) | gmt pstext -R -J -O -K -N -F+f18p,Times-Roman+jCB >> %ps%
-REM
-gmt surface @table_5.11 -R -I0.2 -Graws5.nc -T0.5
-gmt grdview raws5.nc -R -J -B -C@ex_16.cpt -Qs -O -K -Y-3.75i -X-3.5i >> %ps%
-echo 3.25 7 surface (tension = 0.5) | gmt pstext -R -J -O -K -N -F+f18p,Times-Roman+jCB >> %ps%
-REM
-gmt triangulate @table_5.11 -Grawt.nc -R -I0.2
-gmt grdfilter rawt.nc -Gfiltered.nc -D0 -Fc1
-gmt grdview filtered.nc -R -J -B -C@ex_16.cpt -Qs -O -K -X3.5i >> %ps%
-echo 3.25 7 triangulate @~\256@~ gmt grdfilter | gmt pstext -R -J -O -K -N -F+f18p,Times-Roman+jCB >> %ps%
-echo 3.2125 7.5 Gridding of Data | gmt pstext -R0/10/0/10 -Jx1i -O -K -N -F+f32p,Times-Roman+jCB -X-3.5i >> %ps%
-gmt psscale -Dx3.25i/-0.4i+jTC+w5i/0.25i+h -C@ex_16.cpt -O >> %ps%
-del *.nc
-del gmt.conf
+gmt begin ex16
+	gmt set FONT_ANNOT_PRIMARY 9p FONT_TITLE 18p,Times-Roman
+	gmt subplot begin 2x2 -M0.05i -Fs3.25i/0 -R0/6.5/-0.2/6.5 -Jx1i -SCb -SRl+t -Bwesn -Y2i -T"Gridding of Data"
+		gmt contour @Table_5_11.txt -C@ex_16.cpt -I -B+t"contour (triangulate)" -c0,0
+
+		gmt surface @Table_5_11.txt -R0/6.5/-0.2/6.5 -I0.2 -Graws0.nc
+		gmt grdview raws0.nc -C@ex_16.cpt -Qs -B+t"surface (tension = 0)" -c0,1
+		gmt surface @Table_5_11.txt -Graws5.nc -T0.5
+		gmt grdview raws5.nc -C@ex_16.cpt -Qs -B+t"surface (tension = 0.5)" -c1,0
+
+		gmt triangulate @Table_5_11.txt -Grawt.nc
+		gmt grdfilter rawt.nc -Gfiltered.nc -D0 -Fc1
+		gmt grdview filtered.nc -C@ex_16.cpt -Qs -B+t"triangulate @~\256@~ grdfilter" -c1,1
+	gmt subplot end
+	gmt colorbar -DJBC+w5i/0.25i+h -C@ex_16.cpt
+gmt end show
+del raws0.nc raws5.nc rawt.nc filtered.nc
