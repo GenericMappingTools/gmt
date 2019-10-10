8222111914ed57f903588a52b1b3f6d144a0f196
Dongdong Tian <seisman.info@gmail.com>
2019-10-10 15:42:46 -0400

    Use -l auto-legend in batch scripts

--- doc/examples/ex07/ex07.bat
+++ doc/examples/ex07/ex07.bat
@@ -6,10 +6,10 @@ REM
 gmt begin ex07
 	gmt coast -R-50/0/-10/20 -JM9i -Slightblue -GP26+r300+ftan+bdarkbrown -Dl -Wthinnest -B --FORMAT_GEO_MAP=dddF
 	gmt plot @fz_07.txt -Wthinner,-
-	gmt plot @quakes_07.txt -h1 -Sci -i0,1,2+s0.01 -Gred -Wthinnest
+	gmt plot @quakes_07.txt -h1 -Sci -i0,1,2+s0.01 -Gred -Wthinnest -l"ISC Earthquakes"+s0.08i
 	gmt plot @isochron_07.txt -Wthin,blue
 	gmt plot @ridge_07.txt -Wthicker,orange
-	echo S 0.1i c 0.08i red thinnest 0.3i ISC Earthquakes | gmt legend -DjTR+w2.2i+o0.2i -F+pthick+ithinner+gwhite --FONT_ANNOT_PRIMARY=18p,Times-Italic
+	gmt legend -DjTR+w2i+o0.2i -F+pthick+ithinner+gwhite --FONT_ANNOT_PRIMARY=18p,Times-Italic
 	echo -43 -5 SOUTH	>  tmp
 	echo -43 -8 AMERICA >> tmp
 	echo -7 11 AFRICA	>> tmp
