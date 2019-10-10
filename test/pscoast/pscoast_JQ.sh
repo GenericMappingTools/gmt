4513654dc406f527872b1200c5147f4f9bb3afa9
Paul Wessel <pwessel@hawaii.edu>
2019-10-09 21:01:51 -1000

    Adding new failing script
    Will work on this soon

---
+++ test/pscoast/pscoast_JQ.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Problem is https://github.com/GenericMappingTools/gmt/issues/1759
+# Can test with -+83 for a single square
+# GMT_KNOWN_FAILURE
+
+ps=pscoast_JQ.ps
+
+pscoast -JQ200/6i -R-42.2839/223.375/-90/90 -BWeSn -Baf -Dc -S180/220/255 -P > $ps
