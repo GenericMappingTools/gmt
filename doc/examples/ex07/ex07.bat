ae8e825b54b0ad65b2187a75ea94a27efab10007
Paul Wessel <pwessel@hawaii.edu>
2019-10-15 08:18:06 -1000

    Same for bat files

--- doc/examples/ex07/ex07.bat
+++ doc/examples/ex07/ex07.bat
@@ -9,7 +9,7 @@ gmt begin ex07
 	gmt plot @quakes_07.txt -h1 -Sci -i0,1,2+s0.01 -Gred -Wthinnest -l"ISC Earthquakes"+s0.08i
 	gmt plot @isochron_07.txt -Wthin,blue
 	gmt plot @ridge_07.txt -Wthicker,orange
-	gmt legend -DjTR+w2i+o0.2i -F+pthick+ithinner+gwhite --FONT_ANNOT_PRIMARY=18p,Times-Italic
+	gmt legend -DjTR+o0.2i -F+pthick+ithinner+gwhite --FONT_ANNOT_PRIMARY=18p,Times-Italic
 	echo -43 -5 SOUTH	>  tmp
 	echo -43 -8 AMERICA >> tmp
 	echo -7 11 AFRICA	>> tmp
