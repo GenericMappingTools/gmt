9ab2caea6d6e28c01acda7589312e280c21c7a46
Paul Wessel <pwessel@hawaii.edu>
2019-10-26 11:44:59 -1000

    Broaden to also handle homebrew distro

--- admin/build-macos-external-list.sh
+++ admin/build-macos-external-list.sh
@@ -1,24 +1,48 @@
 #!/usr/bin/env bash
 #
+# Note: Requires coreutils to be installed so grealpath is available
+#
 # Build include file for cpack to build a complete macOS Bundle.
 # List of executables whose shared libraries must also be included
 
-# 1. List of executables whose shared libraries also are needed.
-#    Note: gs is just a link to gsc so we list both
-EXE="gs gsc gm ffmpeg"
-# 2. List of executables whose shared libraries have been included via GDAL
-which ogr2ogr > /tmp/raw.lis
-which gdal_translate >> /tmp/raw.lis
-# 3. Use otools -L to list shared libraries used but exclude system libraries
-for P in $EXE; do
+# 1a. List of executables whose shared libraries also are needed.
+EXEPLUSLIBS="gsc gm ffmpeg"
+# 1b. List of symbolic links.
+EXELINKS="gs"
+# 1c. List of executables whose shared libraries have been included via GDAL
+EXEONLY="ogr2ogr gdal_translate"
+#-----------------------------------------
+# 2a. Add the executables to the list given their paths
+rm -f /tmp/raw.lis
+for P in ${EXEONLY} ${EXEPLUSLIBS}; do
 	path=`which $P`
-	echo $path >> /tmp/raw.lis
-	otool -L $path | egrep -v '/usr/lib|/System/Library' | tr ':' ' ' | awk '{print $1}' >> /tmp/raw.lis
+	if [ -L $path ]; then # A symlink
+		grealpath $path >> /tmp/raw.lis
+	else
+		echo $path >> /tmp/raw.lis
+	fi
 done
-# 4. sort into unique list, then split executables from libraries
-sort -u /tmp/raw.lis > /tmp/unique.lis
-grep dylib /tmp/unique.lis > /tmp/libraries.lis
-grep -v dylib /tmp/unique.lis > /tmp/programs.lis
+# 2b. Add the symbolic links to the list given their paths as is
+rm -f /tmp/raw.lis
+for P in $EXELINKS; do
+	which $P >> /tmp/raw.lis
+done
+# 2c. Use otools -L to list shared libraries used but exclude system libraries
+for P in $EXEPLUSLIBS; do
+	path=`which $P`
+	otool -L $path | egrep -v '/usr/lib|/System/Library' | tr ':' ' ' | awk '{print $1}' > /tmp/dump.lis
+	while read file; do
+		if [ -L $file ]; then # A symlink
+			grealpath $file >> /tmp/raw.lis
+		else
+			echo $file >> /tmp/raw.lis
+		fi
+	done < /tmp/dump.lis
+done
+# 4. sort into unique list then split executables from libraries
+sort -u /tmp/raw.lis > /tmp/final.lis
+grep dylib /tmp/final.lis > /tmp/libraries.lis
+grep -v dylib /tmp/final.lis > /tmp/programs.lis
 
 # 5. Build the include file for cpack
 cat << EOF
