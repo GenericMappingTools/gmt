a9e9d904471d75b2ccac2a382941c3ea8e5b73de
Paul Wessel <pwessel@hawaii.edu>
2019-10-26 11:53:39 -1000

    Tell macports, homebrew, fink apart

--- admin/build-macos-external-list.sh
+++ admin/build-macos-external-list.sh
@@ -5,6 +5,13 @@
 # Build include file for cpack to build a complete macOS Bundle.
 # List of executables whose shared libraries must also be included
 
+if [ `which cmake` = "/opt/local/bin/cmake" ]; then
+	distro=MacPorts
+elif [ `which cmake` = "/usr/local/bin/cmake" ]; then
+	distro=HomeBrew
+else
+	distro=Fink
+fi
 # 1a. List of executables whose shared libraries also are needed.
 EXEPLUSLIBS="gsc gm ffmpeg"
 # 1b. List of symbolic links.
@@ -47,7 +54,7 @@ grep -v dylib /tmp/final.lis > /tmp/programs.lis
 # 5. Build the include file for cpack
 cat << EOF
 # List of extra executables and shared libraries to include in the macOS installer
-# This file is prepared to use ${USER} installation paths.
+# This file was prepared under $distro and used the installation paths of ${USER}.
 
 install (PROGRAMS
 EOF
