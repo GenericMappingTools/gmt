186f989d695ad91d751f86ac6980113a2860a384
Paul Wessel <pwessel@hawaii.edu>
2019-11-01 09:11:35 -1000

    Isolating gmt inset's effect on the current frame settings (#1957)
    * Restore gmt.frame after inset
    
    See issue #1956.  Closes that issue.
    
    * Improve and chop newlines
    
    * Use the same test in both places

--- src/inset.c
+++ src/inset.c
@@ -332,6 +332,23 @@ int GMT_inset (void *V_API, int mode, void *args) {
 		gmt_history_tag (API, tag);
 		sprintf (ffile, "%s/%s.%s", API->gwf_dir, GMT_HISTORY_FILE, tag);
 		gmt_remove_file (GMT, ffile);
+		/* Restore the old frame B setting to what it was before inset begin was called, if any */
+		if ((fp = fopen (file, "r"))) {	/* There is a gmt.frame file */
+			while (fgets (Bopts, PATH_MAX, fp) && strncmp (Bopts, "# FRAME: ", 9U));	/* Wind to reading the frame setting */
+			gmt_chop (Bopts);
+			fclose (fp);	/* Done reading the gmt.frame file */
+			if (!strncmp (Bopts, "# FRAME: ", 9U) && strlen (Bopts) > 9 && Bopts[9]) {	/* Got a previously saved -B frame setting */
+				sprintf (ffile, "%s/gmt%d.%s/gmt.frame", API->session_dir, GMT_MAJOR_VERSION, API->session_name);
+				if ((fp = fopen (ffile, "w")) == NULL) {	/* Not good */
+					GMT_Report (API, GMT_MSG_NORMAL, "Cannot create frame file %s\n", ffile);
+					Return (GMT_ERROR_ON_FOPEN);
+				}
+				GMT_Report (API, GMT_MSG_DEBUG, "inset: Restore previous frame in %s\n", ffile);
+				/* Restore the previous frame setting in gmt.frame */
+				fprintf (fp, "%s\n", &Bopts[9]);
+				fclose (fp);
+			}
+		}
 		/* Remove the inset information file */
 		gmt_remove_file (GMT, file);
 		GMT_Report (API, GMT_MSG_DEBUG, "inset: Removed inset file\n");
