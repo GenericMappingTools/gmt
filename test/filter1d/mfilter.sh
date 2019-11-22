738207acf665ac1fd21e515e03fdfd8745206f77
Paul Wessel <pwessel@hawaii.edu>
2019-11-22 10:51:25 -1000

    Update test/filter1d/mfilter.sh
    Co-Authored-By: Dongdong Tian <seisman.info@gmail.com>

--- test/filter1d/mfilter.sh
+++ test/filter1d/mfilter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Get and filter the Keeling curve with monthly averages
 ps=mfilter.ps
-gmt set MAP_ANNOT_ORTHO "" IO_HEADER_MARKER \" FORMAT_TIME_PRIMARY_MAP Abbreviated FORMAT_DATE_MAP o
+gmt set MAP_ANNOT_ORTHO "" FORMAT_TIME_PRIMARY_MAP Abbreviated FORMAT_DATE_MAP o
 URL=https://scrippsco2.ucsd.edu/assets/data/atmospheric/stations/in_situ_co2/weekly/weekly_in_situ_co2_mlo.csv
 gmt filter1d $URL -T1o+t -Fg30 --TIME_UNIT=d -f0T --IO_HEADER_MARKER=% > monthly.txt
 gmt psxy -R1958T/2018T/300/430 -JX6iT/4i -Bxa10Yf5y -Byaf+u" ppm" -BWSne -W0.25p monthly.txt -P -K -Xc > $ps
