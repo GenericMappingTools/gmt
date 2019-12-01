a9a0326b56c0fecdb44499eef33166ad0cf51b99
Paul Wessel <pwessel@hawaii.edu>
2019-12-01 06:47:45 -1000

    Deprecate modifiers +p- and +g- (#2193)
    The trailing - is not needed to turn off vector head outline and fill, respectively, but we will accept them as backwards compatible versions of +p and +g (i.e., no arguments turns off the outline and fill).

--- test/modern/vector.sh
+++ test/modern/vector.sh
@@ -5,14 +5,16 @@ echo 1 1 4 1 | gmt plot -R0/5/0/2 -Jx1i -Sv0.2i+s+b+e
 
 echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e -W1p -Y1c
 echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e+p -W1p -Y1c
+# Try the deprecated +p-
 echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e+p- -W1p -Y1c
 echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e+p3p -W1p -Y1c
 
 echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e -Gblack -Y1c
 echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e+g -Gblack -Y1c
+# Try the deprecated +g-
 echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e+g- -Gblack -Y1c
 echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e+gred -Gblack -Y1c
 
 echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e -W1p -Gblack -Y1c
-echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e+p-+gred -W1p -Gblack -Y1c
+echo 1 1 4 1 | gmt plot -Sv0.2i+s+b+e+p+gred -W1p -Gblack -Y1c
 gmt end show
