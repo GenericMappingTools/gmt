6bfde867e242c5d4adfee729b83a06a2bd8f8593
Paul Wessel <pwessel@hawaii.edu>
2020-01-25 23:26:07 -1000

    Move ex01 to using SI units (#2518)
    * Move ex01 to using SI units
    
    Per #2471, we will use SI units for map dimensions and points for smaller adjustments, such as text offsets, tick lengths, etc.
    
    * Update ex01.bat

--- doc/examples/ex01/ex01.bat
+++ doc/examples/ex01/ex01.bat
@@ -5,13 +5,13 @@ REM GMT modules:	set, subplot, grdcontour, coast
 REM
 gmt begin ex01
 	gmt set MAP_GRID_CROSS_SIZE_PRIMARY 0 FONT_ANNOT_PRIMARY 10p
-	gmt subplot begin 2x1 -A -M0.25i -Blrtb -Bafg -T"Low Order Geoid" -Fs6.5i/0 -Rg -JH6.5i
+	gmt subplot begin 2x1 -A -M0.5c -Blrtb -Bafg -T"Low Order Geoid" -Fs16c/0 -Rg -JH16c
 		gmt coast -JH? -Glightbrown -Slightblue -c0,0
-		gmt grdcontour @osu91a1f_16.nc -C10 -A50+f7p -Gd4i -Ln -Wcthinnest,- -Wathin,- -T+d0.1i/0.02i+l
-		gmt grdcontour @osu91a1f_16.nc -C10 -A50+f7p -Gd4i -LP -T+d0.1i/0.02i+l
+		gmt grdcontour @osu91a1f_16.nc -C10 -A50+f7p -Gd10c -Ln -Wcthinnest,- -Wathin,- -T+d8p/2p+l
+		gmt grdcontour @osu91a1f_16.nc -C10 -A50+f7p -Gd10c -LP -T+d8p/2p+l
 
 		gmt coast -JH0/? -Glightbrown -Slightblue -c1,0
-		gmt grdcontour @osu91a1f_16.nc -C10 -A50+f7p -Gd4i -Ln -Wcthinnest,- -Wathin,- -T+d0.1i/0.02i
-		gmt grdcontour @osu91a1f_16.nc -C10 -A50+f7p -Gd4i -LP -T+d0.1i/0.02i
+		gmt grdcontour @osu91a1f_16.nc -C10 -A50+f7p -Gd10c -Ln -Wcthinnest,- -Wathin,- -T+d8p/2p
+		gmt grdcontour @osu91a1f_16.nc -C10 -A50+f7p -Gd10c -LP -T+d8p/2p
 	gmt subplot end
 gmt end show
