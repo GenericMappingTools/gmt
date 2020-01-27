d621f000173a30eaa6c9a656a6ffa7e150a785a7
Paul Wessel <pwessel@hawaii.edu>
2020-01-26 14:24:42 -1000

    move ex20 to SI
    See #2471.

--- doc/examples/ex20/ex20.bat
+++ doc/examples/ex20/ex20.bat
@@ -8,7 +8,7 @@ REM Plot a world-map with volcano symbols of different sizes at hotspot location
 REM using table from Muller et al., 1993, Geology.
 gmt begin ex20
 	gmt set PROJ_LENGTH_UNIT inch
-	gmt coast -Rg -JR9i -B -B+t"Hotspot Islands and Hot Cities" -Gdarkgreen -Slightblue -A5000
+	gmt coast -Rg -JR22c -B -B+t"Hotspot Islands and Hot Cities" -Gdarkgreen -Slightblue -A5000
 	gmt plot @hotspots.txt -Skvolcano -Wthinnest -Gred
 
 	REM Overlay a few bullseyes at NY, Cairo, Perth, and Montevideo
