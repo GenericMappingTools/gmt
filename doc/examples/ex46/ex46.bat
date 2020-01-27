400189aa15deee916f6405b932faf9c6c4ebb750
Paul Wessel <pwessel@hawaii.edu>
2020-01-27 09:13:15 -1000

    Move ex40 to SI
    See #2471.

--- doc/examples/ex46/ex46.bat
+++ doc/examples/ex46/ex46.bat
@@ -5,7 +5,7 @@ REM GMT modules:  solar, coast, plot
 REM
 
 gmt begin ex46
-	gmt coast -Rd -JKs0/10i -A5000 -W0.5p -N1/0.5p,gray -S175/210/255 -Bafg --MAP_FRAME_TYPE=plain -Xc
+	gmt coast -Rd -JKs0/25c -A5000 -W0.5p -N1/0.5p,gray -S175/210/255 -Bafg --MAP_FRAME_TYPE=plain
 	gmt solar -Td+d2016-02-09T16:00:00 -Gnavy@95
 	gmt solar -Tc+d2016-02-09T16:00:00 -Gnavy@85
 	gmt solar -Tn+d2016-02-09T16:00:00 -Gnavy@80
