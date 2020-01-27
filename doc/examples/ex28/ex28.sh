dac1f8e36f19ecc27868f8d9eede6dbd8ba4083b
Paul Wessel <pwessel@hawaii.edu>
2020-01-26 18:07:35 -1000

    move ex28 to SI
    See #2471.

--- doc/examples/ex28/ex28.sh
+++ doc/examples/ex28/ex28.sh
@@ -13,8 +13,8 @@ gmt begin ex28
 	gmt coast -R@Kilauea.utm.nc -Ju5Q/1:160000 -Df+ -Slightblue -W0.5p -B5mg5m -BNE \
 		--FONT_ANNOT_PRIMARY=12p --FORMAT_GEO_MAP=ddd:mmF
 	echo 155:16:20W 19:26:20N KILAUEA | gmt text -F+f12p,Helvetica-Bold+jCB
-	gmt basemap --FONT_ANNOT_PRIMARY=9p -LjRB+c19:23N+f+w5k+l1:160,000+u+o0.2i --FONT_LABEL=10p
+	gmt basemap --FONT_ANNOT_PRIMARY=9p -LjRB+c19:23N+f+w5k+l1:160,000+u+o0.5c --FONT_LABEL=10p
 	# Annotate in km but append ,000m to annotations to get customized meter labels
 	gmt basemap -R@Kilauea.utm.nc+Uk -Jx1:160 -B5g5+u"@:8:000m@::" -BWSne --FONT_ANNOT_PRIMARY=10p \
-		--MAP_GRID_CROSS_SIZE_PRIMARY=0.1i --FONT_LABEL=10p
+		--MAP_GRID_CROSS_SIZE_PRIMARY=0.25c --FONT_LABEL=10p
 gmt end show
