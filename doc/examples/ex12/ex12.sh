aff9406cf66d8acea1b7ba3d97d0f880f60e7a08
Paul Wessel <pwessel@hawaii.edu>
2020-01-26 16:28:49 -1000

    Move ex12 to SI (#2529)
    See #2471.

--- doc/examples/ex12/ex12.sh
+++ doc/examples/ex12/ex12.sh
@@ -10,17 +10,17 @@ gmt begin ex12
 	# color palette (.cpt) file
 	T=`gmt info -T25+c2 @Table_5_11.txt`
 	gmt makecpt -Cjet $T
-	gmt subplot begin 2x2 -M0.05i -Fs3i/0 -SCb -SRl -R0/6.5/-0.2/6.5 -Jx3i -BWSne -T"Delaunay Triangulation"
+	gmt subplot begin 2x2 -M0.1c -Fs8c/0 -SCb -SRl -R0/6.5/-0.2/6.5 -JX8c -BWSne -T"Delaunay Triangulation"
 	# First draw network and label the nodes
 	gmt triangulate @Table_5_11.txt -M > net.xy
 	gmt plot net.xy -Wthinner -c0,0
-	gmt plot @Table_5_11.txt -Sc0.12i -Gwhite -Wthinnest
+	gmt plot @Table_5_11.txt -Sc0.3c -Gwhite -Wthinnest
 	gmt text @Table_5_11.txt -F+f6p+r
 	# Then draw network and print the node values
 	gmt plot net.xy -Wthinner -c0,1
-	gmt plot @Table_5_11.txt -Sc0.03i -Gblack
-	gmt text @Table_5_11.txt -F+f6p+jLM -Gwhite -W -C0.01i -D0.08i/0i -N
-	gmt contour @Table_5_11.txt -Wthin -C -Lthinnest,- -Gd1i -c1,0
+	gmt plot @Table_5_11.txt -Sc0.1c -Gblack
+	gmt text @Table_5_11.txt -F+f6p+jLM -Gwhite -W -C1p -D6p/0i -N
+	gmt contour @Table_5_11.txt -Wthin -C -Lthinnest,- -Gd3c -c1,0
 	# Finally color the topography
 	gmt contour @Table_5_11.txt -C -I -c1,1
 	gmt subplot end
