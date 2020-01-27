4b63e41220c24b652faa757a8200a19e1d96b45d
Paul Wessel <pwessel@hawaii.edu>
2020-01-26 16:27:18 -1000

    Move ex17 to SI (#2534)
    See #2417.

--- doc/examples/ex17/ex17.sh
+++ doc/examples/ex17/ex17.sh
@@ -9,7 +9,7 @@ gmt begin ex17
 
 	# First generate geoid image w/ shading
 	gmt grd2cpt @india_geoid.nc -Crainbow -H > geoid.cpt
-	gmt grdimage @india_geoid.nc -I+d -JM6.5i -Cgeoid.cpt
+	gmt grdimage @india_geoid.nc -I+d -JM16c -Cgeoid.cpt
 
 	# Then use gmt coast to initiate clip path for land
 	gmt coast -R@india_geoid.nc -Dl -G
@@ -19,14 +19,14 @@ gmt begin ex17
 	gmt grdimage @india_topo.nc -I+d
 
 	# Finally undo clipping and overlay basemap
-	gmt coast -Q -B+t"Clipping of Images" -Ba10f5
+	gmt coast -Q -B+t"Clipping of Images" -B
 
 	# Put a colorbar on top of the land mask
-	gmt colorbar -DjTR+o0.3i/0.1i+w4i/0.2i+h -Cgeoid.cpt -Bx5f1 -By+lm -I
+	gmt colorbar -DjTR+o0.8c/0.2c+w10c/0.5c+h -Cgeoid.cpt -Bx5f1 -By+lm -I
 
 	# Add a text paragraph
-	gmt text -M -Gwhite -Wthinner -C+tO -D-0.1i/0.1i -F+f12,Times-Roman+jRB <<- END
-	> 90 -10 12p 3i j
+	gmt text -M -Gwhite -Wthinner -C+tO -Dj-8p/8p -F+f12,Times-Roman+jRB <<- END
+	> 90 -10 12p 8c j
 	@_@%5%Example 17.@%%@_  We first plot the color geoid image
 	for the entire region, followed by a gray-shaded @#etopo5@#
 	image that is clipped so it is only visible inside the coastlines.
