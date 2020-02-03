15e62e051c383e5f7149055a387bfa0c12a4d247
Paul Wessel <pwessel@hawaii.edu>
2020-02-03 04:24:51 -1000

    Refresh ex04, ex05 to use -B+t for title (#2644)
    * Refresh ex04 to use -B+t for title
    
    Want to get away from using separate pstext statements just to set the title.  The title is set in 2-D even for 3-D views like these.
    
    * Same for ex05

--- doc/examples/ex04/ex04.sh
+++ doc/examples/ex04/ex04.sh
@@ -10,8 +10,8 @@ gmt begin
 	gmt makecpt -C255,100 -T-10/10/10 -N
 	gmt grdcontour @HI_geoid_04.nc -R195/210/18/25 -Jm1c -p60/30 -C1 -A5+o -Gd10c
 	gmt coast -p -B2 -BNEsw -Gblack -TdjBR+o0.3c+w3c+l
-	gmt grdview @HI_topo_04.nc -R195/210/18/25/-6/4 -Jz0.8c -p -C -N-6+glightgray -Qsm -B2 -Bz2+l"Topo (km)" -BneswZ -Y5c
-	echo '7 12 H@#awaiian@# R@#idge@#' | gmt text -R0/20/0/20 -Jx1c -F+f50p,ZapfChancery-MediumItalic+jCB
+	gmt grdview @HI_topo_04.nc -R195/210/18/25/-6/4 -Jz0.8c -p -C -N-6+glightgray -Qsm -B2 -Bz2+l"Topo (km)" \
+		-BneswZ+t"H@#awaiian@# R@#idge@#" -Y5c --FONT_TITLE=50p,ZapfChancery-MediumItalic --MAP_TITLE_OFFSET=-3.5c
 
 	gmt figure ex04c
 	gmt grdimage @HI_geoid_04.nc -I+a0+nt0.75 -R195/210/18/25 -JM15c -p60/30 -C@geoid_04.cpt
@@ -19,6 +19,6 @@ gmt begin
 	gmt basemap -p -TdjBR+o0.3c+w3c+l --COLOR_BACKGROUND=red --FONT=red --MAP_TICK_PEN_PRIMARY=thinner,red
 	gmt colorbar -p240/30 -DJBC+o0/1c+w13c/0.75c+h -C@geoid_04.cpt -I -Bx2+l"Geoid (m)"
 	gmt grdview @HI_topo_04.nc -I+a0+nt0.75 -R195/210/18/25/-6/4 -JZ8c -p60/30 -C@topo_04.cpt \
-		-N-6+glightgray -Qc100 -B2 -Bz2+l"Topo (km)" -BneswZ -Y5c
-	echo '7 12 H@#awaiian@# R@#idge@#' | gmt text -R0/20/0/20 -Jx1c -F+f50p,ZapfChancery-MediumItalic+jCB
+		-N-6+glightgray -Qc100 -B2 -Bz2+l"Topo (km)" -Y5c -BneswZ+t"H@#awaiian@# R@#idge@#" \
+		--FONT_TITLE=50p,ZapfChancery-MediumItalic --MAP_TITLE_OFFSET=-3.5c
 gmt end show
--- doc/examples/ex05/ex05.bat
+++ doc/examples/ex05/ex05.bat
@@ -7,7 +7,6 @@ REM
 gmt begin ex05
 	gmt grdmath -R-15/15/-15/15 -I0.3 X Y HYPOT DUP 2 MUL PI MUL 8 DIV COS EXCH NEG 10 DIV EXP MUL = sombrero.nc
 	gmt makecpt -C128 -T-5,5 -N
-	gmt grdview sombrero.nc -JX5i -JZ1.75i -B -Bz0.5 -BSEwnZ -N-1+gwhite -Qs -I+a225+nt0.75 -C -R-15/15/-15/15/-1/1 -p120/30
-	echo 3.5 4.5 z(r) = cos (2@~p@~r/8) @~\327@~e@+-r/10@+ | gmt text -R0/10/0/10 -Jx1i -F+f50p,ZapfChancery-MediumItalic+jBC
+	gmt grdview sombrero.nc -JX5i -JZ1.75i -B -Bz0.5 -BSEwnZ+t"z(r) = cos (2@~p@~r/8) @~\327@~e@+-r/10@+" -N-1+gwhite -Qs -I+a225+nt0.75 -C -R-15/15/-15/15/-1/1 -p120/30 --FONT_TITLE=50p,ZapfChancery-MediumItalic --MAP_TITLE_OFFSET=-1c
 	del sombrero.nc
 gmt end show
