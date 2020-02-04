5f7ca664ba25a7a6213a38a85708fcb4020a8137
Paul Wessel <pwessel@hawaii.edu>
2020-02-03 20:46:09 -1000

    Update gmt_init.c

--- src/gmt_init.c
+++ src/gmt_init.c
@@ -4085,7 +4085,7 @@ GMT_LOCAL int gmtinit_parse5_B_option (struct GMT_CTRL *GMT, char *in) {
 						if (gmt_M_x_is_lon(GMT,GMT_IN) || gmt_M_y_is_lat(GMT,GMT_IN))
 							GMT->current.plot.calclock.geo.wesn = 1;
 						else
-							GMT_Report (GMT->parent, GMT_MSG_WARNING, "Option -B: Cannot use +f for Cartesian basemaps - modifier ignored\n");
+							GMT_Report (GMT->parent, GMT_MSG_WARNING, "Option -B: Cannot use +f for Cartesian axes - modifier ignored\n");
 						break;
 					case 'L':	/* Force horizontal axis label */
 						GMT->current.map.frame.axis[no].label_mode = 1;
