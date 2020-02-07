5dd428dc4cd1821144a89478fd0707b544777a31
Paul Wessel <pwessel@hawaii.edu>
2020-02-06 17:22:27 -1000

    Attempt to prevent lint failures
    Not sure if this will work.  Trying to get pass these:
    
    src/gmt_init.c:13753:69: warning: Possible null pointer dereference: pen [nullPointer]
      if (outline && (S->v.status & PSL_VEC_OUTLINE2) == 0) S->v.pen = *pen; /* If no +p<pen> but -W<pen> was used, use same pen for vector heads */
                                                                        ^
    src/gmt_init.c:14005:47: note: Calling function 'gmt_init_vector_param', 5th argument 'NULL' value is 0
     gmt_init_vector_param (GMT, S, false, false, NULL, false, NULL);
                                                  ^
    src/gmt_init.c:13752:6: note: Assuming condition is true.
     if (set) { /* Determine proper settings for head fill or outline */
         ^
    src/gmt_init.c:13753:69: note: Null pointer dereference
      if (outline && (S->v.status & PSL_VEC_OUTLINE2) == 0) S->v.pen = *pen; /* If no +p<pen> but -W<pen> was used, use same pen for vector heads */
                                                                        ^
    src/gmt_support.c:311:20: error: Array 'fill->pattern[1024]' accessed at index 9998, which is out of bounds. [arrayIndexOutOfBounds]
      if (fill->pattern[i] == ':' && i < len && (fill->pattern[i+1] == 'B' || fill->pattern[i+1] == 'F')) pos = i;
                       ^
    src/gmt_support.c:10518:45: error: Array 'GMT->current.setting.format_float_map[64]' accessed at index 9998, which is out of bounds. [arrayIndexOutOfBounds]
       if (GMT->current.setting.format_float_map[i] == '.') j = i;
                                                ^
    We will see what happens in the CI

--- src/gmt_support.c
+++ src/gmt_support.c
@@ -306,9 +306,9 @@ GMT_LOCAL int gmtsupport_parse_pattern_old (struct GMT_CTRL *GMT, char *line, st
 		fill->pattern[i-1] = '\0';
 	}
 	/* Determine if there are colorizing options applied, i.e. [:F<rgb>B<rgb>] */
-	len = (int)strlen (fill->pattern) - 1;
+	len = (int)MIN(strlen (fill->pattern),PATH_MAX) - 1;
 	for (i = 0, pos = -1; i < len && fill->pattern[i] && pos == -1; i++)
-		if (fill->pattern[i] == ':' && i < len && (fill->pattern[i+1] == 'B' || fill->pattern[i+1] == 'F')) pos = i;
+		if (i < len && fill->pattern[i] == ':' && (fill->pattern[i+1] == 'B' || fill->pattern[i+1] == 'F')) pos = i;
 	if (pos > -1) fill->pattern[pos] = '\0';
 	fill->pattern_no = atoi (fill->pattern);
 	if (fill->pattern_no == 0) fill->pattern_no = -1;
@@ -10513,8 +10513,8 @@ int gmt_get_format (struct GMT_CTRL *GMT, double interval, char *unit, char *pre
 	else if (ndec > 0)
 		sprintf (format, "%%.%df", ndec);
 	else if (!general) {	/* Pull ndec from given format if .<precision> is given */
-		for (i = 0, j = -1; j == -1 && i < ((int)strlen(GMT->current.setting.format_float_map) - 1)
-		                            && GMT->current.setting.format_float_map[i]; i++)
+		for (i = 0, j = -1; j == -1 && i < ((int)MIN(strlen(GMT->current.setting.format_float_map),GMT_LEN64) - 1)
+			&& GMT->current.setting.format_float_map[i]; i++)
 			if (GMT->current.setting.format_float_map[i] == '.') j = i;
 		if (j > -1) ndec = atoi (&GMT->current.setting.format_float_map[j+1]);
 		strcpy (format, GMT->current.setting.format_float_map);
