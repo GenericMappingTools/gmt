90a6675908e12b24047f08861f3e2f4321ba178c
Paul Wessel <pwessel@hawaii.edu>
2020-02-16 08:08:31 -1000

    More warning fixes (#2744)
    * Improve code and scripts based on warnings
    
    See #2738 for background.
    
    * Update gmt_init.c
    
    * Update gmt_init.c
    
    * Update gmt_macros.h

--- doc/scripts/GMT_colorbar.sh
+++ doc/scripts/GMT_colorbar.sh
@@ -2,5 +2,5 @@
 gmt begin GMT_colorbar
 	gmt makecpt -T-15/15 -Cpolar
 	gmt basemap -R0/20/0/1 -JM5i -BWse -B
-	gmt colorbar -C -B -Bx+u"\\232" -By+l@~D@~T -DJBC+e
+	gmt colorbar -C -Bx+u"\\232" -By+l@~D@~T -DJBC+e
 gmt end show
