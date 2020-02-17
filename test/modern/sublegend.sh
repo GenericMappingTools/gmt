a0703a0db6c68938c9f63500bbd794d9f79c7beb
Paul Wessel <pwessel@hawaii.edu>
2020-02-16 15:36:28 -1000

    Address bad -B settings in subplots
    Several scripts had conflicting option where -B was added in odd places.  Also added more debug statements to determine what final options are after gmt_init_module.

--- test/modern/sublegend.sh
+++ test/modern/sublegend.sh
@@ -2,10 +2,10 @@
 # Test that the auto-legend works in subplots
 gmt begin sublegend ps
   gmt subplot begin 2x1 -Fs7i/3i -T"Subplots with legends" -X0.7i
-    gmt plot -R0/7/3/7 -B @Table_5_11.txt -Sc0.15i -Glightgreen -Wfaint -lApples+hLEGEND+f16p+d -c0
+    gmt plot -R0/7/3/7 @Table_5_11.txt -Sc0.15i -Glightgreen -Wfaint -lApples+hLEGEND+f16p+d -c0
     gmt plot @Table_5_11.txt -W1.5p,gray -l"My Lines"+s0.2i
     gmt plot @Table_5_11.txt -St0.15i -Gorange -lOranges
-    gmt plot -R0/7/3/7 -B @Table_5_11.txt -Sc0.15i -Glightgreen -Wfaint -lApples+hLEGEND+f16p+d+jTL -c1
+    gmt plot -R0/7/3/7 @Table_5_11.txt -Sc0.15i -Glightgreen -Wfaint -lApples+hLEGEND+f16p+d+jTL -c1
     gmt plot @Table_5_11.txt -W1.5p,gray -l"My Lines"+s0.2i
     gmt plot @Table_5_11.txt -St0.15i -Gred -lRed
   gmt subplot end
