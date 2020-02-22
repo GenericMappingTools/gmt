e7a77942f2be8adca43ee99f39334d78c400a768
Paul Wessel <pwessel@hawaii.edu>
2020-02-22 10:13:02 -1000

    Handle 3-D layer in grdinterpolate (#2789)
    * Handle 3-D layer in grdinterpolate
    
    Needed to determine if 3D nc cube name contained a specific variable and then use that variable when pulling out 2-D layers.
    Also modified the verbosity of some test regarding grid registration.
    
    * Update grdinterpolate.c
    
    Fix type in message.
    
    * Update grdinterpolate.c
    
    Replace knots with levels
    
    * Update grdinterpolate.c
    
    * Fix typo, move comment

--- src/gmt_nc.c
+++ src/gmt_nc.c
@@ -644,7 +644,7 @@ GMT_LOCAL int gmtnc_grd_info (struct GMT_CTRL *GMT, struct GMT_GRID_HEADER *head
 					GMT_Report (GMT->parent, GMT_MSG_WARNING, "The x-coordinates and range attribute are in conflict but range is exactly 360; we rely on this range\n");
 					if (set_reg && (header->n_columns%2) == 0) {	/* Pixel registration */
 						registration = header->registration = GMT_GRID_PIXEL_REG;
-						GMT_Report (GMT->parent, GMT_MSG_WARNING, "Global longitudes, guessing registration to be %s since nx is odd\n", regtype[header->registration]);
+						GMT_Report (GMT->parent, GMT_MSG_INFORMATION, "Global longitudes, guessing registration to be %s since nx is odd\n", regtype[header->registration]);
 					}
 				}
 			}
@@ -662,12 +662,12 @@ GMT_LOCAL int gmtnc_grd_info (struct GMT_CTRL *GMT, struct GMT_GRID_HEADER *head
 				if (gmt_M_360_range (dummy[0], dummy[1]))
 					GMT_Report (GMT->parent, GMT_MSG_INFORMATION, "Grid x-coordinates after pixel registration adjustment have exactly 360 range\n");
 			}
-			GMT_Report (GMT->parent, GMT_MSG_WARNING, "No range attribute, guessing registration to be %s\n", regtype[header->registration]);
+			GMT_Report (GMT->parent, GMT_MSG_INFORMATION, "No range attribute, guessing registration to be %s\n", regtype[header->registration]);
 		}
 		else {	/* Only has the valid_range settings.  If no registration set, and no dx available, guess based on nx */
 			if (set_reg && (header->n_columns%2) == 0) {	/* Pixel registration */
 				registration = header->registration = GMT_GRID_PIXEL_REG;
-				GMT_Report (GMT->parent, GMT_MSG_WARNING, "No x-coordinates, guessing registration to be %s since nx is odd\n", regtype[header->registration]);
+				GMT_Report (GMT->parent, GMT_MSG_INFORMATION, "No x-coordinates, guessing registration to be %s since nx is odd\n", regtype[header->registration]);
 			}
 		}
 
@@ -814,7 +814,7 @@ GMT_LOCAL int gmtnc_grd_info (struct GMT_CTRL *GMT, struct GMT_GRID_HEADER *head
 			header->z_min = dummy[0], header->z_max = dummy[1];
 		}
 		if (gmt_M_is_dnan (header->z_min) && gmt_M_is_dnan (header->z_max)) {
-			GMT_Report (GMT->parent, GMT_MSG_WARNING, "netCDF grid %s information has zmin = zmax = NaN. Reset to 0/0.\n", HH->name);
+			GMT_Report (GMT->parent, GMT_MSG_INFORMATION, "netCDF grid %s information has zmin = zmax = NaN. Reset to 0/0.\n", HH->name);
 			header->z_min = header->z_max = 0.0;
 		}
 		{	/* Get deflation and chunking info */
