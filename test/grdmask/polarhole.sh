fbad09d1e5f86f394482036dbb00729dd93f0081
Dongdong Tian <seisman.info@gmail.com>
2020-02-25 00:51:53 -0500

    Update to -JP new syntax

--- test/grdmask/polarhole.sh
+++ test/grdmask/polarhole.sh
@@ -12,6 +12,6 @@ gmt grdmask -Gmask_g.grd -I1 -R0/360/-90/90 -N1/1/NaN -S31d << EOF
 EOF
 gmt makecpt -Cjet -T0/10 > t.cpt
 gmt grdimage mask_c.grd -R-180/180/-90/90 -JX5.5id/2.75id -Ct.cpt -Baf -BWSnE -P -K -Xc -Y0.75i --MAP_FRAME_TYPE=plain > $ps
-gmt grdimage mask_c.grd -R0/360/0/80 -JPa2.75i -Ct.cpt -Bafg -O -K -X-0.75i -Y3.25i >> $ps
-gmt grdimage mask_g.grd -R0/360/0/80 -JPa2.75i -Ct.cpt -Bafg -O -K -X3.75i >> $ps
+gmt grdimage mask_c.grd -R0/360/0/80 -JP2.75i+a -Ct.cpt -Bafg -O -K -X-0.75i -Y3.25i >> $ps
+gmt grdimage mask_g.grd -R0/360/0/80 -JP2.75i+a -Ct.cpt -Bafg -O -K -X3.75i >> $ps
 gmt grdimage mask_g.grd -JQ0/5.5i -Ct.cpt -Baf -O -X-3i -Y3.5i >> $ps
