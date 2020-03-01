76acc3d3947b272f3135178676871604b31ed080
Paul Wessel <pwessel@hawaii.edu>
2020-03-01 09:18:41 -1000

    Simplify slices.sh test

--- test/grdinterpolate/slices.sh
+++ test/grdinterpolate/slices.sh
@@ -4,19 +4,12 @@
 
 gmt begin slices ps
 	gmt set PROJ_ELLIPSOID sphere
-	curl -q http://ds.iris.edu/spudservice/data/17996723 -o S362ANI_kmps.nc
-	gmt grdinterpolate S362ANI_kmps.nc?vsv -E0/0/180/0+i0.25d+g+p -T25/2890/5 -Gvsv.nc
-	gmt grdinterpolate S362ANI_kmps.nc?vsh -E0/0/180/0+i0.25d+g+p -T25/2890/5 -Gvsh.nc
-	gmt grdinterpolate S362ANI_kmps.nc?vs -E0/0/180/0+i0.25d+g+p -T25/2890/5 -Gvs.nc
+	curl -s http://ds.iris.edu/spudservice/data/17996723 -o S362ANI_kmps.nc
+	gmt grdinterpolate S362ANI_kmps.nc?vs -E0/0/180/0+i0.5d+g+p -T30/2890/10 -Gvs.nc
 	gmt makecpt -Cseis -T4.25/7.35
-	gmt subplot begin 4x2 -Fs8c/5c -Bafg -R0/180/0/2900 -X1.5c -A
+	gmt subplot begin 3x1 -Fs12c/7c -Bafg -R0/180/0/2900  -A
 		gmt grdimage vs.nc -JP?+fp -c  -BWESn
 		gmt grdimage vs.nc -JP?+fp+a+t90 -c -BWESn
-		gmt grdimage vsv.nc -JP?+fp -c  -BWESn
-		gmt grdimage vsv.nc -JP?+fp+a+t90 -c -BWESn
-		gmt grdimage vsv.nc -JP?+fp+zp+a+t90 -c -BWESn
-		gmt grdimage vsh.nc -JP?+fp -c  -BWESn
-		gmt grdimage vsh.nc -JP?+fp+a+t90 -c -BWESn
-		gmt grdimage vsh.nc -JP?+fp+zp+a+t90 -c -BWESn
+		gmt grdimage vs.nc -JP?+fp+zp+a+t90 -c -BWESn
 	gmt subplot end
 gmt end show
