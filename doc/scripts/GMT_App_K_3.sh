b5736342d707df02a9816dcac2129db9352d0db6
Dongdong Tian <seisman.info@gmail.com>
2020-03-06 01:40:29 -0500

    Fix scripts for GSHHG (#2850)
    Fix psxy to plot and indentations.

--- doc/scripts/GMT_App_K_3.sh
+++ doc/scripts/GMT_App_K_3.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 gmt begin GMT_App_K_3
-	gmt coast -Rk-500/500/-500/500 -JE130.35/-0.2/3.5i -Di -A20 \
-	  -Gburlywood -Sazure -Wthinnest -N1/thinnest,- -B2g1 -BWSne
+	gmt coast -Rk-500/500/-500/500 -JE130.35/-0.2/3.5i -Di -A20 -Gburlywood -Sazure -Wthinnest -N1/thinnest,- -B2g1 -BWSne
 	echo 133 2 | gmt plot -Sc1.4i -Gwhite
-	gmt basemap -Tmg133/2+w1i+t45/10/5+jCM --FONT_TITLE=12p --MAP_TICK_LENGTH_PRIMARY=0.05i \
-	  --FONT_ANNOT_SECONDARY=8p
-	echo 130.35 -0.2 | gmt psxy -SJ-200 -Wthicker
+	gmt basemap -Tmg133/2+w1i+t45/10/5+jCM --FONT_TITLE=12p --MAP_TICK_LENGTH_PRIMARY=0.05i --FONT_ANNOT_SECONDARY=8p
+	echo 130.35 -0.2 | gmt plot -SJ-200 -Wthicker
 gmt end show
