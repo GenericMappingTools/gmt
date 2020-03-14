c5f345e22bcd786e5d7a6c4af0497323e8af1e93
Paul Wessel <pwessel@hawaii.edu>
2020-03-13 16:29:23 -1000

    Change -Rk to -R...+uk in scripts

--- doc/scripts/GMT_App_K_3.sh
+++ doc/scripts/GMT_App_K_3.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 gmt begin GMT_App_K_3
-	gmt coast -Rk-500/500/-500/500 -JE130.35/-0.2/3.5i -Di -A20 -Gburlywood -Sazure -Wthinnest -N1/thinnest,- -B2g1 -BWSne
+	gmt coast -R-500/500/-500/500+uk -JE130.35/-0.2/3.5i -Di -A20 -Gburlywood -Sazure -Wthinnest -N1/thinnest,- -B2g1 -BWSne
 	echo 133 2 | gmt plot -Sc1.4i -Gwhite
 	gmt basemap -Tmg133/2+w1i+t45/10/5+jCM --FONT_TITLE=12p --MAP_TICK_LENGTH_PRIMARY=0.05i --FONT_ANNOT_SECONDARY=8p
 	echo 130.35 -0.2 | gmt plot -SJ-200 -Wthicker
--- test/potential/flexure_e.sh
+++ test/potential/flexure_e.sh
@@ -9,7 +9,7 @@ cat << EOF > t.txt
 #lon lat azimuth, semi-major, semi-minor, height
 300	200	70	70	30	5000
 EOF
-gmt grdseamount -Rk0/600/0/400 -I1000 -Gsmt.nc t.txt -Dk -E -F$f -C$m
+gmt grdseamount -R0/600/0/400+uk -I1000 -Gsmt.nc t.txt -Dk -E -F$f -C$m
 gmt grdcontour smt.nc+Uk -Jx0.01i -Xc -P -A1 -GlLM/RM -Bafg -K -Z+s0.001 > $ps
 gmt grdflexure smt.nc -D3300/2700/2400/1030 -E5k -Gflex_e.nc
 gmt grdcontour flex_e.nc+Uk -J -O -K -C0.2 -A1 -Z+s0.001 -GlLM/RM -Bafg -BWsNE+t"Elastic Plate Flexure, T@-e@- = 5 km" -Y4.4i >> $ps
