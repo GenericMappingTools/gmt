3c03a5137b6b13164b391dea50b5433ecc981f31
Paul Wessel <pwessel@hawaii.edu>
2020-03-16 15:11:47 -1000

    30 arc sec filter is now 1.0 km, not 0.9 km (#2931)
    0.9 was just too short and lead to NaNs at the S pole.

--- src/gmt_remote.h
+++ src/gmt_remote.h
@@ -53,7 +53,7 @@ GMT_LOCAL struct GMT_DATA_INFO gmt_data_info[GMT_N_DATA_INFO_ITEMS] = {
 	{"03m",  "27M", "Earth Relief at 3x3 arc minutes obtained by Gaussian Cartesian filtering (5.6 km fullwidth) of SRTM15+V2.1 [Tozer et al., 2019]"},
 	{"02m",  "58M", "Earth Relief at 2x2 arc minutes obtained by Gaussian Cartesian filtering (3.7 km fullwidth) of SRTM15+V2.1 [Tozer et al., 2019]"},
 	{"01m", "214M", "Earth Relief at 1x1 arc minutes obtained by Gaussian Cartesian filtering (1.9 km fullwidth) of SRTM15+V2.1 [Tozer et al., 2019]"},
-	{"30s", "765M", "Earth Relief at 30x30 arc seconds obtained by Gaussian Cartesian filtering (0.9 km fullwidth) of SRTM15+V2.1 [Tozer et al., 2019]"},
+	{"30s", "765M", "Earth Relief at 30x30 arc seconds obtained by Gaussian Cartesian filtering (1.0 km fullwidth) of SRTM15+V2.1 [Tozer et al., 2019]"},
 	{"15s", "2.6G", "Earth Relief at 15x15 arc seconds provided by SRTM15+V2.1 [Tozer et al., 2019]"},
 	{"03s", "6.8G", "Earth Relief at 3x3 arc seconds tiles provided by SRTMGL3 (land only) [NASA/USGS]"},
 	{"01s",  "41G", "Earth Relief at 1x1 arc seconds tiles provided by SRTMGL1 (land only) [NASA/USGS]"}
