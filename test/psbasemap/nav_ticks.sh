d9f13f83b4930b2ec7bd13cdad8801d51ddf18ef
Paul Wessel <pwessel@hawaii.edu>
2020-03-22 16:14:30 -1000

    Update script and PS
    This uses coarser interval as secondary.  Another PR will deal with sorting primary vs secondary so the -Bs vs -Bp will be less of an issue.

--- test/psbasemap/nav_ticks.sh
+++ test/psbasemap/nav_ticks.sh
@@ -2,5 +2,5 @@
 # Check the centered and asymmetrical gridline ticks for negative grid cross sizes on an azimuthal projection
 gmt begin nav_ticks ps
   gmt basemap -Rg -JG8.1703/49.9816/18c -Bg
-  gmt basemap -Bpg5d -Bsg1d --MAP_GRID_CROSS_SIZE_PRIMARY=+5p --MAP_GRID_CROSS_SIZE_SECONDARY=-3p --MAP_GRID_PEN_PRIMARY=default,red --MAP_GRID_PEN_SECONDARY=default,blue
+  gmt basemap -Bsg5d -Bpg1d --MAP_GRID_CROSS_SIZE_PRIMARY=-3p --MAP_GRID_CROSS_SIZE_SECONDARY=+5p --MAP_GRID_PEN_PRIMARY=default,blue --MAP_GRID_PEN_SECONDARY=default,red
 gmt end show
