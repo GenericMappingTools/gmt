c85d15ab15e2fe7319f10491a07d31f395637bff
Paul Wessel <pwessel@hawaii.edu>
2020-04-22 18:05:52 -1000

    Eliminate old gmt_supplements_module.[ch]
    Made gmt_suppl_glue.[ch].in after gmt_glue.[ch] and gmt_suppl_modules.h after gmt_modules.h and the contents of the old gmt_supplements_module.h.  I include gmt_suppl_modules.h from gmt_dev.h and gmt_suppl_glue.h from gmt_api.c.  I updated the CMakefile to configure the two new files.  All compiles but currently the gmt_suppl_glue.[ch] file says "core" instead of "suppl" in the function names.

--- src/gmt_api.c
+++ src/gmt_api.c
@@ -184,6 +184,7 @@
 #include "gmt_internals.h"
 #include "gmt_sharedlibs.h" 	/* Common shared libs structures */
 #include "gmt_glue.h"
+#include "gmt_suppl_glue.h"
 #include <stdarg.h>
 
 #ifdef HAVE_DIRENT_H_
