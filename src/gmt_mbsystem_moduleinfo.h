0613fc6aa4d990d608e09c168ef15ecaef579aac
Paul Wessel <pwessel@hawaii.edu>
2020-04-25 09:30:43 -1000

    Update the name of mbsystem suppl (#3171)
    mbgmt is something JL does privately

--- src/gmt_mbsystem_moduleinfo.h
+++ src/gmt_mbsystem_moduleinfo.h
@@ -1,3 +1,3 @@
-	{"mbcontour", "mbcontour", "mbgmt",  "Plot swath bathymetry, amplitude, or backscatter",  ""},
-	{"mbgrdtiff", "mbgrdtiff", "mbgmt",  "Project grids or images and plot them on maps",  ""},
-	{"mbswath", "mbswath", "mbgmt",  "Plot swath bathymetry, amplitude, or backscatter",  ""},
+	{"mbcontour", "mbcontour", "mbsystem",  "Plot swath bathymetry, amplitude, or backscatter",  ""},
+	{"mbgrdtiff", "mbgrdtiff", "mbsystem",  "Project grids or images and plot them on maps",  ""},
+	{"mbswath", "mbswath", "mbsystem",  "Plot swath bathymetry, amplitude, or backscatter",  ""},
