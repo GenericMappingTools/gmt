08f7f18138961832b0f4316f1752308502116731
Dongdong Tian <seisman.info@gmail.com>
2020-05-06 15:40:44 -0400

    Reduce test PS sizes for pssolar (#3266)
    Using lower resolution coastlines and don't plot small areas.

--- test/pssolar/shading.sh
+++ test/pssolar/shading.sh
@@ -2,7 +2,7 @@
 # Test pssolar with shading
 
 ps=shading.ps
-gmt pscoast -Rg -JG30/30/6i -P -Glightbrown -Sazure1 -K -Bg -Xc -Y0.5i > $ps
+gmt pscoast -Rg -JG30/30/6i -P -Glightbrown -Sazure1 -Dc -A50000 -K -Bg -Xc -Y0.5i > $ps
 gmt pssolar -R -J -O -K -Gblack@70 -T+d2012-6-12T18:00:00 >> $ps
-gmt pscoast -Rg -JH0/6i -O -Glightbrown -Sazure1 -K -Bg -Y6.5i >> $ps
+gmt pscoast -Rg -JH0/6i -O -Glightbrown -Sazure1 -Dc -A50000 -K -Bg -Y6.5i >> $ps
 gmt pssolar -R -J -O -Gblack@70 -T+d2012-6-12T18:00:00 >> $ps
