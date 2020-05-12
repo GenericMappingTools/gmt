b127c6be839399c69704a34cb0f86ea9b3ae9bc5
Paul Wessel <pwessel@hawaii.edu>
2020-05-11 22:02:38 -1000

    Add test example

--- test/grdmath/daynight.sh
+++ test/grdmath/daynight.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+# Test the DAYNIGHT operator
+
 ps=daynight.ps
-gmt grdmath -Rd -I1 30 20 0 DAYMASK = a.grd
-gmt grdmath -Rd -I1 30 20 5 DAYMASK = b.grd
+gmt grdmath -Rd -I1 30 20 0 DAYNIGHT = a.grd
+gmt grdmath -Rd -I1 30 20 5 DAYNIGHT = b.grd
 echo "0 black 1 white" > t.cpt
-gmt grdimage a.grd -Ct.cpt -Baf -P -K -JQ6i > $ps
-echo 30 20 | gmt psxy -R -J -O -K -Sc0.25c -Gred >> $ps
-gmt grdimage b.grd -Ct.cpt -Baf -O -K -Y4i >> $ps
-echo 30 20 | gmt psxy -R -J -O -K -Sc0.25c -Gred >> $ps
+gmt grdimage a.grd -Ct.cpt -Baf -B+t"Transition = 0" -P -K -JQ6.5i > $ps
+echo 30 20 | gmt psxy -R -J -O -K -Sk@sunglasses/1.5c >> $ps
+gmt grdimage b.grd -Ct.cpt -Baf -B+t"Transition = 5" -J -O -K -Y5i >> $ps
+echo 30 20 | gmt psxy -R -J -O -Sc0.25c -Sk@sunglasses/1.5c >> $ps
