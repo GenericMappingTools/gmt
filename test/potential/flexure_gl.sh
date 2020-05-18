0a6df7b7c4ee8ebd2ee311761bffc865bdf09be5
Paul Wessel <pwessel@hawaii.edu>
2020-05-18 13:32:07 -1000

    Update test

--- test/potential/flexure_gl.sh
+++ test/potential/flexure_gl.sh
@@ -13,4 +13,4 @@ EOF
 gmt grdseamount -R0/600/0/400+uk -I1000 -Gsmt.nc t.txt -Dk -E -F$f -C$m
 gmt grdcontour smt.nc+Uk -Jx0.01i -Xc -P -A1 -GlLM/RM -Bafg -K -Z+s0.001 > $ps
 gmt grdflexure smt.nc -D3300/2700/2400/1030 -E20k/10k -M10k -Gflex_gl_%g.nc -T50k
-gmt grdcontour flex_gl_100000.nc+Uk -J -O -C0.2 -A1 -Z+s0.001 -GlLM/RM -Bafg -BWsNE+t"GLVE Plate Flexure, T@-e@- = 20/10 km, t/t@-m@- = 5" -Y4.4i >> $ps
+gmt grdcontour flex_gl_50000.nc+Uk -J -O -C0.2 -A1 -Z+s0.001 -GlLM/RM -Bafg -BWsNE+t"GLVE Plate Flexure, T@-e@- = 20/10 km, t/t@-m@- = 5" -Y4.4i >> $ps
