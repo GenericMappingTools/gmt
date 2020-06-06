c334d5e71eddd46dd40f899bdffecf5b1b0d71cb
Paul Wessel <pwessel@hawaii.edu>
2020-06-06 11:08:19 -1000

    updates

--- doc/scripts/GMT_earthmask.sh
+++ doc/scripts/GMT_earthmask.sh
@@ -3,16 +3,16 @@
 # Plot the earth masks
 
 gmt begin earthmask ps
-	gmt makecpt -Clightblue,darkbrown,cyan,lightgreen,white -T-0.5/4.5/1 -N
+	gmt makecpt -Clightblue,burlywood,cyan,red,white -T-0.5/4.5/1 -N
 	gmt grdimage @earth_mask_05m_p.grd -JQ0/15c -Baf -BWsNe
 	# BUG IN MASK. The next call covered it up for the orig so it will fail until we fix the mask
 	#echo -40 -82 | gmt plot -Ss0.3c -Gdarkbrown
 	gmt legend -DJBC+w15c+o0/0.5c -F+p1p <<- EOF
 	N 5
 	S - s 0.5c lightblue 0.25p - Ocean
-	S - s 0.5c darkbrown 0.25p - Land
+	S - s 0.5c burlywood 0.25p - Land
 	S - s 0.5c cyan 0.25p - Lakes
-	S - s 0.5c lightgreen 0.25p - Islands
+	S - s 0.5c red 0.25p - Islands
 	S - s 0.5c white 0.25p - Ponds
 	EOF
 gmt end show
