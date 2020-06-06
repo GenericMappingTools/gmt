e93cb33ba5e74251675767793bbb53d1bfde4ed6
Paul Wessel <pwessel@hawaii.edu>
2020-06-06 11:53:11 -1000

    Update GMT_earthmask.sh

--- doc/scripts/GMT_earthmask.sh
+++ doc/scripts/GMT_earthmask.sh
@@ -5,8 +5,6 @@
 gmt begin GMT_earthmask ps
 	gmt makecpt -Clightblue,burlywood,blue,red,white -T-0.5/4.5/1 -N
 	gmt grdimage @earth_mask_05m_p.grd -JQ0/15c -Baf -BWsNe
-	# BUG IN MASK. The next call covered it up for the orig so it will fail until we fix the mask
-	#echo -40 -82 | gmt plot -Ss0.3c -Gdarkbrown
 	gmt legend -DJBC+w15c+o0/0.5c -F+p1p <<- EOF
 	N 5
 	S - s 0.5c lightblue 0.25p - Ocean [0]
