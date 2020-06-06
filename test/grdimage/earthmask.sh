43601bce2fd407c1ef2710ea5c0f3e81384d0da8
Paul Wessel <pwessel@hawaii.edu>
2020-06-05 16:59:46 -1000

    Update earthmask.sh

--- test/grdimage/earthmask.sh
+++ test/grdimage/earthmask.sh
@@ -4,7 +4,7 @@
 
 gmt begin earthmask ps
 	gmt makecpt -Clightblue,darkbrown,cyan,lightgreen,white -T-0.5/4.5/1 -N
-	gmt grdimage /Users/pwessel/GMTdev/gmtserver-admin/staging/earth/earth_mask/earth_mask_05m_p.grd -JQ0/15c -Baf -BWsNe
+	gmt grdimage @earth_mask_05m_p.grd -JQ0/15c -Baf -BWsNe
 	# BUG IN MASK. The next call covered it up for the orig so it will fail until we fix the mask
 	#echo -40 -82 | gmt plot -Ss0.3c -Gdarkbrown
 	gmt legend -DJBC+w15c+o0/0.5c -F+p1p <<- EOF
