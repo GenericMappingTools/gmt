6b06d32652ecfaef1b7bd53658671deea68ceee8
Dongdong Tian <seisman.info@gmail.com>
2020-06-08 12:20:04 -0400

    Fix test/x2sys_05.sh

--- test/x2sys/x2sys_05.sh
+++ test/x2sys/x2sys_05.sh
@@ -6,7 +6,7 @@ OLDX=$X2SYS_HOME
 export X2SYS_HOME=.
 gmt x2sys_init TEST -Dmgd77 -Emgd77 -F -Gd -Rd -I15m
 echo "$X2SYS_HOME" >> $X2SYS_HOME/TEST/TEST_paths.txt
-gmt x2sys_binlist -TTEST 01010221.mgd77 -D --FORMAT_FLOAT_OUT=%.2f > x2sys_05.txt
+gmt x2sys_binlist -TTEST 01010221.mgd77 -D --FORMAT_FLOAT_OUT=%.3f > x2sys_05.txt
 rm -rf TEST
 diff --strip-trailing-cr x2sys_05.txt "${GMT_SRCDIR:-.}"/x2sys_05.txt > fail
 
