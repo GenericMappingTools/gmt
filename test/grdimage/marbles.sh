98768afd3f8400c821af8fd6e19eb636f485521e
Paul Wessel <pwessel@hawaii.edu>
2020-06-07 15:52:52 -1000

    Fix the duplication and freeing of GMT_IMAGE (#3438)
    * Fix the duplication and freeing of GMT_IMAGE
    
    Several problems fixed, but not completed:
    1. GMT_IMAGE members colormap and color_interp were not duplicated by GMT_Duplicate_Data.
    2. grdimage projected I to Img_proj and deleted I, but then refered to I->colormap instead of Img_proj->colormap
    
    However, the colormap entries we obtain do not match the ones gdalinfo reports on the commandline and the image looks like shit.
    
    * Various
    
    Fix registration issues in marbles and update orig, add GMT_IMAGE_NO_INDEX mode flag to ensure the returned image is not indexed but expanded to colors.  This is needed in the plotters like grdimage, grdview and grdmix due to projection or blending.
    
    * Update readwrite_withgdal.ps
    
    * Update gmt_grdio.c
    
    * Handle TRP and TRB
    
    * Make it a static local function
    
    * Forgot about updating the dics, need this automated I think
    
    * Comment out wrong test for mem laoyout?

--- test/grdimage/marbles.sh
+++ test/grdimage/marbles.sh
@@ -3,5 +3,5 @@
 ps=marbles.ps
 gmt grdimage -JG0/0/3i @earth_day_01d -P -Y7i -K > $ps
 gmt grdimage -JG90/30/3i @earth_day_01d -O -K -X3.5i >> $ps
-gmt grdimage -JH270/6.5i @earth_day_01d -O -K -X-3.5i Y-3.5i >> $ps
+gmt grdimage -JH270/6.5i @earth_day_01d -O -K -X-3.5i -Y-3.5i >> $ps
 gmt grdimage -JM6.5i -R0/360/-45/45 -Bag @earth_day_01d -O -Y-2.5i >> $ps
