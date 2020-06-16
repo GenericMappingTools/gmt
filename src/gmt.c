6a6ac5692abc7ae3aa2a52eb727e89a49a5d65a5
Joaquim Luis <jmfluis@gmail.com>
2020-06-16 22:09:07 +0100

    Update gmr.rst

--- src/gmt.c
+++ src/gmt.c
@@ -191,12 +191,6 @@ int main (int argc, char *argv[]) {
 				status = GMT_NOERROR;
 			}
 
-			/* Show user's ~/.gmt hidden dir */
-			else if (!strncmp (argv[arg_n], "--show-userdir", 14U)) {
-				fprintf(stdout, "%s/.gmt\n", api_ctrl->GMT->session.HOMEDIR);
-				status = GMT_NOERROR;
-			}
-
 			/* Show the directory that contains the 'gmt' executable */
 			else if (!strncmp (argv[arg_n], "--show-bindir", 10U)) {
 				fprintf (stdout, "%s\n", api_ctrl->GMT->init.runtime_bindir);
@@ -218,6 +212,12 @@ int main (int argc, char *argv[]) {
 			/* Show share directory */
 			else if (!strncmp (argv[arg_n], "--show-sharedir", 12U)) {
 				fprintf (stdout, "%s\n", api_ctrl->GMT->session.SHAREDIR);
+				status = GMT_NOERROR;
+			}
+
+			/* Show user's ~/.gmt hidden dir */
+			else if (!strncmp (argv[arg_n], "--show-userdir", 14U)) {
+				fprintf(stdout, "%s/.gmt\n", api_ctrl->GMT->session.HOMEDIR);
 				status = GMT_NOERROR;
 			}
 
@@ -339,11 +339,11 @@ int main (int argc, char *argv[]) {
 			fprintf (stderr, "  --show-datadir    Show directory/ies with user data.\n");
 			fprintf (stderr, "  --show-dataserver Show URL of the remote GMT data server.\n");
 			fprintf (stderr, "  --show-doi        Show the DOI for the current release.\n");
-			fprintf (stderr, "  --show-userdir    Show full path of user's ~/.gmt dir\n");
 			fprintf (stderr, "  --show-library    Show path of the shared GMT library.\n");
 			fprintf (stderr, "  --show-modules    Show all modern module names.\n");
 			fprintf (stderr, "  --show-plugindir  Show directory for plug-ins.\n");
 			fprintf (stderr, "  --show-sharedir   Show directory for shared GMT resources.\n");
+			fprintf (stderr, "  --show-userdir    Show full path of user's ~/.gmt dir\n");
 			fprintf (stderr, "  --version         Print GMT version number.\n\n");
 			fprintf (stderr, "if <module-options> is \'=\' we call exit (0) if module exist and non-zero otherwise.\n\n");
 		}
