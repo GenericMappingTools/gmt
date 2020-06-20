1374a611af4c6844ae1f427d8728afe5c066ab43
Paul Wessel <pwessel@hawaii.edu>
2020-06-19 21:49:59 -1000

    Add test and comments

--- src/testapi_matrix_as_grid.c
+++ src/testapi_matrix_as_grid.c
@@ -1,5 +1,11 @@
 #include "gmt_dev.h"
 
+/* Test passing a gridline and pixel-registered matrix as grids to grdimage.
+ * the two plots should be identical.  This tests was made to demonstrate a
+ * bug in the API that was fixed in https://github.com/GenericMappingTools/gmt/pull/3503.
+ * The problem was an error in gmt_M_get_n and failure to set M->registration for matrices.
+ */
+
 int main () {
 	unsigned int mode = GMT_SESSION_EXTERNAL;
 	struct GMT_MATRIX *M_p = NULL, *M_g = NULL;
@@ -17,7 +23,7 @@ int main () {
 	if ((M_g = GMT_Create_Data (API, GMT_IS_GRID|GMT_VIA_MATRIX, GMT_IS_SURFACE, GMT_CONTAINER_ONLY, NULL, range_g, inc, GMT_GRID_NODE_REG, 0, NULL)) == NULL) return (EXIT_FAILURE);
 	GMT_Put_Matrix (API, M_g, GMT_DOUBLE, 0, coord);
 	GMT_Open_VirtualFile (API, GMT_IS_GRID|GMT_VIA_MATRIX, GMT_IS_SURFACE, GMT_IN|GMT_IS_REFERENCE, M_g, input_g);
-	sprintf (args_g, "%s -R-1/3/-1/3 -JX6c -Baf -BWSen+tGridline -K > test_matrix.ps", input_g);
+	sprintf (args_g, "%s -R-1/3/-1/3 -JX6c -Baf -BWSen+tGridline -K -P > api_matrix_as_grid.ps", input_g);
 	GMT_Call_Module (API, "grdimage", GMT_MODULE_CMD, args_g);
 	GMT_Close_VirtualFile (API, input_g);
 
@@ -26,7 +32,7 @@ int main () {
 	if ((M_p = GMT_Create_Data (API, GMT_IS_GRID|GMT_VIA_MATRIX, GMT_IS_SURFACE, GMT_CONTAINER_ONLY, NULL, range_p, inc, GMT_GRID_PIXEL_REG, 0, NULL)) == NULL) return (EXIT_FAILURE);
 	GMT_Put_Matrix (API, M_p, GMT_DOUBLE, 0, coord);
 	GMT_Open_VirtualFile (API, GMT_IS_GRID|GMT_VIA_MATRIX, GMT_IS_SURFACE, GMT_IN|GMT_IS_REFERENCE, M_p, input_p);
-	sprintf (args_p, "%s -R-1/3/-1/3 -JX6c -Baf -BWSen+tPixel -O -X8c >> test_matrix.ps", input_p);
+	sprintf (args_p, "%s -R-1/3/-1/3 -JX6c -Baf -BWSen+tPixel -O -X8c >> api_matrix_as_grid.ps", input_p);
 	GMT_Call_Module (API, "grdimage", GMT_MODULE_CMD, args_p);
 	GMT_Close_VirtualFile (API, input_p);
 
