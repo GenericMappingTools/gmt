925e272850f66da03d664ef5c9a99a50513b9674
Paul Wessel <pwessel@hawaii.edu>
2020-06-25 21:08:06 -1000

    Let gmt.history and gmt.conf be hierarchical and separate for panels and figures (#3523)
    * Let gmt.history be hierarchical and separate for panels and fitures
    
    We do not want an automatic region determined for one panel to apply to another panel.
    
    * Update inset.c
    
    Fix the inset stuff.
    
    * Do the same with gmt.conf
    
    * Update gmt_init.c
    
    * Update features.rst
    
    * Update features.rst
    
    * Update scrips and examples to follow rules on hierarchical entries
    
    We had several instances wehre we set -R in a panel expecting it to carry over to othre panels.
    
    * Let subplot -R set both grid and plot regions
    
    * Update subplot.rst
    
    * grdflexure -Q needs to change NEEDS from g to blank
    
    Otherwise we are looking to find -R when not required or wanted.
    
    * Update inset test script to better fit original
    
    New version is correct in that -Jx implies equal scaling so it should not scale x and y sepsrately.
    
    * Add example of hierarchical history
    
    * Update test/modern/panels_history.sh
    
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>
    
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- test/modern/somepanels.sh
+++ test/modern/somepanels.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Test only giving a few subplots, Roman labeling, and -A override for one subplot
 gmt begin somepanels ps
-  gmt subplot begin 3x3 -F6i/8.5i -M5p -A1+c+r+o0.2i+jBR -SCb+lXLABEL -SRl+lYLABEL -Bwstr -T"FIGURE HEADER"
-    gmt basemap -R0/100/0/10 -c0
+  gmt subplot begin 3x3 -F6i/8.5i -M5p -A1+c+r+o0.2i+jBR -SCb+lXLABEL -SRl+lYLABEL -Bwstr -T"FIGURE HEADER" -R0/100/0/10
+    gmt basemap -c0
     gmt subplot 1,1 -Apink
     gmt basemap -B+gpink
     gmt basemap -c2,2
