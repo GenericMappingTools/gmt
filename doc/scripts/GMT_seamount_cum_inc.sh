22f69f471347e3fa747b728a6765d926db843c65
Paul Wessel <pwessel@hawaii.edu>
2020-07-25 10:46:13 -1000

    Update GMT_seamount_cum_inc.sh

--- doc/scripts/GMT_seamount_cum_inc.sh
+++ doc/scripts/GMT_seamount_cum_inc.sh
@@ -8,17 +8,20 @@ gmt begin GMT_seamount_cum_inc ps
 	gmt subplot begin 5x2 -SCb+tc -SRl -A -R40/160/0/5.010 -Fs3i/0.5i -M8p
 	gmt set FONT_TAG 16p,Times-Italic,black
 	row=4
+	tt=0
 	while read t file rest; do
-		gmt subplot set ${row},0 -A"t = $row"
+		gmt subplot set ${row},0 -A"t = $tt"
 		gmt grdtrack -G${file} -E40000/75000/160000/75000 -o0,2 | gmt plot -W1p -i0+s0.001,1+s0.001 -L+y0 -Gblack
 		let row--
+		let tt++
 	done < c.lis
 	row=4
+	tt=0
 	while read t file rest; do
-		gmt subplot set ${row},1 -A"t = $row"
+		gmt subplot set ${row},1 -A"t = $tt"
 		gmt grdtrack -G${file} -E40000/75000/160000/75000 -o0,2 | gmt plot -W1p -i0+s0.001,1+s0.001 -L+y0 -Gblack
 		let row--
+		let tt++
 	done < i.lis
 	gmt subplot end
-	rm -f smt[ci]*.nc ?.lis t.txt
 gmt end show
