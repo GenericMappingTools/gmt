5936dc87119350d81e3ce00b6273ffab4d25083f
Paul Wessel <pwessel@hawaii.edu>
2020-08-04 19:02:11 -1000

    Update testapi_grid2matrix.c

--- src/testapi_grid2matrix.c
+++ src/testapi_grid2matrix.c
@@ -14,7 +14,7 @@ int main () {
 	/* Read in earth_relief_01d as a matrix from the remote grid */
 	M = GMT_Read_Data (API, GMT_IS_MATRIX, GMT_IS_FILE, GMT_IS_SURFACE, GMT_READ_NORMAL, NULL, "@earth_relief_01d", NULL);
 	/* Write out the matrix to another grid file */
-	GMT_Write_Data (API, GMT_IS MATRIX, GMT_IS_FILE, GMT_IS_SURFACE GMT_WRITE_NORMAL, NULL, "dump_01d.grd", M)
+	GMT_Write_Data (API, GMT_IS_MATRIX, GMT_IS_FILE, GMT_IS_SURFACE, GMT_WRITE_NORMAL, NULL, "dump_01d.grd", M);
 
 	if (GMT_Destroy_Session (API)) return EXIT_FAILURE;
 	exit (0);
