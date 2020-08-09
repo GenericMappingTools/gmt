80cb44f0d69518ce8df642b63df129484cd6fc2b
Paul Wessel <pwessel@hawaii.edu>
2020-08-08 14:46:42 -1000

    Update GMT_base_symbols5.sh

--- doc/scripts/GMT_base_symbols5.sh
+++ doc/scripts/GMT_base_symbols5.sh
@@ -8,7 +8,7 @@ gmt begin GMT_base_symbols5 ps
 EOF
 # Circular arrows
 	gmt plot -S -W2p -Gred --MAP_VECTOR_SHAPE=0.5 -X2i << EOF
-1	0.3	2	0	90	m0.3i+bt+e
+1	0.3	2c	0	90	m0.3i+bt+e
 EOF
 # Geo arrows
 	gmt plot -R0/90/-41.17/41.17 -JM1.75i -S -W2p -Gred --MAP_VECTOR_SHAPE=0.5 -X2i --MAP_FRAME_TYPE=plain << EOF
