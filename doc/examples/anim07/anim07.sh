d4acaa91bab050ea706424ba25b3743d3c8d89af
Paul Wessel <pwessel@hawaii.edu>
2020-08-15 13:14:33 -1000

    Update anim07.sh

--- doc/examples/anim07/anim07.sh
+++ doc/examples/anim07/anim07.sh
@@ -5,7 +5,7 @@
 # by position relative to an artificial sun in the east.  A progress slice is added as well.
 # We add a 1 second fade in and a 1 second fade out for the animation
 # DEM:   @earth_relief_02m
-# Ages:   @earth_age_02m.grd
+# Ages:  @earth_age_02m
 # The resulting movie was presented at the Fall 2019 AGU meeting in an eLighting talk:
 # P. Wessel, 2019, GMT science animations for the masses, Abstract IN21B-11.
 # The finished movie is available in our YouTube channel as well (without fading):
@@ -19,8 +19,13 @@ gmt begin
 	gmt math -T-12/372/0.5 -I T 5 SUB = longitudes.txt
 	# Extract a topography CPT
 	gmt makecpt -Cdem2 -T0/6000 -H > z.cpt
+	# Extract a Age CPT
+	gmt makecpt -C@age_chrons_GTS2012_2020.cpt -H > t.cpt
+	# Get single local global grids for DEM and crustal ages
+	gmt grdcut -Rg @earth_relief_02m -GDEM2m.grd
+	gmt grdcut -Rg @earth_age_02m -GAGE2m.grd
 	# Get gradients of the relief from N45E
-	gmt grdgradient @earth_relief_02m -Nt1.2 -A45 -Gintens.grd
+	gmt grdgradient DEM2m.grd -Nt1.2 -A45 -Gintens.grd
 gmt end
 EOF
 # 2. Set up main script
@@ -31,14 +36,14 @@ gmt begin
 	# Fake simulation of sun illumination from east added to relief intensities
 	gmt grdmath intens.grd X \${MOVIE_COL1} SUB SIND 0.8 MUL ADD 0.2 SUB = s.nc
 	# Plot age grid first using EarthByte age cpt
-	gmt grdimage @earth_age_02m.grd+s0.01 -Is.nc -C@crustal_age.cpt -JG\${MOVIE_COL0}/15/10.8c -X0 -Y0
+	gmt grdimage AGE2m.grd -Is.nc -JG\${MOVIE_COL0}/15/10.8c -Ct.cpt -X0 -Y0
 	# Clip to expose land areas only
 	gmt coast -G -Di
 	# Overlay relief over land only using dem cpt
-	gmt grdimage @earth_relief_02m -Is.nc -Cz.cpt
+	gmt grdimage DEM2m.grd -Is.nc -Cz.cpt
 	# Undo clipping and overlay gridlines
 	gmt coast -Q -B30g30
-gmt end
+gmt end show
 EOF
 # 3. Run the movie, requesting a fade in/out via white
 gmt movie main.sh -Sbpre.sh -C10.8cx10.8cx100 -Tlongitudes.txt -Nanim07 -Lf -K+gwhite -H8 -Pa+w1c+Gwhite -Fmp4 -V -W -Zs
