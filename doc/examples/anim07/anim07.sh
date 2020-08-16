1041169ce7ad924e20cd3bb52ca267c96eaa02f1
Paul Wessel <pwessel@hawaii.edu>
2020-08-15 17:06:17 -1000

    Simplify anim07 (#3977)
    * Update anim07.sh
    
    * Update anim07.sh
    
    Simplify the movie using 6m grids so it only takes 20 minutes to complete.
    
    * Update anim07.sh

--- doc/examples/anim07/anim07.sh
+++ doc/examples/anim07/anim07.sh
@@ -4,13 +4,14 @@
 # and topographic relief on land, with shading given by the global relief and modified
 # by position relative to an artificial sun in the east.  A progress slice is added as well.
 # We add a 1 second fade in and a 1 second fade out for the animation
-# DEM:   @earth_relief_02m
-# Ages:   @earth_age_02m.grd
-# The resulting movie was presented at the Fall 2019 AGU meeting in an eLighting talk:
+# DEM:   @earth_relief_06m
+# Ages:  @earth_age_06m
+# A similar movie was presented at the Fall 2019 AGU meeting in an eLighting talk:
 # P. Wessel, 2019, GMT science animations for the masses, Abstract IN21B-11.
 # The finished movie is available in our YouTube channel as well (without fading):
 # https://youtu.be/KfBwQlyjz5w
-# The movie took ~2.5 hours to render on a 24-core MacPro 2013.
+# The movie takes about ~20 minutes to render on a 24-core MacPro 2013.
+# The higher resolution movie on YouTube used the 02m data set resolution.
 
 # 1. Create background plot and data files needed in the loop
 cat << EOF > pre.sh
@@ -20,7 +21,7 @@ gmt begin
 	# Extract a topography CPT
 	gmt makecpt -Cdem2 -T0/6000 -H > z.cpt
 	# Get gradients of the relief from N45E
-	gmt grdgradient @earth_relief_02m -Nt1.2 -A45 -Gintens.grd
+	gmt grdgradient @earth_relief_06m -Nt1.2 -A45 -Gintens.grd
 gmt end
 EOF
 # 2. Set up main script
@@ -31,14 +32,14 @@ gmt begin
 	# Fake simulation of sun illumination from east added to relief intensities
 	gmt grdmath intens.grd X \${MOVIE_COL1} SUB SIND 0.8 MUL ADD 0.2 SUB = s.nc
 	# Plot age grid first using EarthByte age cpt
-	gmt grdimage @earth_age_02m.grd+s0.01 -Is.nc -C@crustal_age.cpt -JG\${MOVIE_COL0}/15/10.8c -X0 -Y0
+	gmt grdimage @earth_age_06m -Is.nc -JG\${MOVIE_COL0}/15/10.8c -X0 -Y0
 	# Clip to expose land areas only
 	gmt coast -G -Di
 	# Overlay relief over land only using dem cpt
-	gmt grdimage @earth_relief_02m -Is.nc -Cz.cpt
+	gmt grdimage @earth_relief_06m -Is.nc -Cz.cpt
 	# Undo clipping and overlay gridlines
 	gmt coast -Q -B30g30
-gmt end
+gmt end show
 EOF
 # 3. Run the movie, requesting a fade in/out via white
 gmt movie main.sh -Sbpre.sh -C10.8cx10.8cx100 -Tlongitudes.txt -Nanim07 -Lf -K+gwhite -H8 -Pa+w1c+Gwhite -Fmp4 -V -W -Zs
