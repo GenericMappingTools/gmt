3c47299216bbc10f03e51de8a8ca30f55660a87e
Paul Wessel <pwessel@hawaii.edu>
2020-08-21 19:10:31 -1000

    Update doc/examples/ex32/ex32.bat
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- doc/examples/ex32/ex32.bat
+++ doc/examples/ex32/ex32.bat
@@ -22,7 +22,7 @@ gmt begin ex32
 	REM Now get the topography for the same area, mask out the oceans and store it as topo_32.nc.
 
 	gmt grdcut @earth_relief_30s_p %Rflag% -Gtopo_32.nc=ns
-	gmt grdcut @earth_mask_30s_p $Rflag -Gmask_32.nc=ns
+	gmt grdcut @earth_mask_30s_p %Rflag% -Gmask_32.nc=ns
 	gmt grdmath topo_32.nc mask_32.nc 0 GT 0 NAN MUL = topo_32.nc
 
 	REM The color map assigns "Reflex Blue" to the lower half of the 0-255 range and
