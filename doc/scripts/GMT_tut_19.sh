5467b1cbdd84a4ab2de7c669c0832b63f39ec8ce
Paul Wessel <pwessel@hawaii.edu>
2020-08-21 14:32:24 -1000

    Eliminate subsets of topo from cache and use global grids
    Try to prune things like topo_32.nc when we can extract it on the fly.

--- doc/scripts/GMT_tut_19.sh
+++ doc/scripts/GMT_tut_19.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 gmt begin GMT_tut_19
 	gmt makecpt -Cdem2 -T1000/5000
-	gmt grdgradient @tut_relief.nc -Ne0.8 -A100 -fg -Gus_i.nc
-	gmt grdview tut_relief.nc -JM4i -p135/35 -Qi50 -Ius_i.nc -B -JZ0.5i -C
+	gmt grdcut @earth_relief_30s -R-108/-103/35/40 -Gtut_relief.nc
+	gmt grdgradient tut_relief.nc -Ne0.8 -A100 -fg -Gus_i.nc
+	gmt grdview tut_relief.nc -JM4i -p135/35 -Qi50 -Ius_i.nc -B -JZ0.5i
 gmt end show
