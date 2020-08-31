199c0e059b083f3a2c98386bddc04bb3b55a03ff
Paul Wessel <pwessel@hawaii.edu>
2020-08-30 14:10:38 -1000

    Regino for 3s tiles fails if not multiple of 2s
    See form post for details. This PR simply adds a failing test.

---
+++ test/grdimage/rounding.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Failing script while fixing https://forum.generic-mapping-tools.org/t/error-pygmt-gmtcliberror-module-grdimage-failed-with-status-code-78/829
+# Now reported as issue ###
+gmt begin rounding ps
+	gmt subplot begin 2x1 -SCb -SRl -Bwsne -Fs10c/0 -R3:57/4:18/44:00/44:15 -JM10c -X5c
+		gmt grdimage -R3:57/4:18/44:00/44:15 -JM? @earth_relief_03s -Cgeo -I -c0
+		gmt grdimage -R3.970/4.270/44.000/44.250 -JM? @earth_relief_03s -Cgeo -I -c1
+	gmt subplot end
+gmt end show
