ad16ae38722c73b45b7a366c325a045d02894d15
Joaquim Luis <jmfluis@gmail.com>
2020-09-01 11:44:39 +0100

    Skip record if gmt_create_array() returns error and thus prevent crash.

--- src/sample1d.c
+++ src/sample1d.c
@@ -458,7 +458,10 @@ EXTERN_MSC int GMT_sample1d (void *V_API, int mode, void *args) {
 				double min, max;
 				min = (Ctrl->T.T.delay[GMT_X]) ? ceil (S->data[Ctrl->N.col][0] / Ctrl->T.T.inc) * Ctrl->T.T.inc : Ctrl->T.T.min;
 				max = (Ctrl->T.T.delay[GMT_Y]) ? floor (S->data[Ctrl->N.col][S->n_rows-1] / Ctrl->T.T.inc) * Ctrl->T.T.inc : Ctrl->T.T.max;
-				gmt_create_array (GMT, 'T', &(Ctrl->T.T), &min, &max);
+				if (gmt_create_array(GMT, 'T', &(Ctrl->T.T), &min, &max) != GMT_NOERROR) {
+					GMT_Report(API, GMT_MSG_WARNING, "Segment %" PRIu64 " in table %" PRIu64 " had troubles.\n", seg, tbl);
+					continue;
+				}
 				m = Ctrl->T.T.n;
 				t_out = Ctrl->T.T.array;
 			}
