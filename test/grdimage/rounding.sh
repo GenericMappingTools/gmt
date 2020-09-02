afd0e0f1b0fe7e2300e3d70dcd33c8441faceec4
Dongdong Tian <seisman.info@gmail.com>
2020-09-02 01:49:40 -0400

    Remove one unnecessary comment

--- test/grdimage/rounding.sh
+++ test/grdimage/rounding.sh
@@ -2,7 +2,6 @@
 # Failing script while fixing https://forum.generic-mapping-tools.org/t/error-pygmt-gmtcliberror-module-grdimage-failed-with-status-code-78/829
 # Now reported as issue https://github.com/GenericMappingTools/gmt/pull/4130
 
-# Make the plot
 gmt begin rounding ps
 	gmt subplot begin 2x1 -SCb -SRl -Bwsne -Fs10c/0 -R3:57/4:18/44:00/44:15 -JM10c -X5c
 		gmt grdimage -R3:57/4:18/44:00/44:15 -JM? @earth_relief_03s -Cgeo -I -c0
