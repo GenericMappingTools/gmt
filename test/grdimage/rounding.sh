e5acedcd073b92ac82e0dbb5e7f5a7ead5232250
Paul Wessel <pwessel@hawaii.edu>
2020-09-01 20:15:13 -1000

    Region for 3s tiles fails if not multiple of 3s (#4124)
    * Regino for 3s tiles fails if not multiple of 2s
    
    See form post for details. This PR simply adds a failing test.
    
    * Add orig PS
    
    * Pre-download two 3s tiles
    
    * Cache two tiles of earth_relief_03s data
    
    * Remove one unnecessary comment
    
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

---
+++ test/grdimage/rounding.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Failing script while fixing https://forum.generic-mapping-tools.org/t/error-pygmt-gmtcliberror-module-grdimage-failed-with-status-code-78/829
+# Now reported as issue https://github.com/GenericMappingTools/gmt/pull/4130
+
+gmt begin rounding ps
+	gmt subplot begin 2x1 -SCb -SRl -Bwsne -Fs10c/0 -R3:57/4:18/44:00/44:15 -JM10c -X5c
+		gmt grdimage -R3:57/4:18/44:00/44:15 -JM? @earth_relief_03s -Cgeo -I -c0
+		gmt grdimage -R3.970/4.270/44.000/44.250 -JM? @earth_relief_03s -Cgeo -I -c1
+	gmt subplot end
+gmt end show
