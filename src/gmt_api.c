457e590ba8765c722e287249d73ff1fc109389a9
Paul Wessel <pwessel@hawaii.edu>
2020-09-02 20:48:10 -1000

    Update gmt_api.c

--- src/gmt_api.c
+++ src/gmt_api.c
@@ -11028,9 +11028,9 @@ int GMT_Call_Module (void *V_API, const char *module, int mode, void *args) {
 		gmt_manage_workflow (API, GMT_USE_WORKFLOW, NULL);		/* First detect and set modern mode if modern mode session dir is found */
 		gmtapi_check_for_modern_oneliner (API, module, mode, args);	/* If a modern mode one-liner we must switch run--mode here */
 		if (API->external && gmt_M_is_verbose (API->GMT, GMT_MSG_INFORMATION)) {
-			/* For externals, print the equivalent command line string under -Vi */
+			/* For externals only, print the equivalent command-line string under -Vi */
 			char *text = (mode == GMT_MODULE_OPT) ? GMT_Create_Cmd (API, args) : args;
-			GMT_Report (API, GMT_MSG_INFORMATION, "GMT_Call_Command string: %s\n", text);
+			GMT_Report (API, GMT_MSG_INFORMATION, "GMT_Call_Command string: %s %s\n", module, text);
 			if (mode == GMT_MODULE_OPT) GMT_Destroy_Cmd (API, &text);
 		}
 		status = (*p_func) (V_API, mode, args);				/* Call the module in peace */
