d49a3900e18b19990c5080dc1beaef98366e513a
Paul Wessel <pwessel@hawaii.edu>
2020-09-03 19:37:02 -1000

    Set width first and foremost.

--- src/pshistogram.c
+++ src/pshistogram.c
@@ -931,17 +931,16 @@ EXTERN_MSC int GMT_pshistogram (void *V_API, int mode, void *args) {
 		Ctrl->T.T.max = F.T->inc * ceil  (F.wesn[XHI] / F.T->inc);
 	}
 
-	if (GMT->common.l.active) {	/* Can we do auto-legend? */
-		/* Always plot rectangle, possibly degenerated to square, using fill and pen */
+	if (GMT->common.l.active) {	/* Add auto-legend entry */
+		/* Always plot a 3:2 width:height rectangle, possibly via user sizing, using fill and pen */
 		struct GMT_SYMBOL S;
-		double scl = (GMT->common.l.item.scale == 0.0) ? 1.5 : GMT->common.l.item.scale;
 		gmt_M_memset (&S, 1U, struct GMT_SYMBOL);
 		S.symbol = PSL_RECT;
-		if (GMT->common.l.item.size == 0.0)	/* Select default height given by annotation height */
-			GMT->common.l.item.size = GMT_LET_HEIGHT * GMT->current.setting.font_annot[GMT_PRIMARY].size * GMT->session.u2u[GMT_PT][GMT_INCH];
-		S.size_x = scl * GMT->common.l.item.size;
-		S.size_y = GMT->common.l.item.size;
-		GMT->common.l.item.scale = GMT->common.l.item.size = 0.0;	/* Reset if set */
+		if (GMT->common.l.item.size == 0.0)	/* Select default width given by annotation height scaled by actual fractional height times 1.5 */
+			S.size_x = 1.5 * GMT_LET_HEIGHT * GMT->current.setting.font_annot[GMT_PRIMARY].size * GMT->session.u2u[GMT_PT][GMT_INCH];
+		else	/* Use given size as rectangle width */
+			S.size_x = GMT->common.l.item.size;
+		S.size_y = S.size_x / 1.5;	/* Width to height ratio is 3:2 */
 		gmt_add_legend_item (API, &S, Ctrl->G.active, &(Ctrl->G.fill), Ctrl->W.active, &(Ctrl->W.pen), &(GMT->common.l.item));
 	}
 
