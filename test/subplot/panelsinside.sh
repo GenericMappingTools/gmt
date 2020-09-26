9bb5b0947076f69c815ca6a5d7da5786f02618ae
Paul Wessel <pwessel@hawaii.edu>
2020-09-25 17:48:22 -1000

    Add simple test for inside map frame type in subplots

--- test/subplot/panelsinside.sh
+++ test/subplot/panelsinside.sh
@@ -2,7 +2,7 @@
 # Test very minimal 2x2 basemap matrix under inside frame type
 gmt begin panelsinside ps
   gmt set MAP_FRAME_TYPE inside
-  gmt subplot begin 2x2 -Fs3i -M5p -A -SCb -SRl -Bwstr -R0/80/0/10
+  gmt subplot begin 2x2 -Fs3i -M5p -A -SCb -SRl -Bwstr -R0/80/0/10 -T"Inside annotations"
     gmt basemap
     gmt basemap -c
     gmt basemap -c
