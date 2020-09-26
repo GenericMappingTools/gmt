19b993948770fbf9b9a20dd5656018c9bbcbedb8
Paul Wessel <pwessel@hawaii.edu>
2020-09-25 17:44:13 -1000

    Address the effect of MAP_FRAME_TYPE=inside on subplots
    First effort is to measure margins correctly.

---
+++ test/subplot/panelsinside.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Test very minimal 2x2 basemap matrix under inside frame type
+gmt begin panelsinside ps
+  gmt set MAP_FRAME_TYPE inside
+  gmt subplot begin 2x2 -Fs3i -M5p -A -SCb -SRl -Bwstr -R0/80/0/10
+    gmt basemap
+    gmt basemap -c
+    gmt basemap -c
+    gmt basemap -c
+  gmt subplot end
+gmt end show
