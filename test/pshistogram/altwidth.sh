1446a8f90c216946f6d144e714853c912c6daa37
Paul Wessel <pwessel@hawaii.edu>
2020-09-27 14:47:54 -1000

    Update altwidth.sh

--- test/pshistogram/altwidth.sh
+++ test/pshistogram/altwidth.sh
@@ -4,13 +4,17 @@ gmt begin altwidth ps
 	gmt subplot begin 3x2 -Fs7c -SCb -SRl -R3/7/0/8 -BWSrt -A
 		gmt subplot set -A"original"
 		gmt histogram  hist.txt -T0.2 -Gred -W0.5p
-		gmt subplot set -A"New x-width"
+		gmt subplot set -A"Width 0.1"
 		gmt histogram  hist.txt -T0.2 -Gred -W0.5p -E0.1
-		gmt histogram  hist.txt -T0.2 -Gred -W0.5p -E0.1+o-0.05 -c
+		gmt subplot set -A"Width 0.1 and offset -0.05"
+		gmt histogram  hist.txt -T0.2 -Gred -W0.5p -E0.1+o-0.05
 		gmt histogram  hist.txt -T0.2 -Ggreen -W0.5p -E0.1+o0.05
-		gmt histogram  hist.txt -T0.2 -Gred -W0.5p -E0.2c -c
-		gmt histogram  hist.txt -T0.2 -Gred -W0.5p -E0.2c+o-0.1c -c
-		gmt histogram  hist.txt -T0.2 -Gred -W0.5p -E0.2c+o-0.1c -c
+		gmt subplot set -A"Width 0.2c"
+		gmt histogram  hist.txt -T0.2 -Gred -W0.5p -E0.2c
+		gmt subplot set -A"Width 0.2c and offset -0.1c"
+		gmt histogram  hist.txt -T0.2 -Gred -W0.5p -E0.2c+o-0.1c
+		gmt subplot set -A"Width 0.2c and offset -0.1c"
+		gmt histogram  hist.txt -T0.2 -Gred -W0.5p -E0.2c+o-0.1c
 		gmt histogram  hist.txt -T0.2 -Ggreen -W0.5p -E0.2c+o0.1c
 	gmt subplot end
 gmt end show
