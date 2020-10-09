c187a7db5d0add08fa08da4da89c901e7e21fd1a
Paul Wessel <pwessel@hawaii.edu>
2020-10-09 09:10:07 -1000

    Update ErrorEllipseGeo.sh

--- test/psxy/ErrorEllipseGeo.sh
+++ test/psxy/ErrorEllipseGeo.sh
@@ -3,9 +3,9 @@
 
 ps=ErrorEllipseGeo.ps
 
-gmt psxy -R-280/80/-70/70 -JM6i -P -K -W3p,blue -Gred -SE -N <<END > $ps
+gmt psxy -R-280/80/-70/70 -JM6i -P -K -W3p,blue -Gred -SE <<END > $ps
 50	0	45	12000	6000
 END
-gmt psxy -R -J -O -W2p -Ggreen -SE -N -Baf -B+t"Red ellipse should repeat at west boundary" <<END >> $ps
+gmt psxy -R -J -O -W2p -Ggreen -SE -Baf -B+t"Red ellipse should repeat at west boundary" <<END >> $ps
 50	0	-45	5000	3000
 END
