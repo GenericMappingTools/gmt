5e28792bca798e7322fc333ee40a864886feae85
Joaquim Luis <jmfluis@gmail.com>
2020-10-17 14:17:38 +0100

    Add the GMT general header/license comment

--- src/seis/shake.c
+++ src/seis/shake.c
@@ -1,24 +1,25 @@
 /*--------------------------------------------------------------------
  *
- *	Copyright (c) 2004-2012 by J. Luis
+ *	Copyright (c) 1991-2020 by the GMT Team (https://www.generic-mapping-tools.org/team.html)
+ *	See LICENSE.TXT file for copying and redistribution conditions.
  *
- * 	This program is part of Mirone and is free software; you can redistribute
- * 	it and/or modify it under the terms of the GNU Lesser General Public
- * 	License as published by the Free Software Foundation; either
- * 	version 2.1 of the License, or any later version.
- * 
- * 	This program is distributed in the hope that it will be useful,
- * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
- * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * 	Lesser General Public License for more details.
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU Lesser General Public License as published by
+ *	the Free Software Foundation; version 3 or any later version.
  *
- *	Contact info: w3.ualg.pt/~jluis/mirone
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Lesser General Public License for more details.
+ *
+ *	Contact info: www.generic-mapping-tools.org
  *--------------------------------------------------------------------*/
+/*
 
 /*--------------------------------------------------------------------
- * Mex function to compute Peak Ground Acceleration/Velocity and Intensity
+ * Compute Peak Ground Acceleration/Velocity and Intensity
  * Author:	J. Luis, after the FORTRAN version from J M Miranda
- * Date: 	06-Apr-2010
+ * Date: 	Original from 06-Apr-2010 for Mirone usage. Adapted to GMT in 2020
  *
  */ 
 
