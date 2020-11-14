110266942557eb840493b035cad653c963b2c0ce
Paul Wessel <pwessel@hawaii.edu>
2020-11-13 17:03:50 -1000

    Update testapi_datacube.c

--- src/testapi_datacube.c
+++ src/testapi_datacube.c
@@ -15,7 +15,7 @@ int main () {
  	/* Create a blank matrix container that will hold our user in_data */
 	if ((C = GMT_Read_Data (API, GMT_IS_DATACUBE, GMT_IS_FILE, GMT_IS_VOLUME, GMT_CONTAINER_AND_DATA, NULL, "cube.nc", NULL)) == NULL) return (EXIT_FAILURE);
 	/* Hook the user input array up to this container */
-	if (C = GMT_Write_Data (API, GMT_IS_DATACUBE, GMT_IS_FILE, GMT_IS_VOLUME, GMT_CONTAINER_AND_DATA, NULL, "cube-out.nc", NULL)) return (EXIT_FAILURE);
+	if (GMT_Write_Data (API, GMT_IS_DATACUBE, GMT_IS_FILE, GMT_IS_VOLUME, GMT_CONTAINER_AND_DATA, NULL, "cube-out.nc", C)) return (EXIT_FAILURE);
 	/* Destroy session, which will free all GMT-allocated memory */
 	if (GMT_Destroy_Session (API)) return EXIT_FAILURE;
 	exit (0);
