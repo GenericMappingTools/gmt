d29f29012ecd84317e691ddb2e7297ef4748487d
Paul Wessel <pwessel@hawaii.edu>
2020-11-15 09:54:53 -1000

    Update gspline_4.sh

--- test/greenspline/gspline_4.sh
+++ test/greenspline/gspline_4.sh
@@ -6,9 +6,9 @@ ps=gspline_4.ps
 # Figure 5 in Wessel, P. (2009), A general-purpose Green's function-based
 #	interpolator, Computers & Geosciences, 35, 1247-1254.
 
-R3D=5/40/-5/10/5/16
 R2D=12/32/0/6
 Z=5/10
+R3D=$R2D/$Z
 dz=0.25
 view=200/25
 method=r
@@ -24,7 +24,7 @@ let k=0
 while read grid; do
 	z=$(gmt math -Q 5 $k $dz MUL ADD =)
 #	echo "Doing z = $z"
-	gmt grdcontour $grid -JX -C10 -L9/11 -S8 -Ddump
+	gmt grdcontour -JX $grid -C10 -L9/11 -S8 -Ddump
 	$AWK '{if ($1 == ">") {print $0} else {print $1, $2, '$z'}}' dump > tmp
 	gmt psxyz -R$R2D/$Z -JX -JZ -p$view -O -K tmp -Gp39+r300+fgray+b- -Wthin >> $ps
 	let k=k+1
