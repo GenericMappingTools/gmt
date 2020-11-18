293a2f0763ed0f70bdf239c2e49d2a3e6d620c1e
Paul Wessel <pwessel@hawaii.edu>
2020-11-17 17:28:47 -1000

    fix tests

--- test/api/apicubeplot.sh
+++ test/api/apicubeplot.sh
@@ -2,5 +2,8 @@
 #
 # Test the C API for reading via a matrix and making a plot
 ps=apicubeplot.ps
+if [ ! -f cube.nc ]; then
+	cp "${src}"/../grdinterpolate/cube.nc .
+fi
 testapi_cube 
-gmt grdcontour newlayer.nc -JX15c -C5 -A10 -Baf -P > t.ps
+gmt grdcontour newlayer.nc -JX15c -C5 -A10 -Baf -P > $ps
