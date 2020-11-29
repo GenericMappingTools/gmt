b1364c24e0af513867a229e280dec896e0e7543a
Paul Wessel <pwessel@hawaii.edu>
2020-11-28 14:58:50 -1000

    Make sure past color and pen settings are reset before basemap
    Because modules like plot can set pens that may remain the default, we need to wipe that memory so that the basemap pens can be always be set as they are.  This PR also fixes subtle bugs related to the completion PS functions regarding textbox outline and fill memories.

--- src/gmt_plot.c
+++ src/gmt_plot.c
@@ -2597,7 +2597,7 @@ GMT_LOCAL void gmtplot_map_boundary (struct GMT_CTRL *GMT) {
 	w = GMT->common.R.wesn[XLO], e = GMT->common.R.wesn[XHI], s = GMT->common.R.wesn[YLO], n = GMT->common.R.wesn[YHI];
 
 	PSL_comment (PSL, "Start of map frame\n");
-	PSL_command (PSL, "0 A [] 0 B\n");	/* Also ensure full reset to black solid pen color */
+
 	gmt_setpen (GMT, &GMT->current.setting.map_frame_pen);
 	PSL_setcolor (PSL, GMT->current.setting.map_frame_pen.rgb, PSL_IS_STROKE);
 
@@ -5652,6 +5652,11 @@ void gmt_map_basemap (struct GMT_CTRL *GMT) {
 
 	PSL_setdash (PSL, NULL, 0);	/* To ensure no dashed pens are set prior */
 
+	/* These three commands resets the memory of PSL regarding pen width, color, and outline */
+	gmt_M_memcpy (PSL->current.rgb[PSL_IS_STROKE], GMT->session.no_rgb, 3, double);	/* Reset to -1,-1,-1 so it can be reset below */
+	PSL->current.linewidth = -1.0;	/* For a reset of internal setting in PSL */
+	PSL->current.outline = -1;		/* Will now be changed by first PSL_setfill */
+
 	if (GMT->current.proj.three_D && GMT->current.map.frame.drawz) GMT->current.map.frame.plotted_header = true;	/* Just so it is not plotted by gmtplot_map_boundary first */
 
 	if (GMT->current.proj.got_azimuths) gmt_M_uint_swap (GMT->current.map.frame.side[E_SIDE], GMT->current.map.frame.side[W_SIDE]);	/* Temporary swap to trick justify machinery */
@@ -8116,7 +8121,7 @@ struct PSL_CTRL *gmt_plotinit (struct GMT_CTRL *GMT, struct GMT_OPTION *options)
 			plot_y -= (P->dy);
 			PSL_command (PSL, "/PSL_plot_completion {\nV\n");
 			PSL_comment (PSL, "Start of panel tag for panel (%d,%d)\n", P->row, P->col);
-			PSL_comment (PSL, "Will not execute until end of panel\n");
+			PSL_comment (PSL, "Will not execute until end of panel\nFQ O0\n");
 			PSL_setorigin (PSL, GMT->current.setting.map_origin[GMT_X] - P->gap[XLO], GMT->current.setting.map_origin[GMT_Y] - P->gap[YLO], 0.0, PSL_FWD);
 			justify = gmt_just_decode (GMT, P->justify, PSL_NO_DEF);	/* Convert XX refpoint code to PSL number */
 			gmt_smart_justify (GMT, justify, 0.0, P->off[GMT_X], P->off[GMT_Y], &plot_x, &plot_y, 1);	/* Shift as requested */
@@ -8129,6 +8134,7 @@ struct PSL_CTRL *gmt_plotinit (struct GMT_CTRL *GMT, struct GMT_OPTION *options)
 				int outline = 0;
 				struct GMT_FILL fill;
 				gmt_init_fill (GMT, &fill, -1.0, -1.0, -1.0);	/* No fill */
+				PSL_command (PSL, "FQ O0\n");	/* Ensure fill/pen have been reset */
 				if (P->pen[0]) {	/* Want to draw outline of tag box */
 					struct GMT_PEN pen;
 					gmt_M_memset (&pen, 1, struct GMT_PEN);
@@ -8194,6 +8200,7 @@ struct PSL_CTRL *gmt_plotinit (struct GMT_CTRL *GMT, struct GMT_OPTION *options)
 			if (!(PP[0] == '-' && FF[0] == '-')) {	/* Requested textbox fill and/or outline */
 				int outline = 0;	/* No outline */
 				struct GMT_FILL fill;
+				PSL_command (PSL, "FQ O0\n");	/* Ensure fill/pen have been reset */
 				gmt_init_fill (GMT, &fill, -1.0, -1.0, -1.0);	/* Initialize to no fill */
 				if (PP[0] != '-') {	/* Want to draw outline of tag box */
 					struct GMT_PEN pen;
