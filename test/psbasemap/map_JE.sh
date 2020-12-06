74efdca0d1f918122fce5e1472291a50588ce6e6
Dongdong Tian <seisman.info@gmail.com>
2020-12-06 00:44:45 -0500

    Be able to exclude tests that are not fail on all platforms (#4543)
    Some tests are known to fail, and we have the GMT_KNOWN_FAILURE flag to exclude those tests.
    
    A few tests pass on macOS, but fail on Linux or Windows. For example, gmtmex/WL_example_3.sh fails on Linux and Windows, and grdvector/bothg.sh fails on Windows.
    
    This PR allows us to skip a few tests on specific platforms, by adding flags GMT_KNOWN_FAILURE_WINDOW, GMT_KNOWN_FAILURE_LINUX, and/or GMT_KNOWN_FAILURE_MACOS, so that we can make all CI jobs green.
    
    Please note that these flags only work for CI and have no effects for local tests.
    
    I also add these new flags to some long-standing failures. Now the only two failures are gspline_4.sh and gdal_nn.sh.

--- test/psbasemap/map_JE.sh
+++ test/psbasemap/map_JE.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # Check gridlines for non-global -JE map
+#
+# GMT_KNOWN_FAILURE_WINDOWS
+#
 ps=map_JE.ps
 
 gmt psbasemap -B10g10 -Je-70/-90/1:10000000 -R-95/-75/-60/-55r -P -Xc > $ps
