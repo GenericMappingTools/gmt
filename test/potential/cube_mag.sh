9e347d771a883ebc2d7202b0f09c11e6419ed782
Joaquim Luis <jmfluis@gmail.com>
2020-12-16 23:11:10 +0000

    Update to -M+s form

--- test/potential/cube_mag.sh
+++ test/potential/cube_mag.sh
@@ -4,7 +4,7 @@
 
 ps=cube_mag.ps
 
-gmt gmtgravmag3d -R-15/15/-15/15 -I1 -H10/60/10/-10/40 -M+prism,1/1/1/-5 -Gcube_mag.grd
+gmt gmtgravmag3d -R-15/15/-15/15 -I1 -H10/60/10/-10/40 -M+sprism,1/1/1/-5 -Gcube_mag.grd
 
 gmt grd2cpt cube_mag.grd -E20 -D > m.cpt
 gmt grdimage cube_mag.grd -Cm.cpt -JX12c -Ba -P > $ps
