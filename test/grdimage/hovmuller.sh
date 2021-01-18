14e1c8a4d343f446a6268acbde9f6fc8f98fd7a5
Paul Wessel <pwessel@hawaii.edu>
2021-01-17 16:33:54 -1000

    Improved parsing of grid increments (#4666)
    * Ignore bad units in -I if not geographic data
    
    For an -R with thousands of meters, let -I30e be ignored with a warning that e is not necessary.
    
    * Update docs and fix quit-n-dirty -I in the hovmuller test
    
    The -I10/5m relied on the fact that GMT did not worry that you gave a minute-unit to a y-data that is in years and you wanted 1/60 of the year, not "minutes".
    
    * Update gmt_support.c
    
    * Update explain_-I.rst_

--- test/grdimage/hovmuller.sh
+++ test/grdimage/hovmuller.sh
@@ -9,5 +9,5 @@ gmt set MAP_ANNOT_ORTHO ""
 
 gmt makecpt -Crainbow -T-1/1/0.05 > tmp.cpt
 # Make a fake longitude/time grid
-gmt grdmath -R-180/180/0/3 -f0x,1t -I10/5m X SIND Y 2 MUL PI MUL SIN MUL = tmp.nc
+gmt grdmath -R-180/180/0/3 -f0x,1t -I10/37+n X SIND Y 2 MUL PI MUL SIN MUL = tmp.nc
 gmt grdimage tmp.nc -Ctmp.cpt -JX15c/22cT -Bx30f10 -By1O -Bsy1Y -E100 -P -Xc > $ps
