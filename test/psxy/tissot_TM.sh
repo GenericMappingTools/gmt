43f681fa9114578ad8a9fb290baf193895e98354
Paul Wessel <pwessel@hawaii.edu>
2021-01-22 10:23:48 -1000

    Update tissot_TM.sh

--- test/psxy/tissot_TM.sh
+++ test/psxy/tissot_TM.sh
@@ -3,4 +3,4 @@
 # Demonstrate failure to clip ellipses against periodic TM boundary in y.
 # See https://forum.generic-mapping-tools.org/t/how-to-avoid-the-funky-look-of-jt-and-se/1224
 ps=tissot_TM.ps
-echo 30 -60 2000k | gmt psxy -R0/360/-80/80 -JT-15/15/16c -Bafg -SE- -Gred@50 -Wred -P > $ps
+echo 30 -60 2000k | gmt psxy -R0/360/-80/80 -JT-15/15/16c -Bafg -SE- -Gred -Wthin -P > $ps
