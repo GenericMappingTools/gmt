db0e920657ebee3d846df135b288ba37afe920b9
Paul Wessel <pwessel@hawaii.edu>
2021-01-30 13:50:07 -1000

    Update gmt_map.c

--- src/gmt_map.c
+++ src/gmt_map.c
@@ -6669,12 +6669,14 @@ void gmt_auto_frame_interval (struct GMT_CTRL *GMT, unsigned int axis, unsigned 
 	}
 	f *= GMT->session.u2u[GMT_INCH][GMT_PT];	/* Change to points */
 
+#ifndef NO_THEMES
 	if (GMT->current.setting.run_mode == GMT_MODERN && gmt_M_axis_is_geo (GMT, axis)) {	/* Need more space for degree symbol and WESN letters not considered in the algorithm */
 		if (strchr (GMT->current.setting.format_geo_map, 'F'))	/* Need more space for degree symbol and letter */
 			d *= 1.75;
 		else	/* Just more space for degree symbol */
 			d *= 1.25;
 	}
+#endif
 
 	/* First guess of interval */
 	d *= MAX (0.05, MIN (5.0 * GMT->current.setting.font_annot[item].size / f, 0.20));
