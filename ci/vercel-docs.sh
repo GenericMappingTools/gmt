cae695eb88ace184fdb7ac4eb570dae47a2b56fc
Dongdong Tian <seisman.info@gmail.com>
2021-03-06 23:54:32 -0500

    sphinx-panels is no longer a dependency

--- ci/vercel-docs.sh
+++ ci/vercel-docs.sh
@@ -10,7 +10,7 @@ set -x -e
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install cmake3 ninja-build libcurl-devel netcdf-devel
 # Install Python packages
-pip install -r doc/rst/requirements.txt
+pip install sphinx
 
 # Install latest gs
 curl -SLO https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9533/ghostscript-9.53.3-linux-x86_64.tgz
