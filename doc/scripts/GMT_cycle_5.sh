fe36b36803295feaed5e2b875e050fadbb71ef8b
Meghan Jones <meghanj@hawaii.edu>
2021-03-11 19:09:45 -0500

    Update failing tests

--- doc/scripts/GMT_cycle_5.sh
+++ doc/scripts/GMT_cycle_5.sh
@@ -5,5 +5,5 @@ gmt begin GMT_cycle_5
 	gmt set GMT_THEME cookbook
 	gmt xyz2grd @mississippi.txt -i0,1+s1e-3,1+s1e-3 -wy -R0/1/0/50 -I50+n -r -Gtmp.grd
  	gmt grdimage tmp.grd -JX15c/8c -BWSen -Bxaf+l"Normalized year" -Byaf+l"Discharge (10@+3@+ m@+3@+/s)"
- 	gmt colorbar -DJRM
+ 	gmt colorbar -DJRM -Baf
 gmt end show
