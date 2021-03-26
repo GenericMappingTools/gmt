85d8b40a60ca13225bff03cdd05febdc3eea03f0
Paul Wessel <pwessel@hawaii.edu>
2021-03-26 11:50:54 -1000

    Update vonmises.sh

--- test/gmtmath/vonmises.sh
+++ test/gmtmath/vonmises.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Testing gmt math for VPDF
-# Red dots are output from MATLAB for comparison
 ps=vonmises.ps
 gmt math -T0/360/1 T 150 10 VPDF = | gmt psxy -R0/360/0/1.5 -JX6i/6i -Bxaf+l"Angles" -Byaf+l"PDF" -BWSne+t"Von Mises probability density function" -W0.25p,red -P -K > $ps
 gmt math -T0/360/1 T 150 4 VPDF = | gmt psxy -R -J -W0.5p,cyan -O -K >> $ps
