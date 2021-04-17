fbe05ecb15418e9127b414bcdc5157b7cfc4aee1
Paul Wessel <pwessel@hawaii.edu>
2021-04-16 15:04:46 -1000

    Update gmtmex.c

--- src/gmtmex/gmtmex.c
+++ src/gmtmex/gmtmex.c
@@ -1885,7 +1885,7 @@ void GMT_mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]
 				if (X[kk].object == ppp) X[kk].object = NULL;
 		}
 	}
-	free (X);
+	GMT_Free (API, &X);
 
 	/* 9. Destroy linked option list */
 
