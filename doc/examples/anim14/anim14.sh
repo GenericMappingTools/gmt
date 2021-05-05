575886e6fe8b4a73262d99bd2e20dedf20d6f095
Federico Esteban <federico.esteban@gmail.com>
2021-05-05 14:52:17 -0300

    Update doc/examples/anim14/anim14.sh
    Co-authored-by: Meghan Jones <meghanj@alum.mit.edu>

--- doc/examples/anim14/anim14.sh
+++ doc/examples/anim14/anim14.sh
@@ -10,7 +10,7 @@
 # The original data file jan76_dec17.ndk can be downloaded and rearranged to a gmt format with the followings commands:
 # URL="https://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/jan76_dec17.ndk"
 # gmt which $URL -G 
-# gawk '/^PDE/ {Date=$2; Time=$3; Lat=$4; Long=$5; Depth=$6; getline; Name=$1; getline; getline; Exp=$1; getline; mant=$11; strike1=$12; dip1=$13; rake1=$14; strike2=$15; dip2=$16; rake2=$17; print Long, Lat, Depth, strike1, dip1, rake1, strike2, dip2, rake2, Exp, Date "T" Time, Name}' jan76_dec17.ndk | sed 's/\//-/g' > meca.gmt
+# gawk '/^PDE/ {Date=$2; Time=$3; Lat=$4; Long=$5; Depth=$6; getline; Name=$1; getline; getline; Exp=$1; getline; mant=$11; strike1=$12; dip1=$13; rake1=$14; strike2=$15; dip2=$16; rake2=$17; print Long, Lat, Depth, strike1, dip1, rake1, strike2, dip2, rake2, mant, Exp, Date "T" Time, Name}' jan76_dec17.ndk | sed 's/\//-/g' > meca.gmt
 # The meca.gmt file is in the GMT Dataserver cache and can be accessed via @meca.gmt
 # The finished movie is available in our YouTube channel as well:
 # https://youtu.be/9D1qANCoptE
