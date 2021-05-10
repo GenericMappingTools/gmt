053bf4ec30367498eef6ec85d73075526209879b
Dongdong Tian <seisman.info@gmail.com>
2021-05-10 18:33:31 -0400

    Pin sphinx version to 3.5.4 (#5212)

--- ci/install-dependencies-macos.sh
+++ ci/install-dependencies-macos.sh
@@ -38,7 +38,7 @@ fi
 brew install ${packages}
 
 if [ "$BUILD_DOCS" = "true" ]; then
-	pip3 install --user docutils==0.16 sphinx
+	pip3 install --user docutils==0.16 sphinx==3.5.4
     # Add sphinx to PATH
     echo "$(python3 -m site --user-base)/bin" >> $GITHUB_PATH
 fi
