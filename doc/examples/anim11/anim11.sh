180bbe3fc64e85f6196489100277d077209a111f
Paul Wessel <pwessel@hawaii.edu>
2021-05-27 13:02:00 -1000

    Simplify animation scripts by using 'EOF' (#5271)
    * Simplify animation scripts by using 'EOF'
    
    See #5268 for background.  This PR implements the suggestion.
    
    * Fix some issues
    
    * restore anim14.sh

--- doc/examples/anim11/anim11.sh
+++ doc/examples/anim11/anim11.sh
@@ -14,12 +14,12 @@
 # The movie took ~2.75 hour to render on a 24-core MacPro 2013.
 
 # 1. Create background plot and data files needed in the loop
-cat << EOF > pre.sh
+cat << 'EOF' > pre.sh
 gmt begin
   # Set view longitudes 0-360 with steps of 0.25 degree
   gmt math -T-180/180/0.25 -o0 T = view.txt
   # Make global grid with a smooth 2-degree day/night transition for the 2020 solstice.
-  gmt grdmath -Rd -I2m -rp \$(gmt solar -C -o0:1 -I+d2020-06-20+z-10) 2 DAYNIGHT = daynight.grd
+  gmt grdmath -Rd -I2m -rp $(gmt solar -C -o0:1 -I+d2020-06-20+z-10) 2 DAYNIGHT = daynight.grd
   # We will create an intensity grid based on a DEM so that we can see structures in the oceans
   gmt grdgradient @earth_relief_02m -Nt0.75 -A45 -Gintens.grd
   # Make sure our remote files have been downloaded
@@ -27,7 +27,7 @@ gmt begin
 gmt end
 EOF
 # 2. Set up main script
-cat << EOF > main.sh
+cat << 'EOF' > main.sh
 gmt begin
   # Let HSV minimum value go to zero and faint map border
   gmt set COLOR_HSV_MIN_V 0 MAP_FRAME_PEN=faint
@@ -35,7 +35,7 @@ gmt begin
   # we get the daytime view, and then adjust colors based on the intensity.
   gmt grdmix @earth_day_02m @earth_night_02m -Wdaynight.grd -Iintens.grd -Gview.tif
   # Plot this image on an Earth with view from current longitude
-  gmt grdimage view.tif -JG\${MOVIE_COL0}/30N/21.6c -Bafg -X0 -Y0
+  gmt grdimage view.tif -JG${MOVIE_COL0}/30N/21.6c -Bafg -X0 -Y0
 gmt end
 EOF
 # 3. Run the movie, requesting a fade in/out via white
