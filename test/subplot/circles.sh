df7d86f36e0db84c48657436aaf47810f98d9469
Paul Wessel <pwessel@hawaii.edu>
2021-05-29 18:36:52 -1000

    Introduce +af|s and use +as in circles.sh
    This keeps new failures to a minimum.  circles.sh still fails because of fonts, not frame pen.

--- test/subplot/circles.sh
+++ test/subplot/circles.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# Testing gmt subplot 12 cm circles and framepen scaling
+# Testing gmt subplot 12 cm circles and framepen scaling via +as
 
 gmt begin circles ps
-  gmt subplot begin 2x1 -Fs12c -Baf -Rg -M0 -Y1.25c
+  gmt subplot begin 2x1 -Fs12c+as -Baf -Rg -M0 -Y1.25c
     gmt basemap -JG0/10/? -c
     gmt basemap -JG0/-10/? -c
   gmt subplot end
