5509b494ea0946cad00d4b3d1994a273c2a7dfb7
Paul Wessel <pwessel@hawaii.edu>
2021-06-15 08:51:45 -1000

    use remote file

--- doc/examples/ex53/ex53.sh
+++ doc/examples/ex53/ex53.sh
@@ -8,6 +8,7 @@
 gmt begin ex53 ps
 	gmt set PROJ_ELLIPSOID Sphere MAP_ANNOT_OBLIQUE lon_horizontal,lat_parallel,tick_normal FONT_TAG 10p PS_MEDIA letter
 	gmt makecpt -Cterra
+	data=$(gmt which -G @Top12Cities.txt)
 	gmt subplot begin 4x3 -Fs6c -A+gwhite+p0.25p+o0.2c+sblack@50 -Baf -R-1500/1500/-1500/1500+uk -X1c -Y0.9c
 		while read lon lat population iso name; do
 			gmt subplot set -A"${name} [${population}M]"
@@ -17,6 +18,6 @@ gmt begin ex53 ps
 			gmt clip -C
 			gmt coast -E${iso}+pfaint -N1/thinner,darkred
 			echo ${lon} ${lat} | gmt plot -Skcity/0.25c -Gred -W0.5p -B0
-		done < Top12Cities.txt
+		done < $data
 	gmt subplot end
 gmt end show
