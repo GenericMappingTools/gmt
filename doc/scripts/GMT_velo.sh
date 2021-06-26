b89cf1d39a451cd2f8e355e08c3885ab2206069d
Dongdong Tian <seisman.info@gmail.com>
2021-06-26 17:06:42 -0400

    Add bash shebang to GMT_velo.sh (#5393)

--- doc/scripts/GMT_velo.sh
+++ doc/scripts/GMT_velo.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 gmt begin GMT_velo
 	gmt velo <<- EOF -JM12c -R0/4/0/1 -Sw0.915c/5.e7 -W0.5p -Elightred -Glightblue -L0.5p -D2
 	0.5 0.493  5.65E-08 1.17E-08
@@ -6,7 +7,7 @@ gmt begin GMT_velo
 	1.25     0.21     3.0    3.0     2.0    1.0  0.300
 	EOF
 	gmt velo -Sn0.425c -Glightred -Elightred  -W4p,orange <<- EOF
-	2.3     0.215     3.0    4.0 
+	2.3     0.215     3.0    4.0
 	EOF
 	gmt velo -Sx0.405c -A0.5c+h0.5 -W2p,red <<- EOF
 	3.5     0.5     3.5    -2.5 30
