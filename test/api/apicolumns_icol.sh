eb4e8c606d633ee4ce80937c3605249d8b2b1f3a
Paul Wessel <pwessel@hawaii.edu>
2021-06-25 20:29:40 -1000

    Update apicolumns_icol.sh

--- test/api/apicolumns_icol.sh
+++ test/api/apicolumns_icol.sh
@@ -5,5 +5,6 @@
 # This file holds what is expected to be produced on output
 gmt set FORMAT_FLOAT_OUT %8.3f
 gmt convert @wus_gps_final_crowell.txt -i1,0,3,3+d2,2,5+s10,6+o10  > answer.txt
-testapi_columns > results.txt
-diff -q --strip-trailing-cr results.txt answer.txt > fail
+testapi_columns
+diff -q --strip-trailing-cr matrix.txt answer.txt > fail
+diff -q --strip-trailing-cr vector.txt answer.txt >> fail
