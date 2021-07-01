6800f4d0e4206c5581c015a07e6c2996fd7b2682
Paul Wessel <pwessel@hawaii.edu>
2021-06-30 17:33:19 -1000

    Update src/blockmode.c
    Co-authored-by: Meghan Jones <meghanj@alum.mit.edu>

--- src/blockmode.c
+++ src/blockmode.c
@@ -73,7 +73,7 @@ static int usage (struct GMTAPI_CTRL *API, int level) {
 	const char *name = gmt_show_name_and_purpose (API, THIS_MODULE_LIB, THIS_MODULE_CLASSIC_NAME, THIS_MODULE_PURPOSE);
 	if (level == GMT_MODULE_PURPOSE) return (GMT_NOERROR);
 	const char *extra1[2] = {" [-G<grdfile>]", ""}, *extra2[2] = {" (requires -G)", ""};
-	GMT_Usage (API, 0, "usage: %s [<table>] %s %s [-A<fields>] [-C] [-D<width>[+c][+l|h]] [-E[r|s[+l|h]]%s "
+	GMT_Usage (API, 0, "usage: %s [<table>] %s %s [-A<fields>] [-C] [-D[<width>][+c][+a|l|h]] [-E[r|s[+l|h]]]%s "
 		"[-Q] [-W[i|o][+s]] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s]\n",
 		name, GMT_I_OPT, GMT_Rgeo_OPT, extra1[API->external], GMT_V_OPT, GMT_a_OPT, GMT_b_OPT, GMT_d_OPT, GMT_e_OPT,
 		GMT_f_OPT, GMT_h_OPT, GMT_i_OPT, GMT_o_OPT, GMT_q_OPT, GMT_r_OPT, GMT_w_OPT, GMT_colon_OPT, GMT_PAR_OPT);
