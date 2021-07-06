eeca0067d8f92007a52d35fb37326ac6458d594e
Paul Wessel <pwessel@hawaii.edu>
2021-07-06 06:22:36 -1000

    Update src/gmtconnect.c
    Co-authored-by: Meghan Jones <meghanj@alum.mit.edu>

--- src/gmtconnect.c
+++ src/gmtconnect.c
@@ -118,7 +118,7 @@ static int usage (struct GMTAPI_CTRL *API, int level) {
 	const char *name = gmt_show_name_and_purpose (API, THIS_MODULE_LIB, THIS_MODULE_CLASSIC_NAME, THIS_MODULE_PURPOSE);
 	if (level == GMT_MODULE_PURPOSE) return (GMT_NOERROR);
 	GMT_Usage (API, 0, "usage: %s [<table>] [-C<closedfile>] [-D[<template>]] [-L[<linkfile>]] [-Q<listfile>] "
-		"-T[%s[+s<sdist>]] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s]\n",
+		"[-T[%s[+s<sdist>]]] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s] [%s]\n",
 		name, GMT_DIST_OPT, GMT_V_OPT, GMT_a_OPT, GMT_b_OPT, GMT_d_OPT, GMT_e_OPT, GMT_f_OPT, GMT_g_OPT,
 		GMT_h_OPT, GMT_i_OPT, GMT_o_OPT, GMT_q_OPT, GMT_s_OPT, GMT_colon_OPT, GMT_PAR_OPT);
 
@@ -940,4 +940,3 @@ EXTERN_MSC int GMT_gmtstitch (void *V_API, int mode, void *args) {
 	GMT_Report (API, GMT_MSG_ERROR, "Shared GMT module not found: gmtstitch\n");
 	return (GMT_NOT_A_VALID_MODULE);
 }
-
