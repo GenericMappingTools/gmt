58dbb53593c5c5fad610dd25a64874ef77ed6a66
Paul Wessel <pwessel@hawaii.edu>
2021-07-05 16:01:09 -1000

    Sort -B options under the hood to ensure proper order for parsing (#5441)
    * Sort arguments so that -B items are in expected order
    
    It is generally best for frame and axis settings to come after interval settings.
    
    * Update gmt_parse.c
    
    * Fix some bad -B in scripts
    
    * Update gmt_parse.c
    
    * Update gmt_parse.c

--- test/grdimage/twogrids.sh
+++ test/grdimage/twogrids.sh
@@ -10,12 +10,12 @@ gmt begin twogrids
 	gmt subplot begin 3x1 -Fs8c -Baf -R180/190/10/20 -JM7.5c -A+gwhite+p0.25p+o0.2c+jTC -Y1.5c
 		gmt subplot set -A"GRD2 same resolution [6m]"
 		gmt grdcut -R180/190/10/20 @earth_age_06m -Gt.grd
-		gmt grdimage t.grd -Iz.grd+d -B
+		gmt grdimage t.grd -Iz.grd+d
 		gmt subplot set -A"GRD2 courser resolution [10m]"
 		gmt grdcut -R180/190/10/20 @earth_age_10m -Gt.grd
-		gmt grdimage t.grd -Iz.grd+d -B
+		gmt grdimage t.grd -Iz.grd+d
 		gmt subplot set -A"GRD2 finer resolution [2m]"
 		gmt grdcut -R180/190/10/20 @earth_age_02m -Gt.grd
-		gmt grdimage t.grd -Iz.grd+d -B
+		gmt grdimage t.grd -Iz.grd+d
 	gmt subplot end
 gmt end show
