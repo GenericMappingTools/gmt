a5ec68483db8626fd55471053fed7545d69f1b5b
Paul Wessel <pwessel@hawaii.edu>
2021-07-09 10:41:36 -1000

    Update src/grdvolume.c
    Co-authored-by: Meghan Jones <meghanj@alum.mit.edu>

--- src/grdvolume.c
+++ src/grdvolume.c
@@ -310,7 +310,7 @@ static int usage (struct GMTAPI_CTRL *API, int level) {
 	const char *name = gmt_show_name_and_purpose (API, THIS_MODULE_LIB, THIS_MODULE_CLASSIC_NAME, THIS_MODULE_PURPOSE);
 	if (level == GMT_MODULE_PURPOSE) return (GMT_NOERROR);
 	GMT_Usage (API, 0, "usage: %s <ingrid> [-C<cval> or -C<low>/<high>/<delta> or -Cr<low>/<high> or -Cr<cval>] "
-		"[-D] [-L<base>] [-S<unit>] [-T[c|h]] [%s] [%s] [-Z<fact>[/<shift>]] [%s] [%s] [%s] [%s]\n",
+		"[-D] [-L<base>] [%s] [-S<unit>] [-T[c|h]] [%s] [-Z<fact>[/<shift>]] [%s] [%s] [%s] [%s]\n",
 		name, GMT_Rgeo_OPT, GMT_V_OPT, GMT_f_OPT, GMT_ho_OPT, GMT_o_OPT, GMT_PAR_OPT);
 
 	if (level == GMT_SYNOPSIS) return (GMT_MODULE_SYNOPSIS);
