40caaac49470b71ed1c9c46442cbcc2c0f77eec9
Paul Wessel <pwessel@hawaii.edu>
2021-09-09 10:37:45 -1000

    Restore default SIGINT behavior before terminating

--- src/gmt_common_sighandler.c
+++ src/gmt_common_sighandler.c
@@ -177,58 +177,23 @@ static void process_info() {
 }
 
 void sig_handler(int sig_num, siginfo_t *info, void *ucontext) {
-	int size;
-	void *array[50];
-	ucontext_t *uc = (ucontext_t *)ucontext;
-
-	array [0] = UC_IP (uc); /* caller's address */
-	array [1] = info->si_addr; /* address of faulting instruction */
-
 	if (sig_num == SIGINT) {
-		/* catch ctrl-c and remove modern session directory before exit*/
-		gmtlib_terminate_session ();
-		exit (EXIT_FAILURE);
-	}
-	else {
-#ifdef HAVE_STRSIGNAL
-		fprintf (stderr, "ERROR: Caught signal number %d (%s) at\n", sig_num, strsignal(sig_num));
-#else
-		fprintf (stderr, "ERROR: Caught signal number %d (%s) at\n", sig_num, sys_siglist[sig_num]);
-#endif
-		backtrace_symbols_fd (array, 2, STDERR_FILENO); /* print function with faulting instruction */
-		size = backtrace (array, 50); /* get void*'s for all entries on the stack */
-		fprintf (stderr, "Stack backtrace:\n");
-		backtrace_symbols_fd (array, size, STDERR_FILENO); /* print out all the frames to stderr */
-		exit (EXIT_FAILURE);
-	}
-}
-
-#if 0/* This was the old signal handler doing the backtrace etc */
-void sig_handler(int sig_num, siginfo_t *info, void *ucontext) {
-	int size;
-	void *array[50];
-	ucontext_t *uc = (ucontext_t *)ucontext;
-
-	array [0] = UC_IP (uc); /* caller's address */
-	array [1] = info->si_addr; /* address of faulting instruction */
-
-	if (sig_num == SIGINT) {
-		/* catch ctrl-c */
-		int c;
+		/* Catch Ctrl-c and remove modern session sub-directory (if it exists) before exit */
 		struct sigaction act, oldact;
 		sigemptyset (&act.sa_mask);
 		act.sa_flags = 0;
-		act.sa_handler = SIG_DFL; /* restore default action (do not catch SIGINT again) */
-		sigaction (SIGINT, &act, &oldact); /* change signal handler */
-		fprintf (stderr, "\rInterrupt at ");
-		backtrace_symbols_fd (array, 1, STDERR_FILENO); /* print interrupted function */
-		process_info();
-		fprintf (stderr, "Press return to continue, ctrl-c to quit.");
-		while (( c = getchar()) != '\n' && c != EOF );
-		sigaction (SIGINT, &oldact, NULL); /* restore old action */
-		return;
+		act.sa_handler = SIG_DFL; /* Restore default action (do not catch SIGINT again) */
+		sigaction (SIGINT, &act, &oldact); /* Change signal handler */
+		gmtlib_terminate_session ();	/* Delete session dir and call GMT_Destroy_Session */
 	}
-	else {
+	else {	/* Report backtrace after a crash */
+		int size;
+		void *array[50];
+		ucontext_t *uc = (ucontext_t *)ucontext;
+
+		array [0] = UC_IP (uc); /* caller's address */
+		array [1] = info->si_addr; /* address of faulting instruction */
+
 #ifdef HAVE_STRSIGNAL
 		fprintf (stderr, "ERROR: Caught signal number %d (%s) at\n", sig_num, strsignal(sig_num));
 #else
@@ -238,10 +203,9 @@ void sig_handler(int sig_num, siginfo_t *info, void *ucontext) {
 		size = backtrace (array, 50); /* get void*'s for all entries on the stack */
 		fprintf (stderr, "Stack backtrace:\n");
 		backtrace_symbols_fd (array, size, STDERR_FILENO); /* print out all the frames to stderr */
-		exit (EXIT_FAILURE);
 	}
+	exit (EXIT_FAILURE);	/* Now exit process */
 }
-#endif
 
 /*
  * install signal handler like this:
