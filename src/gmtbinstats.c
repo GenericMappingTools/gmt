de42617cdee0b7cb2b1e31b81f03391fa15eb5a0
Meghan Jones <meghanj@alum.mit.edu>
2021-09-14 17:29:00 -0400

    Update wording

--- src/gmtbinstats.c
+++ src/gmtbinstats.c
@@ -289,7 +289,7 @@ static int parse (struct GMT_CTRL *GMT, struct GMTBINSTATS_CTRL *Ctrl, struct GM
 			n_errors += gmt_M_check_condition (GMT, Ctrl->E.active, "Option -Th: The -E option is not allowed for hexagonal tiling\n");
 		}
 		else {
-			n_errors += gmt_M_check_condition (GMT, !Ctrl->G.active, "Option -Tr: The -G option is a required argument when -Tr is used\n");
+			n_errors += gmt_M_check_condition (GMT, !Ctrl->G.active, "Option -Tr: -G is a required argument when -Tr is used\n");
 		}
 		n_errors += gmt_M_check_condition (GMT, Ctrl->S.active, "Option -T: No search radius -S can be set for tiling\n");
 	}
