2a713b40b97ae86f3ded2ae1753abf601f4260e7
Paul Wessel <pwessel@hawaii.edu>
2021-09-15 09:49:18 -1000

    Update src/grdcut.c
    Co-authored-by: Meghan Jones <meghanj@alum.mit.edu>

--- src/grdcut.c
+++ src/grdcut.c
@@ -31,7 +31,7 @@
 #define THIS_MODULE_MODERN_NAME	"grdcut"
 #define THIS_MODULE_LIB		"core"
 #define THIS_MODULE_PURPOSE	"Extract subregion from a grid"
-#define THIS_MODULE_KEYS	"<G{,FD(=,>D),GG}"
+#define THIS_MODULE_KEYS	"<G{,FD(=,>DD,GG}"
 #define THIS_MODULE_NEEDS	""
 #define THIS_MODULE_OPTIONS "-JRVf"
 
