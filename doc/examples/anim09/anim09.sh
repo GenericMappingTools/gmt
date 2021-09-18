1c10cba798ebcff9729152551575c1eb8de2ee0d
Paul Wessel <pwessel@hawaii.edu>
2021-09-17 15:41:59 -1000

    Update official scripts, movies, examples to modern format

--- doc/examples/anim09/anim09.sh
+++ doc/examples/anim09/anim09.sh
@@ -38,7 +38,7 @@ cat << 'EOF' > main.sh
 # Main frame script that makes a single frame given the location and twist from the data file
 # and the other view parameters from the include file.
 gmt begin
-	gmt grdimage -Rg -JG${MOVIE_COL0}/${MOVIE_COL1}/${ALTITUDE}/${MOVIE_COL2}/${TILT}/${MOVIE_COL3}/${WIDTH}/${HEIGHT}/${MOVIE_WIDTH} \
+	gmt grdimage -Rg -JG${MOVIE_COL0}/${MOVIE_COL1}/${MOVIE_WIDTH}+z${ALTITUDE}+a${MOVIE_COL2}+t${TILT}+w${MOVIE_COL3}+v${WIDTH}/${HEIGHT} \
 	  -Y0 -X0 @earth_relief_30s -I/tmp/earth_relief_30s+2.5_int.nc -CMOR_topo.cpt
 	gmt events MOR_names.txt -L100 -Et+r6+f6 -T${MOVIE_FRAME} -F+f12p,Helvetica-Bold,yellow
 gmt end
