bee4f5ac17373a20e02786ccac7f6606c0bef0d1
Federico Esteban <federico.esteban@gmail.com>
2021-09-20 23:35:37 -0300

    Update anim04.sh

--- doc/examples/anim04/anim04.sh
+++ doc/examples/anim04/anim04.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #               GMT ANIMATION 04
 #
-# Purpose:      Make custom 480p movie of NY to Miami flight
-# GMT modules:  set, grdgradient, grdimage, makecpt, project, plot, movie
+# Purpose:      Make custom full hd movie of NY to Miami flight
+# GMT modules:  set, grdimage, project, plot, movie
 # Unix progs:   cat
 # Note:         Run with any argument to build movie; otherwise 1st frame is plotted as PS only.
 
@@ -16,18 +16,14 @@ cat << 'EOF' > pre.sh
 # Set up flight path
 gmt begin
 	gmt project -C-73.8333/40.75 -E-80.133/25.75 -G10 -Q > flight_path.txt
-	gmt grdcut @earth_relief_02m -R-100/-70/10/50 -GUSEast_Coast.nc
-	gmt grdgradient USEast_Coast.nc -A90 -Nt1 -Gint_US.nc
-	gmt makecpt -Cglobe -H > globe_US.cpt
 gmt end
 EOF
 # 2. Set up the main frame script
 cat << 'EOF' > main.sh
 gmt begin
-	gmt grdimage -JG${MOVIE_COL0}/${MOVIE_COL1}/160/210/55/0/36/34/${MOVIE_WIDTH}+du \
-		-Rg USEast_Coast.nc -Iint_US.nc -Cglobe_US.cpt -X0 -Y0
-	gmt plot -W1p flight_path.txt
+	gmt grdimage -JG${MOVIE_COL0}/${MOVIE_COL1}/${MOVIE_WIDTH}+du+z160+a210+t55+v36 \
+	-Rg @earth_night_01m -Xc -Y-5c
 gmt end
 EOF
 # 3. Run the movie
-gmt movie main.sh -C7.2ix4.8ix100 -Nanim04 -Tflight_path.txt -Sbpre.sh -Zs -H2 -Lf+o0.1i+f14p,Helvetica-Bold $opt
+gmt movie main.sh -Chd -Nanim04 -Tflight_path.txt -Sbpre.sh -Zs -H2 -Lf+o0.25c+f14p,Helvetica-Bold,white -Gmidnightblue $opt
