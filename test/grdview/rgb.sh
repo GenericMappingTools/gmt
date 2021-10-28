6a91d32e87f9c3e02b32e09b8f2bd0b2c882a732
Paul Wessel <pwessel@hawaii.edu>
2021-10-27 17:22:16 -1000

    Let GMT_GRAPHICS_DPU be the default in grdview -Qi (#5925)
    * Let GMT_GRAPHICS_DPU be the default in grdview -Qi
    
    The 100 dpi was set as a default decades ago when computers were slow and we were impatient.  Now that we have a GMT defaults that controls dpi we will use it. We let the tests stick with 100 where they previously just said -Qi so we dont have to update PS originals.
    
    * Update viewpluscpt.sh
    
    * Update ex32.ps

--- test/grdview/rgb.sh
+++ test/grdview/rgb.sh
@@ -6,5 +6,5 @@ gmt grdmath -R0/6/0/6 -I0.1 X 6 DIV 255 MUL = r.nc
 gmt grdmath -R0/6/0/6 -I0.1 Y 6 DIV 255 MUL = g.nc
 gmt grdmath -R0/6/0/6 -I0.1 3 3 CDIST DUP UPPER DIV 255 MUL  = b.nc
 gmt grdcut @earth_relief_06m -R0/6/0/6 -Gtopo.nc
-gmt grdview topo.nc -I+ -JM4i -JZ2i -p145/35 -Gr.nc,g.nc,b.nc -Baf -BWSne -Qi -P -K -X1.5i -Y0.75i > $ps
-gmt grdview topo.nc -I+ -J -JZ -p -Gr.nc -Gg.nc -Gb.nc -Baf -BWSne -Qi -O -Y4.75i >> $ps
+gmt grdview topo.nc -I+ -JM4i -JZ2i -p145/35 -Gr.nc,g.nc,b.nc -Baf -BWSne -Qi100i -P -K -X1.5i -Y0.75i > $ps
+gmt grdview topo.nc -I+ -J -JZ -p -Gr.nc -Gg.nc -Gb.nc -Baf -BWSne -Qi100i -O -Y4.75i >> $ps
