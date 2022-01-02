d646ae9583b6d6dc3b118d3ba878876b04830ba2
Paul Wessel <pwessel@hawaii.edu>
2022-01-02 09:55:20 -1000

    Remove boundary annotations

--- test/grdimage/defcpt.sh
+++ test/grdimage/defcpt.sh
@@ -5,7 +5,7 @@
 gmt begin defcpt
 	gmt grdcut @earth_relief_01d_g -R0/10/0/10 -Gtest.nc
 	gmt subplot begin 2x1 -R0/10/0/10 -JQ10c -Fs10c
-		gmt grdimage @earth_relief_01d_g -R0/10/0/10 -B -c
-		gmt grdimage test.nc -B -c
+		gmt grdimage @earth_relief_01d_g -R0/10/0/10 -B0 -c
+		gmt grdimage test.nc -B0 -c
 	gmt subplot end
 gmt end show
