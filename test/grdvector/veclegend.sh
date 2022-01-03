4d6c53825275c8beb55829da6e6d5b2b103658c1
Paul Wessel <pwessel@hawaii.edu>
2022-01-03 13:42:57 -1000

    Truncate vector size if head width exceeds H

--- test/grdvector/veclegend.sh
+++ test/grdvector/veclegend.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # Demonstrate the auto-legend entry for grdvector
-# Make a fake data set of data vectors in mm/yr
+# Make a fake data set of data vectors in mm/yr, plot them with a reference length of 25 mm/yr
+# and just plot something else (a circle) later to make sure of alignment
 gmt grdmath -R0/30/30/60 -I5 X = x.grd
 gmt grdmath -R0/30/30/60 -I5 Y 30 SUB = y.grd
-# Set the LL values to the same as the UR values
+# Set the LL grid values to the same as the UR values
 echo 0 30 30 > t.txt
 gmt grdedit -Nt.txt x.grd
 gmt grdedit -Nt.txt y.grd
-# use reference length as 30 mm/year in the legend
+# Use reference length as 25 mm/year in the legend
 gmt begin veclegend
-	gmt grdvector x.grd y.grd -R-2/38/29/65 -JM15c -B -Q14p+e+n100k/0 -Gred -W1p -S15c+r30 -l"Velocity (30 mm/yr)"
+	gmt grdvector x.grd y.grd -R-2/38/29/65 -JM15c -B -Q14p+e+n100k/0 -Gred -W1p -S15c+s25 -l"Velocity (25 mm/yr)"
 	echo 13 47 | gmt plot -Sc0.1i -Gblue -l"Arbitrary point"
 gmt end show
