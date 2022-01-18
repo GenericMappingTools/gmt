71025eddbe0ec2de950d9a9bcf7a833bfae35d4a
Paul Wessel <pwessel@hawaii.edu>
2022-01-17 16:47:42 -1000

    Update line_shortening.sh

--- test/psxy/line_shortening.sh
+++ test/psxy/line_shortening.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 # Ensure the bug reported by https://github.com/GenericMappingTools/gmt/issues/6217 is gone
 # Black dots are input points to be connected by blue line
-gmt begin line_shortening
-	cat <<- EOF > t.txt
+gmt begin line_shortening png
+	cat <<- EOF > v.txt
 	2 4
 	2 1
 	2 3.5
 	EOF
-	gmt plot -R0/5/0/5 -JX8c -Ba1g1 -W1p,blue t.txt
-	gmt plot -Sc3p -Gblack t.txt
+	cat <<- EOF > h.txt
+	1	0.5
+	4	0.5
+	3.5	0.5
+	EOF
+	gmt plot -R0/5/0/5 -JX8c -Ba1g1 -W1p,blue v.txt h.txt
+	gmt plot -Sc3p -Gblack v.txt h.txt
 gmt end show
