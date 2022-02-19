c7c24895d1b6764acc3a383e945f199c59072c16
Dongdong Tian <seisman.info@gmail.com>
2022-02-19 22:34:25 +0800

    Fix

--- ci/vercel-docs.sh
+++ ci/vercel-docs.sh
@@ -15,7 +15,7 @@ curl -o ~/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Lin
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH=$HOME/miniconda/bin:$PATH
 conda install mamba -c conda-forge -y
-mamba install sphinx dvc ghostscript
+mamba install sphinx dvc ghostscript -c conda-forge -y
 
 # Following variables can be modified via environment variables
 GMT_INSTALL_DIR=${HOME}/gmt-install-dir
