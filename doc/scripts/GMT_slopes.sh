4cd45e3843c57df74f2c1911c68468fb7e0ca10d
Meghan Jones <meghanj@alum.mit.edu>
2022-04-04 13:12:31 -0400

    Update scripts for docs to use modern mode (#6518)
    * Update scripts for docs to use modern mode
    
    * Add trailing line to scripts

--- doc/scripts/GMT_slopes.sh
+++ doc/scripts/GMT_slopes.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Show gmtregress -A option in effect
-ps=GMT_slopes.ps
+gmt begin GMT_slopes
+    gmt set GMT_THEME cookbook  
 cat << EOF > data
 5.2957	-19.5631
 7.087	-36.0337
@@ -22,15 +23,16 @@ cat << EOF > data
 11.28	-37.4959
 EOF
 
-gmt regress data -A-90/90/0.1 -Eo -Nr > tmp.txt
-gmt psxy tmp.txt -R-90/90/0/35 -JX15c/5c -Bxa30+u@.+l"Regression line angle, @~a@~" -Byaf+l"Misfit, E(@~a@~)" -BWSrt -P -K -W0.75p > $ps
-echo -90 5.29462 90 5.29462 | gmt psxy -R -J -O -K -Sv12p+s -W0.25p,dashed >> $ps
-gmt psxy -R -J -O -K -Sv12p+b -Gblack -W0.5p --MAP_VECTOR_SHAPE=0.5 << EOF >> $ps
+    gmt regress data -A-90/90/0.1 -Eo -Nr > tmp.txt
+    gmt plot tmp.txt -R-90/90/0/35 -JX15c/5c -Bxa30+u@.+l"Regression line angle, @~a@~" -Byaf+l"Misfit, E(@~a@~)" -BWSrt -W0.75p
+    echo -90 5.29462 90 5.29462 | gmt plot -Sv12p+s -W0.25p,dashed
+    gmt plot -Sv12p+b -Gblack -W0.5p --MAP_VECTOR_SHAPE=0.5 << EOF
 78.6 6.32 90 2.6c
 -78.3 5.3 90 2.7c
 EOF
-gmt pstext -R -J -O -F+f12p+jCB -Dj0/0.1c << EOF >> $ps
+    gmt text -F+f12p+jCB -Dj0/0.1c << EOF
 78.6 25 78.6@.
 -78.3 25 -78.3@.
 EOF
+gmt end show
 rm -f data tmp.txt
