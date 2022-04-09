d921b4522d0fb7f1e1ab9db87f4d518f8ce27774
Paul Wessel <pwessel@hawaii.edu>
2022-04-09 17:36:16 +0100

    Fix regression in parsing of -W (#6539)
    See #6538 for the problem.  We had a regression in all the blockm* modules where a -W+s argument would yield a parsing error but not tell anyone - just exiting.  Unfortunately there is no workaround since adding i or o turns off the opposite direction; here the user wants weights in and sigma out.
    Closes #6538.

--- src/blockmode.c
+++ src/blockmode.c
@@ -244,12 +244,14 @@ static int parse (struct GMT_CTRL *GMT, struct BLOCKMODE_CTRL *Ctrl, struct GMT_
 				n_errors += gmt_M_repeated_module_option (API, Ctrl->Q.active);
 				Ctrl->Q.active = true;
 				break;
-			case 'W':	/* Use in|out weights */
+			case 'W':	/* Use in|out weights -W[i|o][+s] */
 				n_errors += gmt_M_repeated_module_option (API, Ctrl->W.active);
 				Ctrl->W.active = true;
-				if (gmt_validate_modifiers (GMT, opt->arg, 'W', "s", GMT_MSG_ERROR)) n_errors++;
-				sigma = (gmt_get_modifier (opt->arg, 's', arg)) ? true : false;
-				switch (opt->arg[0]) {
+				if ((c = strstr (opt->arg, "+s"))) {	/* Gave +s */
+					sigma = true;
+					c[0] = '\0';	/* Hide modifier */
+				}
+				switch (opt->arg[0]) {	/* Check for -Wi or -Wo as well */
 					case '\0':
 						Ctrl->W.weighted[GMT_IN] = Ctrl->W.weighted[GMT_OUT] = true;
 						Ctrl->W.sigma[GMT_IN] = Ctrl->W.sigma[GMT_OUT] = sigma;
@@ -263,9 +265,11 @@ static int parse (struct GMT_CTRL *GMT, struct BLOCKMODE_CTRL *Ctrl, struct GMT_
 						Ctrl->W.sigma[GMT_OUT] = sigma;
 						break;
 					default:
+						GMT_Report (API, GMT_MSG_ERROR, "Option -W: Unrecognized argument %s!\n", opt->arg);
 						n_errors++;
 						break;
 				}
+				if (c) c[0] = '+';	/* Restore modifier */
 				break;
 
 			default:	/* Report bad options */
