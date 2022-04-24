200325c5ddd99acaebdbd50d29b3b25f09792e21
Paul Wessel <pwessel@hawaii.edu>
2022-04-24 10:16:41 +0100

    Improve detection of cubes
    Add two macros working on virtual names that can detect cubes vs grids.  Also, use n_bands to help detect cubes vs grids.
    Now works in Julia again.  Closes #6617.

--- src/grdinfo.c
+++ src/grdinfo.c
@@ -564,18 +564,25 @@ EXTERN_MSC int GMT_grdinfo (void *V_API, int mode, void *args) {
 	for (opt = options; opt; opt = opt->next) {	/* Loop over arguments, skip options */
 
 		if (opt->option != '<') continue;	/* We are only processing filenames here */
-		if ((G = GMT_Read_Data (API, GMT_IS_GRID, GMT_IS_FILE, GMT_IS_SURFACE, GMT_CONTAINER_ONLY, NULL, opt->arg, NULL)) == NULL) {
-			Return (API->error);
-		}
-		header = G->header;
-		if (gmtlib_is_nc_grid (GMT, header))	/* Not netCDF so must be grid */
+
+		if (gmt_M_memfile_is_grid (opt->arg))	/* Can just check file name */
 			ng++;
-		else if (gmt_nc_is_cube (API, opt->arg))	/* Determine if this netCDF file is a cube or not */
+		else if (gmt_M_memfile_is_cube (opt->arg))	/* Can just check file name */
 			nc++;
-		else
-			ng++;
-		if (GMT_Destroy_Data (API, &G) != GMT_NOERROR){
-			Return (API->error);
+		else {	/* Must read file header (as if a grid) to figure it out */
+			if ((G = GMT_Read_Data (API, GMT_IS_GRID, GMT_IS_FILE, GMT_IS_SURFACE, GMT_CONTAINER_ONLY, NULL, opt->arg, NULL)) == NULL) {
+				Return (API->error);
+			}
+			header = G->header;
+			if (gmtlib_is_nc_grid (GMT, header) || header->n_bands == 1)	/* Not netCDF or a single layer so must be grid */
+				ng++;
+			else if (gmt_nc_is_cube (API, opt->arg))	/* Determine if this netCDF file is a cube or not */
+				nc++;
+			else
+				ng++;
+			if (GMT_Destroy_Data (API, &G) != GMT_NOERROR){
+				Return (API->error);
+			}
 		}
 	}
 
