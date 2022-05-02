cef2c7795346d826a6f490f35db1922a00cef0d0
Federico Esteban <federico.esteban@gmail.com>
2022-05-02 09:29:48 -0300

    Update GMT_splines.sh
    Updated script following Paul's suggestions.

--- doc/scripts/GMT_splines.sh
+++ doc/scripts/GMT_splines.sh
@@ -13,12 +13,12 @@ cat << EOF > t.txt
 EOF
 # Splines
 gmt begin doc_splines png
-gmt basemap -R-0.1/6.1/-0.1/3.1 -JX15c/6c -Baf+l"u" -By+l"u(x)" -BWSne
-gmt sample1d t.txt -T0.01 -Fc	| gmt plot -W1p		  -l"Cubic spline (-Fc)"
-gmt sample1d t.txt -T0.01 -Fs1	| gmt plot -W1p,orange	  -l"Smooth cubic spline (-Fs1)"
-gmt sample1d t.txt -T0.01 -Fl	| gmt plot -W1p,blue	  -l"Linear spline (-Fl)"
-gmt sample1d t.txt -T0.01 -Fa	| gmt plot -W1p,red	  -l"Akima spline (-Fa)"
-gmt sample1d t.txt -T0.01 -Fn	| gmt plot -W1p,darkgreen -l"Nearest neighbor (-Fn)"
-gmt plot t.txt -Sc0.25c -Gred -Wthin -l"Data"
-gmt legend -DjTL+w4.9c+o0.2c -F+p1p+gwhite+s 
-gmt end
+  gmt basemap -R-0.1/6.1/-0.1/3.1 -JX15c/6c -Baf+l"u" -By+l"u(x)" -BWSne
+  gmt sample1d t.txt -T0.01 -Fc	| gmt plot -W1p		  -l"Cubic spline (-Fc)"
+  gmt sample1d t.txt -T0.01 -Fs1	| gmt plot -W1p,orange	  -l"Smooth cubic spline (-Fs1)"
+  gmt sample1d t.txt -T0.01 -Fl	| gmt plot -W1p,blue	  -l"Linear spline (-Fl)"
+  gmt sample1d t.txt -T0.01 -Fa	| gmt plot -W1p,red	  -l"Akima spline (-Fa)"
+  gmt sample1d t.txt -T0.01 -Fn	| gmt plot -W1p,darkgreen -l"Nearest neighbor (-Fn)"
+  gmt plot t.txt -Sc0.25c -Gred -Wthin -l"Data"
+  gmt legend -DjTL+w4.9c+o0.2c -F+p1p+gwhite+s 
+gmt end show
