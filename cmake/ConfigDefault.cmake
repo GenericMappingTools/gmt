471ae97dc4869dd75a6a211408fc18f9d9a233d7
Paul Wessel <pwessel@hawaii.edu>
2022-06-08 10:51:06 +0100

    Update DOI for 6.4.0 (#6773)
    As title says.

--- cmake/ConfigDefault.cmake
+++ cmake/ConfigDefault.cmake
@@ -41,7 +41,7 @@ set (GMT_PACKAGE_DESCRIPTION_SUMMARY "The Generic Mapping Tools")
 set (GMT_VERSION_YEAR "2022")
 
 # The GMT release DOI
-set (GMT_VERSION_DOI "https://doi.org/10.5281/zenodo.5708769")
+set (GMT_VERSION_DOI "https://doi.org/10.5281/zenodo.6623271")
 
 # The GMT release citation
 set (GMT_VERSION_CITATION "Wessel, P., Luis, J. F., Uieda, L., Scharroo, R., Wobbe, F., Smith, W. H. F., & Tian, D. (2019). The Generic Mapping Tools version 6. Geochemistry, Geophysics, Geosystems, 20, 5556–5564. https://doi.org/10.1029/2019GC008515")
