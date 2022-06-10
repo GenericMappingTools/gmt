075dcf1394cbe619a7e6c16680f7fa8a11a3c672
Paul Wessel <pwessel@hawaii.edu>
2022-06-10 08:50:27 +0100

    Update gmt_support.c

--- src/gmt_support.c
+++ src/gmt_support.c
@@ -487,19 +487,14 @@ GMT_LOCAL void gmtsupport_rgb_to_cmyk (double rgb[], double cmyk[]) {
 	int i;
 
 	cmyk[4] = rgb[3];	/* Pass transparency unchanged */
-	cmyk[3] = 1.0 - MAX (cmyk[0], MAX (cmyk[1], cmyk[2]));	/* Default Black generation */
-	if (cmyk[3] == 1.0)
+	cmyk[3] = 1.0 - MAX (rgb[0], MAX (rgb[1], rgb[2]));	/* Black K */
+	if (doubleAlmostEqual (cmyk[3], 1.0))	/* All black */
 		cmyk[0] = cmyk[1] = cmyk[2] = 0.0;
-	else
+	else	/* Safe to divide by 1-k */
 		for (i = 0; i < 3; i++) cmyk[i] = (1.0 - rgb[i] - cmyk[3]) / (1.0 - cmyk[3]);
-	if (cmyk[3] < GMT_CONV8_LIMIT) cmyk[3] = 0.0;
+	if (cmyk[3] < GMT_CONV8_LIMIT) cmyk[3] = 0.0;	/* Fix minor black round-off */
 
 	/* To implement device-specific blackgeneration, supply lookup table K = BG[cmyk[3]] */
-
-//	for (i = 0; i < 3; i++) {
-//		cmyk[i] -= cmyk[3];		/* Default undercolor removal */
-//		if (cmyk[i] < GMT_CONV8_LIMIT) cmyk[i] = 0.0;
-//	}
 
 	/* To implement device-specific undercolor removal, supply lookup table u = UR[cmyk[3]] */
 }
@@ -512,7 +507,10 @@ GMT_LOCAL void gmtsupport_cmyk_to_rgb (double rgb[], double cmyk[]) {
 	int i;
 
 	rgb[3] = cmyk[4];	/* Pass transparency unchanged */
-	for (i = 0; i < 3; i++) rgb[i] = (1.0 - cmyk[i]) * (1.0 - cmyk[3]);
+	if (doubleAlmostEqual (cmyk[3], 1.0))	/* All black */
+		rgb[0] = rgb[1] = rgb[2] = 0.0;
+	else	/* Safe to divide by 1-k */
+		for (i = 0; i < 3; i++) rgb[i] = (1.0 - cmyk[i]) * (1.0 - cmyk[3]);
 }
 
 /*! . */
