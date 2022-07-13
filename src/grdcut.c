56d3bc401e660a70a8cf651058be61bcf8755a01
Paul Wessel <pwessel@hawaii.edu>
2022-07-13 21:44:34 +0100

    Update grdcut.c

--- src/grdcut.c
+++ src/grdcut.c
@@ -72,7 +72,7 @@ struct GRDCUT_CTRL {
 	} S;
 	struct GRDCUT_Z {	/* -Z[min/max][+n|N|r] */
 		bool active;
-		unsigned int mode;	/* 0-2, see below */
+		unsigned int mode;	/* 0-2, see below for values */
 		double min, max;
 	} Z;
 };
