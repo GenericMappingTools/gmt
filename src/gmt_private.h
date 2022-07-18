01f04bed80385d71dfd2242a3a2a861326740c01
Paul Wessel <pwessel@hawaii.edu>
2022-07-18 15:26:29 +0100

    Fix #6891 to just not fuss but stick with gridline registration
    The initial fix in #6891 caused a few failures since grd2cpt used a different resolution than in the calculations.

--- src/gmt_private.h
+++ src/gmt_private.h
@@ -150,6 +150,7 @@ struct GMTAPI_CTRL {
 	bool no_history;					/* true if we want to disable the gmt.history mechanism */
 	bool got_remote_wesn;				/* true if we obtained w/e/sn via a remote grid/image with no resolution given */
 	bool use_gridline_registration;	/* true if default remote grid registration should be gridline, not pixel */
+	bool use_gridline_registration_warn;	/* true if we should warn about the above */
 	size_t n_objects_alloc;			/* Allocation counter for data objects */
 	int error;				/* Error code from latest API call [GMT_OK] */
 	int last_error;				/* Error code from previous API call [GMT_OK] */
