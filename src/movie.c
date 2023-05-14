b42fd65ea4d3492562ddc9a7a958cc9d90d40913
Paul Wessel <pwessel@hawaii.edu>
2023-05-14 17:55:37 +0200

    Update movie.c

--- src/movie.c
+++ src/movie.c
@@ -686,7 +686,7 @@ static int parse (struct GMT_CTRL *GMT, struct MOVIE_CTRL *Ctrl, struct GMT_OPTI
 	int n;
 	bool do_view = false;
 	char txt_a[GMT_LEN32] = {""}, txt_b[GMT_LEN32] = {""}, arg[GMT_LEN64] = {""}, p[GMT_LEN256] = {""};
-	char *c = NULL, *s = NULL, string[GMT_LEN128] = {""};
+	char *c = NULL, *s = NULL, *o = NULL, string[GMT_LEN128] = {""};
 	double width = 24.0, height16x9 = 13.5, height4x3 = 18.0, dpu = 160.0;	/* SI values for dimensions and dpu */
 	struct GMT_FILL fill;	/* Only used to make sure any fill is given with correct syntax */
 	struct GMT_PEN pen;	/* Only used to make sure any pen is given with correct syntax */
@@ -852,6 +852,7 @@ static int parse (struct GMT_CTRL *GMT, struct MOVIE_CTRL *Ctrl, struct GMT_OPTI
 				if ((c = gmt_first_modifier (GMT, opt->arg, "lostv"))) {	/* Process any modifiers */
 					do_view = false;
 					pos = 0;	/* Reset to start of new word */
+					o = NULL;
 					while (gmt_getmodopt (GMT, 'F', c, "lostv", &pos, p, &n_errors) && n_errors == 0) {
 						switch (p[0]) {
 							case 'l':	/* Specify loops for GIF */
@@ -859,7 +860,7 @@ static int parse (struct GMT_CTRL *GMT, struct MOVIE_CTRL *Ctrl, struct GMT_OPTI
 								Ctrl->F.loops = (p[1]) ? atoi (&p[1]) : 0;
 								break;
 							case 'o':	/* FFmpeg option to pass along */
-								s = strdup (&p[1]);	/* Retain start of encoding options for later */
+								o = strdup (&p[1]);	/* Retain start of encoding options for later */
 								break;
 							case 's':	/* Specify GIF stride, 2,5,10,20,50,100,200,500 etc. */
 								Ctrl->F.skip = true;
@@ -919,9 +920,9 @@ static int parse (struct GMT_CTRL *GMT, struct MOVIE_CTRL *Ctrl, struct GMT_OPTI
 					Ctrl->F.active[k] = true;
 					Ctrl->F.view[k] = do_view;
 					if (k != MOVIE_PNG) Ctrl->animate = true;
-					if (s) {	/* Gave specific encoding options */
+					if (o) {	/* Gave specific encoding options */
 						if (Ctrl->F.options[k]) gmt_M_str_free (Ctrl->F.options[k]);	/* Free old setting first */
-						Ctrl->F.options[k] = s;
+						Ctrl->F.options[k] = o;
 					}
 				}
 				if (c) c[0] = '+';	/* Now we can restore the optional text we chopped off */
