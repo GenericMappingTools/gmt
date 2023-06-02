c4857e60fb3537f24373231e72b0ba14a266f0b9
Esteban82 <federico.esteban@gmail.com>
2023-06-02 11:06:28 -0300

    Add -Fe to GMT_splines.sh

--- doc/scripts/GMT_splines.sh
+++ doc/scripts/GMT_splines.sh
@@ -13,8 +13,9 @@ cat << EOF > t.txt
 EOF
 # Splines
 gmt begin GMT_splines
-	gmt basemap -R-0.1/6.1/-0.1/3.1 -JX15c/6c -Baf+l"u" -By+l"u(x)" -BWSne
+	gmt basemap -R-0.1/6.1/-0.1/3.1 -JX15c/7c -Baf+l"u" -By+l"u(x)" -BWSne
 	gmt sample1d t.txt -T0.01 -Fc  | gmt plot -W1p -l"Cubic spline (-Fc)"
+   	gmt sample1d t.txt -T0.01 -Fe  | gmt plot -W1p,purple -l"Step-up curve (-Fe)"
 	gmt sample1d t.txt -T0.01 -Fs1 | gmt plot -W1p,orange -l"Smooth cubic spline (-Fs1)"
 	gmt sample1d t.txt -T0.01 -Fl  | gmt plot -W1p,blue -l"Linear spline (-Fl)"
 	gmt sample1d t.txt -T0.01 -Fa  | gmt plot -W1p,red -l"Akima spline (-Fa)"
