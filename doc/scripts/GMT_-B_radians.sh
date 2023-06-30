353e777d256d2cfb334e6b2ff80b82a51710c872
Paul Wessel <pwessel@hawaii.edu>
2023-06-30 14:52:14 +0200

    Update GMT_-B_radians.sh
    Show an example of radians annotations

--- doc/scripts/GMT_-B_radians.sh
+++ doc/scripts/GMT_-B_radians.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gmt basemap -R-4/2/0/3 -Jx1i -Bxapi3fpi6 -BS --GMT_THEME=cookbook -view GMT_-B_radians
+gmt basemap -R-4/2/0/1 -Jx2.5c -Bxapi3fpi6 -BS --GMT_THEME=cookbook -view GMT_-B_radians
