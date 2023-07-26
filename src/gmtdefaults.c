479a87e772e361c821755589026ea4b87fa0d766
Paul Wessel <pwessel@hawaii.edu>
2023-07-26 18:27:32 +0200

    Update gmtdefaults.c

--- src/gmtdefaults.c
+++ src/gmtdefaults.c
@@ -99,7 +99,7 @@ static int parse (struct GMT_CTRL *GMT, struct GMTDEFAULTS_CTRL *Ctrl, struct GM
 				if (opt->arg[0]) {	/* Specified and argument */
 					n_errors += gmt_get_required_char (GMT, opt->arg, opt->option, 0, &Ctrl->D.mode);
 					if (strchr ("su", Ctrl->D.mode) == NULL) {
-						GMT_Report (API, GMT_MSG_COMPAT, "Option -D: Argument %s is not recognized.\n", opt->arg);
+						GMT_Report (API, GMT_MSG_ERROR, "Option -D: Argument %s is not recognized.\n", opt->arg);
 						n_errors++;
 					}
 				}
