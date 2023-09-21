a711b06e286e7da2f8311380205f288a6445f99b
Dongdong Tian <seisman.info@gmail.com>
2023-09-21 16:45:21 +0800

    CI: Use the libmamba solver to speedup conda (#7823)

--- ci/install-dependencies-linux.sh
+++ ci/install-dependencies-linux.sh
@@ -41,8 +41,8 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends --no-install-suggests $packages
 
 # Install packages via conda
-conda update -n base -c defaults conda
-conda install ${conda_packages} -c conda-forge
+conda update -n base -c conda-forge conda --solver libmamba
+conda install ${conda_packages} -c conda-forge --solver libmamba
 echo "${CONDA}/bin" >> $GITHUB_PATH
 
 set +x +e
