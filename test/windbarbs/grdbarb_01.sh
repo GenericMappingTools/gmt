f9a4e1bcbf8b1ad1cf75b154e616feeb7bf5b1a3
Joaquim <jluis@ualg.pt>
2023-09-27 14:21:37 +0100

    Add 3 more tests (with slight editions).

--- test/windbarbs/grdbarb_01.sh
+++ test/windbarbs/grdbarb_01.sh
@@ -11,6 +11,6 @@ gmt grdmath -Rg -f0f,1y -I10 Y ABS 2 DIV = spd.grd
 gmt grdmath dir.grd 180 SUB SIND spd.grd MUL = u.grd
 gmt grdmath dir.grd 180 SUB COSD spd.grd MUL = v.grd
 
-gmt grdbarb u.grd v.grd -W -JX24/12 -Rg -Ba30 -B+t"$title" -P > $ps
+gmt grdbarb u.grd v.grd -W -JX15c/0 -Rg -Ba -BWSen+t"$title" -P > $ps
 
 \rm *.grd
---
+++ test/windbarbs/psbarb_02.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# psbarb_02.sh
+#   3-D plot with -JZ -p
+#
+
+ps=psbarb_02.ps
+title="$ps  3-D plot with -JZ -p"
+
+awk '
+function abs(x) { return x>=0 ? x : -x }
+BEGIN {
+  for(y = -90; y <= 90; y += 10)
+    for(x = 0; x <= 360; x += 10) {
+      z = y + 90
+      dir = x
+      spd = abs(y) / 2
+      print x,y,z, dir, spd
+    }
+}' > wind.txt
+
+gmt psbarb wind.txt -Q0.4c -Wdefault,black -JQ15c -R0/360/-90/90/0/200 -Ba -BwESnZ1234+t"$title" -JZ6c -Bza50 -p150/45 -P > $ps
+
+\rm wind.txt
