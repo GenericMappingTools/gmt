94f6a61f2aff0c302844edec7883e33dbb1e7429
Paul Wessel <pwessel@hawaii.edu>
2023-10-13 23:32:48 +0200

    Update gmt_support.c (#7902)
    The -Fe interpolator could only go up but now it can go down as well.

--- src/gmt_support.c
+++ src/gmt_support.c
@@ -1818,7 +1818,7 @@ GMT_LOCAL int gmtsupport_intpol_sub (struct GMT_CTRL *GMT, double *x, double *y,
 				else if (j == 0)	/* Start at the beginning */
 					v[i] = y[0];
 				else	/* Pick the smallest before the step happens */
-					v[i] = MIN (y[j], y[j+1]);
+					v[i] = (y[j+1] >= y[j]) ? MIN (y[j], y[j+1]) : MAX (y[j], y[j+1]);
 				break;
 			case GMT_SPLINE_LINEAR+GMT_SPLINE_SLOPE:	/* Linear spline v'(u) */
 				v[i] = (y[j+1]-y[j])/(x[j+1]-x[j]);
