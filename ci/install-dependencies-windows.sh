6b3ad8e4782aa72d83b163c99e61d4e2bdd80d3b
Dongdong Tian <seisman.info@gmail.com>
2023-11-01 15:59:19 +0800

    Remove one more

--- ci/install-dependencies-windows.sh
+++ ci/install-dependencies-windows.sh
@@ -54,7 +54,7 @@ echo "$CONDA\\Library\\bin" >> $GITHUB_PATH
 echo "$CONDA\\Scripts" >> $GITHUB_PATH
 
 # Remove pcre-config from the CONDA's PATH to avoid conflicts
-rm -f ${CONDA}\\Library\\bin\\pcre-config
+rm -f ${CONDA}\\Library\\bin\\pcre-config ${CONDA}\\Library\\bin\\pcre.dll
 
 # Install Sphinx extensions
 if [ "$BUILD_DOCS" = "true" ]; then
