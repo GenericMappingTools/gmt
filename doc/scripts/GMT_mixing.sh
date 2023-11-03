0a2f2287c4fa8bf5f16ae19976430ca9d293a9d9
Paul Wessel <pwessel@hawaii.edu>
2023-11-03 16:55:05 +0100

    Replace test script for grdmix with doc figure instead
    New fig in grdmix.rst. Remove test/grdmixmixcompose.sh

--- doc/scripts/GMT_mixing.sh
+++ doc/scripts/GMT_mixing.sh
@@ -2,13 +2,13 @@
 # Test grdmix by deconstructing an image into r,g,b grids the
 # reassembly to another image.
 
-gmt begin mixcompose
-	gmt set MAP_FRAME_TYPE plain PS_MEDIA letter
+gmt begin GMT_mixing
+	gmt set MAP_FRAME_TYPE plain FONT_ANNOT_PRIMARY 7p,Helvetica,black
+	gmt grdgradient @earth_relief_20m_p -A45 -Nt1 -Gint.grd
+	gmt grdimage @earth_day_20m_p -JQ9c -B0 -Iint.grd
 	gmt grdmix @earth_day_01d -D -Glayer_%c.grd
-	gmt grdmix layer_R.grd layer_G.grd layer_B.grd -Ni -C -Gearth_day.tif
-	gmt grdimage earth_day.tif -JQ18c -B
-	gmt subplot begin 2x2 -Fs8.5c/4.25c -JQ8.5c -Rd -Srl -Scb -Y10c -M10p/4p -T"Deconstruct/construct Earth Day"
-		gmt grdimage @earth_day_01d -c
+	gmt subplot begin 2x2 -Fs4.2c/2.125c -JQ4.25c -Rd -Srr -Scb -B0 -M1p/0.5p -X9.3c
+		gmt grdimage int.grd -Cgray -c
 		gmt makecpt -T0/255 -Cwhite,red -H > t.cpt
 		gmt grdimage layer_R.grd -Ct.cpt -c
 		gmt makecpt -T0/255 -Cwhite,green -H > t.cpt
