f16f4aee05454d1a0ced61b0d2ec91fc01e23327
Federico Esteban <federico.esteban@gmail.com>
2023-11-28 17:52:13 -0300

    Update anim05.sh. Fix typos
    I fix some errors in the comments of the script.

--- doc/examples/anim05/anim05.sh
+++ doc/examples/anim05/anim05.sh
@@ -35,11 +35,11 @@ gmt end
 EOF
 cat << 'EOF' > main.sh
 gmt begin
-	# Plot current solution with contors in the right panel
+	# Plot current solution with contors in the center panel
 	gmt grdimage -JM8c -X8c -Y0 geo_cum_${MOVIE_ITEM}.grd -Cz.cpt -B0 -Ei -I+d
 	gmt grdcontour geo_cum_${MOVIE_ITEM}.grd -C200 -S8
 	gmt plot geo.txt -Ss2p -Cz.cpt
-	# Plot the increments in the center panel and add color bar at bottom
+	# Plot the increments in the right panel and add color bar at top
 	gmt grdimage -X8c geo_inc_${MOVIE_ITEM}.grd -Cdz.cpt -B0 -Ei
 	gmt colorbar -Cdz.cpt -DjTR+w4c+h+o19p/8p+e -Bxaf -By+l"m" -F+gwhite+p0.5p
 gmt end
