f1b87b4d05b5577bf8370db33e065c1971846b42
Joaquim <jluis@ualg.pt>
2023-12-08 13:27:36 +0000

    Reinforce the test

--- src/gmt_grdio.c
+++ src/gmt_grdio.c
@@ -3523,7 +3523,7 @@ int gmt_raster_type (struct GMT_CTRL *GMT, char *file, bool extra) {
 				code = GMT_IS_GRID;
 			else if (I->type == GMT_FLOAT)		/* No doubt in this case */
 				code = GMT_IS_GRID;
-			else if (I->type == GMT_SHORT)		/* No so sure here but a Int16 is much likely a grid */
+			else if (I->type == GMT_SHORT && I->header->n_bands == 1)	/* No so sure here but a Int16 is much likely a grid */
 				code = GMT_IS_GRID;
 			else if (HH->orig_datatype == GMT_UCHAR || HH->orig_datatype == GMT_CHAR)	/* Got a gray or RGB image with or without transparency */
 				code = GMT_IS_IMAGE;
