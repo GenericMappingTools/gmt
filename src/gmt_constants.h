53b041202e87eb237852f3a26e03e9d1b4b8f942
Roger Davis <rbdavis@users.noreply.github.com>
2023-12-10 21:08:23 -1000

    Enable longoptions for grd2kml (#8177)
    * Enable longoptions support for grd2kml.
    
    * Enable longoptions support for grd2kml.

--- src/gmt_constants.h
+++ src/gmt_constants.h
@@ -245,7 +245,7 @@ enum GMT_time_period {
  * non-user-facing translation entries.
  */
 #define GMT_I_INCREMENT_KW { '/', 'I', "increment|inc", "", "", "e,n", "exact,number", GMT_TP_STANDARD }
-#define GMT_C_CPT_KW	{ 0, 'C', "cpt|cmap", "", "", "h,i,u,U", "hinge,zinc,fromunit,tounit", GMT_TP_STANDARD }
+#define GMT_C_CPT_KW	{ 0, 'C', "cpt|cmap", "", "", "h,i,s,u,U", "hinge,zinc,file,fromunit,tounit", GMT_TP_STANDARD }
 #define GMT_G_OUTGRID_KW { 0, 'G', "outgrid", "", "", "d,n,o,s,c,l", "divide,nan,offset,scale,gdal,list", GMT_TP_STANDARD }
 #define GMT_W_PEN_KW	{ 0, 'W', "pen", "", "", "c", "color", GMT_TP_STANDARD }
 
