a0533213378db8229a78221976d58192c6ac52ba
Joaquim <jluis@ualg.pt>
2024-03-03 12:52:57 +0000

    Update foew longnanes.

--- test/grdedit/grdedit-l2s.sh
+++ test/grdedit/grdedit-l2s.sh
@@ -34,9 +34,9 @@ gmt $m $l2s --cmdhist=both --command_history=current >> $b
 gmt $m $l2s --cmdhist=none --command_history=previous >> $b
 gmt $m $l2s --outgrid=/some/file.grd=nf+divide:2+nan:-99 >> $b
 gmt $m $l2s --outgrid=/other/file.grd=nf+offset:6+scale:1.5 >> $b
-gmt $m $l2s --netcdf+cpt:/some.cpt+dname:sillydata+invalid:-99 >> $b
-gmt $m $l2s --netCDF+cpt:-+offset:1+remark:'Smart Grid' >> $b
-gmt $m $l2s --ncheader+scale:2+title:"Best Grid Ever"+varname:myvar >> $b
+gmt $m $l2s --header+cpt:/some.cpt+dname:sillydata+invalid:-99 >> $b
+gmt $m $l2s --metadata+cpt:-+offset:1+remark:'Smart Grid' >> $b
+gmt $m $l2s --header+scale:2+title:"Best Grid Ever"+varname:myvar >> $b
 gmt $m $l2s --netcdf+xname:Dr.X+yname:Dr.Y+zname:Dr.Z >> $b
 gmt $m $l2s --transform=hvflip --transform=xyswap --transform=hflip >> $b
 gmt $m $l2s --transform=rot90ccw --transform=rot90cw >> $b
