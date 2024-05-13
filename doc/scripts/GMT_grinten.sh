d25a9c4d16950ae4fcd4aa7b8262ea5b5b0d385b
Dongdong Tian <seisman.info@gmail.com>
2024-05-13 19:51:12 +0800

    Remove --GMT_THEME=cookbook from examples included in the docs (#8485)

--- doc/scripts/GMT_grinten.sh
+++ doc/scripts/GMT_grinten.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # GRAPHICSMAGICK_RMS = 0.0035
-gmt coast -Rg -JV10c -Bg -Dc -Glightgray -Scornsilk -A10000 -Wthinnest --GMT_THEME=cookbook -view GMT_grinten
+gmt coast -Rg -JV10c -Bg -Dc -Glightgray -Scornsilk -A10000 -Wthinnest -view GMT_grinten
