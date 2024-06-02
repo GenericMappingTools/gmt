15f58ccfa600a3319cc140818efa466aea806297
Dongdong Tian <seisman.info@gmail.com>
2024-06-02 10:58:06 +0800

    doc: Remove GMT_THEME setting from GMT_tut_*.sh scripts

--- doc/scripts/GMT_tut_19.sh
+++ doc/scripts/GMT_tut_19.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 gmt begin GMT_tut_19
-	gmt set GMT_THEME cookbook
 	gmt makecpt -Cdem2 -T1000/5000
 	gmt grdview @earth_relief_30s -R-108/-103/35/40 -JM4i -p135/35 -Qi50 -I+a100+ne0.8 -B -JZ0.5i
 gmt end show
