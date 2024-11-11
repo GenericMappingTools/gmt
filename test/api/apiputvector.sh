548e6ed6165a031d832f09044fdc6edf969e9d2e
Dongdong Tian <seisman.info@gmail.com>
2024-11-11 21:52:35 +0800

    Fix the expected result in test/api/apiputvector.sh (#8615)

--- test/api/apiputvector.sh
+++ test/api/apiputvector.sh
@@ -6,7 +6,7 @@
 # This file holds what is expected to be produced on output
 cat << EOF > answer.txt
 134.9	2020-06-01T14:55:33	-12.76	-16.25
--158	2020-06-01T16:00:51	19.8	29.5
+202	2020-06-01T16:00:51	19.8	29.5
 EOF
 testapi_putvector > results.txt
 diff -q --strip-trailing-cr results.txt answer.txt > fail
