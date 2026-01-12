1b97db1f8d78ab50ed09c478ab28cea55a869ab2
Xingchen He <chuanjun1978@gmail.com>
2026-01-12 11:12:31 +0800

    Update test/seis/seis_equal_dip.sh
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- test/seis/seis_equal_dip.sh
+++ test/seis/seis_equal_dip.sh
@@ -9,13 +9,4 @@ echo 7.0  5.0 10.0 0 90 0  5.0 0 0 Strike-slip | gmt meca -Sa3.2c+m -Gblue -W0.8
 
 echo 3.0  1.8 5   1 360 45 1 90 0 1 180 45   1 Dip=45 | gmt meca -Sy3.2c+m -Gpurple   -W0.8p,black
 echo 7.0  1.8 5   1 360 45 1 90 0 1 180 45.1 1 Dip=45.1 | gmt meca -Sy3.2c+m -Gdarkgreen -W0.8p,black
-
-# Labels 
-gmt text -F+f8p,Helvetica-Bold,black+jCB -N << EOF
-3.0  3.6 Vertical dip-slip
-7.0  3.6 Strike-slip
-3.0  0.50 Equal dip 45
-7.0  0.50 Near-equal 45.1
-EOF
-
 gmt end
