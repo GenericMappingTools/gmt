55a28a40b9bd1790ce010c1ba254ac012768615f
Xingchen He <chuanjun1978@gmail.com>
2026-01-12 11:12:24 +0800

    Update test/seis/seis_equal_dip.sh
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- test/seis/seis_equal_dip.sh
+++ test/seis/seis_equal_dip.sh
@@ -7,8 +7,8 @@ gmt basemap -R0/10/0/6.5 -JM15c -B0 -BWSen+t"Focal Mechanisms Comparison"
 echo 3.0  5.0 10.0 0 90 90 5.0 0 0 Vertical dip-slip | gmt meca -Sa3.2c+m -Gred -W0.8p,black
 echo 7.0  5.0 10.0 0 90 0  5.0 0 0 Strike-slip | gmt meca -Sa3.2c+m -Gblue -W0.8p,black
 
-echo 3.0  1.8 5   1 360 45 1 90 0 1 180 45.01 1 45.01 | gmt meca -Sa3.2c+m -Gpurple   -W0.8p,black
-echo 7.0  1.8 5   1 360 45 1 90 0 1 180 45.1  1 45.1  | gmt meca -Sa3.2c+m -Gdarkgreen -W0.8p,black
+echo 3.0  1.8 5   1 360 45 1 90 0 1 180 45   1 Dip=45 | gmt meca -Sy3.2c+m -Gpurple   -W0.8p,black
+echo 7.0  1.8 5   1 360 45 1 90 0 1 180 45.1 1 Dip=45.1 | gmt meca -Sy3.2c+m -Gdarkgreen -W0.8p,black
 
 # Labels 
 gmt text -F+f8p,Helvetica-Bold,black+jCB -N << EOF
