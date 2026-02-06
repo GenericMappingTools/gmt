53a7dcd72b2c59243e41fea37447c440e40ed9a4
Solar Smith <solar.smith@earthscope.org>
2026-02-06 11:52:50 -0500

    Use fill_transparency output name

--- test/psxy/fill_transparency.sh
+++ test/psxy/fill_transparency.sh
@@ -6,7 +6,7 @@
 set -e
 export GMT_SESSION_NAME=$$	# Set a unique session name
 
-gmt begin fillTransparency ps
+gmt begin fill_transparency ps
   gmt basemap -R-2/2/-2/2 -JX10c/4c -B
   echo -1 0 | gmt plot -Ss1c -W1p,blue@60 -Gbrown
   echo 0 0 | gmt plot -Ss1c -W1p,blue -Gbrown@30
