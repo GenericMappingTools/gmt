3f0a2312ecacd16c400e4b671fc578bfa65fba4b
Joaquim <jluis@ualg.pt>
2026-09-13 01:13:46 +0100

    Update the eqearth test script

--- test/psbasemap/eqearth.sh
+++ test/psbasemap/eqearth.sh
@@ -2,7 +2,7 @@
 # Test frame, annotations, ticks and gridlines for the PROJ Equal Earth projection.
 # Global with the default central meridian, global centered on the Dateline, and a subregion.
 gmt begin eqearth ps
-	gmt basemap -Rd -J+proj=eqearth+width=12c -Bxa60g30 -Bya30g30 -BWSen
-	gmt basemap -Rg -J"+proj=eqearth +lon_0=180 +width=12c" -Bxa60g30 -Bya30g30 -BWSen -Y9c
-	gmt basemap -R-100/40/-20/70 -J+proj=eqearth+width=12c -Bafg -BWSen -Y9c
+	gmt basemap -Rd -J+proj=eqearth+width=11c -Bxa60g30 -Bya30g30 -BWSen
+	gmt basemap -Rg -J"+proj=eqearth +lon_0=180 +width=11c" -Bxa60g30 -Bya30g30 -BWSen -Y7c
+	gmt basemap -R-100/40/-20/70 -J+proj=eqearth+width=11c -Bafg -BWSen -Y7c
 gmt end show
